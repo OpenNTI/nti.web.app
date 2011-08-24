fdd947c74fb33fc9f9ee76bdff0fd8e949a5de24
Jonathan Grimes <jonathan.grimes@nextthought.com>
2011-08-24 22:42:19 +0000

    add logout (click on the gear for now)
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@1272 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- app/NextThought/view/widgets/SessionInfo.js
+++ app/NextThought/view/widgets/SessionInfo.js
@@ -12,13 +12,15 @@ Ext.define('NextThought.view.widgets.SessionInfo', {
 	extend: 'Ext.panel.Panel',
     alias: 'widget.session-info',
 
+    cls: 'x-session-controls',
+
     width: MIN_SIDE_WIDTH,
     height: 25,
     border: false,
-    layout: 'hbox',
+    layout: {type:'hbox', pack: 'end'},
     defaults: {
-        border: false,
-        defaults: {}
+        height: 25,
+        border: false
     },
 
     initComponent: function() {
@@ -28,11 +30,25 @@ Ext.define('NextThought.view.widgets.SessionInfo', {
     		n = u.get('realname'),
     		a = u.get('avatarURL');
 
-        this.add({html: n});
+        this.add({cls: 'x-username',
+            html: '<span style="padding: 5px; padding-top: 6px;font-size: 12px; vertical-align: middle; cursor: pointer;">'+n+'</span>'
+                + '<img src="'+a+'" width=24 height=24 valign=middle>' });
+
         this.add({
             html: '<span style="width: 24px; height: 23px; padding-top: 2px; display: inline-block; text-align: center; cursor: pointer; vertical-align: middle;margin-top: 2px; background: url(\'resources/images/notify.png\') no-repeat -25px 0px;">0</span>'
         });
-        this.add({xtype: 'image', src: a,	height: 24, width: 24});
-        this.add({xtype: 'image', src:'resources/images/gear.png', width: 19, height: 19 });
+
+        this.add({xtype: 'image', src:'resources/images/gear.png?', height: 26, width: 26, margin: '0 3px 0 0', settings: true});
+    },
+
+
+    render: function(){
+        this.callParent(arguments);
+        this.down('image[settings]').el.on('click', this._click, this);
+    },
+
+    _click: function(){
+        console.log('click');
+        this.fireEvent('logout');
     }
 });
