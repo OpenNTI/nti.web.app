4b3f13b16e1aa90dba606806333b0b5f356a54fe
Jonathan Grimes <jonathan.grimes@nextthought.com>
2011-08-29 21:22:34 +0000

    use a more compatible dataserver url
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@1323 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 var _AppConfig = {
 	"server" : {
-         "host": "http://astro.local:8080",
-        "host": "http://tdaley.local:8080",
+        "host": "http://localhost:8080",
+        //"host": "http://astro.local:8080",
+        //"host": "http://tdaley.local:8080",
         //"host": "http://curie.local:8080",
         //"host": "http://alpha.nextthought.com",
         "data": "/dataserver/",
