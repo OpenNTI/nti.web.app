65e5d3a7e37325c62344a5a570023e6608b29d32
Jonathan Grimes <jonathan.grimes@nextthought.com>
2011-09-20 20:29:49 +0000

    add smarts to 'setGroups' to do diff/update of groups/people
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@1946 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- app/NextThought/view/widgets/chat/Friends.js
+++ app/NextThought/view/widgets/chat/Friends.js
@@ -1,5 +1,5 @@
 Ext.define('NextThought.view.widgets.chat.Friends', {
-	extend:'Ext.panel.Panel',
+    extend:'Ext.panel.Panel',
     alias: 'widget.chat-friends-view',
 
     requires: [
@@ -17,19 +17,24 @@ Ext.define('NextThought.view.widgets.chat.Friends', {
 
     setGroups: function(groups) {
         var me = this,
-            prevGroups = Ext.Array.clone(me._groups) || [];
+            prevGroups = me._groups || {},
+            newGroups = {};
 
 
 
         Ext.each(groups, function(g){
             if(/everyone/i.test(g.get('id'))) return; //skip everyone group
-            var groupPanel = me.down('panel[groupId='+g.getId()+']');
+
+            var gid = g.getId(),
+                groupPanel = me.down('panel[groupId='+gid+']');
+
             if(!groupPanel){
                 groupPanel = me.add({
                     title: g.get('realname'),
                     collapsible: true,
                     collapseFirst: false,
-                    groupId: g.getId(),
+                    groupId: gid,
+                    friends: {},
                     tools:[
                         {
                             type: 'gear',
@@ -40,21 +45,42 @@ Ext.define('NextThought.view.widgets.chat.Friends', {
                 });
             }
 
+            newGroups[gid] = groupPanel;
+            delete prevGroups[gid];
+
+            var prevFriends = groupPanel.friends, newFriends = {}, uid;
 
             Ext.each(g.get('friends'), function(f){
-                var item = groupPanel.down('chat-friend-entry[userId='+f.getId()+']');
+                uid = f.getId();
+                var item = groupPanel.down('chat-friend-entry[userId='+uid+']');
                 if(item){
                     item.update(f);
-                    return;
                 }
-                groupPanel.add({
-                    xtype: 'chat-friend-entry',
-                    user: f,
-                    userId: f.getId()
-                });
-            })
+                else {
+                    item = groupPanel.add({
+                        xtype: 'chat-friend-entry',
+                        user: f,
+                        userId: uid
+                    });
+                }
+
+                newFriends[uid] = item;
+                delete prevFriends[uid];
+            });
+
+            clean(prevFriends, newFriends, groupPanel, 'friends');
+
         });
 
-        me._groups = groups;
+        clean(prevGroups, newGroups, me, '_groups');
+
+
+        function clean(dirty, clean, obj, key){
+            obj[key] = clean;
+            for(var k in dirty){
+                obj.remove(dirty[k]);
+                delete dirty[k];
+            }
+        }
     }
 });
