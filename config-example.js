4e32951528f0db1ebd8cf0f6d5d9c82792857420
Jonathan Grimes <jonathan.grimes@nextthought.com>
2011-09-30 19:27:57 +0000

    ignore config.js, add example config
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@2255 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- config-example.js
+++ config-example.js
@@ -1,9 +1,6 @@
 var _AppConfig = {
 	"server" : {
-        //"host": "http://localhost:8080",
-        //"host": "http://astro.local:8080",
-        "host": "http://tdaley.local:8080",
-        //"host": "http://curie.local:8080",
+        "host": "http://localhost:8080",
         //"host": "http://alpha.nextthought.com",
         //"host": "http://excelsior.nextthought.com:3774",
         "data": "/dataserver/",
