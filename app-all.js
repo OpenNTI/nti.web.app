f2665aac56c8c3d0f54031c073ca7d2c838dffb7
Jonathan Grimes <jonathan.grimes@nextthought.com>
2011-10-17 21:25:41 +0000

    Hangouts...
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@2661 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- app-all.js
+++ app-all.js
@@ -88570,5 +88570,49 @@ Ext.define('Ext.grid.plugin.HeaderReorderer', {
 
 
 
+document.write('<script	src="'+_AppConfig.server.host+'/socket.io/static/socket.io.js"></script>');
+
+Ext.application({
+    name: 'NextThought',
+    appFolder: 'src/main/NextThought',
+
+    controllers: [
+        'State',
+        'Chat',
+        'Account',
+        'Annotations',
+        'Application',
+        'FilterControl',
+        'Groups',
+        'Home',
+        'Session',
+        'Modes',
+        'ObjectExplorer',
+        'Reader',
+        'Search',
+        'Stream'
+    ],
+
+    launch: function() {
+        NextThought.isDebug = true;
+
+        Ext.JSON.encodeDate = encodeDate
+
+        fixIE();
+        Ext.Ajax.timeout==60000;
+        Ext.Ajax.on('beforerequest', beforeRequest);
+        Ext.EventManager.onWindowResize(resizeBlocker);
+
+        setTimeout(
+            function clearMask(){
+                Ext.get('loading').remove();
+                Ext.get('loading-mask').fadeOut({remove:true});
+                resizeBlocker(Ext.Element.getViewWidth());
+            },
+            100);
+
+        NextThought.controller.Session.login();
+    }
+});
 
 
