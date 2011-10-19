dd79f99c1381036f7fd57c7fde05904256644f06
Jonathan Grimes <jonathan.grimes@nextthought.com>
2011-10-19 16:22:29 +0000

    google hangout hook in app.js, add some convenience steps in the build script
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@2737 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 sencha create jsb -a http://localhost/WebApp/index.html -p app.jsb3
 sencha build -p app.jsb3 -d .
 rm all-classes.js
+#svn commit app-all.js -m "hangout..."
+#curl http://alpha.nextthought.com/_update.cgi
--- src/main/app.js
+++ src/main/app.js
@@ -22,24 +22,27 @@ Ext.application({
         'Stream'
     ],
 
-    launch: function() {
-        NextThought.isDebug = true;
-
-        Ext.JSON.encodeDate = encodeDate
-
-        fixIE();
-        Ext.Ajax.timeout==60000;
-        Ext.Ajax.on('beforerequest', beforeRequest);
-        Ext.EventManager.onWindowResize(resizeBlocker);
-
-        setTimeout(
-            function clearMask(){
-                Ext.get('loading').remove();
-                Ext.get('loading-mask').fadeOut({remove:true});
-                resizeBlocker(Ext.Element.getViewWidth());
-            },
-            100);
-
-        NextThought.controller.Session.login();
-    }
+    launch: typeof gapi == 'undefined'? launch : function(){gapi.hangout.addApiReadyListener(launch)}
 });
+
+
+function launch() {
+    NextThought.isDebug = true;
+
+    Ext.JSON.encodeDate = encodeDate
+
+    fixIE();
+    Ext.Ajax.timeout==60000;
+    Ext.Ajax.on('beforerequest', beforeRequest);
+    Ext.EventManager.onWindowResize(resizeBlocker);
+
+    setTimeout(
+        function clearMask(){
+            Ext.get('loading').remove();
+            Ext.get('loading-mask').fadeOut({remove:true});
+            resizeBlocker(Ext.Element.getViewWidth());
+        },
+        100);
+
+    NextThought.controller.Session.login();
+}
