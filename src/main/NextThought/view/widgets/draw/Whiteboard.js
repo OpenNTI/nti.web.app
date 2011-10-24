39c9662e6ec89da99e29b42322c590c407330ed6
Jonathan Grimes <jonathan.grimes@nextthought.com>
2011-10-24 16:38:39 +0000

    stubbing
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@2862 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/main/NextThought/view/widgets/draw/Whiteboard.js
@@ -0,0 +1,9 @@
+Ext.define('NextThought.view.widgets.draw.Whiteboard', {
+	extend		: 'Ext.draw.Component',
+	alias		: 'widget.whiteboard',
+	viewBox		: false,
+
+	initComponent: function(){
+		this.callParent(arguments);
+	}
+});
