0538e017d25be6438d96b2dfa5aa8debfe7f9431
Jonathan Grimes <jonathan.grimes@nextthought.com>
2011-11-04 00:22:25 +0000

    use ExtJS's HSL color space conversion instead of our own
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@3109 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/NextThought/util/Color.js
+++ src/main/NextThought/util/Color.js
@@ -43,57 +43,11 @@ Ext.define('NextThought.util.Color',{
 			var hue = ridx / Math.pow(2, bitcount);
 
 			/* Start at .6 (216 degrees) */
-			return (hue + .166) % 1;
+			return (hue + .6) % 1;
 		},
 
 		getColor: function(idx){
-			var degrees = Math.round(this.hue(idx) * 360);
-			//console.debug('degrees', degrees);
-			return hsl2rgb(degrees, 100, 50);
-
-			/*
-			 HSL to RGB function sourced from:
-			 http://www.codingforums.com/showpost.php?s=acaa80143f9fa9f2bb768131c14bfa3b&p=54172&postcount=2
-			 */
-			function hsl2rgb(h, s, l) {
-				var m1, m2, hue;
-				var r, g, b
-				s /=100;
-				l /= 100;
-				if (s == 0)
-					r = g = b = (l * 255);
-				else {
-					if (l <= 0.5)
-						m2 = l * (s + 1);
-					else
-						m2 = l + s - l * s;
-					m1 = l * 2 - m2;
-					hue = h / 360;
-					r = HueToRgb(m1, m2, hue + 1/3);
-					g = HueToRgb(m1, m2, hue);
-					b = HueToRgb(m1, m2, hue - 1/3);
-				}
-				return Ext.create('Ext.draw.Color',r, g, b);
-			}
-
-			function HueToRgb(m1, m2, hue) {
-				var v;
-				if (hue < 0)
-					hue += 1;
-				else if (hue > 1)
-					hue -= 1;
-
-				if (6 * hue < 1)
-					v = m1 + (m2 - m1) * hue * 6;
-				else if (2 * hue < 1)
-					v = m2;
-				else if (3 * hue < 2)
-					v = m1 + (m2 - m1) * (2/3 - hue) * 6;
-				else
-					v = m1;
-
-				return Math.round(255 * v);
-			}
+			return Ext.draw.Color.fromHSL(Math.round(this.hue(idx) * 360), 100, 50);
 		}
 	}
 });
