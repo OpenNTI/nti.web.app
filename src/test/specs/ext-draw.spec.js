92b65d67ff4e067788a5c1dafeb2d0a31f251f7e
Jonathan Grimes <jonathan.grimes@nextthought.com>
2011-11-11 05:40:13 +0000

    add unit test to verify extjs patch is present
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@3265 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/test/specs/ext-draw.spec.js
@@ -0,0 +1,25 @@
+describe("ExtJS Path Verification", function() {
+
+    it("has fixed path conversion", function() {
+		//Ext.draw.Draw.path2curve()
+		//Jira Issue Tag: EXTJSIV-4399
+		//Forum post: http://www.sencha.com/forum/showthread.php?153418-bug-in-path-conversion-Ext.draw.Draw.path2curve()-(as-of-4.0.7)
+
+		var pathStr = Ext.String.format("M0,-{1}A{0},{1},0,1,1,0,{1}A{0},{1},0,1,1,0,-{1}z",1, 1);
+		var path = Ext.draw.Draw.parsePathString(pathStr);
+
+		expect(path.length).toBe(4);
+
+		var curve = Ext.draw.Draw.path2curve(path);
+
+		expect(curve.length).toBe(6);
+
+		expect(curve[0][0]).toBe('M');
+		for(var i=1; i<curve.length; i++){
+			expect(curve[i][0]).toBe('C');
+			expect(curve[i].length).toBe(7);
+		}
+
+    });
+
+});
