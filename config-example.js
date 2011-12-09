f41b7488b43f5694a967c620c7d43696bb8aa919
Jonathan Grimes <jonathan.grimes@nextthought.com>
2011-12-09 21:07:52 +0000

    update example config
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@3772 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- config-example.js
+++ config-example.js
@@ -3,7 +3,6 @@ var _AppConfig = {
         "host": "http://localhost:8080",
         //"host": "http://alpha.nextthought.com",
         //"host": "http://excelsior.nextthought.com:3774",
-        "data": "/dataserver/",
-        "library": "/library/library.json"
+        "data": "/dataserver2/"
     }
 };
