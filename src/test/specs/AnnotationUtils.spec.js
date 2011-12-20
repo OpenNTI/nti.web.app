acdb8c5d2eef5e4941d1d37611bfdda82fd46c32
Jonathan Grimes <jonathan.grimes@nextthought.com>
2011-12-20 17:07:34 +0000

    stubbing for AnnotationUtils tests
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@4019 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/test/specs/AnnotationUtils.spec.js
@@ -0,0 +1,8 @@
+describe("Annotation Utils", function() {
+
+    it("AnnotationUtils is defined", function() {
+		expect(NextThought.util.AnnotationUtils).toBeDefined();
+		expect(AnnotationUtils).toBeDefined();
+    });
+
+});
