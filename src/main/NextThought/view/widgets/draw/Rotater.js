000d825e30f1b5606179f93e7921b8a74cea1b66
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-01-03 23:35:20 +0000

    more rotater cleanup... almost usable
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@4148 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/NextThought/view/widgets/draw/Rotater.js
+++ src/main/NextThought/view/widgets/draw/Rotater.js
@@ -5,49 +5,48 @@ Ext.define('NextThought.view.widgets.draw.Rotater', {
 		'NextThought.view.widgets.draw.RotaterNib'
 	],
 
-	nibSize: 20,
+	nibSize: 18,
 
 	constructor: function(whiteboard,sprite){
 		this.callParent([{surface: whiteboard.getSurface()}]);
 
-		var s = this.surface,
+		var c,
+			m = Ext.create('Ext.draw.Matrix'),
+			s = this.surface,
 			degrees = sprite.attr.rotation.degrees,
 			group = s.createSvgElement ? s.createSvgElement('g') : s.createNode('group');
 
 		s.el.appendChild(group);
 
-//		console.dir(sprite.attr.rotation);
-
-		this.group = Ext.get(group);
+		this.group = group;
 		this.whiteboard = whiteboard;
 		this.sprite = sprite;
 		this.drawNibs();
 		this.hookDrag(sprite);
 
-		if(degrees){
-			var c = this.getCenter();
-			group.setAttribute('transform',
-					Ext.String.format('rotate({0},{1},{2})', degrees, c.x, c.y));
-		}
+			c = this.getCenter();
+			m.rotate(degrees, c.x, c.y);
+			group.setAttribute('transform',m.toSvg());
 
+
+		this.matrix = m;
 		return this;
 	},
 
 
 	destroy: function(){
-		var s = this.surface;
 		this.sprite.dd.startDrag = Ext.draw.SpriteDD.prototype.startDrag;
 		this.sprite.dd.onDrag = Ext.draw.SpriteDD.prototype.onDrag;
 
-		this.hide(true);
-
 		//un-group the nibs from the <g> element we created
+		var s = this.surface, g = Ext.get(this.group);
 		this.each(function(o){ s.el.appendChild(o.el); },this);
-		while(this.group.first()){
+		while(g.first()){
 			s.el.appendChild(this.group.first().dom);
 			console.error('Group node had extra elements in it!? reparenting before we remove it. destroy selection before adding new shapes to the canvas.');
 		}
-		this.group.remove();
+		g.remove();
+		this.callParent(arguments);
 	},
 
 
@@ -65,23 +64,29 @@ Ext.define('NextThought.view.widgets.draw.Rotater', {
 	},
 
 	drawNibs: function(){
-		this.reset();
 
-		var g	= this.groups,
-			b	= this.sprite.getBBox(),
-			s	= this.nibSize,
-			r	= s/2,
+		function length(x,y,x1,y1){
+			return Math.sqrt(Math.pow(x-x1,2)+Math.pow(y-y1,2));
+		}
 
-			x	= b.x-r,
-			y	= b.y-r,
+		var b	= this.sprite.getBBox(),
+			x	= b.x,
+			y	= b.y,
+			x2	= x+b.width,
+			xc	= x+b.width/2,
+			yc	= y+b.height/2,
 
-			x2	= x+b.width-r,
-			y2	= y+b.height-r;
+			r = length(xc,yc,x2,y);
 
-		this.addNib({translate:{x: x,	y: y }});
-		this.addNib({translate:{x: x2,	y: y }, rotate:{degrees:  90}});
-		this.addNib({translate:{x: x2,	y: y2}, rotate:{degrees: 180}});
-		this.addNib({translate:{x: x,	y: y2}, rotate:{degrees: -90}});
+		this.addNib({
+//			translate:{
+				x: xc+r,
+				y: yc
+//			},
+//			rotate: {
+//				degrees: 135
+//			}
+		});
 
 
 	},
@@ -95,15 +100,16 @@ Ext.define('NextThought.view.widgets.draw.Rotater', {
 			Ext.draw.SpriteDD.prototype.onDrag.apply(this, arguments);
 
 			var xy = e.getXY(),
-				t = me.first().attr.translation;
+				t = me.first().attr.translation,
+				tx,ty;
 
 			xy = rel.apply(s,xy);
-			me.setAttributes({
-				translate: {
-					x: t.x + xy[0] - me.prev[0],
-					y: t.y + xy[1] - me.prev[1]
-				}
-			}, true);
+			tx = t.x + xy[0] - me.prev[0];
+			ty = t.y + xy[1] - me.prev[1];
+
+			me.matrix.translate(tx,ty);
+			me.group.setAttribute('transform', me.matrix.toSvg());
+
 			me.prev = xy;
 
 		};
@@ -124,14 +130,13 @@ Ext.define('NextThought.view.widgets.draw.Rotater', {
 		r.dd.afterRepair = function(){};//we don't need to repair
 		r.dd.onDrag = function(e){
 
-			var xy = e.getXY(), dx, dy, c;
-			xy = this.sprite.surface.transformToViewBox(xy[0], xy[1]);
-			dx = xy[0] - this.prev[0];
-			dy = xy[1] - this.prev[1];
+			var c;//xy = e.getXY(), dx, dy, c;
+			//xy = this.sprite.surface.transformToViewBox(xy[0], xy[1]);
+			//dx = xy[0] - this.prev[0];
+			//dy = xy[1] - this.prev[1];
 
 
 			c = {rotate:{degrees:0}};
-
 			me.sprite.setAttributes(c,true);
 
 			Ext.draw.SpriteDD.prototype.onDrag.apply(this, arguments);
@@ -145,7 +150,7 @@ Ext.define('NextThought.view.widgets.draw.Rotater', {
 
 		r.dd.endDrag = function(){
 			Ext.draw.SpriteDD.prototype.endDrag.apply(this, arguments);
-			me.drawNibs();
+			//me.drawNibs();
 		};
 
 		return r;
@@ -154,24 +159,8 @@ Ext.define('NextThought.view.widgets.draw.Rotater', {
 	add: function(c){
 		var r = this.surface.add(c);
 		this.callParent(arguments);
-		this.group.appendChild(r.el);
+		Ext.fly(this.group).appendChild(r.el);
 		return r;
-	},
-
-	reset: function(){
-		var me = this,
-			surface = me.getSurface(),
-			item;
-
-		if (surface) {
-			while (me.getCount() > 0) {
-				item = me.first();
-				me.remove(item);
-				surface.remove(item,false);
-			}
-		}
 	}
-
-
 
 });
--- src/main/NextThought/view/widgets/draw/RotaterNib.js
+++ src/main/NextThought/view/widgets/draw/RotaterNib.js
@@ -4,34 +4,46 @@ Ext.define('NextThought.view.widgets.draw.RotaterNib', {
 
 	constructor: function(config){
 
-		var c,i,
-			p = [
-			['M', .25, 1],
-			['l', -.2,-.3],
-			['h', .125],
-			['q', -.02,-.55,  .525,-.525],
-			['v', -.125],
-			['l', .3,.2],
-			['l', -.3,.2],
-			['v', -.125],
-			['q', -.4,-.02,  -.375,.375],
-			['h', .125],
-			['z']
-		];
-
-		for(c in p){
-			for(i=1; i<p[c].length;i++)
-				p[c][i] *= config.nibSize;
-		}
-
+//		var c,i,
+//			p = [
+//				['m', 0.25, 1],
+//				['l', -0.2,-0.3],
+//				['h', 0.125],
+//				['q', -0.02,-0.55,  0.525,-0.525],
+//				['v', -0.125],
+//				['l', 0.3,0.2],
+//				['l', -0.3,0.2],
+//				['v', -0.125],
+//				['q', -0.4,-0.02,  -0.375,0.375],
+//				['h', 0.125],
+//				['z']
+//			];
+//
+//		for(c in p){
+//			if(!p.hasOwnProperty(c))continue;
+//			for(i=1; i<p[c].length;i++)
+//				p[c][i] *= config.nibSize;
+//		}
+//
+//		this.callParent([Ext.apply({
+//			draggable: true,
+//			type: 'path',
+//			path: p,
+//			fill: '#cccccc',
+//			stroke: '#0000ff',
+//			'stroke-width': 2,
+//			group: 'rotate'
+//		},config)]);
 		this.callParent([Ext.apply({
 			draggable: true,
-			type: 'path',
-			path: p,
+			type: 'circle',
+			radius: config.nibSize/2,
+			width: config.nibSize,
+			height: config.nibSize,
 			fill: '#cccccc',
 			stroke: '#0000ff',
-			'stroke-width': 1,
-			group: 'rotate'
+			'stroke-width': 1.5,
+			group: 'resize'
 		},config)]);
 
 		this.id = Ext.id(null, 'nti-sprite-rotater-nib-');
