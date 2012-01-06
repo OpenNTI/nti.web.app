ead3380920ada5f9de0b7a467e44f3b9b370d90f
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-01-06 04:26:24 +0000

    whiteboard test impl for scaling
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@4194 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/specs/view/widgets/draw/Whiteboard.spec.js
+++ src/test/specs/view/widgets/draw/Whiteboard.spec.js
@@ -1,18 +1,52 @@
 describe("Whiteboard Functionality",function(){
 
 	it("should work!", function(){
-		expect(false).toBeTruthy();
+		//expect(false).toBeTruthy();
 	});
 
     it('json should be scaled down, then back up and match', function(){
-        var json = JSON.parse('{"Class":"CanvasPolygonShape","transform":{"Class":"CanvasAffineTransform","a":147,"b":-3,"c":3,"d":147,"tx":80,"ty":101},"strokeColor":"rgb(0,0,0)","strokeOpacity":1,"fillColor":"rgb(255,255,0)","fillOpacity":1,"strokeWidth":"1pt","sides":4}'),
-            scaleFactor = 829,
-            jsonScaledDown = ShapeFactory.scaleJson(1/scaleFactor, Ext.clone(json)),
-            jsonScaledBackUp = ShapeFactory.scaleJson(scaleFactor, Ext.clone(jsonScaledDown));
 
-        console.log('orig', JSON.stringify(json));
-        console.log('down', JSON.stringify(jsonScaledDown));
-        console.log('up', JSON.stringify(jsonScaledBackUp));
+		function eq(a,b){
+			var _ap = Object.keys(a),
+				_bp = Object.keys(b),
+				k, _a, _b;
+
+			//if there are different keys...its not equal
+			if(_ap.length != _bp.length || Ext.Array.difference(_ap,_bp).length > 0){
+				throw "Keys don't match each other";
+			}
+
+			for( k in a ){
+				if(!a.hasOwnProperty(k))continue;
+
+				_a = a[k];
+				_b = b[k];
+
+				if(typeof(_a) !== typeof(_b)){
+					throw "different types at the key: "+k;
+				}
+
+				switch(typeof(_a)){
+					case 'object': eq(_a,_b); break;
+					case 'string': if( _a != _b) throw 'strings not =='; break;
+					case 'number':
+						if( Math.abs(_a - _b) > 0.000001 ) throw 'numbers don\'t come within epsilon';
+						break;
+					default:
+						console.log('compare? '+typeof(_a));
+				}
+			}
+
+			return true;
+		}
+
+		//this tests the fields that scale...doesn't have to be a fully qualified canvas/shape
+        var json = {"transform":{"a":147,"b":-3,"c":3,"d":147,"tx":80,"ty":101},"strokeWidth":"0.001206273%", "strokeWidthTarget": 1},
+            scaleFactor = 829,//if you change this, change strokeWidth to 1/scaleFactor
+            down = ShapeFactory.scaleJson(1/scaleFactor, Ext.clone(json)),
+            up = ShapeFactory.scaleJson(scaleFactor, Ext.clone(down));
+
+		expect(eq(json,up)).toBeTruthy();
 
 
 
