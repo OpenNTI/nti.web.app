acd3226249be2355ac82feffb07dd2356753feb9
Jason Madden <jason.madden@nextthought.com>
2012-01-17 20:17:00 +0000

    Make executable. Make use zeta.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@4431 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- gencss.sh
+++ gencss.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+PATH=$PATH:/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin
 
-SCSS=`which scss`
-SASS=${SCSS:-`which sass`}
+if [ -n `which zeta` ]; then
+	set -e
+	# Errors in the source prevent compression from working, so -c is 
+	# disabled for now
+	zeta pack -p '' resources/scss/main.scss  -o resources/css
+	mv resources/css/main.scss resources/css/main.css
+	exit 0
+fi
+
+SASS=`which sass`
 
 if [ -z "$SASS" ]; then
     echo "Sass not installed"
@@ -13,6 +22,7 @@ if [ -z "$SASS" ]; then
     echo ""
     exit 1
 fi
+set -e
 
 if [[ "`$SASS --help`" == *-m* ]] ; then
 	SASS="$SASS -m"
