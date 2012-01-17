dcbf4876fca13f683e0e56bd286c4a7aa56ba4b9
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-01-17 22:07:42 +0000

    make gencss.sh use exclusively Zeta-Library to compile SCSS. (if you want to use the ruby gem, us it on your own)
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@4439 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- gencss.sh
+++ gencss.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
+FILE=resources/scss/main.scss
 PATH=$PATH:/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin
 
-#if you have the ruby gem installed... just use it for now
-#if [ -n `which sass` ]; then
-#	sass --scss --force resources/scss/main.scss resources/css/main.css
-#	exit 0
-#fi
-
-
-FILE=resources/scss/main.scss
 if [ -n `which zeta` ]; then
 	set -e
 	# Errors in the source prevent compression from working, so -c
@@ -18,31 +11,13 @@ if [ -n `which zeta` ]; then
 	cd resources/scss
 	zeta pack main.scss &>/dev/null
 	cd ../..
-	FILE=resources/scss/_main.scss
+	mv resources/scss/_main.scss resources/css/main.css
+	exit 0
 fi
 
-SASS=`which scss`
-
-if [ -z "$SASS" ]; then
-    echo "Sass not installed"
-    echo "run one of these commands:"
-    echo ""
-    echo "    sudo easy_install scss"
-    echo "- or -"
-    echo "    sudo gem install sass"
-    echo ""
-    exit 1
-fi
-set -e
-
-if [[ "`scss --help`" == *-m* ]] ; then
-	SASS="scss -m -W -C -S"
-fi
-
-#echo "Using: $SASS"
-
-$SASS $FILE resources/css/main.css
-
-if [ -e resources/scss/_main.scss ]; then
-	rm resources/scss/_main.scss
-fi
+echo "Sass for Python is not installed"
+echo "run this command:"
+echo ""
+echo "    sudo easy_install zetalibrary"
+echo ""
+exit 1
