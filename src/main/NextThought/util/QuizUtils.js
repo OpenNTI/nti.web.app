077e7dfe6f4d0f7922922fb59db9ee52d53a7624
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-02-17 20:45:11 +0000

    use singleton instead of static
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@5198 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/NextThought/util/QuizUtils.js
+++ src/main/NextThought/util/QuizUtils.js
@@ -1,149 +1,147 @@
 Ext.define('NextThought.util.QuizUtils', {
+	singleton: true,
 	requires: [
 		'NextThought.util.ParseUtils',
 		'NextThought.providers.Location'
 	],
 	alternateClassName: 'QuizUtils',
-	statics: {
 
-		/**
-		 *
-		 * @param iterationCallback Optional - a function that takes three arguments: function(id, inputEl, containerEl)
-		 */
-		getProblemElementMap: function(iterationCallback,scope){
-			var problems = {};
-			 Ext.each(
-				Ext.query('.worksheet-problems input'),
-				function(v){
-					var id = v.getAttribute('id'),
-						el = Ext.get(v);
+	/**
+	 *
+	 * @param iterationCallback Optional - a function that takes three arguments: function(id, inputEl, containerEl)
+	 */
+	getProblemElementMap: function(iterationCallback,scope){
+		var problems = {};
+		 Ext.each(
+			Ext.query('.worksheet-problems input'),
+			function(v){
+				var id = v.getAttribute('id'),
+					el = Ext.get(v);
 
-					el.setVisibilityMode(Ext.Element.DISPLAY);
-					problems[id] = el.up('.problem');
+				el.setVisibilityMode(Ext.Element.DISPLAY);
+				problems[id] = el.up('.problem');
 
-					if(iterationCallback){
-						iterationCallback.call(scope||window, id, el, problems[id]);
-					}
-				},
-				this);
-
-			return problems;
-		},
-
-		submitAnswers: function(){
-			var me = this,
-				ntiid = LocationProvider.currentNTIID,
-				url = $AppConfig.service.getQuizSubmitURL(ntiid),
-				problems,
-				data = {},
-				vp = Ext.getBody();
-
-			function iter(id,v){
-				data[id] = v.getValue();
-				v.hide();
-			}
-
-			vp.mask('Grading...');
-
-			problems = me.getProblemElementMap(iter,me);
-
-			Ext.Ajax.request({
-				url: url,
-				jsonData: Ext.JSON.encode(data),
-				method: 'POST',
-				scope: me,
-				callback: function(){ vp.unmask(); },
-				failure: function(){
-					//TODO: hook up to error handling
-					console.error('FAIL', arguments);
-				},
-				success: function(r){
-					var quizResults = ParseUtils.parseItems([ Ext.JSON.decode(r.responseText) ]);
-					me.showQuizResult(quizResults[0], problems);
+				if(iterationCallback){
+					iterationCallback.call(scope||window, id, el, problems[id]);
 				}
-			});
-		},
+			},
+			this);
 
+		return problems;
+	},
 
-		showQuizResult: function(quizResult, problemsElementMap) {
-			var mathCls = 'mathjax tex2jax_process ',
-				ntiid = LocationProvider.currentNTIID,
-				problems = problemsElementMap || this.getProblemElementMap();
+	submitAnswers: function(){
+		var me = this,
+			ntiid = LocationProvider.currentNTIID,
+			url = $AppConfig.service.getQuizSubmitURL(ntiid),
+			problems,
+			data = {},
+			vp = Ext.getBody();
 
-			if(ntiid !== quizResult.get('ContainerId')){
-				Ext.Error.raise('Result does not match the page!');
+		function iter(id,v){
+			data[id] = v.getValue();
+			v.hide();
+		}
+
+		vp.mask('Grading...');
+
+		problems = me.getProblemElementMap(iter,me);
+
+		Ext.Ajax.request({
+			url: url,
+			jsonData: Ext.JSON.encode(data),
+			method: 'POST',
+			scope: me,
+			callback: function(){ vp.unmask(); },
+			failure: function(){
+				//TODO: hook up to error handling
+				console.error('FAIL', arguments);
+			},
+			success: function(r){
+				var quizResults = ParseUtils.parseItems([ Ext.JSON.decode(r.responseText) ]);
+				me.showQuizResult(quizResults[0], problems);
 			}
+		});
+	},
 
-			Ext.each(
-				quizResult.get('Items'),
-				function(qqr){
-					var q,p,r,id;
-					q = qqr.get('Question');
-					id = q.get('ID');
-					p = problems[id];
-					r = p.next('.result');
 
-					r.removeCls('hidden');
-					r.addCls((qqr.get('Assessment')?'':'in')+'correct');
+	showQuizResult: function(quizResult, problemsElementMap) {
+		var mathCls = 'mathjax tex2jax_process ',
+			ntiid = LocationProvider.currentNTIID,
+			problems = problemsElementMap || this.getProblemElementMap();
 
-					r.createChild({
-						tag : 'div',
-						html: 'Your response: \\('+qqr.get('Response')+'\\)',
-						cls: mathCls+'response'
-					});
+		if(ntiid !== quizResult.get('ContainerId')){
+			Ext.Error.raise('Result does not match the page!');
+		}
 
-					r.createChild({
-						tag : 'div',
-						html: 'Correct answer(s): '+q.get('Answers').join(', ').replace(/\$(.+?)\$/ig,'\\($1\\)'),
-						cls: mathCls+'answer'
-					});
+		Ext.each(
+			quizResult.get('Items'),
+			function(qqr){
+				var q,p,r,id;
+				q = qqr.get('Question');
+				id = q.get('ID');
+				p = problems[id];
+				r = p.next('.result');
+
+				r.removeCls('hidden');
+				r.addCls((qqr.get('Assessment')?'':'in')+'correct');
+
+				r.createChild({
+					tag : 'div',
+					html: 'Your response: \\('+qqr.get('Response')+'\\)',
+					cls: mathCls+'response'
 				});
 
-			try {
-				MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-			}
-			catch(e){
-				console.warn('No MathJax? ',e);
-			}
+				r.createChild({
+					tag : 'div',
+					html: 'Correct answer(s): '+q.get('Answers').join(', ').replace(/\$(.+?)\$/ig,'\\($1\\)'),
+					cls: mathCls+'answer'
+				});
+			});
 
-			this.scrollUp();
-
-			Ext.get('submit').update('Reset');
-		},
-
-		resetQuiz: function() {
-			Ext.get('submit').update('Submit');
-
-			this.getProblemElementMap(
-				function(id,v,c){
-					v.dom.value='';
-					var r = c.next('.result');
-
-					r.addCls('hidden');
-					r.removeCls(['correct','incorrect']);
-
-					r.down('.response').remove();
-					r.down('.answer').remove();
-
-					v.show();
-				},
-				this);
-
-			this.scrollUp();
-		},
-
-
-		scrollUp: function(){
-			var p = Ext.getCmp('readerPanel');
-			p.relayout();
-			p.scrollTo(0);
+		try {
+			MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 		}
+		catch(e){
+			console.warn('No MathJax? ',e);
+		}
+
+		this.scrollUp();
+
+		Ext.get('submit').update('Reset');
+	},
+
+	resetQuiz: function() {
+		Ext.get('submit').update('Submit');
+
+		this.getProblemElementMap(
+			function(id,v,c){
+				v.dom.value='';
+				var r = c.next('.result');
+
+				r.addCls('hidden');
+				r.removeCls(['correct','incorrect']);
+
+				r.down('.response').remove();
+				r.down('.answer').remove();
+
+				v.show();
+			},
+			this);
+
+		this.scrollUp();
+	},
+
+
+	scrollUp: function(){
+		var p = Ext.getCmp('readerPanel');
+		p.relayout();
+		p.scrollTo(0);
 	}
-},
-		function(){
-			window.QuizUtils = this;
-		}
-);
+
+}, function(){
+	window.QuizUtils = this;
+});
 
 
 /*********************************************************
