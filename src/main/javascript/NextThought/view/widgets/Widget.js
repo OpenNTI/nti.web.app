4672e1fa11deff2edec4b2721c1d132231e6b2ef
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-03-12 21:42:13 +0000

    remove unreferenced function that duplicates the same function in AnnotationUtils
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@5703 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/widgets/Widget.js
+++ src/main/javascript/NextThought/view/widgets/Widget.js
@@ -33,16 +33,6 @@ Ext.define('NextThought.view.widgets.Widget', {
 		return el;
 	},
 	
-	getNodeFromXPath: function(xpath) {
-		try {
-			return document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null).iterateNext();
-		}
-		catch(e) {
-			console.error('getNodeFromXPath: ',xpath, e.stack);
-			return null;
-		}
-	},
-	
 	cleanup: function(){
 		throw 'Must override';
 	}
