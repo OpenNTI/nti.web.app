011a90fe3fa760fb93812b6187f2f977ea6c65c9
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-03-26 20:51:07 +0000

    we no longer depend on Ext.draw.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@6113 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/javascript/app.js
+++ src/test/javascript/app.js
@@ -130,8 +130,7 @@ Ext.application({
 			'assets/js-test/specs/view/widgets/draw/Whiteboard.spec.js',
 			'assets/js-test/specs/cache/UserRepository.spec.js',
 			'assets/js-test/specs/util/AnnotationUtils.spec.js',
-			'assets/js-test/specs/util/Color.spec.js',
-			'assets/js-test/specs/ext-draw.spec.js'],
+			'assets/js-test/specs/util/Color.spec.js'],
 		go);
 	}
 });
