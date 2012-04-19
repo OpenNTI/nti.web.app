3b30f387c54b4311b18f078a5390d4a488e79722
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-04-19 19:22:11 +0000

    patch "up" to also include floatParents if ownerCt isn't defined.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@6702 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/util/Globals.js
+++ src/main/javascript/NextThought/util/Globals.js
@@ -312,6 +312,21 @@ Ext.define('NextThought.util.Globals',
 			}
 		});
 
+
+		Ext.AbstractComponent.override({
+			up: function(selector) {
+				var result = this.ownerCt||this.floatParent;
+				if (selector) {
+					for (; result; result = result.ownerCt||result.floatParent) {
+						if (Ext.ComponentQuery.is(result, selector)) {
+							return result;
+						}
+					}
+				}
+				return result;
+			}
+		});
+
 		Ext.Ajax.cors = true;
 		Ext.Ajax.disableCaching = false;
 		Ext.data.proxy.Server.override({noCache: false});
