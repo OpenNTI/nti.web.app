3619c8b964f7ebb9df863d5c9aa83c64bd56b0fb
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-05-04 17:53:53 +0000

    make a tab panel to switch between contacts and groups
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/branches/NewSkin@7068 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/account/MyContacts.js
+++ src/main/javascript/NextThought/view/account/MyContacts.js
@@ -1,5 +1,5 @@
 Ext.define('NextThought.view.account.MyContacts',{
-	extend: 'Ext.container.Container',
+	extend: 'Ext.tab.Panel',
 	alias: 'widget.my-contacts',
 	requires: [
 		'NextThought.view.account.contacts.Card',
@@ -7,35 +7,39 @@ Ext.define('NextThought.view.account.MyContacts',{
 	],
 	ui: 'my-contacts',
 	cls: 'my-contacts',
+	plain: true,
+	tabBar: {
+		baseCls: 'my-contacts-tab-bar',
+		plain: true,
+		ui: 'my-contacts',
+		defaults: {
+			plain: true,
+			ui: 'my-contacts'
+		}
+	},
+	defaults :{
+		autoScroll: true,
+		overflowX: 'hidden',
+		xtype: 'container'
+	},
 
 	items: [
-		{xtype: 'component', cls:'my-contacts-label', autoEl: 'div', html: 'Contacts'},
+//		{xtype: 'component', cls:'my-contacts-label', autoEl: 'div', html: 'Contacts'},
 		{
-			xtype: 'container',
-			cls: 'something-clever',
-			autoScroll: true,
-			overflowX: 'hidden',
+			title: 'People',
 			defaults: {
 				width: 275,
 				xtype: 'contacts-panel',
 				defaultType: 'contact-card'
 			},
 			items: [
-				{
-					id: 'activity-stream',
-					title: 'Active in...'
-				},
-				{
-					id: 'online-contacts',
-					title: 'Online'
-				},
-				{
-					id: 'offline-contacts',
-					collapsed: true,
-					showCount: false,
-					title: 'Offline'
-				}
+				{ id: 'activity-stream', title: 'Active' },
+				{ id: 'online-contacts', title: 'Online' },
+				{ id: 'offline-contacts', title: 'Offline', collapsed: true, showCount: false }
 			]
+		},
+		{
+			title: 'Groups'
 		}
 	]
 });
