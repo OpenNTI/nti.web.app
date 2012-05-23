bb5618a068cbdb396f50985ee158f80081eff44a
Troy Daley <troy.daley@nextthought.com>
2012-05-23 16:02:16 +0000

    update sample config to look like what we actually use these days
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@7522 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/WebApp/config-example.js
+++ src/main/WebApp/config-example.js
@@ -1,8 +1,6 @@
 var $AppConfig = {
 	"server" : {
-        "host": "http://localhost:8080",
-        //"host": "http://alpha.nextthought.com",
-        //"host": "http://excelsior.nextthought.com:3774",
+        "host": "http://api.dev",
         "data": "/dataserver2/",
 		"login": "/login/"
     }
