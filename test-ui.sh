0d25a30badd8c7a7c6ac53a3a11afb69adb06c60
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-05-23 16:24:50 +0000

    update python test launcher
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@7524 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- test-ui.sh
+++ test-ui.sh
@@ -8,37 +8,10 @@ export SELENIUM_DRIVER=*googlechrome
 #export SELENIUM_DRIVER=*firefox
 
 export SELENIUM_JAR=~/Applications/bin/selenium-server-standalone-2.16.1.jar
-
 export PORT=8181
-
-if [[ ! -z $FLAGGED ]] ; then
-	export TEST_URL=http://localhost/index.html
-	#do dataserver setup...
-
-else #just launch assuming everything is running
-	echo "Starting SimpleHTTP Server"
-
-	python -m SimpleHTTPServer $PORT >/dev/null 2>&1 &
-
-	HPID=`jobs -l 1 | awk '{print $2}'`
-	export TEST_URL=http://localhost:$PORT/index.html
-fi
-
+export TEST_URL=http://dev/WebApp/
 
 echo "Running tests at $TEST_URL"
 
 #run tests
 nosetests-2.7 -v -d -w ./src/test/python/
-
-
-
-#shutdown/cleanup
-
-if [[ ! -z $HPID ]] ; then
-	# kill the http server
-	echo "Stopping SimpleHTTP Server"
-	kill -9 $HPID
-fi
-
-
-
