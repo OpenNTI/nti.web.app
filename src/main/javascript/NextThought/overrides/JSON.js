2832a6863a58c9a834e714b68575ad562baae144
Troy Daley <troy.daley@nextthought.com>
2012-05-31 18:21:32 +0000

    remove dumb log
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@7727 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/overrides/JSON.js
+++ src/main/javascript/NextThought/overrides/JSON.js
@@ -2,7 +2,6 @@ Ext.define('NextThought.overrides.JSON',{
 	override: 'Ext.JSON',
 	encodeDate: function(d){
 		var t = d.getTime();
-		console.log('time in', t, 'time out', t/1000);
 		return t/1000;
 		//return Ext.Date.format(d, 'U');
 	}
