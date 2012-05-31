b59d70afb9b20fd769b11c8e6f6ba200c38e1063
Logan Testi <logan.testi@nextthought.com>
2012-05-31 16:34:54 +0000

    allowed use of firefox for tests and added users
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@7720 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/python/base.py
+++ src/test/python/base.py
@@ -1,4 +1,9 @@
 import os, time, unittest, webtest
+from ConfigParser import SafeConfigParser
+
+import selenium, time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 TEST_URL = os.environ.get('TEST_URL', 'http://localhost:8081/NextThoughtWebApp/')
 TEST_USER = os.environ.get('TEST_USER', 'jonathan.grimes@nextthought.com')
@@ -12,12 +17,27 @@ class WebAppTestBase(unittest.TestCase):
 	
 	@classmethod
 	def setUpApp(cls, test_url=TEST_URL):
+		cls.parser_config = SafeConfigParser()
+		cls.parser_config.read('config.ini')
+		cls.user1 = cls.parser_config.get('data', 'user1').split(',')
+		cls.user2 = cls.parser_config.get('data', 'user2').split(',')
+		cls.user3 = cls.parser_config.get('data', 'user3').split(',')
+		cls.user4 = cls.parser_config.get('data', 'user4').split(',')
+		os.environ.setdefault('SELENIUM_DRIVER', '*firefox')
 		cls.url = os.environ.get('TEST_URL', 'http://localhost:8081/NextThoughtWebApp/')
 		cls.app = webtest.SeleniumApp(url=cls.url)
 		
 	@classmethod
 	def tearDownClass(cls):
 		cls.app.close()
+	
+	# give enough time for elements to appear and be selectable
+#	def listen_for_element_presents(self, element_name, obj, timeout=30):
+#		start_time = time.time()
+#		while(not obj.find_element_by_id(element_name).is_displayed()):
+#			time.sleep(0.2)
+#		if (time.time() - start_time) > timeout: raise Exception
+#		time.sleep(1)
 	
 	def setUp(self):
 		try:
@@ -30,11 +50,13 @@ class WebAppTestBase(unittest.TestCase):
 		
 		self.wait_for_text("Username:","//label")
 		
-		resp.doc.input(name="username").value = user
-		resp.doc.input(name="password").value = password
-		resp.doc.button(buttonid='loginButton-btnEl').click()
-		
-		self.wait_for_node('//div[@id="top-controls"]')
+		resp.doc.input(name="username").value = self.user1[0]
+		resp.doc.input(Keys.RETURN)
+		resp.doc.input(name="password").value = self.user2[1]
+		resp.doc.button(buttonid='submit').click()
+		time.sleep(5)
+#		self.listen_for_element_presents("submit", resp)
+#		self.wait_for_node('//div[@id="top-controls"]')
 	
 	def logout(self):
 		self.resp.doc.xpath("//img[contains(@class, 'session-logout')]/..").click()		
