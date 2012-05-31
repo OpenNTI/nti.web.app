eb4964c6b7433c10ec4a9b5120f953301ea4f106
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-05-31 20:16:54 +0000

    rework code for Natalie to be more modular when reading a config file
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@7736 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/python/base.py
+++ src/test/python/base.py
@@ -2,51 +2,80 @@ import os
 import time
 import webtest
 import unittest
+import ConfigParser
 
-from ConfigParser import SafeConfigParser
 from selenium.webdriver.common.keys import Keys
-
 
 # ----------------------------------
 
-class Configuration ():
+class Configuration():
 	
-	def __init__(self):
-		self.config = SafeConfigParser() 	
+	def __init__(self, users=(), url=None, driver=None):
+		self.url = url
+		self.users = users
+		self.driver = driver
 		
-	def users(self):
-		self.config.read('config.ini') 
-		tuples = self.config.get('data', 'users').split(':')
-		users = [] 
-		for toople in tuples: 
-			users.append((toople.split (',')[0],toople.split(',')[1]))
-		return users 
-	
-	def url(self):
-		self.config.read ('config.ini')
-		url = self.config.get ('data', 'url')
-		return url
-	
-	def driver(self):
-		self.config.read ('config.ini')
-		driver = self.config.get ('data', 'driver')
-		return driver 
-	
+	@classmethod
+	def read(cls, source):
+		config = ConfigParser.ConfigParser()
+		config.read(source) 
+		
+		# users
+		users = cls._get_str_option(config, section='data', name="users")
+		assert  users, 'users were not found in config file'
+		users = eval(users)
+		if not users or not isinstance(users, (list, tuple)):
+			raise ValueError('invalid users array value')
+		for t in users:
+			assert isinstance(t, tuple) and len(t)==2, 'incorrect user/password tuple'
+			
+		# url
+		url = cls._get_str_option(config, section='data', name="url")
+		
+		# driver
+		driver = cls._get_str_option(config, section='data', name="driver")
+		
+		c = Configuration(users, url, driver)
+		return c
+		
+	@classmethod
+	def _get_str_option(cls, config, section=ConfigParser.DEFAULTSECT, name=None, default=None):
+		return cls._get_option(config.get, section, name, default)
+
+	@classmethod
+	def _get_bool_option(cls, config, section=ConfigParser.DEFAULTSECT, name=None, default=False):
+		return cls._get_option(config.getboolean, section, name, default)
+
+	@classmethod
+	def _get_int_option(cls, config, section=ConfigParser.DEFAULTSECT, name=None, default=None):
+		return cls._get_option(config.getint, section, name, default)
+
+	@classmethod
+	def _get_float_option(cls, config, section=ConfigParser.DEFAULTSECT, name=None, default=None):
+		return cls._get_option(config.getfloat, section, name, default)
+		
+	@classmethod
+	def _get_option(method, section, name, default):
+		try:
+			return method(section, name)
+		except:
+			return default
+		
 #-----------------------------------------------	
 	
-def is_node_displayed(resp, ID, xpath, timeout=60):
+def is_node_displayed(resp, _id, xpath, timeout=60):
 	for _ in range(timeout):
 		try:
-			if ID == resp.doc.xpath(xpath).id: 
-				if id.is_displayed():
-					break
+			node_id = resp.doc.xpath(xpath).id
+			if _id == node_id and id.is_displayed():
+				break
 		except:
 			pass
 		time.sleep(1)
 		return True
-	else: 
+	else:
 		return False
-	
+
 def wait_for_node(resp, xpath, timeout=60):
 	for _ in range(timeout):
 		try:
@@ -74,8 +103,8 @@ def wait_for_text(resp, text, xpath, timeout=60):
 def login(resp, user, password, wait_after_login=5):
 	wait_for_text(resp, "Username:","//label")
 	resp.doc.input(name="username").value = user
+	resp.doc.input(Keys.RETURN)
 	resp.doc.input(name="password").value = password
-	is_node_displayed(resp, 'submit', '//button')
 	resp.doc.button(buttonid='submit').click()
 	if wait_after_login:
 		time.sleep(wait_after_login)
@@ -87,22 +116,24 @@ def logout(resp):
 
 # ----------------------------------
 
+TEST_URL = os.environ.get('TEST_URL', 'http://localhost:8081/NextThoughtWebApp/')
+
 class WebAppTestBase(unittest.TestCase):
 	
 	@classmethod
 	def setUpClass(cls):
+		#TODO: pass the config file
 		cls.setUpApp()
 		
 	@classmethod
-	def setUpApp(cls):
-		config = Configuration()
-		cls.users = config.users()
-		cls.user = cls.users[0]
-		url = config.url()
-		environ = config.driver() 
-		os.environ.setdefault('SELENIUM_DRIVER', environ)
-		cls.url = os.environ.get('TEST_URL', url)
-		cls.app = webtest.SeleniumApp(url=url)
+	def setUpApp(cls, ini_file):
+		config = Configuration.read(ini_file)
+		cls.users = config.users
+		cls.url = config.url or TEST_URL
+		cls.driver = config.driver
+		if cls.driver:
+			os.environ.setdefault('SELENIUM_DRIVER', cls.driver)
+		cls.app = webtest.SeleniumApp(url= cls.url)
 		
 	@classmethod
 	def tearDownClass(cls):
@@ -115,8 +146,9 @@ class WebAppTestBase(unittest.TestCase):
 			self.fail(str(e))
 			
 	def login(self, user=None, password=None):
-		if not user: user = self.users[0][0] + '@nextthought.com'
-		if not password: password = self.users[0][1]
+		credentials = self.users[0]
+		user = user or credentials[0]
+		password = password or credentials[1]
 		login(self.resp, user, password)
 
 	def logout(self):
