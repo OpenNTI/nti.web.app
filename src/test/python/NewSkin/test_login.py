1a1383420fb6d87f93c8fcf811bf76e4a8d904d8
Logan Testi <logan.testi@nextthought.com>
2012-06-01 02:23:11 +0000

    added tests for login, incorrect password login, and incorrect username login. Refactored the tests
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@7762 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/python/NewSkin/test_login.py
+++ src/test/python/NewSkin/test_login.py
@@ -1,22 +1,37 @@
-import os
+'''
+Created on May 31, 2012
+
+@author: ltesti
+'''
 import unittest
 
-from lxml import etree
 from base import WebAppTestBase
 from hamcrest import assert_that, is_
 
-__path__ = os.path.split(__file__)[0]
-
-class TestLogin(WebAppTestBase):
+class Test_Login(WebAppTestBase):
     
-    ini_file = os.path.join(__path__, 'config.ini')
+    ini_file = '/Users/ltesti/Projects/NextThoughtWebApp/src/test/python/NewSkin/config.ini'
+    
+    # --------login tests----------
     
     def test_login(self):
-        self.login(self.users[0][0], self.users[0][1])
-        tree = etree.ElementTree(self.resp.lxml)
-        assert_that(etree.tostring(tree.find('//title')), is_('<title>NextThought App</title>'))
+        self.login()
+        assert_that(self.elem_in_tree('title', 'text', 'NextThought App'), is_(True))
     
-    def test_logout(self):pass
+    def test_failed_user_login(self):
+        self.login('incorrect_user', 'incorrect_password')
+        assert_that(self.elem_in_tree('title', 'text', 'NextThought Login'), is_(True))
+        
+    def test_failed_password_login(self):
+        self.login(password='incorrect_password')
+        assert_that(self.elem_in_tree('title', 'text', 'NextThought Login'), is_(True))
+        assert_that(self.elem_in_tree('div', 'id', 'message'), is_(True))
+        assert_that(self.elem_in_tree('div', 'class', 'message'), is_(True))
+        assert_that(self.elem_in_tree('div', 'text', 'Please try again, there was a problem logging in.'), is_(True))
+    
+    # --------logout tests---------
+    
+#    def test_logout(self):pass
 #        self.login()
     
 if __name__ == "__main__":
