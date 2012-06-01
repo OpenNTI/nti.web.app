af7d37e621363f7f5705444a5ba4ee2af04a26a6
Logan Testi <logan.testi@nextthought.com>
2012-06-01 03:27:50 +0000

    changed the method calls to take the new methods
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@7768 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/python/NewSkin/test_login.py
+++ src/test/python/NewSkin/test_login.py
@@ -16,23 +16,24 @@ class Test_Login(WebAppTestBase):
     
     def test_login(self):
         self.login()
-        assert_that(self.elem_in_tree('title', 'text', 'NextThought App'), is_(True))
+        assert_that(self.text_in_tree('title', 'NextThought App'), is_(True))
     
     def test_failed_user_login(self):
         self.login('incorrect_user', 'incorrect_password')
-        assert_that(self.elem_in_tree('title', 'text', 'NextThought Login'), is_(True))
+        assert_that(self.text_in_tree('title', 'NextThought Login'), is_(True))
         
     def test_failed_password_login(self):
         self.login(password='incorrect_password')
-        assert_that(self.elem_in_tree('title', 'text', 'NextThought Login'), is_(True))
+        assert_that(self.text_in_tree('title', 'NextThought Login'), is_(True))
         assert_that(self.elem_in_tree('div', 'id', 'message'), is_(True))
         assert_that(self.elem_in_tree('div', 'class', 'message'), is_(True))
-        assert_that(self.elem_in_tree('div', 'text', 'Please try again, there was a problem logging in.'), is_(True))
+        assert_that(self.text_in_tree('div', 'Please try again, there was a problem logging in.'), is_(True))
     
     # --------logout tests---------
     
-#    def test_logout(self):pass
+#    def test_logout(self):
 #        self.login()
+#        self.logout()
     
 if __name__ == "__main__":
     unittest.main()
