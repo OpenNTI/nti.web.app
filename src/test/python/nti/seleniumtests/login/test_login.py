1a0b5208839e708203e7a6c5242c054d36f98056
Logan Testi <logan.testi@nextthought.com>
2012-06-04 16:15:40 +0000

    changed the emplementation of the matcher
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@7799 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/python/nti/seleniumtests/login/test_login.py
+++ src/test/python/nti/seleniumtests/login/test_login.py
@@ -3,33 +3,33 @@ import unittest
 
 from nti.seleniumtests.base import  WebAppTestBase
 
-from nti.seleniumtests import as_the_text_of
-from nti.seleniumtests import as_the_value_of
 from nti.seleniumtests.matchers import is_in_tree
 from hamcrest import assert_that
 
 __path__ = os.path.split(__file__)[0]
 
 class TestLogin(WebAppTestBase):
-    
-    ini_file = os.path.join(__path__, '../config/main.ini')
-    def test_login(self):
-        self.login()
-        assert_that('NextThought App', is_in_tree(as_the_text_of('title', self.resp)))
-    
-    def test_failed_user_login(self):
-        self.login('incorrect_user', 'incorrect_password')
-        assert_that('NextThought Login', is_in_tree(as_the_text_of('title', self.resp)))
-        
-    def test_failed_password_login(self):
-        self.login(password='incorrect_password')
-        assert_that('NextThought Login', is_in_tree(as_the_text_of('title', self.resp)))
-        assert_that('message', is_in_tree(as_the_value_of('id', 'div', self.resp)))
-        assert_that('message', is_in_tree(as_the_value_of('class', 'div', self.resp)))
-        assert_that('Please try again, there was a problem logging in.', is_in_tree(as_the_text_of('div', self.resp)))
-        
-#    def test_logout(self):pass
-#        self.login()
-    
+	
+	ini_file = os.path.join(__path__, '../config/main.ini')
+	
+	def test_login(self):
+		self.login()
+		assert_that('NextThought App', is_in_tree('title', self.resp))
+	
+	def test_failed_user_login(self):
+		self.login('incorrect_user', 'incorrect_password')
+		assert_that('NextThought Login', is_in_tree('title', self.resp))
+		
+	def test_failed_password_login(self):
+		self.login(password='incorrect_password')
+		assert_that('NextThought Login', is_in_tree('title', self.resp))
+		assert_that('message', is_in_tree('div', self.resp, 'id'))
+		assert_that('message', is_in_tree('div', self.resp, 'class'))
+		assert_that('Please try again, there was a problem logging in.', is_in_tree('div', self.resp))
+		
+	def test_logout(self):
+		self.login()
+		self.logout()
+	
 if __name__ == "__main__":
-    unittest.main()
+	unittest.main()
