c970426861130998c33cebe489312e02a5914963
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-06-05 14:20:37 +0000

    update user repo spec
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@7826 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/javascript/specs/cache/UserRepository.spec.js
+++ src/test/javascript/specs/cache/UserRepository.spec.js
@@ -7,7 +7,7 @@ describe("User Repository/Store/Cache Behavior", function(){
 
 		//this used to check to make sure communities don't get requested, we no longer resolve
 		//users for friends lists so we don't need the workaround to not resolve communities.
-		expect(UserRepository.makeRequest).toHaveBeenCalled();
+		expect(UserRepository.makeRequest).not.toHaveBeenCalled();
 
 		UserRepository.getUser('jonathan.grimes@foo');
 		expect(UserRepository.makeRequest).toHaveBeenCalled();
@@ -16,6 +16,6 @@ describe("User Repository/Store/Cache Behavior", function(){
 
 	it("should maintain user/communty model instances", function()
 	{
-		expect(UserRepository.getStore().getCount()).toBe(3);//The configured test user, the Community 'TestersAnnon', and the test@foo
+		expect(UserRepository.getStore().getCount()).toBe(4);//The configured test user, the Communities 'TestersAnnon', 'NextThought', and 'Everyone'
 	});
 });
