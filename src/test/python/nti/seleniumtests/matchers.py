0149f13f382b6f90b0aab0afe37bf28fcede9a6f
Nathalie Kaligirwa <nathalie.kaligirwa@nextthought.com>
2012-06-05 21:46:55 +0000

    changed matchers to work with SST.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@7851 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/python/nti/seleniumtests/matchers.py
+++ src/test/python/nti/seleniumtests/matchers.py
@@ -1,8 +1,14 @@
 from hamcrest.core.base_matcher import BaseMatcher
 from lxml import etree
+import html5lib 
+from html5lib import treewalkers, serializer, treebuilders
+from sst.actions import *
 
-def _values_of_node(node, resp, element):		
-	tree = etree.ElementTree(resp.lxml)
+def _values_of_node(node, element):		
+	#tree = etree.ElementTree(resp.lxml)
+	html = get_page_source()
+	p = html5lib.HTMLParser( tree=treebuilders.getTreeBuilder("lxml"), namespaceHTMLElements=False )
+	tree = p.parse (html)
 	items = []
 	for item in tree.iter(node):
 		if element: value = item.get(element)
@@ -12,13 +18,12 @@ def _values_of_node(node, resp, element):
 
 class HasElementWithText(BaseMatcher):
 
-	def __init__( self, resp, element ):
+	def __init__( self, element ):
 		super(HasElementWithText, self).__init__( )
-		self.resp = resp
 		self.element = element
 
 	def _matches( self, item ):
-		return _values_of_node(self.resp, self.element, str(item))
+		return _values_of_node(self.element, str(item))
 
 	def describe_to( self, description ):
 		description.append_text('text in element').append( str(self.element) )
@@ -27,14 +32,13 @@ def is_in_text_for_element( resp, element ):
 	return HasElementWithText( resp, element )
 
 class IsInTree(BaseMatcher):
-	def __init__(self, node, resp, element):
+	def __init__(self, node, element):
 		self.node = node
-		self.resp = resp
 		self.element = element
 		
 	def _matches(self, item, mismatch_desciption=None):
 		self.item = item
-		matcher = _values_of_node(self.node, self.resp, self.element)
+		matcher = _values_of_node(self.node, self.element)
 		return item in matcher
 	
 	def describe_mismatch(self, item, description):
@@ -43,5 +47,5 @@ class IsInTree(BaseMatcher):
 	def describe_to(self, description):
 		description.append_description_of(self.node + ' attribute to contain ' + self.item)
 	
-def is_in_tree(node, resp, element=None):
-	return IsInTree(node, resp, element)
+def is_in_tree(node, element=None):
+	return IsInTree(node, element)
