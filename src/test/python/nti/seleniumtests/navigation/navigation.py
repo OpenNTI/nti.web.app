389c02be40eba7a1a842f8d58ef8f6dc6408cedc
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-06-06 15:17:55 +0000

    clean code a bit
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@7871 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/python/nti/seleniumtests/navigation/navigation.py
+++ src/test/python/nti/seleniumtests/navigation/navigation.py
@@ -1,7 +1,8 @@
-from nti.seleniumtests.base import WebAppTestBase
-import time
-from sst.actions import *
+from sst.actions import get_element
+from sst.actions import get_elements
+from sst.actions import switch_to_frame
 
+from nti.seleniumtests.base import WebAppTestBase
 
 LIBRARY_CLASS = "//span[contains(@class, 'x-btn-icon library')]/.."
 BOOK_CLASS = "//div[contains (@class, 'title') and text () ='"
@@ -9,59 +10,41 @@ CHAPTER_CLASS = "//div[contains (@class, 'x-grid-row') and text() ='"
 SECTION_CLASS = "//div[contains (@class, 'x-grid-row  x-grid-tree-node-leaf') and text () = '"
 
 class WebAppNavigation (WebAppTestBase):
-     
-    
-    def setUp(self):
-        super (WebAppNavigation, self).setUp()
-        self.login()
-        
-    def open_library (self):
-        element = get_element (css_class = 'library', tag = 'span')
-        element.click()
-    
-    def open_book(self,title = 'Prealgebra'):
-        self.open_library()
-        element = get_element (css_class = 'title', tag = 'div', text = 'Prealgebra')
-        element.click()
-        elements = get_elements (css_class = 'x-grid-row', tag= 'tr')
-        values  = []
-        for element in elements:
-            if element.text:  
-                values.append (element.text)
-        
-        return values
-            
-        
-        
-        
-        
-    def open_chapter (self, title = 'Exponents'):
-        self.open_book()
-        element = get_element (css_class = 'x-grid-row', tag = 'tr', text = title)
-        element.click()
-        elements = get_elements (css_class = 'x-grid-tree-node-leaf', tag= 'tr')
-        values  = []
-        for element in elements:
-            if element.text:  
-                values.append (element.text)
-        
-        return values
-    
-        
-      
-    def open_section (self, title = 'Squares'):
-        self.open_chapter()
-        element = get_element (css_class = 'x-grid-row', tag = 'div', text = title)
-        element.click()
-    
-    def get_page_section_title (self, frameName = 'component-1036'):
-        switch_to_frame (frameName)
-        element_chapter = get_element (css_class ='chapter title')
-        
-        return element_chapter.find_element_by_class_name ('label')
-    
 
-        
-    def tearDown (self):
-        super(WebAppNavigation, self).tearDown()
-        
+	def setUp(self):
+		super (WebAppNavigation, self).setUp()
+		self.login()
+		
+	def open_library (self):
+		element = get_element(css_class='library', tag='span')
+		element.click()
+	
+	def open_book(self, title='Prealgebra'):
+		self.open_library()
+		element = get_element(css_class='title', tag='div', text=title)
+		element.click()
+		elements = get_elements(css_class = 'x-grid-row', tag= 'tr')
+		values  = [element.text for element in elements if element.text]		
+		return values
+			
+	def open_chapter (self, title='Exponents'):
+		self.open_book()
+		element = get_element (css_class='x-grid-row', tag='tr', text=title)
+		element.click()
+		elements = get_elements (css_class='x-grid-tree-node-leaf', tag='tr')
+		values  = [element.text for element in elements if element.text]	
+		return values
+	
+	def open_section (self, title='Squares'):
+		self.open_chapter()
+		element = get_element (css_class='x-grid-row', tag='div', text=title)
+		element.click()
+	
+	def get_page_section_title (self, frameName='component-1036'):
+		switch_to_frame (frameName)
+		element_chapter = get_element (css_class ='chapter title')
+		return element_chapter.find_element_by_class_name ('label')
+
+	def tearDown (self):
+		super(WebAppNavigation, self).tearDown()
+		
--- src/test/python/nti/seleniumtests/navigation/test_navigation.py
+++ src/test/python/nti/seleniumtests/navigation/test_navigation.py
@@ -1,9 +1,10 @@
 import os
-import time
 import unittest
+
 from nti.seleniumtests.navigation.navigation import WebAppNavigation
-from nti.seleniumtests.base import  WebAppTestBase
+
 from nti.seleniumtests.matchers import is_in_tree
+
 from hamcrest import assert_that
 
 __path__ = os.path.split(__file__)[0]
@@ -15,42 +16,38 @@ CHAPTERS = ['Exponents','Number Theory','Decimals']
 #random sections
 SECTIONS = ['Index', 'Squares', 'Higher Exponents']
 
-
 class TestNavigation(WebAppNavigation):
-    
-    ini_file = os.path.join(__path__, '../config/main.ini')
+	
+	ini_file = os.path.join(__path__, '../config/main.ini')
 
-    def test_open_library(self):
-        self.open_library()
-        
-        #TO-DO: Optimize this
-        for book in BOOKS: 
-            assert_that (book, is_in_tree ('div'))
-                    
-    def test_open_book(self):
-        values = self.open_book ()
-        
-        #TO_DO: Optimize this 
-        assert_that(CHAPTERS, list ((set(values) & set (CHAPTERS))))
-        
-        #Info: the code below is a better option but chapter names are buried to deep in the div to be recognizable by the lxml
-        #the same applies to open_chapter and open_section
-        #for chapter in CHAPTERS:
-        #   assert_that (chapter, is_in_tree('div'))
-                
-    def test_open_chapter (self):  
-        values = self.open_chapter()
-        
-        #TO-DO: Optimize this
-        assert_that(SECTIONS, list ((set(values) & set (CHAPTERS))))
-        #for section in SECTIONS: 
-         #   assert_that (section,is_in_tree( 'div', self.resp, 'class'))
-            
-    def test_open_section(self):
-        self.open_section()
-        value = self.get_section_chapter_title()
-        assert_that(value, text = 'Squares')
-        
-        
+	def test_open_library(self):
+		self.open_library()		
+		#TO-DO: Optimize this
+		for book in BOOKS: 
+			assert_that (book, is_in_tree ('div'))
+					
+	def test_open_book(self):
+		values = self.open_book ()
+		#TO_DO: Optimize this 
+		assert_that(CHAPTERS, list ((set(values) & set (CHAPTERS))))
+		
+		#Info: the code below is a better option but chapter names are buried to deep in the div to be recognizable by the lxml
+		#the same applies to open_chapter and open_section
+		#for chapter in CHAPTERS:
+		#   assert_that (chapter, is_in_tree('div'))
+				
+	def test_open_chapter (self):  
+		values = self.open_chapter()
+		
+		#TO-DO: Optimize this
+		assert_that(SECTIONS, list ((set(values) & set (CHAPTERS))))
+		#for section in SECTIONS: 
+		#   assert_that (section,is_in_tree( 'div', self.resp, 'class'))
+			
+	def test_open_section(self):
+		self.open_section()
+		value = self.get_section_chapter_title()
+		assert_that(value, text = 'Squares')
+		
 if __name__ == "__main__":
-    unittest.main()
+	unittest.main()
