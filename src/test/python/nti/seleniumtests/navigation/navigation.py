1482e8aad02b196e310090908d6fcb819f07f51c
Nathalie Kaligirwa <nathalie.kaligirwa@nextthought.com>
2012-06-08 01:29:25 +0000

    - open items refactoring
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@7935 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/python/nti/seleniumtests/navigation/navigation.py
+++ src/test/python/nti/seleniumtests/navigation/navigation.py
@@ -3,7 +3,7 @@ from sst.actions import get_elements
 from sst.actions import switch_to_frame
 from sst.actions import click_element
 from sst.actions import get_elements_by_xpath
-from nti.seleniumtests import wait_for_element_id
+from nti.seleniumtests import wait_for_element
 from nti.seleniumtests import wait_for_element_xpath
 import time
 
@@ -19,46 +19,53 @@ class WebAppNavigation (WebAppTestBase):
     def setUp(self):
         super (WebAppNavigation, self).setUp()
         self.login()
-        self.books = [] 
-        self.chapters = []    # list of chapter elements for the current book being navigated
-        self.section_elements = []      # list of sections
         
-    def open_library (self):
-        wait_for_element_xpath (self.xpath_contains_builder('span', 'class', 'library') + '/..')
-        element = get_element(css_class='library', tag='span')
+    def open_level (self, tag = None, attribute = None, attribute_value = None, text_value = None):
+        wait_for_element_xpath (self.xpath_contains_builder (tag, attribute, attribute_value ) + '/..')
+        if not text_value:
+            element = get_element (css_class = attribute_value, tag = tag)
+        else: 
+            wait_for_element_xpath (self.xpath_contains_and_text_builder(tag, attribute, attribute_value, text_value) + '/..')
+            element = get_element (css_class = attribute_value, tag = tag, text = text_value)
+            time.sleep(1)
         click_element (element)
-        wait_for_element_xpath ('//div[contains (@class, "title")]/..')
-        self.books = get_elements(css_class = 'title', tag= 'div')
-        self.library_values  = [element.text for element in self.books if element.text]
+    
+    def open_library (self):
+        self.open_level('span', 'class', 'library')
+        library_elements = get_elements (css_class = 'title', tag = 'div')  
+        books  = [element.text for element in library_elements if element.text]
+        return books
     
     def open_book(self, book):
-        #Find a better way to handle opening a book
-        wait_for_element_xpath ('//div[contains (@class, "title")]/..')
-        element = get_element(css_class='title', tag='div', text=book)
-        click_element(element)
-        wait_for_element_xpath ('//div[contains (@class, "x-grid-row")]/..')
-        self.chapter_elements = get_elements(css_class = 'x-grid-row', tag= 'tr')
-        self.chapters  = [element.text for element in self.chapters if element.text]
-            
+        self.open_level('div', 'class', 'title', book)
+        wait_for_element_xpath (self.xpath_contains_builder('tr', 'class', 'x-grid-row') + '/..')
+        book_elements = get_elements(css_class = 'x-grid-row', tag= 'tr')
+        chapters  = [element.text for element in book_elements if element.text]
+        return chapters 
+    
     def open_chapter (self, chapter):
-        wait_for_element_xpath ('//div[contains (@class, "x-grid-row")]/..')
-        element = get_element (css_class='x-grid-row', tag='tr', text=chapter)
-        click_element (element, wait=False)
-        time.sleep(1)
-        xpath = "//tr[contains (@class, 'x-grid-tree-node-leaf')]/*"
-        self.section_elements = get_elements_by_xpath(xpath)
-        self.sections  = [element.text for element in self.section_elements if element.text]
+        self.open_level ('tr', 'class', 'x-grid-row', chapter)    
+        xpath = self.xpath_contains_builder('tr', 'class', 'x-grid-tree-node-leaf') + '/*'
+        #xpath = "//tr[contains (@class, 'x-grid-tree-node-leaf')]/*"
+        self.chapter_elements = get_elements_by_xpath(xpath)
+        sections  = [element.text for element in self.chapter_elements if element.text]
+        return sections
     
     def open_section (self, section):
-        xpath = "//tr[contains (@class, 'x-grid-tree-node-leaf')]/*"
-        self.section_elements = get_elements_by_xpath(xpath)
-        for elem in self.section_elements:
-            if elem.text == section:
-                wait_for_element_xpath('//tr[contains (@class, "x-grid-tree-node-leaf") and text() ="' + section + '"]/*')
-                click_element (elem)
-                break 
-        self.open_library()
         
+        self.open_level ('tr', 'class', 'x-grid-tree-node-leaf', section)
+#        xpath = self.xpath_contains_builder('tr', 'class', 'x-grid-tree-node-leaf') + '/*' 
+#        #chapter_elements = get_elements_by_xpath(xpath)
+#        
+#        for elem in self.chapter_elements:
+#            if elem.text == section:
+#        # find an xpath builder for expressions with more than contains...
+#                wait_for_element_xpath(self.xpath_contains_and_text_builder ('tr', 'class', 'x-grid=tree-node-keaf', section) + '/..')
+#                click_element(elem, wait = True)
+#                break 
+        self.open_library()
+    
+    
     
     def navigate_to(self, book, chapter=None, section=None):
         
@@ -87,7 +94,7 @@ class WebAppNavigation (WebAppTestBase):
             self.open_section(section)
     
     def get_page_section_title (self, frameName='component-1036'):
-        wait_for_element_id (frameName)
+        wait_for_element(frameName)
         switch_to_frame (frameName)
         element_chapter = get_element (css_class ='chapter title')
         return element_chapter.find_element_by_class_name ('label').text
--- src/test/python/nti/seleniumtests/navigation/test_navigation.py
+++ src/test/python/nti/seleniumtests/navigation/test_navigation.py
@@ -19,25 +19,31 @@ SECTIONS = ['Index', 'Squares', 'Higher Exponents']
 class TestNavigation(WebAppNavigation):
 	
 	ini_file = os.path.join(__path__, '../config/main.ini')
-#
-#	def test_open_library(self):
-#		books = self.open_library()		
-#		#TO-DO: Optimize this
-#		if not books: 
-#			print 'Library is empty'
-#					
-#	def test_open_book(self):
-#		chapters = self.open_book ()
-#		#TO_DO: Optimize this 
-#		if not chapters: 
-#			print 'Book has no chapter'
+
+	def test_open_library(self):
+		books = self.open_library()	
+		
+		#TO-DO: Optimize this
+		if not books: 
+			print 'Library is empty'
+					
+	def test_open_book(self):
+		self.open_library()
+		chapters = self.open_book ('Prealgebra')
+		#TO_DO: Optimize this 
+		if not chapters: 
+			print 'Book has no chapter'
 #						
-#	def test_open_chapter (self): 
-#		sections = self.open_chapter()
-#		#TO-DO: Optimize this
-#		if not sections: 
-#			print 'Chapter has no sections'
-			
+	def test_open_chapter (self): 
+		self.open_library()
+		self.open_book('Prealgebra')
+		sections = self.open_chapter('Decimals')
+		#self.open_section('Rounding')
+	
+		#TO-DO: Optimize this
+		if not sections: 
+			print 'Chapter has no sections'
+#			
 	def test_open_section(self):
 		self.navigate_to(book='Prealgebra', chapter='Decimals', section='Rounding')
 		value = self.get_page_section_title()
