c377ff61efb4acfd475a8dbd18f2cda797d55755
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-06-11 23:37:58 +0000

    we can pass the html source
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@8033 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/python/nti/seleniumtests/matchers.py
+++ src/test/python/nti/seleniumtests/matchers.py
@@ -4,9 +4,9 @@ from nti.seleniumtests import html_parse
 
 from hamcrest.core.base_matcher import BaseMatcher
 
-def _values_of_node(node, element):	
+def _values_of_node(node, element, html=None):	
 	items = []	
-	html = get_page_source()
+	html = html or get_page_source()
 	tree = html_parse(html)
 	for item in tree.iter(node):
 		if element: 
@@ -32,13 +32,14 @@ def is_in_text_for_element( resp, element ):
 	return HasElementWithText( resp, element )
 
 class IsInTree(BaseMatcher):
-	def __init__(self, node, element):
+	def __init__(self, node, element, html=None):
 		self.node = node
+		self.html = html
 		self.element = element
-		
+
 	def _matches(self, item, mismatch_desciption=None):
 		self.item = item
-		matcher = _values_of_node(self.node, self.element)
+		matcher = _values_of_node(self.node, self.element, self.html)
 		return item in matcher
 	
 	def describe_mismatch(self, item, description):
@@ -47,5 +48,5 @@ class IsInTree(BaseMatcher):
 	def describe_to(self, description):
 		description.append_description_of(self.node + ' attribute to contain ' + self.item)
 	
-def is_in_tree(node, element=None):
-	return IsInTree(node, element)
+def is_in_tree(node, element=None, html=None):
+	return IsInTree(node, element, html)
