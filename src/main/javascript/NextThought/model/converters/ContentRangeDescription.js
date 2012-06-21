dc593b90512795be8875b69776767ad54c949132
Troy Daley <troy.daley@nextthought.com>
2012-06-21 21:39:49 +0000

    placeholder notes causing terrible stack traces on conversion
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@8287 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/model/converters/ContentRangeDescription.js
+++ src/main/javascript/NextThought/model/converters/ContentRangeDescription.js
@@ -5,7 +5,12 @@ Ext.define('NextThought.model.converters.ContentRangeDescription', {
 		type: 'ContentRangeDescription',
 		convert: function(v,record) {
 			try {
-				return NextThought.model.anchorables.ContentRangeDescription.createFromObject(v);
+				if (v) {
+					return NextThought.model.anchorables.ContentRangeDescription.createFromObject(v);
+				}
+				else {
+					return null;
+				}
 			}
 			catch (e) {
 				console.error('CRD: Parsing Error: ',e.message, e.stack, arguments);
