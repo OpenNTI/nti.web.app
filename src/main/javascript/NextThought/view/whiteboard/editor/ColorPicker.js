1ec14357f5b0341644fd974968ed7706e90fdea3
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-06-26 02:07:10 +0000

    progress on text options
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@8385 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/whiteboard/editor/ColorPicker.js
+++ src/main/javascript/NextThought/view/whiteboard/editor/ColorPicker.js
@@ -19,10 +19,17 @@ Ext.define('NextThought.view.whiteboard.editor.ColorPicker',{
 			xtype: 'wb-color-option'
 		},
 		items: [
-			{color: 'black'},
-			{color: 'grey1'},
-			{color: 'grey2'},
-			{color: 'grey3'}
+			{ color: 'black', stroke: '#333333' },
+			{ color: 'grey1', stroke: '#707070' },
+			{ color: 'grey2', stroke: '#acacac' },
+			{ color: 'grey3', stroke: '#e1e1e1' },
+			{ color: 'red', stroke: '#d34f39' },
+			{ color: 'blue', stroke: '#2b89c5' },
+			{ color: 'green', stroke: '#a0c94c' },
+			{ color: 'orange', stroke: '#fa8700' },
+			{ color: 'magenta', stroke: '#b42789' },
+			{ color: 'purple', stroke: '#6f3d93' },
+			{ color: 'yellow', stroke: '#fff02a' }
 		]
 	}]
 });
