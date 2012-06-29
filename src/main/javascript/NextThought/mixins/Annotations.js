c6389d71c006fd8b03111c13c00e9cf0f94313db
Troy Daley <troy.daley@nextthought.com>
2012-06-29 18:49:15 +0000

    snap to word selections.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/branches/NoteRedesign@8508 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/mixins/Annotations.js
+++ src/main/javascript/NextThought/mixins/Annotations.js
@@ -483,6 +483,8 @@ Ext.define('NextThought.mixins.Annotations', {
 
 
 	getSelection: function() {
+		this.snapSelectionToWord();
+
 		var doc = this.getDocumentElement(),
 			win = doc.parentWindow,
 			range, selection;
@@ -506,5 +508,60 @@ Ext.define('NextThought.mixins.Annotations', {
 				win.getSelection().removeAllRanges();
 		}
 		catch(e){console.warn(e.stack||e.toString());}
+	},
+
+
+	/*
+	 * Snap the selection to whole words as opposed to partial words.  This code is taken and only
+	 * minimally adjusted, from here:
+	 * http://stackoverflow.com/questions/10964016/how-do-i-extend-selection-to-word-boundary-using-javascript-once-only/10964743#10964743
+	 */
+	snapSelectionToWord: function() {
+		var sel,
+			doc = this.getDocumentElement(),
+			window = doc.parentWindow;
+
+		// Check for existence of window.getSelection() and that it has a
+		// modify() method. IE 9 has both selection APIs but no modify() method.
+		if (window.getSelection && (sel = window.getSelection()).modify) {
+			sel = window.getSelection();
+			if (!sel.isCollapsed) {
+
+				// Detect if selection is backwards
+				var range = doc.createRange();
+				range.setStart(sel.anchorNode, sel.anchorOffset);
+				range.setEnd(sel.focusNode, sel.focusOffset);
+				var backwards = range.collapsed;
+				range.detach();
+
+				// modify() works on the focus of the selection
+				var endNode = sel.focusNode, endOffset = sel.focusOffset;
+				sel.collapse(sel.anchorNode, sel.anchorOffset);
+
+				var direction = [];
+				if (backwards) {
+					direction = ['backward', 'forward'];
+				} else {
+					direction = ['forward', 'backward'];
+				}
+
+				sel.modify("move", direction[0], "character");
+				sel.modify("move", direction[1], "word");
+				sel.extend(endNode, endOffset);
+				sel.modify("extend", direction[1], "character");
+				sel.modify("extend", direction[0], "word");
+			}
+		} else if ( (sel = doc.selection) && sel.type != "Control") {
+			var textRange = sel.createRange();
+			if (textRange.text) {
+				textRange.expand("word");
+				// Move the end back to not include the word's trailing space(s),
+				// if necessary
+				while (/\s$/.test(textRange.text)) {
+					textRange.moveEnd("character", -1);
+				}
+				textRange.select();
+			}
+		}
 	}
 });
