35628d656dd5f68c8d6a69c820a69b515987afc4
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-07-03 20:02:25 +0000

    saving progress... reworked mouse tracking
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/branches/NoteRedesign@8542 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/content/reader/NoteOverlay.js
+++ src/main/javascript/NextThought/view/content/reader/NoteOverlay.js
@@ -5,7 +5,15 @@ Ext.define('NextThought.view.content.reader.NoteOverlay', {
 	constructor: function(){
 		this.on({
 			scope: this,
-			'content-updated': this.resolveNewClientRects,
+			'content-updated': function(){
+				var doc = this.getDocumentElement();
+				var root = doc.querySelector('#NTIContent > .page-contents');
+				var data = this.noteOverlayData;
+				var r = root.getBoundingClientRect();
+				data.left = r.left;
+				data.width = r.width;
+				data.root = root;
+			},
 			'afterRender': this.insertOverlay
 		});
 
@@ -13,17 +21,16 @@ Ext.define('NextThought.view.content.reader.NoteOverlay', {
 	},
 
 
-	resolveNewClientRects: function(){
-		var doc = this.getDocumentElement(),
+	resolveClientRects: function(node){
+		if(!node){return null;}
+		var doc = node.ownerDocument,
 			range = doc.createRange(),
-			old = this.noteOverlayData.range,
-			n = doc.querySelector('#NTIContent > .page-contents');
+			rects;
 
-		if(old){ old.detach(); }
-
-		range.selectNode(n);
-		this.noteOverlayData.range = range;
-		this.noteOverlayData.bound = n;
+		range.selectNode(node);
+		rects = range.getClientRects();
+		range.detach();
+		return rects;
 	},
 
 
@@ -49,14 +56,15 @@ Ext.define('NextThought.view.content.reader.NoteOverlay', {
 						top: 0, left: 0,
 						background: 'rgba(127,127,127,0.6)',
 						width: width,
-						height: '20px',
 						'border-bottom': '2px dotted red'
-					}
+					},
+					html: '&nbsp;'
 				}
 			]
 		};
 		container = Ext.DomHelper.insertAfter(me.body.first(),container);
 		me.noteOverlayData.inputBox = container.firstChild;
+
 		me.on({
 			destroy: function(){ Ext.fly(container).remove(); },
 			'sync-height': function(h){ Ext.get(container).setHeight(h); }
@@ -70,64 +78,69 @@ Ext.define('NextThought.view.content.reader.NoteOverlay', {
 	},
 
 
-	isNotablePlace: function(rect){
-		return true;
-	},
 
+	resolveNodeAt: function(y){
 
-	findRect: function (y, direction){
-		var r = Array.prototype.slice.call(this.noteOverlayData.range.getClientRects());
-		var i = r.length-1;
-		var sign = direction ? -1 : 1;
-		Ext.Array.sort(r,function(a,b){
-			var ab = a.bottom;
-			var bb = b.bottom;
-			return sign*(ab === bb
-					? 0
-					: ab < bb
-						?  1
-						: -1);
-		});
-
-		for(; i>=0; i--){
-			if((direction && r[i].bottom < y)
-			|| (!direction && r[i].top > y)){
-				break;
+		function firstElementOnLine(y,data,o){
+			var right = data.width,
+				el = null;
+			while(right > data.left && (!el || el === data.root || !Ext.fly(data.root).contains(el))){
+				el = doc.elementFromPoint(right,y);
+				right -= 2;
 			}
+			return el;
 		}
 
-		if(!this.isNotablePlace(r[i])){
-			return null;
+		function findBlockParent(e){
+			if(!e || e===data.root){return null;}
+			var d = doc.defaultView.getComputedStyle(e).getPropertyValue('display');
+			if(re.test(d)){
+				return findBlockParent(e.parentNode);
+			}
+			return e;
 		}
 
-		return r[i];
+		var doc = this.getDocumentElement();
+		var data = this.noteOverlayData;
+		var e = firstElementOnLine(y,data,this.getAnnotationOffsets());
+
+		var re = /(inline.*)|(none)|(fixed)/i;
+
+		var o = e;
+		e = findBlockParent(e);
+
+		return Ext.fly(data.root).contains(e)
+				? e
+				: null;
 	},
+
 
 	noteOverlayMouseOver: function(evt,t){
 		evt.stopEvent();
 
+		var doc = this.getDocumentElement();
 		var data = this.noteOverlayData;
-		var o = evt.getTarget('.note-gutter').getBoundingClientRect(),
-			y = evt.getY(),
-			dY = (data.lastY||0) - y,
-			direction = dY === 0 && data.hasOwnProperty('lastDirection') ? data.lastDirection : (dY > 0),
+		var o = this.getAnnotationOffsets(),
+			y = evt.getY() - o.top,
 			b = data.inputBox,
-			r;
+			s = doc.parentWindow.getSelection(),
+			n = this.resolveNodeAt(y);
 
-		if( dY ){//not zero
-			data.lastY = y;
-			data.lastDirection = direction;
-			clearTimeout(data.lastMove);
-			data.lastMove = setTimeout(function(){
-				delete data.lastY;
-				delete data.lastDirection;
-				delete data.lastMove;
-			},500);
+
+		var r = doc.createRange();
+		try {
+			var a = this.resolveClientRects( n );
+			console.log(a);
+			a = a[1];
+			r.selectNode( n );
+
+			Ext.get(b).setHeight(a.height).setY(a.top + o.top).hide().show();
 		}
+		catch(e){}
 
-		r = this.findRect(y - o.top, direction);
-		if(r){
-			Ext.get(b).setY(r.top + o.top);
+		s.removeAllRanges();
+		if(!r.collapsed){
+			s.addRange(r);
 		}
 	}
 });
