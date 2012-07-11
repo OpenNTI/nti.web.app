8573db46140c028065fe9064a8ab414923104c40
Troy Daley <troy.daley@nextthought.com>
2012-07-11 03:21:30 +0000

    initial single note gutter widget minus gradient which is causing me trouble.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/branches/NoteRedesign@8685 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/annotations/Note.js
+++ src/main/javascript/NextThought/view/annotations/Note.js
@@ -3,6 +3,25 @@ Ext.define( 'NextThought.view.annotations.Note', {
 	alias: 'widget.note',
 	requires:[],
 
+	/*
+	singleGutterWidget: null,
+	multiGutterWidget: null,
+	*/
+
+	singleGutterWidgetTmpl: Ext.DomHelper.createTemplate( {
+		cls: 'note-gutter-widget-single',
+		children: [
+			{
+				cls: 'content',
+				children: [
+					{cls: 'name', html: '{0}'},
+					{cls: 'text', html: '{1}'}
+				]
+			},
+			{cls: 'mask'}
+		]
+	} ).compile(),
+
 	constructor: function(config){
 		this.callParent(arguments);
 
@@ -12,9 +31,48 @@ Ext.define( 'NextThought.view.annotations.Note', {
 
 
 	render: function(){
-		if(this.hasHighlight){
-			this.callParent();
+		return this.callParent();
+		//TODO - initally just render the highlight, we want to use styles to not render highlights created from clicking on the left.
+	},
+
+	//Notes don't have controls
+	//getControl: function(){},
+
+
+	getGutterWidget: function(numberOfSiblings){
+		if (numberOfSiblings > 1){
+			if (!this.multiGutterWidget){
+
+			}
+			return this.multiGutterWidget;
+
 		}
+		else {
+			if (!this.singleGutterWidget){
+				this.createSingleGutterWidget();
+			}
+			return this.singleGutterWidget;
+		}
+	},
+
+
+	createSingleGutterWidget: function(){
+		var creator = this.record.get('Creator'),
+			htmlString = this.singleGutterWidgetTmpl.apply([
+				creator,
+				AnnotationUtils.getBodyTextOnly(this.record)]),
+			dom = Ext.DomHelper.createDom({html:htmlString}).firstChild;
+
+		//now create the ext object:
+		this.singleGutterWidget = Ext.get(dom);
+
+		UserRepository.getUser(creator, function(u){
+			var name = u[0].getName();
+			this.singleGutterWidget.down('.name').update(name);
+		}, this);
+
+
+
 	}
 
 });
