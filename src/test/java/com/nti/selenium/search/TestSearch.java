fa776df00f6270cc52e2d5126989773cb916d48e
Logan Testi <logan.testi@nextthought.com>
2012-07-11 20:06:14 +0000

    added the final search tests for selecting multiple search options and switching between exact and partial matches
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@8709 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/java/com/nti/selenium/search/TestSearch.java
+++ src/test/java/com/nti/selenium/search/TestSearch.java
@@ -11,52 +11,52 @@ import org.openqa.selenium.WebElement;
 
 public class TestSearch extends Search{
 
-//	@Test
-//	public void testEnterText(){
-//		this.insertSearchText("math");
-//		assertTrue(this.elementExists(XpathUtilsSearch.getBookSearchResults()));
-//		assertTrue(this.findNumberOfSearchResults() > 0);
-//	}
-//	
-//	@Test
-//	public void testNavigateToItem(){
-//		this.insertSearchText("math");
-//		String sectionTitle = this.findSectionTitle(0);
-//		this.clickSearchedBook(sectionTitle);
-//		this.waitForLoading();
-//		sectionTitle = this.convertSectionTitleToTextTitle(sectionTitle);
-//		assertTrue(this.elementExists(XpathUtilsSearch.getSectionTitle(sectionTitle)));
-//	}
-//	
-//	@Test
-//	public void testSeeAll(){
-//		this.insertSearchText("math");
-//		int beforeSeeAllNumOfResults = this.findNumberOfSearchResults();
-//		this.clickSeeAll();
-//		int afterSeeAllNumOfResults = this.findNumberOfSearchResults();
-//		assertTrue(beforeSeeAllNumOfResults < afterSeeAllNumOfResults);
-//	}
-//	
-//	@Test
-//	public void testClickExpandArrow(){
-//		this.clickSearchTextBoxExpandArrow();
-//		assertTrue(this.elementExists(XpathUtilsSearch.getSearchTextBoxExpandMenu()));
-//	}
-//	
-//	@Test
-//	public void testSearchOptionsEverything(){
-//		this.clickSearchTextBoxExpandArrow();
-//		assertTrue(this.elementExists(XpathUtilsSearch.getSearchTextBoxExpandMenu()));
-//		assertTrue(this.isChecked("Everything"));
-//	}
-//	
-//	@Test
-//	public void testRemoveSearchEverything(){
-//		this.clickSearchTextBoxExpandArrow();
-//		assertTrue(this.elementExists(XpathUtilsSearch.getSearchTextBoxExpandMenu()));
-//		this.clickMenuItem("Everything");
-//		assertTrue(this.isChecked("Everything"));
-//	}
+	@Test
+	public void testEnterText(){
+		this.insertSearchText("math");
+		assertTrue(this.elementExists(XpathUtilsSearch.getBookSearchResults()));
+		assertTrue(this.findNumberOfSearchResults() > 0);
+	}
+	
+	@Test
+	public void testNavigateToItem(){
+		this.insertSearchText("math");
+		String sectionTitle = this.findSectionTitle(0);
+		this.clickSearchedBook(sectionTitle);
+		this.waitForLoading();
+		sectionTitle = this.convertSectionTitleToTextTitle(sectionTitle);
+		assertTrue(this.elementExists(XpathUtilsSearch.getSectionTitle(sectionTitle)));
+	}
+	
+	@Test
+	public void testSeeAll(){
+		this.insertSearchText("math");
+		int beforeSeeAllNumOfResults = this.findNumberOfSearchResults();
+		this.clickSeeAll();
+		int afterSeeAllNumOfResults = this.findNumberOfSearchResults();
+		assertTrue(beforeSeeAllNumOfResults < afterSeeAllNumOfResults);
+	}
+	
+	@Test
+	public void testClickExpandArrow(){
+		this.clickSearchTextBoxExpandArrow();
+		assertTrue(this.elementExists(XpathUtilsSearch.getSearchTextBoxExpandMenu()));
+	}
+	
+	@Test
+	public void testSearchOptionsEverything(){
+		this.clickSearchTextBoxExpandArrow();
+		assertTrue(this.elementExists(XpathUtilsSearch.getSearchTextBoxExpandMenu()));
+		assertTrue(this.isChecked("Everything"));
+	}
+	
+	@Test
+	public void testRemoveSearchEverything(){
+		this.clickSearchTextBoxExpandArrow();
+		assertTrue(this.elementExists(XpathUtilsSearch.getSearchTextBoxExpandMenu()));
+		this.clickMenuItem("Everything");
+		assertTrue(this.isChecked("Everything"));
+	}
 	
 	@Test
 	public void testSearchBook(){
@@ -94,24 +94,76 @@ public class TestSearch extends Search{
 		assertFalse(this.isChecked("Books"));
 	}
 	
-//	@Test
-//	public void testSwitchBetweenMultipleItemsAndEverything(){
-//		
-//	}
-//	
-//	@Test
-//	public void testSwitchToExactMatches(){
-//		
-//	}
-//	
-//	@Test
-//	public void testSwitchToPartialMatches(){
-//		
-//	}
-//	
-//	@Test
-//	public void testSelectAll(){
-//		
-//	}
+	@Test
+	public void testSwitchToMultipleSearchOptions(){
+		this.clickSearchTextBoxExpandArrow();
+		assertTrue(this.elementExists(XpathUtilsSearch.getSearchTextBoxExpandMenu()));
+		assertTrue(this.isChecked("Everything"));
+		this.clickMenuItem("Books");
+		this.clickMenuItem("Highlights");
+		this.clickMenuItem("Notes");
+		assertFalse(this.isChecked("Everything"));
+		assertTrue(this.isChecked("Books"));
+		assertTrue(this.isChecked("Highlights"));
+		assertTrue(this.isChecked("Notes"));
+	}
+	
+	@Test
+	public void testSwitchBetweenMultipleItemsAndEverything(){
+		this.clickSearchTextBoxExpandArrow();
+		assertTrue(this.elementExists(XpathUtilsSearch.getSearchTextBoxExpandMenu()));
+		assertTrue(this.isChecked("Everything"));
+		this.clickMenuItem("Books");
+		this.clickMenuItem("Highlights");
+		this.clickMenuItem("Notes");
+		assertFalse(this.isChecked("Everything"));
+		assertTrue(this.isChecked("Books"));
+		assertTrue(this.isChecked("Highlights"));
+		assertTrue(this.isChecked("Notes"));
+		this.clickMenuItem("Everything");
+		assertTrue(this.isChecked("Everything"));
+		assertFalse(this.isChecked("Books"));
+		assertFalse(this.isChecked("Highlights"));
+		assertFalse(this.isChecked("Notes"));
+	}
+	
+	@Test
+	public void testSwitchToExactMatches(){
+		this.clickSearchTextBoxExpandArrow();
+		assertTrue(this.elementExists(XpathUtilsSearch.getSearchTextBoxExpandMenu()));
+		this.clickMenuItem("Exact Matches");
+		assertTrue(this.isChecked("Exact Matches"));
+		assertFalse(this.isChecked("Partial Matches"));
+	}
+	
+	@Test
+	public void testSwitchToPartialMatches(){
+		this.clickSearchTextBoxExpandArrow();
+		assertTrue(this.elementExists(XpathUtilsSearch.getSearchTextBoxExpandMenu()));
+		this.clickMenuItem("Partial Matches");
+		assertFalse(this.isChecked("Exact Matches"));
+		assertTrue(this.isChecked("Partial Matches"));
+	}
+	
+	@Test
+	public void testSelectAll(){
+		this.clickSearchTextBoxExpandArrow();
+		assertTrue(this.elementExists(XpathUtilsSearch.getSearchTextBoxExpandMenu()));
+		assertTrue(this.isChecked("Everything"));
+		this.clickMenuItem("Books");
+		this.clickMenuItem("Highlights");
+		this.clickMenuItem("Notes");
+		this.clickMenuItem("Transcripts");
+		this.clickMenuItem("Quiz Results");
+		this.clickMenuItem("Bookmarks");
+		this.clickMenuItem("Contacts");
+		assertTrue(this.isChecked("Books"));
+		assertTrue(this.isChecked("Highlights"));
+		assertTrue(this.isChecked("Notes"));
+		assertTrue(this.isChecked("Transcripts"));
+		assertTrue(this.isChecked("Quiz Results"));
+		assertTrue(this.isChecked("Bookmarks"));
+		assertTrue(this.isChecked("Contacts"));
+	}
 	
 }
