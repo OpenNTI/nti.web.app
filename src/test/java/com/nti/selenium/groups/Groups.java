2ab70f91eb418a4bd6fecd24c45230595deaf0e1
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-07-18 21:10:36 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@8998 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/java/com/nti/selenium/groups/Groups.java
+++ src/test/java/com/nti/selenium/groups/Groups.java
@@ -1,17 +1,16 @@
 package com.nti.selenium.groups;
+
 import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.*;
-
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
 
 import com.nti.selenium.Credentials;
-import com.nti.selenium.XpathUtils;
-import com.nti.selenium.navigation.Navigation; 
+import com.nti.selenium.navigation.Navigation;
 
 public class Groups extends Navigation { 
-
 
 	protected Credentials[] credentials; 
 
@@ -23,48 +22,35 @@ public class Groups extends Navigation {
 	}
 
 	@After 
-	public void tearDown(){ 
-		
+	public void tearDown() { 
 		this.openGroups();
-		try{ 
+		try { 
 			this.setActiverDriver(driver[1]);
 			this.removeGroups();
-
-		}
-		catch (Exception e)
-		{ 
+		} catch (Exception e) { 
 			System.out.println("Unable to remove groups/deleting test");
 			System.out.println(e.getMessage());
-		}
-		finally 
-		{ 
+		} finally { 
 			selenium[1].stop(); 
 		}
 	}
 
-	public void removeGroups(){
-		
+	public void removeGroups() {
 		int oldGroupCount = 0;
-		
 		this.openGroups();
-		for (;;)
+		for (int i=0; i<=30;i++)
 		{
 			this.wait_(1);
-			List <WebElement> buttons = this.findElements(XpathUtilsGroups.getPeopleAndGroupsDeleteButton());
-			List <WebElement> groups = this.findElements(XpathUtilsGroups.getPeopleAndGroups());
+			final List <WebElement> buttons =
+					this.findElements(XpathUtilsGroups.getPeopleAndGroupsDeleteButton());
+			final List <WebElement> groups = 
+					this.findElements(XpathUtilsGroups.getPeopleAndGroups());
 
-			if (groups.size() <=1)
-			{
+			if (groups.size() <=1) {
 				break; 
-			}
-
-			else if (oldGroupCount == groups.size())
-			{
+			} else if (oldGroupCount == groups.size()) {
 				break;
-			}
-
-			else 
-			{
+			} else  {
 				oldGroupCount = groups.size();
 				groups.get(1).click();
 				buttons.get(1).click();
@@ -72,46 +58,33 @@ public class Groups extends Navigation {
 		}
 	}
 
-	public void openGroups()
-	{
+	public void openGroups() {
 		//click button
-
-		try{
+		try {
 			this.wait_(2);
-			WebElement element = this.findElement(XpathUtilsGroups.setOpenGroups());
+			final WebElement element = this.findElement(XpathUtilsGroups.setOpenGroups());
 			element.click();
 			//check if fields are available
 			boolean present = true; 
-			try
-			{
+			try {
 				this.findElement(XpathUtilsGroups.getInputUsername()); 
 				this.findElement(XpathUtilsGroups.getInputGroupName());
 				this.findElement(XpathUtilsGroups.getAddButton());
 
-			}
-			catch (final NoSuchElementException e)
-			{
+			} catch (final NoSuchElementException e) {
 				present = false; 
 			}
 
-			if (present == false)
-			{
+			if (present == false) {
 				System.out.println("Some fields are unavailable");
 			}
-
-		}catch(final NoSuchElementException e){
-
+		} catch(final NoSuchElementException e) {
 			System.out.println("no button found");
 			//its not been found
 		}
-
-
-
 	}
 
-	public void addGroup(String groupname)
-	{
-
+	public void addGroup(final String groupname) {
 		this.openGroups();
 		WebElement element = this.findElement (XpathUtilsGroups.getInputGroupName()); 
 		element.sendKeys (groupname); 
@@ -120,22 +93,16 @@ public class Groups extends Navigation {
 	}
 
 
-	public String addPeopleToGroup(String...strings)
-	{
-		String time = Long.toString(System.currentTimeMillis());
+	public String addPeopleToGroup(final String...strings) {
+		final String time = Long.toString(System.currentTimeMillis());
 		this.addGroup(time);
 
-		String user; 
-		int count = 0; 
-		
-		
-
-		for(String str: strings)
+		for(final String str: strings)
 		{
 			this.waitForElement(XpathUtilsGroups.getInputUsername());
 			this.findElement(XpathUtilsGroups.getInputUsername()).sendKeys(str);
 			this.waitForElement(XpathUtilsGroups.getPersonGroupItem(str));
-			if(!this.elementExists(XpathUtilsGroups.getPersonGroupItem(str))){
+			if (!this.elementExists(XpathUtilsGroups.getPersonGroupItem(str))) {
 				System.out.println("taking a break");
 			}
 			this.findElement(XpathUtilsGroups.getPersonGroupItem(str)).click();
@@ -149,34 +116,26 @@ public class Groups extends Navigation {
 		return time; 
 	}
 	
-	public void addOnePersonToGroup()
-	{ 
-		String username = credentials[0].getUserName(); 
+	public void addOnePersonToGroup() { 
+		final String username = credentials[0].getUserName(); 
 		this.addPeopleToGroup(username);
 	}
 	
-	public void addMultiplePeopleToGroup(int num)
-	{ 
-		String[] usernames = new String[num]; 
+	public void addMultiplePeopleToGroup(final int num) { 
+		final String[] usernames = new String[num]; 
 		for ( int i = 0; i < num; i++)
 		{ 
 			usernames[i] = credentials[i].getUserName(); 
 		}
-		
 		this.addPeopleToGroup(usernames);	
 	}
 	
-	public void removePersonFromGroup()
-	{
-		
-		String time = this.addPeopleToGroup(credentials[0].getUserName(), credentials[1].getUserName()); 
+	public void removePersonFromGroup() {
+		this.addPeopleToGroup(credentials[0].getUserName(), credentials[1].getUserName()); 
 		this.findElement(XpathUtilsGroups.getGroupsButton()).click();
 		this.findElement(XpathUtilsGroups.getGroupMember("Test-10")).click();
 		this.findElement(XpathUtilsGroups.getGroupMemberDeleteButton()).click(); 	
-		
 	}
-	
 } 
 
- 
 
