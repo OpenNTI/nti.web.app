8c28d12ca4bd51dd5cceb54dd5c70afc6d21a702
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-07-20 01:39:28 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@9097 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/java/com/nti/selenium/highlights/Highlights.java
+++ src/test/java/com/nti/selenium/highlights/Highlights.java
@@ -19,84 +19,81 @@ public class Highlights extends Navigation {
 	}
 	
 	@After
-	public void tearDown(){
+	public void tearDown() {
 		this.switchToIframe();		
-		try{
+		try {
 			while (this.findElements(XpathUtilsHighlights.getCreatedHighlight()).size() > 0)
 			{
 				this.removeHighlight(this.findElements(XpathUtilsHighlights.getCreatedHighlight()).get(0));
 			}
-		}
-		catch(NoSuchElementException e){
-		}
-		finally{
+		} catch(final NoSuchElementException e) {
+		} finally {
 			selenium[1].stop();
 		}
 	}
 	
-	public void createHighlight(){
+	public void createHighlight() {
 		this.switchToIframe();
 		this.selectText(0, 0, 10);
 		this.switchToDefault();
 		this.findElement(XpathUtilsHighlights.getCreateHighlightButton()).click();
 	}
 	
-	public void removeHighlight(String xpath){
-		WebElement element = this.findElement(xpath);
+	public void removeHighlight(final String xpath) {
+		final WebElement element = this.findElement(xpath);
 		this.switchToIframe();	
 		element.click();
 		this.switchToDefault();
 		this.findElement(XpathUtilsHighlights.getRemoveHighlightButton()).click();
 	}
 	
-	public void removeHighlight(WebElement element){
+	public void removeHighlight(WebElement element) {
 		this.switchToIframe();	
 		element.click();
 		this.switchToDefault();
 		this.findElement(XpathUtilsHighlights.getRemoveHighlightButton()).click();
 	}
 	
-	public void shareHighlight(String xpath){
+	public void shareHighlight(final String xpath) {
 		this.switchToIframe();
 		this.findElement(xpath).click();
 		this.switchToDefault();
 		this.findElement(XpathUtilsHighlights.getShareWithButton()).click();
 	}
 	
-	public void inputSearchForUser(String username){
+	public void inputSearchForUser(final String username) {
 		this.findElement(XpathUtilsHighlights.getShareHighlightInputField()).sendKeys(username);
 	}
 	
-	public void clickSearchResult(String username){
+	public void clickSearchResult(final String username) {
 		this.findElement(XpathUtilsHighlights.getSearchedForUser(username)).click();
 	}
 	
-	public void clickDropDownArrow(){
+	public void clickDropDownArrow() {
 		this.findElement(XpathUtilsHighlights.getShareHighlightInputFieldDropDownArrow()).click();
 	}
 	
-	public void clickEveryoneDropDownItem(){
+	public void clickEveryoneDropDownItem() {
 		this.findElement(XpathUtilsHighlights.getEveryoneDropDownItem()).click();
 	}
 	
-	public void clickGroupDropDownItem(String groupName){
+	public void clickGroupDropDownItem(final String groupName) {
 		this.findElement(XpathUtilsHighlights.getGroupDropDownItem(groupName)).click();
 	}
 	
-	public void clickCloseEveryoneToken(){
+	public void clickCloseEveryoneToken() {
 		this.findElement(XpathUtilsHighlights.getEveryoneTokenLabelCloseButton()).click();
 	}
 	
-	public void clickCloseUserGroupToken(String userNameGroupName){
+	public void clickCloseUserGroupToken(final String userNameGroupName) {
 		this.findElement(XpathUtilsHighlights.getGroupTokenLabelCloseButton(userNameGroupName)).click();
 	}
 	
-	public void clickSaveButton(){
+	public void clickSaveButton() {
 		this.findElement(XpathUtilsHighlights.getShareHighlightSaveButton()).click();
 	}
 	
-	public void clickCancelButton(){
+	public void clickCancelButton() {
 		this.findElement(XpathUtilsHighlights.getShareHighlightCancelButton()).click();
 	}
-	
 }
