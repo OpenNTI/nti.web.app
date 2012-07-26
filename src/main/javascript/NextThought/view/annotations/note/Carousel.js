6abb22a3fd1f7c8fbaa361cf7c93acd619766f65
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-07-26 01:56:24 +0000

    more carousel work
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@9333 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/annotations/note/Carousel.js
+++ src/main/javascript/NextThought/view/annotations/note/Carousel.js
@@ -8,9 +8,10 @@ Ext.define('NextThought.view.annotations.note.Carousel',{
 	renderTpl: Ext.DomHelper.createTemplate([
 			{
 				id: '{id}-body',
+				cls: 'carousel-body',
 				tpl: new Ext.XTemplate('{%this.renderContainer(out,values)%}')
 			},
-			{ cls: 'slide-nav backward' },
-			{ cls: 'slide-nav forward' }
+			{ cls: 'slide-nav backward disabled', cn:[{cls: 'circle'}] },
+			{ cls: 'slide-nav forward disabled', cn:[{cls: 'circle'}] }
 		]).compile()
 });
