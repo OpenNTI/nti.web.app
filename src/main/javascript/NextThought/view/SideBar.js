3e2d5cf7386918ad9362d224e2b712d6cd31a43c
Troy Daley <troy.daley@nextthought.com>
2012-08-02 23:59:20 +0000

    make sidebar go away on smaller browsers, add button to click to expand if you want to see it.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@9620 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/SideBar.js
+++ src/main/javascript/NextThought/view/SideBar.js
@@ -9,9 +9,102 @@ Ext.define('NextThought.view.SideBar',{
 
 	width: 275,
 	layout: 'vbox',
+	floating: false,
+	autoShow: true,
+	shadow: false,
+	cls: 'main-sidebar',
 
 	items: [
 		{xtype: 'my-account'},
 		{xtype: 'my-contacts', flex:1}
-	]
+	],
+
+	toggleTpl: Ext.DomHelper.createTemplate({cls: 'sidebar-toggle'}).compile(),
+
+
+	initComponent: function(){
+		this.callParent(arguments);
+		this.toggle = this.toggleTpl.append(Ext.getBody(), [], true);
+		this.mon(this.toggle, {
+			scope: this,
+			'click': this.toggleFlyover
+		});
+		Ext.EventManager.onWindowResize(this.viewportMonitor,this);
+		//this.viewportMonitor(Ext.Element.getViewportWidth());
+	},
+
+
+	destroy: function(){
+		Ext.EventManager.removeResizeListener(this.viewportMonitor,this);
+		this.toggle.remove();
+	},
+
+
+	viewportMonitor: function(w){
+		if (this.floating) {
+			this.hide();
+		}
+
+		if (w < 1300) {
+			if (!this.floating) {
+				//this.hide();
+				this.collapse();
+				this.toggle.show();
+			}
+		}
+		else if(this.floating){
+			this.expand();
+			this.toggle.hide();
+		}
+	},
+
+
+	collapse: function(){
+		if (!this.floating){
+			this.myOwner.items.remove(this);
+			this.myOwner.floatingItems.add(this);
+			this.hide();
+			this.floating = true;
+			this.hidden = true;
+			this.wrapPrimaryEl(this.el.dom);
+		}
+	},
+
+
+	expand: function(){
+		this.myOwner.items.add(this);
+		this.myOwner.floatingItems.remove(this);
+
+		this.floating = false;
+		this.el.removeCls('x-layer');
+		this.show();
+	},
+
+
+	toggleFlyover: function(){
+		var visible = this.isVisible(),
+			h = Ext.Element.getViewportHeight(),
+			w = Ext.Element.getViewportWidth();
+		if (visible) {
+			this.hide();
+			return;
+		}
+		this.setHeight(h);
+		this.showAt(w-this.width, 0);
+		this.doLayout();
+	},
+
+
+	toFront: function(){
+		this.callParent(arguments);
+		this.toggle.setStyle({zIndex: this.el.zindex+1});
+	},
+
+
+	onAdded: function(o){
+		console.log('added');
+		this.myOwner = o;
+		o.floatingItems = o.floatingItems || new Ext.util.MixedCollection();
+		return this.callParent(arguments);
+	}
 });
