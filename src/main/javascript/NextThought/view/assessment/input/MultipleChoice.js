55af20e2d5b92856dc611725f60e7d61027e6441
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-08-13 21:05:01 +0000

    visually implement multi/single choice multiple choice quesitons
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@10014 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/assessment/input/MultipleChoice.js
+++ src/main/javascript/NextThought/view/assessment/input/MultipleChoice.js
@@ -1,21 +1,49 @@
 Ext.define('NextThought.view.assessment.input.MultipleChoice',{
 	extend: 'NextThought.view.assessment.input.Base',
-	alias: 'widget.question-input-multiplechoicepart',
+	alias: 'widget.question-input-waitingforName',
 
-	inputTpl: Ext.DomHelper.markup({}),
-
-	renderSelectors: {
-	},
+	inputTpl: Ext.DomHelper.markup({ cls: 'multi-choice {choice-style}', cn:[{
+		tag: 'tpl', 'for': 'choices', cn: [{
+			cls: 'choice',
+			cn:[
+				{ tag: 'span', cls: 'control', 'data-index':'{[xindex-1]}'},//xindex is 1 based
+				{ tag: 'span', cls: 'label', html:'{[String.fromCharCode(64+xindex)]}.' },
+				{ tag: 'span', html:'{.}'  }
+			]
+		}]}
+	]}),
 
 
 	initComponent: function(){
 		this.callParent(arguments);
+		this.choices = (this.part.get('choices')||[]).slice();
 
-		console.log(this.part);
+		//clean out markup
+		Ext.each(this.choices,function(v,i,a){
+			a[i] = v.replace(/<.*?>/g, '').replace(/^\s+/,'').replace(/\s+$/,'');
+		});
+
+		this.renderData = Ext.apply(this.renderData||{},{
+			choices: this.choices,
+			'choice-style': 'multi'
+		});
 	},
 
 
 	afterRender: function(){
 		this.callParent(arguments);
+
+		this.mon(this.getEl().select('.choice'),{
+			scope: this,
+			click: this.choiceClicked
+		});
+	},
+
+
+	choiceClicked: function(e){
+		var c = e.getTarget('.choice',null,true);
+		if(!c){return;}
+
+		c.down('.control').toggleCls('checked');
 	}
 });
---
+++ src/main/javascript/NextThought/view/assessment/input/SingleChoice.js
@@ -0,0 +1,20 @@
+Ext.define('NextThought.view.assessment.input.SingleChoice',{
+	extend: 'NextThought.view.assessment.input.MultipleChoice',
+	alias: 'widget.question-input-multiplechoicepart',
+
+	initComponent: function(){
+		this.callParent(arguments);
+		this.renderData = Ext.apply(this.renderData,{
+			'choice-style': 'single'
+		});
+	},
+
+	choiceClicked: function(e){
+		var c = e.getTarget('.choice',null,true);
+		if(!c){return;}
+
+		this.getEl().select('.choice .control').removeCls('checked');
+
+		c.down('.control').addCls('checked');
+	}
+});
