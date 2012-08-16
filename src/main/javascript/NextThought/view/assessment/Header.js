3507223e3aecaa2987e688b44bfe61a377737b06
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-08-16 18:40:41 +0000

    remove likes/favorites from questions
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@10180 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/assessment/Header.js
+++ src/main/javascript/NextThought/view/assessment/Header.js
@@ -6,19 +6,19 @@ Ext.define('NextThought.view.assessment.Header',{
 	ui: 'assessment',
 
 	renderTpl: Ext.DomHelper.markup([
-		{
-			cls: 'controls',
-			cn: [{ cls: 'favorite' },{ cls: 'like' }]
-		},
+//		{
+//			cls: 'controls',
+//			cn: [{ cls: 'favorite' },{ cls: 'like' }]
+//		},
 		{cls:'title',html:'{title}'},
 		{cls: 'status {status}',html:'{status}'}
 	]),
 
 	renderSelectors: {
+//		liked: '.controls .like',
+//		favorites: '.controls .favorite',
 		myTitle: '.title',
-		status: '.status',
-		liked: '.controls .like',
-		favorites: '.controls .favorite'
+		status: '.status'
 	},
 
 
@@ -27,24 +27,25 @@ Ext.define('NextThought.view.assessment.Header',{
 	},
 
 
-	afterRender: function(){
-		this.callParent(arguments);
-		var r = this.question,
-			l = this.liked,
-			f = this.favorites;
-
-		this.mon(l, 'click', function(){ r.like(l); }, this);
-		this.mon(f, 'click', function(){ r.favorite(f); }, this);
-		l.update(r.getFriendlyLikeCount());
-		l[(r.isLiked()?'add':'remove')+'Cls']('on');
-		f[(r.isFavorited()?'add':'remove')+'Cls']('on');
-	},
+//	afterRender: function(){
+//		this.callParent(arguments);
+//		var r = this.question,
+//			l = this.liked,
+//			f = this.favorites;
+//
+//		this.mon(l, 'click', function(){ r.like(l); }, this);
+//		this.mon(f, 'click', function(){ r.favorite(f); }, this);
+//		l.update(r.getFriendlyLikeCount());
+//		l[(r.isLiked()?'add':'remove')+'Cls']('on');
+//		f[(r.isFavorited()?'add':'remove')+'Cls']('on');
+//	},
 
 
 	onAdded: function(assessmentParent){
 		var id = '?unresolved title?';
 		try {
-			this.question = assessmentParent.question;
+//			this.question = assessmentParent.question;
+
 			//HACK: there should be a more correct way to get the problem name/number...
 			id = assessmentParent.question.getId().split('.').last() + '.';
 		}
