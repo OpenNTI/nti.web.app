c9479c5e562be895ad52f3abb655abe37d6eb254
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-08-16 14:08:57 +0000

    fake it 'til you make it...
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@10159 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/content/reader/AssessmentOverlay.js
+++ src/main/javascript/NextThought/view/content/reader/AssessmentOverlay.js
@@ -35,40 +35,53 @@ Ext.define('NextThought.view.content.reader.AssessmentOverlay', {
 	},
 
 
-	injectAssessments: function(items){
+	makeAssessmentQuestion: function(q,set){
+		this.activeAssessments[q.getId()] = Ext.widget('assessment-question',{
+			reader: this,
+			question: q,
+			renderTo: this.assessmentOverlay,
+			questionSet: set || null,
+			contentElement: this.getAssessmentElement('object','data-ntiid', q.getId())
+		});
+	},
+
+
+	makeAssessmentQuiz: function(set){
 		var me = this,
-			c = this.assessmentOverlay;
+			QuestionSet = Ext.ModelManager.getModel('NextThought.model.assessment.QuestionSet'),
+			c = me.assessmentOverlay,
+			guid = guidGenerator(),
+
+		/// FAKE IT 'TIL YOU MAKE IT!!!
+			questions = Ext.isArray(set)? set : set.get('questions');
+
+		if(Ext.isArray(set)){
+			set = new QuestionSet({questions:set});
+		}
+		/// FAKE IT 'TIL YOU MAKE IT!!! (end)
+
+
+		me.activeAssessments[guid+'scoreboard'] = Ext.widget('assessment-scoreboard',{
+			reader: me, renderTo: c, questionSet: set
+		});
+
+		me.activeAssessments[guid+'submission'] = Ext.widget('assessment-quiz-submission',{
+			reader: me, renderTo: c, questionSet: set
+		});
+
+		Ext.each(questions,function(q){me.makeAssessmentQuestion(q,set);});
+
+	},
+
+
+	injectAssessments: function(items){
+		var me = this;
 
 		//nothing to do.
 		if(!items || items.length < 1){
 			return;
 		}
 
-		function makeQuestion(q,set){
-			me.activeAssessments[q.getId()] = Ext.widget('assessment-question',{
-				reader: me,
-				question: q,
-				renderTo: c,
-				questionSet: set || null,
-				contentElement: me.getAssessmentElement('object','data-ntiid', q.getId())
-			});
-		}
-
-		function makeQuiz(set){
-			var guid = guidGenerator();
-			me.activeAssessments[guid+'scoreboard'] = Ext.widget('assessment-scoreboard',{
-				reader: me, renderTo: c, questionSet: set
-			});
-
-			me.activeAssessments[guid+'submission'] = Ext.widget('assessment-quiz-submission',{
-				reader: me, renderTo: c, questionSet: set
-			});
-
-			Ext.each(set.get('questions'),function(q){makeQuestion(q,set);});
-
-		}
-
-		//TODO: Remove all content based submit buttons
 		new Ext.dom.CompositeElement(
 			this.getDocumentElement().querySelectorAll('.x-btn-submit,[onclick^=NTISubmitAnswers]')).remove();
 
@@ -78,10 +91,15 @@ Ext.define('NextThought.view.content.reader.AssessmentOverlay', {
 			return ( ( a === b ) ? 0 : ( ( a > b ) ? 1 : -1 ) );
 		});
 
+		/// FAKE IT 'TIL YOU MAKE IT!!!
+		me.makeAssessmentQuiz(items);
+
+		/*
 		Ext.each(items,function(q){
-			if(q.isSet){ makeQuiz(q); }
-			else { makeQuestion(q); }
+			if(q.isSet){ me.makeAssessmentQuiz(q); }
+			else { me.makeAssessmentQuestion(q); }
 		});
+		*/
 	},
 
 
