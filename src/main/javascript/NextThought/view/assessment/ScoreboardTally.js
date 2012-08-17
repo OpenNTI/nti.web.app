c551692db7d0f5c6fa583a24578df6f66e6d3caa
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-08-17 15:59:21 +0000

    add system for changing messages. hide tally parts that have '0' as their count.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@10264 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/assessment/ScoreboardTally.js
+++ src/main/javascript/NextThought/view/assessment/ScoreboardTally.js
@@ -4,8 +4,24 @@ Ext.define('NextThought.view.assessment.ScoreboardTally',{
 
 	cls: 'tally-box',
 
+
+	messages: {
+		0: ['0 Did you even try? O_o'],
+		1: ['1 Great Job! You&rsquo;re well on your way...'],
+		2: ['2 Great Job! You&rsquo;re well on your way...'],
+		3: ['3 Great Job! You&rsquo;re well on your way...'],
+		4: ['4 Great Job! You&rsquo;re well on your way...'],
+		5: ['5 Great Job! You&rsquo;re well on your way...'],
+		6: ['6 Great Job! You&rsquo;re well on your way...'],
+		7: ['7 Great Job! You&rsquo;re well on your way...'],
+		8: ['8 Great Job! You&rsquo;re well on your way...'],
+		9: ['9 Great Job! You&rsquo;re well on your way...'],
+		10: ['Great Job!']
+	},
+
+
 	renderTpl: Ext.DomHelper.markup([
-		{ cls: 'message', html: 'Great Job! You&rsquo;re well on your way...' },
+		{ cls: 'message' },
 		{ cls: 'tally', cn: [
 			{cls: 'stat correct', cn:[{tag: 'span', cls:'count'},' correct'] },
 			{cls: 'stat incorrect', cn:[{tag: 'span', cls:'count'},' incorrect'] }
@@ -14,6 +30,7 @@ Ext.define('NextThought.view.assessment.ScoreboardTally',{
 
 
 	renderSelectors: {
+		message: '.message',
 		correctBox: '.tally .correct',
 		correctCount: '.tally .correct .count',
 		incorrectBox: '.tally .incorrect',
@@ -25,13 +42,28 @@ Ext.define('NextThought.view.assessment.ScoreboardTally',{
 		this.callParent(arguments);
 		this.correctCount.update('16');
 		this.incorrectCount.update('4');
+
+		this.correctBox.setVisibilityMode(Ext.dom.Element.DISPLAY);
+		this.incorrectBox.setVisibilityMode(Ext.dom.Element.DISPLAY);
 	},
 
 
 
 	setTally: function(correct,total){
+		var incorrect = total-correct,
+			percent = Math.ceil(100*correct/total),
+			messageBucket = this.messages[Math.ceil(percent/10)];
+
 		this.correctCount.update(correct);
-		this.incorrectCount.update(total-correct);
+		this.incorrectCount.update(incorrect);
+
+		this.correctBox[correct === 0?'hide':'show']();
+		this.incorrectBox[incorrect === 0?'hide':'show']();
+
+		this.message.update(
+			messageBucket[Math.floor(Math.random()*100)%messageBucket.length]
+		);
+
 	}
 
 });
