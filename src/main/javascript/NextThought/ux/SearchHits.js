ff2c49be2c3d188b307645389dc0334e4ddab8d3
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2012-08-23 20:24:12 +0000

    Render search hits using divs instead of canvas. It works great and solve highlighting of search results
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@10551 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/ux/SearchHits.js
+++ src/main/javascript/NextThought/ux/SearchHits.js
@@ -1,99 +1,65 @@
 Ext.define('NextThought.ux.SearchHits', {
 	alias: 'widget.search-hits',
+	mixins: {observable: 'Ext.util.Observable'},
+
 
 	constructor: function(config){
 		var me = this;
-
+		me.mixins.observable.constructor.call(this);
 		Ext.apply(me, {
 			selections: config.hits || [],
 			ownerCmp: config.owner,
-			container: config.owner.getInsertionPoint('innerCt').dom,
-			canvas: null,
-			canvasId: 'search-highlight-'+guidGenerator()
+			container: config.owner.getInsertionPoint('innerCt').dom
 		});
 
-		me.color = 'rgba(255,255,0,0.3)';
-		me.canvas =  me.createCanvas();
-
-		me.render = Ext.Function.createBuffered(me.render,100,me,[true]);
-
-		me.ownerCmp.on('resize', me.canvasResize, me);
-
-		if(this.ownerCmp.getIframe().getSize().height !== 0){
-			me.canvasResize(); }
-		else{ setTimeout( function(){me.canvasResize();}, 200 ); }
-
+		this.insertSearchHitsOverlay();
 		return me;
 	},
 
-	cleanup: function(){
-		try{
-			Ext.fly(this.canvas).remove();
-		}
-		catch(e){
-			console.error(e);
-		}
-		delete this.selections;
+	insertSearchHitsOverlay: function(){
+		var container = Ext.DomHelper.append(this.ownerCmp.getInsertionPoint('innerCt'), { cls:'searchHit-overlay' }, true);
+		this.on('destroy' , function(){ container.remove(); });
+		this.searchHitsOverlay = container;
+		this.showAllHits();
 	},
 
-	createElement: function(tag,parent,cls,css,id){
-		var el = document.createElement(tag);
-		if(cls) { Ext.get(el).addCls(cls); }
-		if(css) { el.setAttribute('style',css); }
-		if(id){el.setAttribute('id',id);}
-		parent.appendChild(el);
-		return el;
-	},
-
-	createCanvas: function(){
-		return this.createElement(
-			'canvas',
-			this.container,
-			'search-highlight-object unselectable','position: absolute; pointer-events: none; top: 0; left: 0; z-index: 999',
-			this.canvasId
-			);
-	},
-
-
-	canvasResize: Ext.Function.createBuffered( function(){
-		var c = Ext.get(this.canvas || this.canvasId),
-			cont = Ext.get(this.ownerCmp.getIframe()),
-			pos = cont.getXY(),
-			size = cont.getSize();
-		c.moveTo(pos[0], pos[1]);
-		c.set({
-			width: size.width,
-			height: size.height
-		});
-		this.render();
-	}, 100),
-
-	render: function(){
-		if(!this.selections || !this.canvas){
-			return;
-		}
-
-		var c = this.canvas,
-			w = 'width',
-			ctx = c.getContext("2d");
-
-		c.width = c[w];
-
-		ctx.fillStyle = this.color;
-
+	showAllHits: function(){
 		Ext.each(this.selections, function(sel){
 
 			var s = sel.getClientRects(),
 				i = s.length-1;
 
 			for(; i>=0; i--){
-				this.drawRect(ctx,s[i]);
+				this.showSearchHit(s[i]);
 			}
 		}, this);
 	},
 
-	drawRect: function(ctx, rect){
-		ctx.fillRect(rect.left, rect.top, rect.width, rect.height);
+	showSearchHit: function(range){
+		var c = {
+			cls:'searchHit-entry',
+			style: {
+				height: range.height+'px',
+				width: range.width+'px',
+				top: range.top+'px',
+				left: range.left+'px'
+			}
+
+		};
+
+		Ext.DomHelper.append( this.searchHitsOverlay, c, true);
+
+	},
+
+	cleanup: function(){
+		try{
+			Ext.fly(this.searchHitsOverlay).remove();
+		}
+		catch(e){
+			console.error(e);
+		}
+		delete this.selections;
+		this.clearListeners();
 	}
 
 });
