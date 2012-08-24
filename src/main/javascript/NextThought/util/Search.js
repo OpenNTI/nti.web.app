7cdec34a6e117a6db0ab05e08a11bfc91fe69b00
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2012-08-24 18:39:38 +0000

    fix search highlight bugs
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@10663 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/util/Search.js
+++ src/main/javascript/NextThought/util/Search.js
@@ -1,17 +1,21 @@
 Ext.define('NextThought.util.Search',{
 	singleton: true,
 
-	ignoredWordsRe: /\b(a|an|and|are|as|at|be|but|by|for|if|in|into|is|it|no|not|of|on|or|such|than|that|the|their|then|there|these|they|this|to||was|will|with)\b/ig,
+	ignoredWordsRe: /\b(a|an|and|are|as|at|be|but|by|for|if|in|into|is|it|no|not|of|on|or|the|to|was)\b/ig,
 
 	splitWhitespaceRe: /\W+/,
 
 	trimRe: /^["'\s]+|["'\s]+$/ig,
 
 	searchRe: function(string){
-		var tokens;
-		string = string.replace(this.trimRe,'').replace(this.ignoredWordsRe,'');
-		tokens = Ext.Array.map(string.split(this.splitWhitespaceRe), RegExp.escape);
-
+		var tokens, str;
+		string = string.replace(this.trimRe,'');
+		str = string.replace(this.ignoredWordsRe,'');
+		tokens = Ext.Array.map(str.split(this.splitWhitespaceRe), RegExp.escape);
+		
+		tokens = Ext.Array.clean(tokens);
+		if(tokens.length === 0){ tokens.push(string); } //Avoid searching for an empty string.
+		 
 		return new RegExp([ '([^\\W]*', tokens.join('|'), '[?^\\W]*)' ].join(''), 'ig');
 	}
 
