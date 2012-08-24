5dc83278b95ccda4841d0bae954370aeb02e1515
Vitalik Buterin <vitalik.buterin@nextthought.com>
2012-08-24 03:34:54 +0000

    Added full range of quiz responses
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@10606 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/assessment/ScoreboardTally.js
+++ src/main/javascript/NextThought/view/assessment/ScoreboardTally.js
@@ -6,17 +6,17 @@ Ext.define('NextThought.view.assessment.ScoreboardTally',{
 
 
 	messages: {
-		0: ['0 Did you even try? O_o'],
-		1: ['1 Great Job! You&rsquo;re well on your way...'],
-		2: ['2 Great Job! You&rsquo;re well on your way...'],
-		3: ['3 Great Job! You&rsquo;re well on your way...'],
-		4: ['4 Great Job! You&rsquo;re well on your way...'],
-		5: ['5 Great Job! You&rsquo;re well on your way...'],
-		6: ['6 Great Job! You&rsquo;re well on your way...'],
-		7: ['7 Great Job! You&rsquo;re well on your way...'],
-		8: ['8 Great Job! You&rsquo;re well on your way...'],
-		9: ['9 Great Job! You&rsquo;re well on your way...'],
-		10: ['Great Job!']
+		0:["Whoops! Review material and try again.", "Try again!", "Oh no! Did you forget your material? Try again!"],
+		1:["It&rsquo;s a start! Try again.", "Keep trying!", "Get a few more right, and you&rsquo;re on your way!"],
+		2:["It&rsquo;s a start! Try again.", "Keep trying!", "Get a few more right, and you&rsquo;re on your way!"],
+		3:[" Keep trying. You&rsquo;ll get there!", "Keep studying, and you&rsquo;ll be well on your way!", "Keep trying!", "You can do it! Try again?"],
+		4:[" Keep trying. You&rsquo;ll get there!", "Keep studying, and you&rsquo;ll be well on your way!", "Keep trying!", "You can do it! Try again?"],
+		5:["Almost there! Keep it up!", "A little more practice, and you&rsquo;re on your way!", "Almost!", " Good try!"],
+		6:["Almost there! Keep it up!", "A little more practice, and you&rsquo;re on your way!", "Almost!", " Good try!"],
+		7:["Good job!", ":)", "So close!!", "Good work! Almost there!", "You&rsquo;ve almost got it!"],
+		8:["Good job!", ":)", "So close!!", "Good work! Almost there!", "You&rsquo;ve almost got it!"],
+		9:["Great work!", ":)", "Wow!", "Great job!", "Brilliant!", "Nice Work!"],
+		10:["Excellent! Perfect score!", "Above and beyond! Great job!", ":D", "Quite impressive.", "Genius.", "You&rsquo;re a math ninja!", "Eureka!"]
 	},
 
 
