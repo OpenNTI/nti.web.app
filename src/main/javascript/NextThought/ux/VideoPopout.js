7e7d5c273f9fcd25219033f1f0d3779c06163cd5
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-08-30 05:58:10 +0000

    add video popup widget
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@11027 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/main/javascript/NextThought/ux/VideoPopout.js
@@ -0,0 +1,113 @@
+Ext.define('NextThought.ux.VideoPopout',{
+	extend: 'Ext.panel.Panel',
+	requires: [
+		'Ext.data.Store',
+		'Ext.view.View'
+	],
+
+	modal: true,
+	plain: true,
+	shadow: false,
+	frame: false,
+	border: false,
+	floating: true,
+	cls: 'videos',
+	ui: 'video',
+	width: 640,
+	height: 475,
+	layout: {
+		type: 'vbox',
+		align: 'stretch'
+	},
+	items: [
+		{
+			name: 'video',
+			xtype: 'box',
+			cls: 'video',
+			height: 360,
+			width: 640,
+			autoEl: {
+				src: Ext.SSL_SECURE_URL,
+				tag: 'iframe',
+				frameBorder: 0,
+				marginWidth: 0,
+				marginHeight: 0,
+				seamless: true,
+				transparent: true,
+				allowFullscreen: true,
+				allowTransparency: true,
+				style: 'overflow: hidden',
+				height: 360,
+				width: 640
+			}
+		}
+	],
+
+
+	constructor: function(config){
+		var store = config ? (config.store||undefined) : undefined;
+		this.callParent(arguments);
+
+		this.iframe = this.down('box[name=video]');
+
+
+		this.store = store || Ext.create('Ext.data.Store',{
+			fields: [
+				{name:'video', type:'string'},
+				{name:'thumb', type:'string'}
+			],
+			data : [
+				 {video: 'https://www.youtube.com/embed/S4knArgz7cA?rel=0&wmode=opaque',
+				  thumb: 'http://img.youtube.com/vi/S4knArgz7cA/2.jpg'},
+				 {video: 'https://www.youtube.com/embed/yY2NVw-aXqY?rel=0&wmode=opaque',
+				  thumb: 'http://img.youtube.com/vi/yY2NVw-aXqY/2.jpg'},
+				 {video: 'https://www.youtube.com/embed/j-EB1O-vRS4?rel=0&wmode=opaque',
+				  thumb: 'http://img.youtube.com/vi/j-EB1O-vRS4/2.jpg'}
+			 ]
+		});
+
+		this.others = this.add({
+			xtype: 'dataview',
+			overflowX: 'scroll',
+			overflowY: 'hidden',
+			height: 115,
+			store: this.store,
+			cls: 'carousel',
+			singleSelect: true,
+			overItemCls: 'over',
+			itemSelector: 'div.item-wrap',
+			tpl: Ext.DomHelper.markup(
+				{tag:'tpl', 'for':'.', cn: [
+					{cls:'item-wrap', cn:[{
+						cls:'item',
+						tag: 'img',
+						src: Ext.BLANK_IMAGE_URL,
+						style:{ backgroundImage: 'url({thumb})' }
+					}]
+				}]
+			})
+		});
+		this.mon(this.others,'selectionChange',this.selection,this);
+		return this;
+	},
+
+
+	selection: function(v,s){
+		this.iframe.el.dom.setAttribute('src',s[0].get('video'));
+	},
+
+	onShow: function(){
+		this.others.getSelectionModel().select(0);
+		return this.callParent(arguments);
+	},
+
+
+	afterRender: function(){
+		this.callParent(arguments);
+
+		this.mon( Ext.DomHelper.append(this.el, { cls:'close', title: 'close' }, true),{
+			scope: this,
+			click: this.close
+		});
+	}
+});
