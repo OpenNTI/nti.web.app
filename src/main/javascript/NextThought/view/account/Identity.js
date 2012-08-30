9d96d983af83f9724910dd15588c3097323cdb4a
Chris Utz <chris.utz@nextthought.com>
2012-08-30 00:40:16 +0000

    Use the displayName field in the Identity view also
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@11005 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/account/Identity.js
+++ src/main/javascript/NextThought/view/account/Identity.js
@@ -10,7 +10,7 @@ Ext.define('NextThought.view.account.Identity',{
 		{
 			cls: 'wrap',
 			cn: [{
-				cls: 'name', html: '{realname:ellipsis(30)}'
+				cls: 'name', html: '{displayName:ellipsis(30)}'
 			},{
 				cls: 'status', html: '{status}'
 			}]
