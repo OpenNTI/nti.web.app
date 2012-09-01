1a56325e84261d8fadbc3ccdefc795eb78385eaf
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-09-01 18:56:23 +0000

    fix activity sort order on weekdays and months
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@11176 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/model/Change.js
+++ src/main/javascript/NextThought/model/Change.js
@@ -12,16 +12,18 @@ Ext.define('NextThought.model.Change', {
 				dv = today - day,
 				WEEK = 604800000,//milliseconds in a week
 				MONTH = 2419200000; //millis in a 28day month (4 week)
-//TODO: make this better...
+
+//TODO: make this better...serously.
+
 			if(day.getTime()===today.getTime()){ return 'A '; }
 
 			if(day.getTime()===yesterday.getTime()){ return 'B Yesterday'; }
 
-			if(dv < WEEK){ return 'C'+Ext.Date.format(day,'z l'); }
+			if(dv < WEEK){ return 'C'+(364-parseInt(Ext.Date.format(day,'z'),10))+Ext.Date.format(day,' l'); }
 
 			if(dv < (2*WEEK)){ return 'D Last Week'; }
 
-			if(dv < MONTH) { return 'E'+Ext.Date.format(day,'m F'); }
+			if(dv < MONTH) { return 'E'+(12-parseInt(Ext.Date.format(day,'m'),10))+Ext.Date.format(day,' F'); }
 
 			if(dv < (2*MONTH)){ return 'F Last Month'; }
 
