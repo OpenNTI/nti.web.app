8962f6f58fa60db85a8fc536b67c8683efd80a26
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-09-06 02:20:35 +0000

    kill the pop over on blur
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@11598 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/account/contacts/management/Popout.js
+++ src/main/javascript/NextThought/view/account/contacts/management/Popout.js
@@ -25,5 +25,31 @@ Ext.define('NextThought.view.account.contacts.management.Popout',{
 		this.items = Ext.clone(this.items);
 		this.items[0].user = config.record;
 		return this.callParent(arguments);
+	},
+
+	initComponent: function(){
+		this.callParent(arguments);
+
+		this.on('blur',this.destroy,this);
+	},
+
+
+	destroy: function(){
+		Ext.getBody().un('click',this.detectBlur,this);
+		this.callParent(arguments);
+	},
+
+
+	afterRender: function(){
+		var me = this;
+		me.callParent(arguments);
+		Ext.defer(function(){Ext.getBody().on('click',me.detectBlur,me);},1);
+	},
+
+
+	detectBlur: function(e){
+		if(!e.getTarget('.add-contact-popout')){
+			this.fireEvent('blur');
+		}
 	}
 });
