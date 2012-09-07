ba66d1265601965759a061e6948c59126a3b37cd
Sean Jones <sean.jones@nextthought.com>
2012-09-07 20:08:47 +0000

    Significant rework of the minification script.  There is now a command line option to specify the Google Analytics key if we want Google Analytics code included in index-minify.html.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@11724 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/app-minify.py
+++ src/main/app-minify.py
@@ -4,14 +4,16 @@
 # then, use that list to read our files into Google Closure compiler to minify our code.
 from __future__ import print_function, unicode_literals
 
+import argparse
 import httplib
 import json
 import os
-import re
 import subprocess
 import sys
 import time
 import urllib2 as urllib
+
+BUILDTIME = time.strftime('%Y%m%d%H%M%S')
 
 def _readSenchaProjectFile( filename ):
 	data = None
@@ -40,19 +42,20 @@ def _fixProjectFile( projectFile ):
 	for item in ((projectFile['builds'])[1])['files']:
 		if 'app.js' in item['name']:
 			item['path'] = "javascript/"
-
+	
 	return projectFile
 
 def _buildProjectFile( app_entry, projectFileName ):
+	print('Building Project File')
 	phantomjs_script = '../../phantomjs-jsb.js'
 	command = ['/usr/bin/env', 'phantomjs', '--debug=yes', phantomjs_script, '--app-entry', app_entry, '--project', projectFileName]
-
+	
 	subprocess.check_call(command)
 
 def _cacheExtJSFiles( projectFile ):
 	host = 'https://extjs.cachefly.net'
 
-	# Check for missing files
+	# Check for missing files and download them if necessary.
 	for item in ((projectFile['builds'])[0])['files']:
 		if 'ext-4.1.1' in item['path']:
 			if not os.path.exists( os.path.join(item['path'], item['name'])):
@@ -63,49 +66,63 @@ def _cacheExtJSFiles( projectFile ):
 				with open( os.path.join(item['path'], item['name']), 'wb' ) as file:
 					file.write(r.read())
 
-def _buildRefIndexHTML():
-	contents = """<!DOCTYPE html>
+def _buildIndexHtml( version, analytics_key ):
+	part1 = """<!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>NextThought</title>
+        <title>NextThought</title>
 	<meta http-equiv='Content-Type' content='Type=text/html; charset=utf-8'>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+"""
+	part2 = """        <link rel="shortcut icon" href="resources/images/favicon.ico?_dc=%s">
 
-	<script type="text/javascript" src="resources/misc/base64.min.js"></script>
-	<!--[if gte IE 9]>
+	<link rel="stylesheet" id="ext-base" type="text/css"
+		  href="https://extjs.cachefly.net/ext-4.1.1-gpl/resources/css/ext-all-gray.css">
+	<link rel='stylesheet' type='text/css'
+		  href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,700italic,400italic|Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'>
+        <link rel="stylesheet" id="mathquill-stylesheet" type="text/css"
+		  href="resources/lib/mathquill/mathquill.css?_dc=%s">
+        <link rel="stylesheet" id="main-stylesheet" type="text/css" href="resources/css/main.css?_dc%s">
+
+	<script type="text/javascript" src="resources/misc/base64.min.js?_dc=%s"></script>
+""" % (BUILDTIME, BUILDTIME, BUILDTIME, BUILDTIME)
+	part3 ="""	<!--[if gte IE 9]>
 	<style type="text/css">
 		.gradient { filter: none; }
 	</style>
 	<![endif]-->
 </head>
-<body>
-	<script type="text/javascript" src="config.js"></script>
+"""
+	part4 = """<body>
+	<div id="loading-mask">
+		<div class="x-mask"></div>
+		<div class="x-mask-msg" id="loading"><div>Loading...</div></div>
+	</div>
+	<script type="text/javascript" src="config.js?_dc=%s"></script>
 
 	<!-- jQuery library, for use with mathquill only -->
-	<script type="text/javascript" src="resources/lib/jQuery-1.8.0min.js"></script>
+	<script type="text/javascript" src="resources/lib/jQuery-1.8.0min.js?_dc=%s"></script>
 	<script type="text/javascript"> jQuery.noConflict(); </script>
-	<script type="text/javascript" src="resources/lib/mathquill/mathquill.min.js"></script>
+	<script type="text/javascript" src="resources/lib/mathquill/mathquill.min.js?_dc=%s"></script>
+""" % (BUILDTIME, BUILDTIME, BUILDTIME)
 
-        <script type="text/javascript"
+	part5 = """        <script type="text/javascript"
                         src="https://extjs.cachefly.net/ext-4.1.1-gpl/ext.js"
                         id="ext-js-library"></script>
 
-	<!-- application main entry -->
+"""
+	part6a = """	<!-- application main entry -->
 	<script type="text/javascript" src="javascript/app.js"></script>
-
-	<!--[analytics code here]-->
-</body>
+"""
+	part6b = """	<!-- application main entry -->
+	<script type="text/javascript" src="javascript/app.min.js?_dc=%s"></script>
+""" % (BUILDTIME,)
+	part7 ="""</body>
 </html>
 """
-
-	with open( 'index-ref.html', 'wb' ) as file:
-		file.write(contents)
-
-def _buildMinifyIndexHTML():
-	buildtime = time.strftime('%Y%m%d%H%M%S')
-
-	analytics = """        <script type="text/javascript">
-
+	analytics = """
+        <!--[analytics code here]-->
+        <script type="text/javascript">
 	        var _gaq = _gaq || [];
                 _gaq.push(['_setAccount', '%s']);
                 _gaq.push(['_trackPageview']);
@@ -113,61 +130,34 @@ def _buildMinifyIndexHTML():
 	                var ga = document.createElement('script');
                         ga.type = 'text/javascript';
                         ga.async = true;
-                        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') +
+                        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + 
                                 '.google-analytics.com/ga.js';
                         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
                 })();
-        </script>""" % ('UA-22943650-2', )
+        </script>
+""" % (analytics_key, )
 
-	contents = """<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>NextThought</title>
-	<meta http-equiv='Content-Type' content='Type=text/html; charset=utf-8'>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="shortcut icon" href="resources/images/favicon.ico?_dc=%s">
+	if version == 'ref':
+		return part1 + part3 + part4 + part5 + part6a + part7
+	elif version == 'minify':
+		return part1 + part2 + part3 + part4 + part6b + analytics + part7
+	else:
+		return ''
 
-	<link rel="stylesheet" id="ext-base" type="text/css"
-		  href="https://extjs.cachefly.net/ext-4.1.1-gpl/resources/css/ext-all-gray.css">
-	<link rel='stylesheet' type='text/css'
-		  href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,700italic,400italic|Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'>
-	<link rel="stylesheet" id="mathquill-stylesheet" type="text/css"
-		  href="resources/lib/mathquill/mathquill.css?_dc=%s">
-    <link rel="stylesheet" id="main-stylesheet" type="text/css" href="resources/css/main.css?_dc%s">
+def _buildRefIndexHtml():
+	contents = _buildIndexHtml( 'ref', None )
 
-	<script type="text/javascript" src="resources/misc/base64.min.js?_dc=%s"></script>
-	<!--[if gte IE 9]>
-	<style type="text/css">
-		.gradient { filter: none; }
-	</style>
-	<![endif]-->
-</head>
-<body>
-	<div id="loading-mask">
-		<div class="x-mask"></div>
-		<div class="x-mask-msg" id="loading"><div>Loading...</div></div>
-	</div>
+	with open( 'index-ref.html', 'wb' ) as file:
+		file.write(contents)
 
-	<script type="text/javascript" src="config.js?_dc=%s"></script>
-
-	<!-- jQuery library, for use with mathquill only -->
-	<script type="text/javascript" src="resources/lib/jQuery-1.8.0min.js?_dc=%s"></script>
-	<script type="text/javascript"> jQuery.noConflict(); </script>
-	<script type="text/javascript" src="resources/lib/mathquill/mathquill.min.js?_dc=%s"></script>
-
-	<!-- application main entry -->
-	<script type="text/javascript" src="javascript/app.min.js?_dc=%s"></script>
-
-	<!--[analytics code here]-->
-%s
-</body>
-</html>
-""" % (buildtime, buildtime, buildtime, buildtime, buildtime, buildtime, buildtime, buildtime, analytics)
+def _buildMinifyIndexHtml(analytics_key):
+	contents = _buildIndexHtml( 'minify', analytics_key )
 
 	with open( 'index-minify.html', 'wb' ) as file:
 		file.write(contents)
 
 def _closure_minify( projectFile ):
+	print('Minifying Project')
 	cmd = '/usr/bin/java'
 	optimization_level = 'WHITESPACE_ONLY'
 	output_file = 'javascript/app.min.js'
@@ -175,21 +165,26 @@ def _closure_minify( projectFile ):
 	command = [cmd, "-jar", "../../closure-compiler.jar", "--compilation_level", optimization_level, "--js_output_file", ((projectFile['builds'])[1])['target']]
 
 	for item in ((projectFile['builds'])[0])['files']:
-		if 'https://extjs.cachefly.net/ext-4.1.1-gpl' in item['path']:
-			item['path'] = (item['path']).replace('https://extjs.cachefly.net/ext-4.1.1-gpl','../ext-4.1.1-gpl')
-		elif '../../extjs.cachefly.net/ext-4.1.1-gpl' in item['path']:
-			item['path'] = (item['path']).replace('../../extjs.cachefly.net/ext-4.1.1-gpl','../ext-4.1.1-gpl')
+		if 'https://extjs.cachefly.net/' in item['path']:
+			item['path'] = (item['path']).replace('https://extjs.cachefly.net/','../')
+		elif '../../extjs.cachefly.net/' in item['path']:
+			item['path'] = (item['path']).replace('../../extjs.cachefly.net/','../')
 		command.extend(['--js', os.path.join(item['path'], item['name'])])
 	command.extend(['--js', 'javascript/app.js'])
 
 	_cacheExtJSFiles( projectFile )
 
-	subprocess.call(command)
+	subprocess.check_call(command)
 
 def main():
-	_buildRefIndexHTML()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-a', '--google-analytics', dest='analytics_key', action='store', default=None, help="Key value used with Google Analytics.  If no value is specified, then the index-minify.html will not contain Google Analytics code.")
 
-	app_entry = 'http://localhost/NextThoughtWebApp/index-ref.html'
+	args = parser.parse_args()
+
+	_buildRefIndexHtml()
+
+	app_entry = 'index-ref.html'
 	projectfilename = 'minify.jsb3'
 	_buildProjectFile( app_entry, projectfilename )
 
@@ -198,7 +193,7 @@ def main():
 
 	_closure_minify(projectfile)
 
-	_buildMinifyIndexHTML()
+	_buildMinifyIndexHtml(args.analytics_key)
 
 	# Clean-up
 	os.remove( projectfilename )
