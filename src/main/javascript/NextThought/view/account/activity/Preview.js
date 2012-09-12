6de510e7b18f1ace7d2aa7ce5f24faa9fb43498e
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-09-12 21:37:31 +0000

    fix firefox issues in new redaction styles
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@11858 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/account/activity/Preview.js
+++ src/main/javascript/NextThought/view/account/activity/Preview.js
@@ -1,4 +1,16 @@
 Ext.define('NextThought.view.account.activity.Preview',{
 	extend: 'Ext.Component',
-	alias: 'widget.activity-preview'
+	alias: 'widget.activity-preview',
+
+	renderTpl: Ext.DomHelper.markup([
+		{cls: 'path'},
+		{cls: 'location-label'},
+		{cls: 'context', cn:[
+			{tag: 'canvas'},
+			{cls: 'text'}
+		]},
+		{cls: 'footer', cn: [
+		]}
+
+	])
 });
