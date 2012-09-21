04a6cac726452e000416ef3483f3fa7af1d6cb0e
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-09-21 01:53:05 +0000

    update account menu item
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@12222 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/menus/Settings.js
+++ src/main/javascript/NextThought/view/menus/Settings.js
@@ -24,12 +24,12 @@ Ext.define('NextThought.view.menus.Settings',{
 
         //setup fields:
         var items = [];
+        items.push({ action: 'account', text: 'My Account'});
         items.push({ action: 'about', text: 'About', href: 'http://www.nextthought.com/', hrefTarget: '_blank'});
         items.push({ action: 'help', text: 'Help'});
         items.push({ action: 'privacy', text: 'Privacy'});
         items.push({ action: 'terms', text: 'Terms of Service'});
         items.push({ xtype:'menuseparator' });
-        items.push({ action: 'account', text: 'Account Settings'});
         items.push({ action: 'logout', text: 'Sign Out'});
 
         //add!
