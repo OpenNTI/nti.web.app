c0d8d1afb57c8a6a819649f55536347738d53f18
Troy Daley <troy.daley@nextthought.com>
2012-09-21 18:35:52 +0000

    Only say a model is modifable if it has no references.  This will keep notes with replys from being unshared/reshared/deleted/etc.  This will make some people mad.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@12278 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/model/Base.js
+++ src/main/javascript/NextThought/model/Base.js
@@ -253,8 +253,13 @@ Ext.define('NextThought.model.Base', {
 
 
 	isModifiable: function(){
+        //note: a note is no longer modifable if it's been referenced, like it has a reply.  This is because of reasons
+        //which are beyond my understanding, and since I am incredibly smart, those reasons probably don't make sense.
+        var refedBy = 0;
+        if (this.get('ReferencedByCount')){refedBy = this.get('ReferencedByCount');}
+
 		try{
-			return this.phantom||(this.getLink('edit')!==null&&isMe(this.get('Creator')));
+			return this.phantom||(this.getLink('edit')!==null&&isMe(this.get('Creator'))&&refedBy === 0);
 		}
 		catch(e){
 			console.warn('No getLink()!');
