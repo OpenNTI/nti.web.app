ecb2e28472efd5a90a7c2b3397794cd7b604051e
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2012-10-03 20:06:01 +0000

    revert the previous checkin. Webstorm keeps adding spaces on my commits.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@12833 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/annotations/Base.js
+++ src/main/javascript/NextThought/view/annotations/Base.js
@@ -1,21 +1,20 @@
-Ext.define( 'NextThought.view.annotations.Base', {
+Ext.define('NextThought.view.annotations.Base', {
 
-	requires: [
-		'NextThought.view.annotations.renderer.Manager',
-		'Ext.String'
+	requires:[
+		'NextThought.view.annotations.renderer.Manager', 'Ext.String'
 	],
 
-	mixins: {
-		observable: 'Ext.util.Observable',
-		shareable: 'NextThought.mixins.Shareable'
+	mixins:{
+		observable:'Ext.util.Observable',
+		shareable :'NextThought.mixins.Shareable'
 	},
 
 
-	onClassExtended: function(cls, data, hooks) {
+	onClassExtended:function (cls, data, hooks) {
 		var onBeforeClassCreated = hooks.onBeforeCreated;
 
-		hooks.onBeforeCreated = function(cls, data) {
-			if(data.requestRender){
+		hooks.onBeforeCreated = function (cls, data) {
+			if (data.requestRender) {
 				Ext.Error.raise('You should not replace requestRender');
 			}
 
@@ -23,90 +22,97 @@ Ext.define( 'NextThought.view.annotations.Base', {
 		};
 	},
 
-	hasGutterWidgets: false,
+	hasGutterWidgets:false,
 
-	constructor: function(config) {
-		var me = this,
-			c = config.reader,
-			r = config.record,
-			container = c.body.dom;
+	constructor:function (config) {
+		var me = this, c = config.reader, r = config.record, container = c.body.dom;
 
 		me.mixins.observable.constructor.call(this);
 		me.mixins.shareable.constructor.call(this);
 		me.addEvents('afterrender');
 
 		Ext.applyIf(me, {
-			id: IdCache.getComponentId(r.getId(), null, c.prefix),
-			container: container,
-			ownerCmp: c,
-			doc: c.getDocumentElement(),
-			record: r,
-			userId: r.get('Creator') || $AppConfig.username,
-			isModifiable: r.isModifiable(),
-			isVisible: Boolean(r.phantom || (!c.filter ? true : c.filter.test(r))),
+			id          :IdCache.getComponentId(r.getId(), null, c.prefix),
+			container   :container,
+			ownerCmp    :c,
+			doc         :c.getDocumentElement(),
+			record      :r,
+			userId      :r.get('Creator') || $AppConfig.username,
+			isModifiable:r.isModifiable(),
+			isVisible   :Boolean(r.phantom || (!c.filter ? true : c.filter.test(r))),
 
-			allowShare: $AppConfig.service.canShare(),
+			allowShare:$AppConfig.service.canShare(),
 
-			isSingleAction: false,
-			renderPriority: -1,
+			isSingleAction:false,
+			renderPriority:-1,
 
-			offsets: c.getAnnotationOffsets(),
+			offsets:c.getAnnotationOffsets(),
 
-			prefix: c.prefix || 'default',
+			prefix:c.prefix || 'default',
 
-			requestRender: Ext.Function.createBuffered(me.requestRender, 10, me)
+			requestRender:Ext.Function.createBuffered(me.requestRender, 10, me)
 		});
 
-		if(typeof r.data.sharedWith !== 'undefined'){
-			try{ this.mixins.shareable.afterRender.call(this); }
-			catch(e){
-				console.warn(
-						'attempted to setup dragging on ',
-						r.$className,
-						Globals.getError(e));
+		if (typeof r.data.sharedWith !== 'undefined') {
+			try {
+				this.mixins.shareable.afterRender.call(this);
+			} catch (e) {
+				console.warn('attempted to setup dragging on ', r.$className, Globals.getError(e));
 			}
 		}
 
-		c.on('afterlayout',me.requestRender, me);
+		c.on('afterlayout', me.requestRender, me);
 		Ext.EventManager.onWindowResize(me.requestRender, me);
 
 		me.attachRecord(r);
 
 		AnnotationsRenderer.register(me);
-		if(this.getItemId()){
+		if (this.getItemId()) {
 			Ext.ComponentManager.register(this);
 		}
 	},
 
-	getBubbleTarget: function(){return this.ownerCmp; },
-	getItemId: function(){return this.id; },
-	isXType: function(){return false;},
-	getEl: function(){
+	getBubbleTarget:function () {
+		return this.ownerCmp;
+	},
+	getItemId      :function () {
+		return this.id;
+	},
+	isXType        :function () {
+		return false;
+	},
+	getEl          :function () {
 		return Ext.get(this.img);
 	},
 
 
-	getContainerId: function(){
+	getContainerId:function () {
 		return this.getRecord().get('ContainerId');
 	},
 
 
-	getDocumentElement: function(){
+	getDocumentElement:function () {
 		return this.ownerCmp.getDocumentElement();
 	},
 
 
-	createElement: function(tag,parent,cls,css,id){
+	createElement:function (tag, parent, cls, css, id) {
 		var el = document.createElement(tag);
-		if(cls) { Ext.get(el).addCls(cls); }
-		if(css) { el.setAttribute('style',css); }
-		if(id){el.setAttribute('id',id);}
+		if (cls) {
+			Ext.get(el).addCls(cls);
+		}
+		if (css) {
+			el.setAttribute('style', css);
+		}
+		if (id) {
+			el.setAttribute('id', id);
+		}
 		parent.appendChild(el);
 		return el;
 	},
 
 
-	createNonAnchorableSpan: function(){
+	createNonAnchorableSpan:function () {
 		//NOTE: it is very important to make sure we create the span
 		// in the same doc as the range it will surround.
 		var span = this.doc.createElement('span');
@@ -119,58 +125,60 @@ Ext.define( 'NextThought.view.annotations.Base', {
 	 * Query inside the reader frame
 	 * @param selector
 	 */
-	query: function(selector){
-		return Ext.query(selector,this.doc);
+	query:function (selector) {
+		return Ext.query(selector, this.doc);
 	},
 
-	getSortValue: function(){console.warn('Implement me!!');},
+	getSortValue:function () {
+		console.warn('Implement me!!');
+	},
 
-	getRecord: function(){ return this.record || {get:Ext.emptyFn}; },
-	getRecordField: function(field){ return this.getRecord().get(field); },
+	getRecord     :function () {
+		return this.record || {get:Ext.emptyFn};
+	},
+	getRecordField:function (field) {
+		return this.getRecord().get(field);
+	},
 
-	attachRecord: function(record){
+	attachRecord:function (record) {
 		var old = this.record;
 		this.record = record;
 
-		record.on('updated',this.attachRecord, this, {single: true});
-		record.on('destroy',this.onDestroy, this, {single:true});
+		record.on('updated', this.attachRecord, this, {single:true});
+		record.on('destroy', this.onDestroy, this, {single:true});
 
-		if(old.getId() !== record.getId()){
-			console.warn('Annotation:',old, '!==', record);
+		if (old.getId() !== record.getId()) {
+			console.warn('Annotation:', old, '!==', record);
 		}
 
-		if(old !== record) {
+		if (old !== record) {
 			old.un('updated', this.attachRecord, this);
-			old.un('destroy',this.onDestroy, this);
+			old.un('destroy', this.onDestroy, this);
 		}
 	},
 
 
-	onDestroy: function(){
-		try{
+	onDestroy:function () {
+		try {
 			this.cleanup();
-		}
-		catch(e){
+		} catch (e) {
 			swallow(e);
 		}
 	},
 
 
-	getDisplayName: function(){
+	getDisplayName:function () {
 		return this.$displayName || this.$className.split('.').last();
 	},
 
-	cleanup: function(){
+	cleanup:function () {
 		this.cleanup = Ext.emptyFn;
-		var me = this,
-			r = me.record,
-			id = r.getId(),
-			c = me.ownerCmp;
+		var me = this, r = me.record, id = r.getId(), c = me.ownerCmp;
 
 		delete me.record;
 
 		r.clearListeners();
-		if(me.getItemId()){
+		if (me.getItemId()) {
 			Ext.ComponentManager.unregister(me);
 		}
 		AnnotationsRenderer.unregister(me);
@@ -178,65 +186,69 @@ Ext.define( 'NextThought.view.annotations.Base', {
 		c.un('afterlayout', this.requestRender, me);
 		Ext.EventManager.removeResizeListener(me.requestRender, me);
 
-		if( c.annotationExists(r)){
+		if (c.annotationExists(r)) {
 			c.removeAnnotation(id);
 		}
 
 		this.clearListeners();
 		this.requestRender();
 	},
-	
 
-	updateFilterState: function(newFilter){
+
+	updateFilterState:function (newFilter) {
 		var v = newFilter.test(this.record);
-		if(v !== this.isVisible){
+		if (v !== this.isVisible) {
 			this.isVisible = !!v;
 			this.visibilityChanged(v);
 		}
 	},
-	
 
-	visibilityChanged: function(show){
+
+	visibilityChanged:function (show) {
 		this.requestRender();
 	},
-	
 
-	render: function(){
-		console.warn( Ext.String.format(
-						'{0} does not implement render()',
-						this.$className));
+
+	render:function () {
+		console.warn(Ext.String.format('{0} does not implement render()', this.$className));
 	},
 
 
-	requestRender: function(){
+	requestRender:function () {
 		AnnotationsRenderer.render(this.prefix);
 	},
 
-	
-	remove: function() {
+	getRestrictedRanges:function () {
+		return null;
+	},
+
+
+	remove:function () {
 		this.record.destroy();//the destroy event calls cleanup
 	},
 
 
-	savePhantom: function(callback){
+	savePhantom:function (callback) {
 		var me = this, p;
-		if(!me.record.phantom){return;}
+		if (!me.record.phantom) {
+			return;
+		}
 		me.isSaving = true;
 
 		p = LocationProvider.getPreferences();
 		p = p ? p.sharing : null;
 
-		p = this.allowShare? p : null;
+		p = this.allowShare ? p : null;
 
-		me.record.set('SharedWidth',p);
+		me.record.set('SharedWidth', p);
 
 		me.record.save({
-			scope: me,
-			failure:function(){
+			scope  :me,
+			failure:function () {
 				console.error('Failed to save record', me, me.record);
 				me.cleanup();
 			},
-			success:function(newRecord){
+			success:function (newRecord) {
 				me.record.fireEvent('updated', newRecord);
 				me.record = newRecord;
 				LocationProvider.getStore().add(newRecord);
@@ -248,90 +260,94 @@ Ext.define( 'NextThought.view.annotations.Base', {
 	},
 
 
-	buildMenu: function(items) {
-		var m = this,
-			r = m.getRecord();
+	buildMenu:function (items) {
+		var m = this, r = m.getRecord();
 
 		items = items || [];
 
-		if(items.length) { items.push({xtype: 'menuseparator'}); }
+		if (items.length) {
+			items.push({xtype:'menuseparator'});
+		}
 
-		if(this.isModifiable) {
+		if (this.isModifiable) {
 			items.push({
-				text : (r.phantom?'Save':'Delete')+' '+ m.getDisplayName(),
-				handler: Ext.bind(r.phantom? m.savePhantom : m.remove, m)
+				text   :(r.phantom ? 'Save' : 'Delete') + ' ' + m.getDisplayName(),
+				handler:Ext.bind(r.phantom ? m.savePhantom : m.remove, m)
 			});
 		}
 
-		if(this.allowShare){
+		if (this.allowShare) {
 			items.push({
-				text: m.isModifiable ? 'Share With...' : 'Get Info...',
-				handler: function(){
+				text   :m.isModifiable ? 'Share With...' : 'Get Info...',
+				handler:function () {
 					if (m.record.phantom) {
-						m.record.on('updated', function(){
-							m.ownerCmp.fireEvent('share-with',m.record);
-						},
-						{single: true});
+						m.record.on('updated', function () {
+								m.ownerCmp.fireEvent('share-with', m.record);
+							}, {single:true});
 						m.savePhantom();
-					}
-					else{
-						m.ownerCmp.fireEvent('share-with',m.record);
+					} else {
+						m.ownerCmp.fireEvent('share-with', m.record);
 					}
 				}
 			});
 		}
 
-		return Ext.create('Ext.menu.Menu',{
-			items: items,
-			ui: 'nt',
-			plain: true,
-			showSeparator: false,
-			shadow: false,
-			frame: false,
-			border: false,
-			hideMode: 'display',
-			minWidth: 150,
-			defaults: {ui: 'nt-annotaion', plain: true }
+		return Ext.create('Ext.menu.Menu', {
+			items        :items,
+			ui           :'nt',
+			plain        :true,
+			showSeparator:false,
+			shadow       :false,
+			frame        :false,
+			border       :false,
+			hideMode     :'display',
+			minWidth     :150,
+			defaults     :{ui:'nt-annotaion', plain:true }
 		});
 	},
 
 
-	getMenu: function(isLeaf){
+	getMenu:function (isLeaf) {
 		var m = this.buildMenu([]);
 
-        if (m) {
-		    m.on('hide', function(){
-			if(!isLeaf) { m.destroy(); }
-		});
-        }
+		if (m) {
+			m.on('hide', function () {
+				if (!isLeaf) {
+					m.destroy();
+				}
+			});
+		}
 
 		return m;
 	},
 
 
-	attachEvent: function(event,dom,fn,scope){
-		if(!Ext.isArray(event)){
+	attachEvent:function (event, dom, fn, scope) {
+		if (!Ext.isArray(event)) {
 			event = [event];
 		}
-		Ext.each(event,function(event){
-			function f(e){
-				e = Ext.EventObject.setEvent(e||event);
-				fn.apply(scope||window,[e,dom]);
+		Ext.each(event, function (event) {
+			function f(e) {
+				e = Ext.EventObject.setEvent(e || event);
+				fn.apply(scope || window, [e, dom]);
 			}
-			if(dom.addEventListener) { dom.addEventListener(event,f,false); }
-			else if(dom.attachEvent) { dom.attachEvent(event,f); }
+
+			if (dom.addEventListener) {
+				dom.addEventListener(event, f, false);
+			} else if (dom.attachEvent) {
+				dom.attachEvent(event, f);
+			}
 		});
 	},
 
 
-	onClick: function(e) {
-		if(!this.isVisible){
+	onClick:function (e) {
+		if (!this.isVisible) {
 			console.debug('DEBUG: Ignoring click on hidden annotation');
 			return;
 		}
 
-		var menu,
-			xy = e.getXY().slice();
+		var menu, xy = e.getXY().slice();
 
 		this.offsets = this.ownerCmp.getAnnotationOffsets();
 
@@ -341,15 +357,15 @@ Ext.define( 'NextThought.view.annotations.Base', {
 
 		//single annotation
 		menu = this.getMenu();
-        if (menu){
-            if(this.isSingleAction){
-                menu.items.first().handler.call(menu);
-                return;
-            }
+		if (menu) {
+			if (this.isSingleAction) {
+				menu.items.first().handler.call(menu);
+				return;
+			}
 
-            menu.showAt.apply(menu,xy);
-            menu.setPosition(xy[0]-menu.getWidth()/2,xy[1]+10);
-        }
+			menu.showAt.apply(menu, xy);
+			menu.setPosition(xy[0] - menu.getWidth() / 2, xy[1] + 10);
+		}
 		e.stopEvent();
 		return false;//IE :(
 	},
@@ -358,7 +374,8 @@ Ext.define( 'NextThought.view.annotations.Base', {
 	/**
 	 * Returns a Ext element control for the control gutter.  Many annotations may not have one.
 	 */
-	getControl: function(){},
+	getControl:function () {
+	},
 
 
 	/**
@@ -368,5 +385,6 @@ Ext.define( 'NextThought.view.annotations.Base', {
 	 * @param [numberOfSiblings] - number of siblings in case there is secondary widget renderings.
 	 *                             null implies you do not care or there is no alternate renderings.
 	 */
-	getGutterWidget: function(numberOfSiblings){}
+	getGutterWidget:function (numberOfSiblings) {
+	}
 });
