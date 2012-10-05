a892e9c7195691560772a9b1a97387e569605124
Troy Daley <troy.daley@nextthought.com>
2012-10-05 04:36:28 +0000

    better wording
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@12932 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/account/recovery/Window.js
+++ src/main/javascript/NextThought/view/account/recovery/Window.js
@@ -41,7 +41,16 @@ Ext.define('NextThought.view.account.recovery.Window',{
 
     afterRender: function(){
         this.callParent(arguments);
-        var emailType = this.linkName.indexOf('-contact-') > 0 ? 'Contact Email': 'Email';
-        Ext.fly(this.down('coppa-header-view').el.query('.detail')[0]).setHTML('The '+emailType+' you suppled has bounced, please resubmit an email.')
+
+        if ( this.linkName.indexOf('-contact-') > 0) {
+            //is contact email
+            Ext.fly(this.down('coppa-header-view').el.query('.title')[0]).setHTML('Invalid Parent Email...');
+            Ext.fly(this.down('coppa-header-view').el.query('.detail')[0]).setHTML('Please provide your parent\'s email address to get permission for social features.') ;
+        }
+        else {
+            //regulat email
+            Ext.fly(this.down('coppa-header-view').el.query('.title')[0]).setHTML('Invalid Email...');
+            Ext.fly(this.down('coppa-header-view').el.query('.detail')[0]).setHTML('We couldn\'t deliver your email. Please enter a valid email address.') ;
+        }
     }
 });
