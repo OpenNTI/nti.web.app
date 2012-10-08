b002aeed1bec97a8c63b6ff2bbd7c1ed4cb75684
Troy Daley <troy.daley@nextthought.com>
2012-10-08 20:45:53 +0000

    nodes under the redaction action should also be non-anchorable.  this fixed issues creating notes next to redactions.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@13002 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/annotations/Redaction.js
+++ src/main/javascript/NextThought/view/annotations/Redaction.js
@@ -278,11 +278,11 @@ Ext.define('NextThought.view.annotations.Redaction', {
 
 	var p = this.prototype,
 			tpl = {tag:'span', 'data-non-anchorable':'true', cls: 'redactionAction {style}', cn: [
-					{tag: 'span', cls: 'editableSpan', html: '{replacementContent}'},
-					{tag: 'span', cls: 'controls', cn:[
-						{tag: 'span', cls: 'edit', title: 'edit'},
-						{tag: 'span', cls: 'share', title: 'share'},
-						{tag: 'span', cls: 'delete', title: 'delete'}
+					{tag: 'span', 'data-non-anchorable':'true', cls: 'editableSpan', html: '{replacementContent}'},
+					{tag: 'span', 'data-non-anchorable':'true', cls: 'controls', cn:[
+						{tag: 'span', 'data-non-anchorable':'true', cls: 'edit', title: 'edit'},
+						{tag: 'span', 'data-non-anchorable':'true', cls: 'share', title: 'share'},
+						{tag: 'span', 'data-non-anchorable':'true', cls: 'delete', title: 'delete'}
 					]}
 				]};
 
