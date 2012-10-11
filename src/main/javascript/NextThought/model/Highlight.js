d8a30c3c8bb884d6b5e1aa989a864fa104ab1cc0
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-10-11 16:32:18 +0000

    make highlights group properly in the history view
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@13116 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/model/Highlight.js
+++ src/main/javascript/NextThought/model/Highlight.js
@@ -13,6 +13,8 @@ Ext.define('NextThought.model.Highlight', {
 		{ name: 'tags', type: 'Auto'},
 		{ name: 'selectedText', type: 'string'},
 		{ name: 'style', type: 'string'},
-		{ name: 'applicableRange', type: 'ContentRangeDescription'}
+		{ name: 'applicableRange', type: 'ContentRangeDescription'},
+
+		{ name: 'GroupingField', mapping: 'Last Modified', type: 'groupByTime'}
 	]
 });
