7ac0ca9645670493b7c4cfa9e3e86bc8e326ef7a
Chris Utz <chris.utz@nextthought.com>
2012-10-12 04:44:44 +0000

    Incremental work on search hit highlighting improvements and consistency.  New search hit highlight range finder.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@13163 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/ux/SearchHits.js
+++ src/main/javascript/NextThought/ux/SearchHits.js
@@ -32,33 +32,24 @@ Ext.define('NextThought.ux.SearchHits', {
 	showAllHits: function(){
 		var toAppend = [], range;
 		Ext.each(this.selections, function(sel){
-			var s = sel.getClientRects(),
-				i = s.length-1;
-			for(; i>=0; i--){
-				range = s[i];
-				//Instead of appending one element at a time build them into a list and
-				//append the whole thing.  This is a HUGE improvement for the intial rendering performance
-				toAppend.push({
-					cls:'searchHit-entry',
-				  style: {
-					  height: range.height+'px',
-					  width: range.width+'px',
-					  top: range.top+'px',
-					  left: range.left+'px'
-				  }
-				});
+			var range = sel.getBoundingClientRect();
+			//Instead of appending one element at a time build them into a list and
+			//append the whole thing.  This is a HUGE improvement for the intial rendering performance
+			toAppend.push({
+				cls:'searchHit-entry',
+			  style: {
+				  height: range.height+'px',
+				  width: range.width+'px',
+				  top: range.top+'px',
+				  left: range.left+'px'
+			  }
+			});
 
-				//Arbitrarily cap at 100 until we can figure out a solution other than
-				//a bazillion absolutely positioned divs that make anything but chrome
-				//churn.  Maybe showing these things a secion at a time as the page scrolls
-				//is best
-				if(toAppend.length > 100){
-					break;
-				}
-			}
-			//See comment about cap above
+			//Arbitrarily cap at 100 until we can figure out a solution other than
+			//a bazillion absolutely positioned divs that make anything but chrome
+			//churn.  Maybe showing these things a secion at a time as the page scrolls
+			//is best
 			return toAppend.length <= 100;
-
 		}, this);
 		//TODO should we really use dom helper here? I thought in the past we had performance issues
 		//with it
--- src/main/javascript/NextThought/view/content/reader/Scroll.js
+++ src/main/javascript/NextThought/view/content/reader/Scroll.js
@@ -123,34 +123,170 @@ Ext.define('NextThought.view.content.reader.Scroll',{
 		this.body.scrollTo('top', top, animate!==false);
 	},
 
+	/** 
+	 * A heavily modified version of Raymond Hill's doHighlight code. Attribution below 
+	 *
+	 * @param node - the node to search for ranges beneath
+	 * @param doc - the document fragment node is a child of
+	 * @param searchFor - a string or a regex to search for
+	 * @param which - if provided the subexpression of the regex to be matched
+	 * 
+	 * @returns a list of range objects that represent the portion of text to highlight
+	 **/
+
+	// Author: Raymond Hill
+	// Version: 2011-01-17
+	// Title: HTML text hilighter
+	// Permalink: http://www.raymondhill.net/blog/?p=272
+	// Purpose: Hilight portions of text inside a specified element, according to a search expression.
+	// Key feature: Can safely hilight text across HTML tags.
+	// History:
+	//   2012-01-29
+	//     fixed a bug which caused special regex characters in the
+	//     search string to break the highlighter
+	findHighlightRanges: function(node, doc, searchFor, which){
+
+		// normalize search arguments, here is what is accepted:
+		// - single string
+		// - single regex (optionally, a 'which' argument, default to 0)
+		if(Ext.isString(searchFor)) {
+			// rhill 2012-01-29: escape regex chars first
+			// http://stackoverflow.com/questions/280793/case-insensitive-string-replacement-in-javascript
+			searchFor = new RegExp(searchFor.replace(/[.*+?|()\[\]{}\\$^]/g,'\\$&'),'ig');
+		}
+		which = which || 0;
+
+		// initialize root loop
+		var indices = [],
+			text = [], // will be morphed into a string later
+			iNode = 0,
+			nNodes = node.childNodes.length,
+			nodeText,
+			textLength = 0,
+			stack = [],
+			child, nChildren,
+			state, ranges = [];
+		// collect text and index-node pairs
+		for (;;){
+			while (iNode<nNodes){
+				child = node.childNodes[iNode++];
+
+				// text: collect and save index-node pair
+				if(child.nodeType === 3){
+					indices.push({i:textLength, n:child});
+					nodeText = child.nodeValue;
+					text.push(nodeText);
+					textLength += nodeText.length;
+				}
+				// element: collect text of child elements,
+				// except from script or style tags
+				else if( child.nodeType === 1 ){
+
+					// skip style/script tags
+					if( child.tagName.search(/^(script|style)$/i) >= 0 ){
+						continue;
+					}
+
+					//skip assessment items
+					if( Ext.fly(child).is('.naquestion') ){
+						continue;
+					}
+
+					// add extra space for tags which fall naturally on word boundaries
+					if( child.tagName.search(/^(a|b|basefont|bdo|big|em|font|i|s|small|span|strike|strong|su[bp]|tt|u)$/i) < 0 ){
+						text.push(' ');
+						textLength++;
+					}
+
+					// save parent's loop state
+					nChildren = child.childNodes.length;
+					if (nChildren){
+						stack.push({n:node, l:nNodes, i:iNode});
+						// initialize child's loop
+						node = child;
+						nNodes = nChildren;
+						iNode = 0;
+					}
+				}
+			}
+
+			// restore parent's loop state
+			if (!stack.length){
+				break;
+			}
+			state = stack.pop();
+			node = state.n;
+			nNodes = state.l;
+			iNode = state.i;
+		}
+
+		// quit if found nothing
+		if (!indices.length){
+			return ranges;
+		}
+
+		// morph array of text into contiguous text
+		text = text.join('');
+
+		// sentinel
+		indices.push({i:text.length});
+
+		// find and hilight all matches
+		var iMatch, matchingText,
+			iTextStart, iTextEnd,
+			i, iLeft, iRight,
+			iEntryLeft, iEntryRight, entryLeft, entryRight,
+			parentNode, nextNode, newNode,
+			iNodeTextStart, iNodeTextEnd,
+			textStart, textMiddle, textEnd, range;
+
+		// find entry in indices array (using binary search)
+		function searchForEntry(start, end, lookFor, array){
+			var i;
+			while (start < end) {
+				i = start + end >> 1;
+				if(lookFor < array[i].i){end = i;}
+				else if (lookFor >= array[i+1].i){start = i + 1;}
+				else {start = end = i;}
+			}
+			return start;
+		}
+
+		// loop until no more matches
+		for (;;){
+
+			// find matching text, stop if none
+			matchingText = searchFor.exec(text);
+			if (!matchingText || matchingText.length<=which || !matchingText[which].length){
+				break;
+			}
+
+			// calculate a span from the absolute indices
+			// for start and end of match
+			iTextStart = matchingText.index;
+			for (iMatch=1; iMatch < which; iMatch++){
+				iTextStart += matchingText[iMatch].length;
+			}
+			iTextEnd = iTextStart + matchingText[which].length;
+
+			iEntryLeft = searchForEntry(0, indices.length, iTextStart, indices);
+			iEntryRight = searchForEntry(iEntryLeft, indices.length, iTextEnd, indices);
+
+			entryLeft = indices[iEntryLeft];
+			entryRight = indices[iEntryRight];
+			iNodeTextStart = iTextStart - entryLeft.i;
+			iNodeTextEnd = iTextEnd - entryRight.i;
+
+			range = document.createRange();
+			range.setStart(entryLeft.n, Math.max(iNodeTextStart, 0));
+			range.setEnd(entryRight.n, iNodeTextEnd);
+			ranges.push(range);
+
+		}
+		return ranges;
+	},
+
 	scrollToText: function(text) {
-
-		function findRanges(allNodes, aDoc){
-			var re = SearchUtils.searchRe(text, false, false), match;
-
-			Ext.each(allNodes, function(node) {
-					var nv = node.nodeValue,
-						indexes = [],
-						r;
-
-					while (Boolean(match = re.exec(nv))) {
-							indexes.push( {'start':match.index, 'end': match.index+match[0].length } );
-						}
-
-						Ext.each(indexes, function(index){
-							r = aDoc.createRange();
-							r.setStart(node, index.start);
-							r.setEnd(node, index.end);
-							ranges.push(r);
-						});
-				},
-				this);
-		}
-
-		if (!text) {
-			return;
-		}
-
 		var me = this,
 			doc = me.getDocumentElement(),
 			ranges = [],
@@ -158,46 +294,23 @@ Ext.define('NextThought.view.content.reader.Scroll',{
 			rangeToScrollTo,
 			nodeTop, scrollOffset, a, pos, assessmentAdjustment = 0;
 
-		//An optimized version based on AnnotationUtils.getTextNodes
-		//Not going into question nodes here is much better than checking parent
-		//recursively as we iterate the found nodes.  Profiler shows a noticble speed
-		//up with this change
-		function getTextNodes(root) {
-			var textNodes = [];
-			function getNodes(node) {
-				var child;
-
-				if (node.nodeType === 3) { textNodes.push(node); }
-				else if (node.nodeType === 1) {
-					for (child = node.firstChild; child; child = child.nextSibling) {
-						if(!Ext.fly(child).is('.naquestion')){
-							getNodes(child);
-						}
-					}
-				}
-			}
-			getNodes(root.body || root);
-			return textNodes;
+		if (!text) {
+			return;
 		}
 
-
-		texts = getTextNodes(doc);
-
-		findRanges(texts, doc);
+		ranges = me.findHighlightRanges(doc, doc, text);
 		me.showRanges(ranges);
 
-		//If we found no ranged, try again not in iframe in case of assessments,
+		//If we found no range, try again not in iframe in case of assessments,
 		//this is a bit of a hack to get it working for MC
 		if(!ranges || ranges.length === 0){
-			texts = getTextNodes(document);
-			findRanges(texts, document);
+			ranges = me.findHighlightRanges(document, document, text);
 			assessmentAdjustment = 150;
 		}
 
 		//We may get ranges in our list that don't have any client rects.  A good example
 		//is a node that is currently display none so make sure we account for that.  The
 		//related topics list is one example of where we have seen this occur
-
 		Ext.each(ranges, function(possibleRange){
 			if(possibleRange.getClientRects().length > 0){
 				rangeToScrollTo = possibleRange;
