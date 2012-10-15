03c73765320fa70df839cc70cc71d3028e9ace4b
Chris Utz <chris.utz@nextthought.com>
2012-10-15 20:29:12 +0000

    suspend layouts when rendering annotations.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@13199 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/annotations/renderer/Manager.js
+++ src/main/javascript/NextThought/view/annotations/renderer/Manager.js
@@ -302,6 +302,7 @@ Ext.define('NextThought.view.annotations.renderer.Manager',{
 
 		me.rendering = true;
 		me.events.fireEvent('rendering');
+		Ext.suspendLayouts();
 
 		me.registry[prefix] = Ext.Array.unique(me.registry[prefix]);
 
@@ -358,8 +359,8 @@ Ext.define('NextThought.view.annotations.renderer.Manager',{
 		});
 
 		me.layoutBuckets(prefix);
-
 		me.rendering = false;
+		Ext.resumeLayouts(true);
 		me.events.fireEvent('finish');
 	}
 
