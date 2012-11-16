4896bdaafb03fc1bda6668c340b542b65b93ab7a
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-11-16 05:25:23 +0000

    make the compass-growl gem optional
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@14058 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- config.rb
+++ config.rb
@@ -1,6 +1,8 @@
 # Require any additional compass plugins here.
-#require "growl"
-#require "compass-growl"
+begin
+  require "compass-growl"
+rescue LoadError
+end
 
 # Set this to the root of your project when deployed:
 http_path = "/"
