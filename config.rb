f4315de67742bbca153541e8731c4d3c01e895fc
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-11-16 04:47:22 +0000

    revert config.rb
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@14054 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 # Require any additional compass plugins here.
 #require "growl"
-require "compass-growl"
+#require "compass-growl"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
