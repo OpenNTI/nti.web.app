4dbc537e5acf0ae1e340403bed07bf0df0be910c
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-12-05 15:58:48 +0000

    notes on video js api's from YouTube and Vimeo
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@14437 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/slidedeck/Video.js
+++ src/main/javascript/NextThought/view/slidedeck/Video.js
@@ -1,4 +1,14 @@
 Ext.define('NextThought.view.slidedeck.Video',{
 	extend: 'Ext.Component',
 	alias: 'widget.slidedeck-video'
+
+	// YouTube: https://developers.google.com/youtube/js_api_reference
+	// http://www.youtube.com/v/VIDEO_ID?enablejsapi=1&version=3&playerapiid=player_id
+	// function onYouTubePlayerReady(){}
+	// use ytPlayer.at(millies,callback)
+
+	// Vimeo: http://developer.vimeo.com/player/js-api
+	// https://github.com/vimeo/player-api/tree/master/javascript or direct postMessages
+	// http://player.vimeo.com/video/VIDEO_ID?api=1&player_id=player_id
+	// use event "playProgress" and keep track of our times to fire an event like "at" for youtube.
 });
