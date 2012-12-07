a9480cfe40b934c0854e54ca8bcbc41e4e1b386a
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2012-12-07 02:24:18 +0000

    Fix bug. For groups created while adding a new friend, stop tracking pending groups since now we create them right away.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@14494 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/account/contacts/management/GroupList.js
+++ src/main/javascript/NextThought/view/account/contacts/management/GroupList.js
@@ -1,108 +1,99 @@
-Ext.define('NextThought.view.account.contacts.management.GroupList',{
-	extend: 'Ext.view.BoundList',
-	alias: 'widget.management-group-list',
-	mixins: {
+Ext.define('NextThought.view.account.contacts.management.GroupList', {
+	extend                 :'Ext.view.BoundList',
+	alias                  :'widget.management-group-list',
+	mixins                 :{
 		addgroup:'NextThought.mixins.AddGroup'
 	},
-	ui: 'nt',
-	plain: true,
-	shadow: false,
-	frame: false,
-	border: false,
-	preserveScrollOnRefresh: true,
+	ui                     :'nt',
+	plain                  :true,
+	shadow                 :false,
+	frame                  :false,
+	border                 :false,
+	preserveScrollOnRefresh:true,
 
-	cls: 'group-selection-list',
-	baseCls: 'selection',
-	itemCls: 'selection-list-item multiselect',
-	displayField: 'realname',
-	selModel: { mode: 'SIMPLE' },
+	cls         :'group-selection-list',
+	baseCls     :'selection',
+	itemCls     :'selection-list-item multiselect',
+	displayField:'realname',
+	selModel    :{ mode:'SIMPLE' },
 
-	initComponent: function(){
+	initComponent:function () {
 		this.store = Ext.getStore('FriendsList');
 		this.callParent(arguments);
 		this.itemSelector = '.selection-list-item';
 
-		this.block('mycontacts-'+$AppConfig.username);
+		this.block('mycontacts-' + $AppConfig.username);
 
 		this.allowSelect = this.allowSelect || false;
 
 		this.mon(this.getSelectionModel(), {
-			beforeselect: this.onBeforeSelect,
-			beforedeselect: this.onBeforeDeselect,
-			deselect: this.onDeselect,
-			select: this.onSelect,
-			scope: this
+			beforeselect  :this.onBeforeSelect,
+			beforedeselect:this.onBeforeDeselect,
+			deselect      :this.onDeselect,
+			select        :this.onSelect,
+			scope         :this
 		});
 
 	},
 
 
-	getSelected: function(){
+	getSelected:function () {
 		return this.getSelectionModel().getSelection();
 	},
 
 
-    onUpdate: function(ds, record){
-        if(Ext.Array.contains(this.blocked, record.get('Username'))){
-            console.log('username blocked', record);
-            return;
-        }
-        this.callParent(arguments);
-    },
+	onUpdate:function (ds, record) {
+		if (Ext.Array.contains(this.blocked, record.get('Username'))) {
+			console.log('username blocked', record);
+			return;
+		}
+		this.callParent(arguments);
+	},
 
 
-	refresh: function(){
-		var el = this.getEl(),
-			ul,
-			selection = this.getSelectionModel(),
-			blocked = this.blocked;
+	refresh:function () {
+		var el = this.getEl(), ul, selection = this.getSelectionModel(), blocked = this.blocked;
 
 		this.callParent(arguments);
 
-		if(!this.rendered){
+		if (!this.rendered) {
 			return;
 		}
 
 		ul = el.down('ul');
-		if(!this.allowSelect){
+		if (!this.allowSelect) {
 			ul.addCls('disallowSelection');
 		}
 
-		Ext.each(ul.query('li'), function(li){
+		Ext.each(ul.query('li'), function (li) {
 			var r = this.getRecord(li);
-			if(Ext.Array.contains(blocked||[], r.get('Username'))){
-				Ext.fly(li).setStyle({display: 'none'});
+			if (Ext.Array.contains(blocked || [], r.get('Username'))) {
+				Ext.fly(li).setStyle({display:'none'});
 			}
-			if (!r.isModifiable()){
+			if (!r.isModifiable()) {
 				//Ext.fly(li).down('img.delete-group').remove();
 				Ext.fly(li).setStyle({display:'none'});
 			}
 
-			if(this.username && r.hasFriend(this.username)){
-				selection.select([r],true,true);
+			if (this.username && r.hasFriend(this.username)) {
+				selection.select([r], true, true);
 			}
 		}, this);
 
-		if(this.allowSelect){
-			this.attachAddGroupControl( ul, 'li' );
+		if (this.allowSelect) {
+			this.attachAddGroupControl(ul, 'li');
 		}
-
-		Ext.each(this.pendingGroupsRequests || [], function(i){
-			selection.select(i, true, true);
-		});
 	},
 
 
-	block: function(username){
-		this.blocked = Ext.Array.merge(
-				this.blocked||[],
-				Ext.isArray(username) ? username : [username]);
+	block:function (username) {
+		this.blocked = Ext.Array.merge(this.blocked || [], Ext.isArray(username) ? username : [username]);
 		this.refresh();
 	},
 
 
-	setUser: function(user){
-		if(user && user.isModel){
+	setUser:function (user) {
+		if (user && user.isModel) {
 			user = user.get('Username');
 		}
 		this.username = user;
@@ -110,115 +101,99 @@ Ext.define('NextThought.view.account.contacts.management.GroupList',{
 	},
 
 
-	reset: function(){
-		if(!this.username){
+	reset:function () {
+		if (!this.username) {
 			this.getSelectionModel().deselectAll();
 		}
 		this.refresh();
 	},
 
 
-	getInnerTpl: function(displayField){
-		if (displayField !== 'realname'){
+	getInnerTpl:function (displayField) {
+		if (displayField !== 'realname') {
 			console.warn('displayField is not realname');
 		}
-		return ['<div class="name">',
-				'<tpl if="realname == \'\'">{Username}' +
-					'<tpl else>',
-						'{realname}',
-				'</tpl>',
-				'</div>'
+		return ['<div class="name">', '<tpl if="realname == \'\'">{Username}' + '<tpl else>', '{realname}', '</tpl>', '</div>'
 		].join('');
 	},
 
 
-	handleEvent: function(e){
-		if(e.getTarget('li[role=option]')){
+	handleEvent:function (e) {
+		if (e.getTarget('li[role=option]')) {
 			this.callParent(arguments);
 		}
 	},
 
 
-	onBeforeSelect: function(list,model){
+	onBeforeSelect:function (list, model) {
 		return (this.allowSelect && model.isModifiable());
 	},
 
-	onBeforeDeselect: function(list,model){
+	onBeforeDeselect:function (list, model) {
 		return (!this.allowSelect || model.isModifiable());
 	},
 
-	onMaskBeforeShow: function(){
+	onMaskBeforeShow:function () {
 		this.ignoreSelection = true;
 		this.callParent(arguments);
 		delete this.ignoreSelection;
 	},
 
-	onDeselect: function(view,group){
-		if(this.username && !this.ignoreSelection && group && group.hasFriend(this.username)){
-			this.fireEvent('remove-contact',group,this.username);
+	onDeselect:function (view, group) {
+		if (this.username && !this.ignoreSelection && group && group.hasFriend(this.username)) {
+			this.fireEvent('remove-contact', group, this.username);
 		}
 	},
 
-	onSelect: function(view,group){
-		if(this.username && !this.ignoreSelection && group && !group.hasFriend(this.username)){
-			this.fireEvent('add-contact',this.username,[group]);
-		}
-		else{
-			if(!group){ return;}
-
-			// FIXME: if this user isn't in our contacts, add this group to the pending groups,
-			// that way if I add the user to my contacts, he gets added to the other selected groups.
-			if(!this.pendingGroupsRequests){ this.pendingGroupsRequests = []; }
-			this.pendingGroupsRequests.push(group);
+	onSelect:function (view, group) {
+		if (this.username && !this.ignoreSelection && group && !group.hasFriend(this.username)) {
+			this.fireEvent('add-contact', this.username, [group]);
+		} else {
+			if (!group) {
+				return;
+			}
 		}
 	},
 
 
-	disallowSelection: function(){
+	disallowSelection:function () {
 		this.allowSelect = false;
 		this.getSelectionModel().deselectAll();
 		this.refresh();
 	},
 
 
-	allowSelection: function(){
+	allowSelection:function () {
 		this.allowSelect = true;
 		this.refresh();
 	},
 
 
-	selectNewGroup: function(groupName){
+	selectNewGroup:function (groupName) {
 		//FIXME: what's the purpose of ignoreSelection?
 		//if(this.username && !this.ignoreSelection){
 		//	return;
 		//}
-		var record,s = this.getSelectionModel();
-		if(!this.allowSelect){
+		var record, s = this.getSelectionModel();
+		if (!this.allowSelect) {
 			return;
 		}
 		try {
-			record = this.store.find(this.displayField,groupName, 0, false, true, true);
-			if(typeof record === 'number' && record !== -1){
-				s.select(record,true,true);
-				this.fireEvent('selectionchange',this, s.getSelection());
-
-				//Only adds to pendingList if the user is not in my contacts.
-				if(!this.username){
-					if(!this.pendingGroupsRequests){ this.pendingGroupsRequests = []; }
-					this.pendingGroupsRequests.push(this.getSelected()[0]);
-				}
+			record = this.store.find(this.displayField, groupName, 0, false, true, true);
+			if (typeof record === 'number' && record !== -1) {
+				s.select(record, true, true);
+				this.fireEvent('selectionchange', this, s.getSelection());
 			}
-		}
-		catch(er){
+		} catch (er) {
 			console.error(Globals.getError(er));
 		}
 	},
 
-	afterGroupAdd: function(groupName){
+	afterGroupAdd:function (groupName) {
 		var me = this;
-			me.store.on('datachanged',function(){
-				me.selectNewGroup(groupName);
-			},me,{single:true});
+		me.store.on('datachanged', function () {
+			me.selectNewGroup(groupName);
+		}, me, {single:true});
 	}
 
 });
