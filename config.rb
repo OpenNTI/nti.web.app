5ae822c9093b5b03e342a82f8f565366a06cd5af
Jonathan Grimes <jonathan.grimes@nextthought.com>
2012-12-11 22:28:14 +0000

    bless doesn't like CSS comments(C++ style), switch comments to SASS comments(C style). tweak compass config to compact output.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@14579 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- config.rb
+++ config.rb
@@ -6,6 +6,8 @@ end
 
 blessc_executable = 'blessc';
 
+environment = :production
+
 # Set this to the root of your project when deployed:
 http_path = "./"
 css_dir = "src/main/resources/css"
@@ -14,9 +16,10 @@ images_dir = "src/main/resources/images/"
 javascripts_dir = "src/main/javascript/"
 relative_assets = true
 
-#output_style = :compressed
+output_style = :compact
+line_comments = false
 
 # execute blessc when the stylesheet is generated
 on_stylesheet_saved do |path|
-  print "\n\n\n##############################################\nWARNING: No Blessc found\nExpect problems in IE\n##############################################\n\n\n\n" unless system(blessc_executable+' '+path+' -f -x')
+	print "\n\n\n##############################################\nWARNING: No Blessc found\nExpect problems in IE\n##############################################\n\n\n\n" unless system(blessc_executable+' '+path+' -f -x')
 end
