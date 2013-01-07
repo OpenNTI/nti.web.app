ccb1d7b9732dcdf5f01c57f34f3e928afe6725fd
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-01-07 19:58:03 +0000

    document
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@14919 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/overrides/tip/QuickTip.js
+++ src/main/javascript/NextThought/overrides/tip/QuickTip.js
@@ -1,10 +1,17 @@
+/**
+ * This entire file is a hack.  The ExtJS implementation aligns and positions the tip before it updates the contents of
+ * the tip. So, it is potentially offset.  This attempts to correct that by repeating alignment calculations when it
+ * detects a discrepancy.
+ *
+ * We also try to apply sane defaults.
+ */
 Ext.define('NextThought.overrides.tip.QuickTip',{
 	override: 'Ext.tip.QuickTip',
 
+	//Apply defaults
 	constructor: function(config){
 		config = Ext.apply(config||{},{
 			showDelay: 500,
-			hideDelay: 10000000,
 			anchorTarget: true,
 			trackMouse: false,
 			shadow:false
@@ -12,6 +19,8 @@ Ext.define('NextThought.overrides.tip.QuickTip',{
 		return this.callParent([config]);
 	},
 
+	//Override alignment and force the 'target' element to be the element with the title/tip attribute if not the
+	// registered owner element. Default to a top centered position, unless screen position forces us to reposition.
 	getTargetXY: function getTargetXY(){
 		function getTarget(el){
 			el = Ext.get(el);
@@ -59,7 +68,21 @@ Ext.define('NextThought.overrides.tip.QuickTip',{
 		return r;
 	},
 
+	//Hack: The contents change during show, AFTER positioning and aligning, so if we change size, redo it all.
+	showAt: function(){
+		var size = this.el.getSize(),
+			r = this.callParent(arguments),
+			sizeAfter = this.el.getSize();
 
+		if(size.width !== sizeAfter.width || size.height !== sizeAfter.height){
+			this.showAt(this.getTargetXY());
+		}
+
+		return r;
+	},
+
+
+	//We prefer to align to the center posisitions instead of the corner positions.
 	getAnchorAlign: function() {
         switch (this.anchor) {
         case 'top': return 't-b?';
