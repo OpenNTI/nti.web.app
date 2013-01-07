34c7a2a25e21b79953ae83a939c893273db643be
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2013-01-07 18:31:24 +0000

    work on tips
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@14914 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/overrides/tip/QuickTip.js
+++ src/main/javascript/NextThought/overrides/tip/QuickTip.js
@@ -4,22 +4,37 @@ Ext.define('NextThought.overrides.tip.QuickTip',{
 	constructor: function(config){
 		config = Ext.apply(config||{},{
 			showDelay: 500,
+			hideDelay: 10000000,
 			anchorTarget: true,
 			trackMouse: false,
-			shadow:false,
-			interceptTitles: true
+			shadow:false
 		});
 		return this.callParent([config]);
 	},
 
-	getTargetXY: function(){
-		this.anchorTarget = this.activeTarget.el;
+	getTargetXY: function getTargetXY(){
+		function getTarget(el){
+			el = Ext.get(el);
+			if(el.getAttribute('title') || el.getAttribute('data-qtip')){
+				return el;
+			}
+
+			return el.up('[title]') || el.up('[data-qtip]') || el;
+		}
+
+		if(getTargetXY.recursiveCall){
+			return this.callParent(arguments);
+		}
+
+		getTargetXY.recursiveCall = true;
+
+		this.anchorTarget = Ext.getDom( getTarget(this.activeTarget.el) );
 		this.anchor = 'bottom';
 
 		var doc = Ext.getDom(this.anchorTarget).ownerDocument,
 			vW = ((!Ext.isStrict && !Ext.isOpera) ? doc.body.clientWidth :
 		                   !Ext.isIE ? doc.documentElement.clientWidth : doc.parentWindow.innerWidth),
-
+			w = this.el.getWidth(),
 			r = this.callParent(arguments);
 
 		if(r[1] < 50 ){
@@ -31,7 +46,8 @@ Ext.define('NextThought.overrides.tip.QuickTip',{
 			//needs to swap left
 			this.anchor = 'left';
 		}
-		else if((vW - r[0]) < 50){
+		else if((vW - (r[0] + w ) ) < 50){
+			console.log('swapping to right')
 			//needs to swap left
 			this.anchor = 'right';
 		}
@@ -40,16 +56,17 @@ Ext.define('NextThought.overrides.tip.QuickTip',{
 			r = this.callParent(arguments);
 		}
 
+		delete getTargetXY.recursiveCall;
 		return r;
 	},
 
 
 	getAnchorAlign: function() {
         switch (this.anchor) {
-        case 'top': return 't-b';
-        case 'left': return 'l-r';
-        case 'right': return 'r-l';
-        default: return 'b-t';
+        case 'top': return 't-b?';
+        case 'left': return 'l-r?';
+        case 'right': return 'r-l?';
+        default: return 'b-t?';
         }
     }
 
