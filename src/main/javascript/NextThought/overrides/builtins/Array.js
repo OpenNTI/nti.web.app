c6113a1198efc03a2ba8579eebb7d96f24eaada7
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-01-15 20:43:46 +0000

    tweak our array prototype additions, by making them non-enumerable, so the don't show up in the console when we print out an array.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@15085 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/overrides/builtins/Array.js
+++ src/main/javascript/NextThought/overrides/builtins/Array.js
@@ -1,9 +1,20 @@
 Ext.define('NextThought.overrides.builtins.Array',{
 
 },function(){
-	Ext.applyIf(Array.prototype,{
+	(function(o,a){
+		Ext.Object.each(a,function(k,v){
+			if(!o[k]){
+				o[k] = v;
+				if(Object.defineProperty){
+					Object.defineProperty(o,k,{enumerable: false});
+				}
+			}
+		});
+	}(Array.prototype,{
 		first: function first(){ return this[0]; },
 		last: function last(){ return this[this.length-1]; },
 		peek: function peek(){ return this[this.length-1]; }
-	});
+	}));
+
+
 });
