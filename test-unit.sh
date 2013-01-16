52b61f96a2037c98fa2a1c4c44cb7b057beace4a
Chris Fenner <chris.fenner@nextthought.com>
2013-01-16 17:50:28 +0000

    Add spec file location to output of Jasmine results
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@15124 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- test-unit.sh
+++ test-unit.sh
@@ -8,4 +8,24 @@ if [[ "$PHANTOMJS_VER" != 1.* ]] ; then
 fi
 
 cd src/test
-phantomjs javascript/run-jasmine.js
+# run the Jasmine tests and parse the output line-by-line
+phantomjs javascript/run-jasmine.js | while read LINE
+do
+	echo "$LINE"
+	# search for failed tests so we can print the filename
+	# this sed expression searches for "FAILED: [suite name] > [test name] ..."
+	# and prints the suite name if it finds it
+	SUITE_NAME=`echo "$LINE" | sed -n 's|FAILED: \(.*\) >.*|\1|p'`
+	if [ -n "$SUITE_NAME" ]
+		then
+			# if we find a suite name, find the file to which this suite belongs
+			# and print it
+			FILENAME=`egrep -lr --include=*.spec.js "$SUITE_NAME" ../../`
+			if [ -n "$FILENAME" ]
+				# FILENAME is going to start with "../../" so let's clip that off
+				FILENAME=`echo "$FILENAME" | sed 's|../../||'`
+				then
+				echo "in      $FILENAME"
+			fi
+	fi
+done
