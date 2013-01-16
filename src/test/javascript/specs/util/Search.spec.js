b6942d77a1dff7f92265dddb11aedc0a386a1af6
Chris Utz <chris.utz@nextthought.com>
2013-01-16 03:23:22 +0000

    mas tests
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@15111 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/javascript/specs/util/Search.spec.js
+++ src/test/javascript/specs/util/Search.spec.js
@@ -32,5 +32,99 @@ describe("Search utils", function() {
 		});
 	});
 
+	describe("extractMatchFromFragment", function(){
+		it("Finds partial string", function(){
+			var result = SearchUtils.extractMatchFromFragment('I like cake', [2, 6]);
+		   expect(result).toBe('like');
+		});
+
+		it("Finds entire string", function(){
+			var result = SearchUtils.extractMatchFromFragment('I like cake', [0, 11]);
+			expect(result).toBe('I like cake');
+		});
+	});
+
+	describe("contentRegexPartsForHit", function(){
+		it('Short ciruits for bad input', function(){
+			var hit = Ext.create('NextThought.model.Hit', {
+				Type: 'Content'
+			});
+
+			expect(SearchUtils.contentRegexPartsForHit(hit)).toBeFalsy();
+
+			hit.set('Fragments', [{}]);
+			hit.set('Type', 'Note');
+
+			expect(SearchUtils.contentRegexPartsForHit(hit)).toBeFalsy();
+		});
+
+		function hitWithFragments(frags){
+			var hit = Ext.create('NextThought.model.Hit', {
+				Type: 'Content',
+				Fragments: Ext.isArray(frags) ? frags : [frags]
+			});
+
+			return hit;
+		}
+
+		it("Will pull multiple matches from one fragment", function(){
+			var hit = hitWithFragments({
+				text: 'The quick brown fox',
+				matches: [[1,3], [10, 19]]
+			}), terms;
+
+			terms = SearchUtils.contentRegexPartsForHit(hit);
+			expect(terms.length).toBe(2);
+			expect(Ext.Array.contains(terms, 'he')).toBeTruthy();
+			expect(Ext.Array.contains(terms, 'brown fox')).toBeTruthy();
+		});
+
+		it("Pulls from multiple fragments", function(){
+			var hit = hitWithFragments([{
+				text: 'The quick brown fox',
+				matches: [[1,3], [10, 19]]
+			},{
+				text: 'See spot run',
+				matches: [[4, 8]]
+			}]), terms;
+
+			terms = SearchUtils.contentRegexPartsForHit(hit);
+			expect(terms.length).toBe(3);
+			expect(Ext.Array.contains(terms, 'he')).toBeTruthy();
+			expect(Ext.Array.contains(terms, 'brown fox')).toBeTruthy();
+			expect(Ext.Array.contains(terms, 'spot')).toBeTruthy();
+		});
+
+		it("Returns unique terms", function(){
+			var hit = hitWithFragments([{
+				text: 'The quick quick fox',
+				matches: [[4,9], [10, 15]]
+			},{
+				text: 'See quick spot run',
+				matches: [[4,9], [10, 14]]
+			}]), terms;
+
+			terms = SearchUtils.contentRegexPartsForHit(hit);
+			expect(terms.length).toBe(2);
+			expect(Ext.Array.contains(terms, 'quick')).toBeTruthy();
+			expect(Ext.Array.contains(terms, 'spot')).toBeTruthy();
+		});
+	});
+
+	describe("contentRegexForSearchHit", function(){
+		it('Short ciruits for bad input', function(){
+			var hit = Ext.create('NextThought.model.Hit', {
+				Type: 'Content'
+			});
+
+			expect(SearchUtils.contentRegexForSearchHit(hit)).toBeFalsy();
+
+			hit.set('Fragments', [{}]);
+			hit.set('Type', 'Note');
+
+			expect(SearchUtils.contentRegexForSearchHit(hit)).toBeFalsy();
+		});
+	});
+
 });
 
