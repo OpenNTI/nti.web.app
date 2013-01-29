b8f49d76a1465343cd71113e335fc605f3db54a2
Chris Utz <chris.utz@nextthought.com>
2013-01-29 22:35:10 +0000

    use input to determine if answered or not
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@15446 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/assessment/input/Base.js
+++ src/main/javascript/NextThought/view/assessment/input/Base.js
@@ -417,7 +417,7 @@ Ext.define('NextThought.view.assessment.input.Base',{
 		delete this.submissionDisabled;
 		this.checkItBtn.removeCls('disabled');
 		if( this.questionSet ){
-			this.questionSet.fireEvent('answered', this.question, this.part, true);
+			this.questionSet.fireEvent('answered', this.question, this.part, !Ext.isEmpty(this.getValue()));
 		}
 		this.fireEvent('enable-submission', this.ordinal);
 	},
@@ -427,7 +427,7 @@ Ext.define('NextThought.view.assessment.input.Base',{
 		this.submissionDisabled = true;
 		this.checkItBtn.addCls('disabled');
 		if( this.questionSet ){
-			this.questionSet.fireEvent('answered', this.question, this.part, false);
+			this.questionSet.fireEvent('answered', this.question, this.part, !Ext.isEmpty(this.getValue()));
 		}
 		this.fireEvent('disable-submission', this.ordinal);
 	},
