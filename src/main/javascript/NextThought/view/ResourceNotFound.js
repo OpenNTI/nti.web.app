1b8f13bcdb78e9aaac6b789d8c75aa6529845bbe
Chris Utz <chris.utz@nextthought.com>
2013-02-18 21:37:36 +0000

    Match not found styling to aarons design.  Just need to hook up actions now.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@15988 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/ResourceNotFound.js
+++ src/main/javascript/NextThought/view/ResourceNotFound.js
@@ -4,11 +4,13 @@ Ext.define('NextThought.view.ResourceNotFound',{
 
 	renderTpl: Ext.DomHelper.markup([
 		{ cls: 'resource-not-found', cn:[
-			{ cls: 'heading', html: 'Sorry, this page doesn\'t exist...'},
-			{ cls: 'subtext', html: 'Your link may contain errors or the page may no longer exist.'},
-			{ cls: 'actions', cn: [
-				{cls: 'library', tag: 'a', html: 'Library'},
-				{cls: 'back', tag: 'a', html: 'Previous Page'}
+			{ cls: 'body', cn: [
+				{ cls: 'heading', html: 'Sorry, this page doesn\'t exist...'},
+				{ cls: 'subtext', html: 'Your link may contain errors or the page may no longer exist.'},
+				{ cls: 'actions', cn: [
+					{cls: 'library', tag: 'a', html: 'Library'},
+					{cls: 'back', tag: 'a', html: 'Previous Page'}
+				]}
 			]}
 		]}
 	])
