26c323a353a05fae09b0cb01a727f2a65caba153
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-02-20 20:45:48 +0000

    scroll contacts :/
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@16099 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/contacts/Panel.js
+++ src/main/javascript/NextThought/view/contacts/Panel.js
@@ -2,6 +2,6 @@ Ext.define('NextThought.view.contacts.Panel',{
 	extend: 'Ext.container.Container',
 	alias: 'widget.contacts-tabs-panel',
 	requires: ['NextThought.view.contacts.Card'],
-	defaultType: 'contacts-tabs-card'
-
+	defaultType: 'contacts-tabs-card',
+	autoScroll:true
 });
