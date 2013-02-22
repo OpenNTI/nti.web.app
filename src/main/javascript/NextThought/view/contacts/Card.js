46b5941663c306bc090e1c58dda40312c6367280
Chris Utz <chris.utz@nextthought.com>
2013-02-22 01:33:38 +0000

    Contacts panel honors disableProfiles flag for showing verbose user data
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@16163 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/contacts/Card.js
+++ src/main/javascript/NextThought/view/contacts/Card.js
@@ -18,21 +18,17 @@ Ext.define('NextThought.view.contacts.Card',{
 			cn: [
 				{ cls: 'name', html: '{name}', 'data-field':'name'},
 				{ cls: 'add-to-contacts', html: 'ADD'},
-				{ tag: 'tpl', 'if':'email', cn:[
+				{ tag: 'tpl', 'if':'!hideProfile && email', cn:[
 					{ cls: 'email', html: '{email}', 'data-field':'email' }]},
 
-				{ tag: 'tpl', 'if':'role || affiliation', cn:[
+				{ tag: 'tpl', 'if':'!hideProfile && (role || affiliation)', cn:[
 				{ cls: 'composite-line', cn: [
 					{ tag: 'span', html:'{role}', 'data-field':'role'},
 					{ tag: 'tpl', 'if':'role && affiliation', cn:[{tag: 'span', cls: 'separator', html:' at '}]},
 					{ tag: 'span', html:'{affiliation}', 'data-field':'affiliation' }]}]},
 
-				{ tag: 'tpl', 'if':'location', cn:[
+				{ tag: 'tpl', 'if':'!hideProfile && location', cn:[
 					{ cls: 'location', html:'{location}', 'data-field':'location' }]},
-
-//				{ tag: 'tpl', 'if':'home_page', cn:[
-//					{ cls: 'home-page', 'data-field':'home_page', cn:[
-//						{ tag: 'a', target: '_blank', html: '{home_page}', href: '{home_page}', 'data-field':'home_page' }]}]},
 
 				{ cls: 'actions', cn: [
 					{ cls: 'chat', html: 'Chat'}
@@ -50,6 +46,7 @@ Ext.define('NextThought.view.contacts.Card',{
 		var u = this.record;
 		this.callParent(arguments);
 		this.renderData = Ext.apply(this.renderData||{}, u.getData() );
+		this.renderData = Ext.apply(this.renderData, {hideProfile: $AppConfig.disableProfiles === true});
 		this.renderData.name = u.getName();
 	},
 
