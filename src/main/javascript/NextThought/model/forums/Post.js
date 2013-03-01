e898c04eb6032d39b413b8e048d45d1c027f1e65
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-03-01 14:34:15 +0000

    revert sending class. server no longer requires it.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@16491 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/model/forums/Post.js
+++ src/main/javascript/NextThought/model/forums/Post.js
@@ -4,7 +4,6 @@ Ext.define('NextThought.model.forums.Post', {
 		bodyContent: 'NextThought.mixins.ModelWithBodyContent'
 	},
 	fields: [
-		{ name: 'Class', type: 'string', persist: true },
 		{ name: 'body', type: 'auto' },
 		{ name: 'title', type: 'string' }
 	]
