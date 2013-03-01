9746797681d0fafa792344d46a757a87874df921
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-03-01 16:48:15 +0000

    add publish method
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@16499 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/model/forums/PersonalBlogEntry.js
+++ src/main/javascript/NextThought/model/forums/PersonalBlogEntry.js
@@ -7,8 +7,31 @@ Ext.define('NextThought.model.forums.PersonalBlogEntry', {
 		} }
 	],
 
+
 	isPublished: function(){
 		return Boolean(this.getLink('unpublish'));
+	},
+
+
+	publish:function(widget, cb, scope){
+		var me = this,
+			currentValue = this.isPublished(),
+			action = currentValue ? 'unpublish' : 'publish';
+
+		if ((me.activePostTos && me.activePostTos[action]) || me.phantom){return;}
+
+		widget = widget||{};//default it so we don't blow up if no arg is passed
+
+		//We will assume it completes and then update it if it actually fails
+		Ext.callback(widget.markAsPublished,widget,[!currentValue]);
+
+		me.postTo(action, function(s){
+			Ext.callback(cb,scope||window,s);
+			if (!s) {
+				Ext.callback(widget.markAsPublished,widget,[currentValue]);
+			}
+		});
+
 	}
 });
 
