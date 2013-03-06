b4a8d89f64fac9a0883deee1d70633185b768812
Andrew Ligon <andrew.ligon@nextthought.com>
2013-03-06 20:35:15 +0000

    Fixed the chat search result styling
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@16740 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/menus/search/Result-Chat.js
+++ src/main/javascript/NextThought/view/menus/search/Result-Chat.js
@@ -5,28 +5,30 @@ Ext.define('NextThought.view.menus.search.Result-Chat',{
 
 	requires: ['NextThought.util.Search'],
 
-	renderTpl: Ext.DomHelper.markup([{
-		cls: 'history chat',
-		cn:[
-				{cls: 'occupants', cn:[
-					{tag: 'span', cls: 'names', html: '{occupants}'},
-					{tag: 'span', cls: 'count', html: '{count}'}
-				]},
-				{cls: 'time', cn:[
-					{tag: 'span', cls: 'started', html: '{started}'},
-					{tag: 'span', cls: 'date', html: '{date}'},
-					' - Lasted ',
-					{tag: 'span', cls: 'duration', html:'{duration}'}
-				]},
-				{cls: 'created', cn: [
+	renderTpl: Ext.DomHelper.markup([
+		{ cls:'title', cn: [
+			{cls: 'occupants', cn:[
+					{tag: 'span', cls: 'names', html: '{occupants}'}
+			]},
+			{cls: 'time', cn:[
+				{tag: 'span', cls: 'started', html: 'Sent: {sent}'}
+				
+			]},
+			{cls: 'lasted',cn:[
+				{tag: 'span', cls: 'lasted', html: 'Lasted: {duration}'}
+			]},
+		]},
+
+		{ cls: 'wrap',
+			cn:[
+				{cls: 'name', cn: [
 					{tag: 'span', cls:'created', html:'{creator} said:'}
 				]},
 				{cls: 'fragments', cn: [
 						{tag:'tpl', 'for':'fragments', cn:[
 							{cls: 'fragment', ordinal: '{#}', html: '{.}'}
 						]}
-					]
-				}
+				]}
 			],
 	}]),
 
@@ -56,15 +58,30 @@ Ext.define('NextThought.view.menus.search.Result-Chat',{
 			containerId = hit.get('ContainerId');
 
 		function success(obj){
-			var old = me.renderData;
+			var occupants = obj.get('Occupants'), 
+				started = obj.get('CreatedTime'),
+				ended = hit.get('Last Modified'),
+				date = new Date (hit.get('Last Modified')),
+				creator = hit.get('Creator');
+
+			//look for the user email in occupants and replace it with me
+			occupants.forEach(function(element,index,array){
+				if(isMe(element)){
+					occupants[index] = 'me';
+				}
+				return;
+			});
+
+			//check if the user created it
+			if(isMe(creator)){
+				creator = 'I';
+			}
+
 			me.renderData = Ext.apply(me.renderData || {},{
-				occupants: obj.get('Occupants'),
-	            count: obj.get('MessageCount'),
-	            started: obj.get('CreatedTime'),
-				date: 'Feb 28',
-				duration: '9 min',
-				creator: 'chris.utz@nextthought.com',
-				fragments: Ext.pluck(hit.get('Fragments'), 'text')
+				occupants:"Between "+occupants[0]+" and "+((occupants.length - 1 > 1) ? (occupants.length - 1)+" others" : "1 other"),
+				sent: Ext.Date.format(date, 'M-j g:i A'),
+				duration: obj.timeDifference(ended,started).replace(/ ago/i,''),
+				creator: creator
 			});
 			if(me.rendered){
 				me.renderTpl.overwrite(me.el, me.renderData);
@@ -77,8 +94,18 @@ Ext.define('NextThought.view.menus.search.Result-Chat',{
 		function failure(req,resp){
 			console.log("Error fetching chat transcript");
 		}
-
+		this.wrapFragmentHits();
 		$AppConfig.service.getObject(containerId,success,failure);
+		//this.callParent(arguments);
+		this.getEl().on({
+			scope: this,
+			animationend: this.animationEnd,
+			webkitAnimationEnd: this.animationEnd,
+			click: this.clicked
+		});
+	},
+
+	clicked: function(e){
 	}
 
 	
