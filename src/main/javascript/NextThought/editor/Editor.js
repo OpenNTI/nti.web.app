46f428bc616cf5a96a0c8f9efd37e4920939ba12
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-03-06 17:43:38 +0000

    XTemplates don't like {} in the if.  Add styles.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@16726 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/editor/Editor.js
+++ src/main/javascript/NextThought/editor/Editor.js
@@ -16,44 +16,46 @@ Ext.define('NextThought.editor.Editor',{
 	saveButtonLabel: 'Save',
 	cancelButtonLabel: 'Cancel',
 
+	ui: 'editor',
 	cls: 'editor',
 
 	renderTpl: Ext.DomHelper.markup([
 		{
 			cls: 'main',
-			cn:[{tag:'tpl', 'if':'{enableTitle}', cn:{
+			cn:[{tag:'tpl', 'if':'enableTitle', cn:{
 				cls: 'title',
 				cn:[{tag:'input', tabIndex:-1, type:'text', placeholder: 'Title...'}]
-			}},{tag:'tpl', 'if':'{enablePublishControls || enableTags}', cn:{
+			}},{tag:'tpl', 'if':'enablePublishControls || enableTags', cn:{
 				cls: 'aux',
 				cn:[
-					{tag:'tpl', 'if':'{enablePublishControls}', cn:
-						{cls: 'action publish', tabIndex:-1, 'data-qtip': 'Publish State'}},
-					{tag:'tpl', 'if':'{enableTags}', cn:{cls: 'tags'}}
+					{tag:'tpl', 'if':'enablePublishControls', cn:
+					{cls: 'action publish', tabIndex:-1, 'data-qtip': 'Publish State'}},
+					{tag:'tpl', 'if':'enableTags', cn:{cls: 'tags'}}
 				]
-			}},{
+			}},{tag:'tpl', 'if':'enableTextControls || enableShareControls', cn:{
 				cls: 'toolbar',
-				cn: [{
+				cn: [{tag:'tpl', 'if':'enableTextControls', cn:{
 					cls: 'left',
 					cn: [{cls: 'action bold', tabIndex:-1, 'data-qtip': 'Bold'},
 						{cls:'action italic', tabIndex:-1, 'data-qtip': 'Italic'},
 						{cls:'action underline', tabIndex:-1, 'data-qtip': 'Underline'}]
-				},{tag:'tpl', 'if':'{enableShareControls}', cn:{
+				}},{tag:'tpl', 'if':'enableShareControls', cn:{
 					cls: 'right',
 					cn: [{cls: 'action share', html: 'Only Me', tabIndex:-1, 'data-qtip': 'Shared with'}]
 				}}]
-			},{
+			}},{
 				cls: 'content',
 				contentEditable: true,
 				unselectable: 'off',
 				tabIndex:-1,
 				cn: [{ //inner div for IE
-					html: '&#8203;' //default value (allow the cursor in to this placeholder div, but don't take any space)
+					//default value (allow the cursor in to this placeholder div, but don't take any space)
+					html: '&#8203;'
 				}]
 			}]
 		},{
 			cls: 'footer',
-			cn: [{tag:'tpl', 'if':'{enableWhiteboards}', cn:{
+			cn: [{tag:'tpl', 'if':'enableWhiteboards', cn:{
 				cls: 'left',
 				cn: [{cls: 'action whiteboard', tabIndex:-1, 'data-qtip': 'Create a whiteboard'}]
 			}},{
@@ -86,5 +88,6 @@ Ext.define('NextThought.editor.Editor',{
 	afterRender: function(){
 		this.callParent(arguments);
 		this.mixins.editorActions.constructor.call(this,this,this.el);
+		this.activate();
 	}
 });
