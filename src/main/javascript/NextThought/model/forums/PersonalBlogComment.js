aaca01b1cdf5ec49fdca222210d0d9a55ce1dee7
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-03-08 15:03:42 +0000

    persist should be a boolean
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@16851 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/model/forums/PersonalBlogComment.js
+++ src/main/javascript/NextThought/model/forums/PersonalBlogComment.js
@@ -2,6 +2,6 @@ Ext.define('NextThought.model.forums.PersonalBlogComment', {
 	extend: 'NextThought.model.forums.Post',
 
 	fields: [
-		{ name: 'Deleted', type: 'boolean', persist: 'False'}
+		{ name: 'Deleted', type: 'boolean', persist: false}
 	]
 });
