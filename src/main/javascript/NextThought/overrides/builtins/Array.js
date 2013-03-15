990f63efe050cc7ed163f85e581d9b820ae6e2d9
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-03-15 03:14:53 +0000

    monkey patch in Array.prototype.reduce for IE9 so i can use it later.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@17165 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/overrides/builtins/Array.js
+++ src/main/javascript/NextThought/overrides/builtins/Array.js
@@ -1,6 +1,46 @@
 Ext.define('NextThought.overrides.builtins.Array',{
 
 },function(){
+
+	/** @see https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/Reduce#Compatibility
+	 *
+	 * @param callback
+	 * @param [initialValue]
+	 * @returns {*}
+	 */
+	function reduce(callback, initialValue){
+		'use strict';
+		if (null === this || 'undefined' === typeof this) {
+			// At the moment all modern browsers, that support strict mode, have
+			// native implementation of Array.prototype.reduce. For instance, IE8
+			// does not support strict mode, so this check is actually useless.
+			throw new TypeError(
+					'Array.prototype.reduce called on null or undefined');
+		}
+		if ('function' !== typeof callback) {
+			throw new TypeError(callback + ' is not a function');
+		}
+		var index = 0, length = this.length >>> 0, value, isValueSet = false;
+		if (1 < arguments.length) {
+			value = initialValue;
+			isValueSet = true;
+		}
+		for ( ; length > index; ++index) {
+			if (!this.hasOwnProperty(index)) continue;
+			if (isValueSet) {
+				value = callback(value, this[index], index, this);
+			} else {
+				value = this[index];
+				isValueSet = true;
+			}
+		}
+		if (!isValueSet) {
+			throw new TypeError('Reduce of empty array with no initial value');
+		}
+		return value;
+	}
+
+
 	(function(o,a){
 		Ext.Object.each(a,function(k,v){
 			if(!o[k]){
@@ -13,8 +53,7 @@ Ext.define('NextThought.overrides.builtins.Array',{
 	}(Array.prototype,{
 		first: function first(){ return this[0]; },
 		last: function last(){ return this[this.length-1]; },
-		peek: function peek(){ return this[this.length-1]; }
+		peek: function peek(){ return this[this.length-1]; },
+		reduce: reduce
 	}));
-
-
 });
