c4a3b4676f1958aaf623025a20369f38261285dd
Chris Utz <chris.utz@nextthought.com>
2013-03-18 19:37:30 +0000

    Remove two tests that don't actually test valid behaviour anymore.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@17268 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/javascript/specs/cache/UserRepository.spec.js
+++ src/test/javascript/specs/cache/UserRepository.spec.js
@@ -1,21 +1,3 @@
 describe("User Repository/Store/Cache Behavior", function(){
 
-	it("should use what is given", function()
-	{
-		spyOn(UserRepository, 'makeRequest');
-		UserRepository.getUser({"Class": 'Community', Username: 'TestersAnnon'});
-
-		//this used to check to make sure communities don't get requested, we no longer resolve
-		//users for friends lists so we don't need the workaround to not resolve communities.
-		expect(UserRepository.makeRequest).not.toHaveBeenCalled();
-
-		UserRepository.getUser('jonathan.grimes@foo');
-		expect(UserRepository.makeRequest).toHaveBeenCalled();
-	});
-
-
-	it("should maintain user/communty model instances", function()
-	{
-		expect(UserRepository.getStore().getCount()).toBe(4);//The configured test user, the Communities 'TestersAnnon', 'NextThought', and 'Everyone'
-	});
 });
