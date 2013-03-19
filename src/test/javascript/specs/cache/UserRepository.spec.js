1efdb12956b5b443c09d20fc69dbca266114fb0c
Chris Utz <chris.utz@nextthought.com>
2013-03-19 06:07:35 +0000

    more UserRepository tests.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@17286 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/javascript/specs/cache/UserRepository.spec.js
+++ src/test/javascript/specs/cache/UserRepository.spec.js
@@ -5,6 +5,7 @@ describe("User Repository/Store/Cache Behavior", function(){
 		TUR = {};
 		/*jslint sub:true */ //no way to ignore reserved property if using don notation
 		TUR['__proto__'] = NextThought.cache.UserRepository['__proto__'];
+		TUR.constructor();
 	});
 
 
@@ -23,7 +24,7 @@ describe("User Repository/Store/Cache Behavior", function(){
 
 	it('Owns a store', function(){
 		var s;
-		expect(TUR.store).toBeFalsy();
+		expect(TUR.store).toBeNull();
 
 		s = TUR.getStore();
 		expect(s).toBeTruthy();
@@ -59,6 +60,156 @@ describe("User Repository/Store/Cache Behavior", function(){
 			expect(hans.fireEvent).not.toHaveBeenCalled();
 			expect(hans.get('Presence')).toBe('Offline');
 		})
+	});
+
+	describe('makeRequest', function(){
+
+		var mockAjax, realAjax;
+
+		beforeEach(function(){
+			realAjax = Ext.Ajax;
+			mockAjax = {
+				request: function(cfg){
+					var r = {};
+					r.options = {callback: cfg.callback, scope: cfg.scope};
+					r.finish = function(object){
+						var cb = this.options.callback,
+							resp = {};
+
+						resp.responseText = JSON.stringify(object);
+						Ext.callback(cb, this.options.scope, [{}, true, resp]);
+					}
+					r.fail = function(){
+						var cb = this.options.callback;
+						Ext.callback(cb, this.options.scope, [{}, false, null]);
+					}
+					return r;
+				}
+			};
+
+			Ext.Ajax = mockAjax;
+
+			spyOn(mockAjax, 'request').andCallThrough();
+		});
+
+		afterEach(function(){
+			Ext.Ajax = realAjax;
+		});
+
+		function setupRequest(user){
+			var callbacks = jasmine.createSpyObj('callbacks', ['failure', 'success']),
+				req;
+			TUR.makeRequest('harry', callbacks);
+
+			expect(mockAjax.request).toHaveBeenCalled();
+
+			req = TUR.activeRequests['harry'];
+
+			expect(req).toBeTruthy();
+
+			return {request: req, callbacks: callbacks};
+		}
+
+		function responseForItems(items){
+			if(!Ext.isArray(items)){
+				items = [items];
+			}
+
+			items = Ext.Array.map(items, function(i){
+				return i.data || i;
+			});
+
+			return {Items: items};
+		}
+
+		it('Failure callback will be called', function(){
+			var req = setupRequest('harry');
+			req.request.fail();
+
+			expect(req.callbacks.failure).toHaveBeenCalled();
+			expect(req.callbacks.success).not.toHaveBeenCalled();
+			expect(TUR.activeRequests['harry']).toBeUndefined();
+		});
+
+		it('Calls failure callback if user doesnt resolve', function(){
+			var req = setupRequest('harry');
+			req.request.finish(responseForItems([]));
+
+			expect(req.callbacks.failure).toHaveBeenCalled();
+			expect(req.callbacks.success).not.toHaveBeenCalled();
+			expect(TUR.activeRequests['harry']).toBeUndefined();
+		});
+
+		it('Returns non summary object', function(){
+			var r = setupRequest('harry'),
+				user = createUser('harry'),
+				scb = r.callbacks.success,
+				result;
+
+			expect(user.summaryObject).toBeTruthy();
+
+			r.request.finish(responseForItems(user));
+
+			expect(r.callbacks.failure).not.toHaveBeenCalled();
+			expect(scb).toHaveBeenCalled();
+			expect(TUR.activeRequests['harry']).toBeUndefined();
+
+			result = scb.mostRecentCall.args[0];
+			expect(result.get('Username')).toBe('harry');
+			expect(result.summaryObject).toBeFalsy();
+
+		});
+
+		it('Gracefully handles multiple users being returned', function(){
+			var r = setupRequest('harry'),
+				user1 = createUser('harry'),
+				user2 = createUser('harry'),
+				scb = r.callbacks.success,
+				result;
+
+
+			r.request.finish(responseForItems([user1, user2]));
+
+			expect(r.callbacks.failure).not.toHaveBeenCalled();
+			expect(scb).toHaveBeenCalled();
+			expect(TUR.activeRequests['harry']).toBeUndefined();
+
+			result = scb.mostRecentCall.args[0];
+			expect(result.get('Username')).toBe('harry');
+		});
+
+		it('Wont make requests if in flight', function(){
+			var r = setupRequest('harry'),
+				firstCbs = r.callbacks,
+				user = createUser('harry'),
+				scb = firstCbs.success,
+				secondCbs = jasmine.createSpyObj('secondCbs', ['success', 'failure']),
+				scbTwo = secondCbs.success;
+
+			expect(mockAjax.request.calls.length).toBe(1);
+
+			TUR.makeRequest('harry', secondCbs);
+
+			expect(mockAjax.request.calls.length).toBe(1);
+
+			r.request.finish(responseForItems(user));
+
+			expect(firstCbs.failure).not.toHaveBeenCalled();
+			expect(secondCbs.failure).not.toHaveBeenCalled();
+
+			expect(scb).toHaveBeenCalled();
+			expect(scbTwo).toHaveBeenCalled();
+
+			expect(TUR.activeRequests['harry']).toBeUndefined();
+
+			result = scb.mostRecentCall.args[0];
+			expect(result.get('Username')).toBe('harry');
+			expect(result.summaryObject).toBeFalsy();
+
+			result = scbTwo.mostRecentCall.args[0];
+			expect(result.get('Username')).toBe('harry');
+			expect(result.summaryObject).toBeFalsy();
+		});
 	});
 
 });
