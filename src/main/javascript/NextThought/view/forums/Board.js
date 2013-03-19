a2b43886396cba16f29bb56e40f4a9ac130443d6
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-03-19 16:42:08 +0000

    add links to Aaron's design
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@17298 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/forums/Board.js
+++ src/main/javascript/NextThought/view/forums/Board.js
@@ -1,5 +1,7 @@
 Ext.define('NextThought.view.forums.Board',{
 	/**
+	 * https://docs.google.com/a/nextthought.com/presentation/d/18qyM3011F_AXjwAPGpE-94DPKmuPPPnKQ0EepyAoXmQ/edit#slide=id.g9418ebe4_053
+	 *
 	 * I'm not sure there will be a visual component mapping to a list of boards. I think we will simply list the
 	 * contents of all viewable boards as a flat list of Forums. So this container will be nearly symbolic to its
 	 * relation to the models.
--- src/main/javascript/NextThought/view/forums/Forum.js
+++ src/main/javascript/NextThought/view/forums/Forum.js
@@ -1,5 +1,7 @@
 Ext.define('NextThought.view.forums.Forum',{
 	/**
+	 * https://docs.google.com/a/nextthought.com/presentation/d/18qyM3011F_AXjwAPGpE-94DPKmuPPPnKQ0EepyAoXmQ/edit#slide=id.gb09172a6_02
+	 *
 	 * This will list the contents of the forum. Listing the Topics below it.
 	 *
 	 * When a user selects one we will add that view (Topic) onto the stack, suppressing this one. (The stack and impl
