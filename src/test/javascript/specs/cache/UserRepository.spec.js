c231ea54a12afc5f4460834e052e3363ab23b9c6
Chris Utz <chris.utz@nextthought.com>
2013-03-19 02:57:45 +0000

    Start testing UserRepository
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@17276 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/javascript/specs/cache/UserRepository.spec.js
+++ src/test/javascript/specs/cache/UserRepository.spec.js
@@ -1,3 +1,61 @@
 describe("User Repository/Store/Cache Behavior", function(){
+	var TUR;
+
+	beforeEach(function(){
+		TUR = {};
+		/*jslint sub:true */ //no way to ignore reserved property if using don notation
+		TUR['__proto__'] = NextThought.cache.UserRepository['__proto__'];
+	});
+
+
+	function createUser(username, additional){
+		var cfg = Ext.applyIf(additional || {}, {
+			'Username': username
+		});
+
+		return new NextThought.model.User(cfg);
+	}
+
+
+	it('Defines UserRepository', function(){
+		expect(UserRepository).toBeTruthy();
+	});
+
+	it('Owns a store', function(){
+		var s;
+		expect(TUR.store).toBeFalsy();
+
+		s = TUR.getStore();
+		expect(s).toBeTruthy();
+
+		expect(TUR.getStore()).toBe(s);
+	});
+
+	describe('Presence handling', function(){
+		var hans;
+
+		beforeEach(function(){
+			hans = createUser('hans', {Presence: 'Online'});
+			spyOn(hans, 'fireEvent');
+
+			TUR.cacheUser(hans);
+		});
+
+		it('Updates hans', function(){
+			TUR.presenceChanged('hans', 'Online');
+
+			expect(hans.fireEvent.calls.length).toBe(1);
+			expect(hans.fireEvent).toHaveBeenCalledWith('changed', hans);
+
+			expect(hans.get('Presence')).toBe('Online');
+		});
+
+		it('Survives missing user', function(){
+			TUR.presenceChanged('bruce', 'away');
+
+			expect(hans.fireEvent).not.toHaveBeenCalled();
+			expect(hans.get('Presence')).toBe('Online');
+		})
+	});
 
 });
