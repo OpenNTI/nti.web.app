32795f57bc4e21f4cc221df56c8ff3f762cd65af
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-03-20 17:59:54 +0000

    Move class comments to top of files
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@17350 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/forums/Forum.js
+++ src/main/javascript/NextThought/view/forums/Forum.js
@@ -1,3 +1,11 @@
+/**
+ * https://docs.google.com/a/nextthought.com/presentation/d/18qyM3011F_AXjwAPGpE-94DPKmuPPPnKQ0EepyAoXmQ/edit#slide=id.gb09172a6_02
+ *
+ * This will list the contents of the forum. Listing the Topics below it.
+ *
+ * When a user selects one we will add that view (Topic) onto the stack, suppressing this one. (The stack and impl
+ * to be handled in the controller)
+ */
 Ext.define('NextThought.view.forums.Forum',{
 	extend: 'Ext.view.View',
 	alias: ['widget.forums-forum','widget.forums-topic-list'],
@@ -9,13 +17,4 @@ Ext.define('NextThought.view.forums.Forum',{
 			{ cls: 'item', html: '{title}'}
 		]
 	})
-
-	/**
-	 * https://docs.google.com/a/nextthought.com/presentation/d/18qyM3011F_AXjwAPGpE-94DPKmuPPPnKQ0EepyAoXmQ/edit#slide=id.gb09172a6_02
-	 *
-	 * This will list the contents of the forum. Listing the Topics below it.
-	 *
-	 * When a user selects one we will add that view (Topic) onto the stack, suppressing this one. (The stack and impl
-     * to be handled in the controller)
-	 */
 });
--- src/main/javascript/NextThought/view/forums/Topic.js
+++ src/main/javascript/NextThought/view/forums/Topic.js
@@ -1,9 +1,9 @@
+/**
+ * https://docs.google.com/a/nextthought.com/presentation/d/18qyM3011F_AXjwAPGpE-94DPKmuPPPnKQ0EepyAoXmQ/edit#slide=id.gb09172a6_09
+ *
+ * This will be, for the most part (from Aaron's design), HeadlineTopics. Much like PersonalBlogEntry(s).
+ *
+ * It will define the headline post and contain followup posts.
+ */
 Ext.define('NextThought.view.forums.Topic',{
-	/**
-	 * https://docs.google.com/a/nextthought.com/presentation/d/18qyM3011F_AXjwAPGpE-94DPKmuPPPnKQ0EepyAoXmQ/edit#slide=id.gb09172a6_09
-	 *
-	 * This will be, for the most part (from Aaron's design), HeadlineTopics. Much like PersonalBlogEntry(s).
-	 *
-	 * It will define the headline post and contain followup posts.
-	 */
 });
