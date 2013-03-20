8a66c49ecdbf78bf31fa7daf119748a06a26f174
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-03-20 17:59:47 +0000

    remove db() function. Move class comment to top of file
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@17349 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/forums/Board.js
+++ src/main/javascript/NextThought/view/forums/Board.js
@@ -1,4 +1,15 @@
-function db(){debugger;}
+/**
+ * https://docs.google.com/a/nextthought.com/presentation/d/18qyM3011F_AXjwAPGpE-94DPKmuPPPnKQ0EepyAoXmQ/edit#slide=id.g9418ebe4_053
+ *
+ * I'm not sure there will be a visual component mapping to a list of boards. I think we will simply list the
+ * contents of all viewable boards as a flat list of Forums. So this container will be nearly symbolic to its
+ * relation to the models.
+ *
+ * This will contain a flattened list of Forums.
+ *
+ * When a user selects one we will add that view (Forum) onto the stack, suppressing this one. (The stack and impl
+ * to be handled in the controller)
+ */
 Ext.define('NextThought.view.forums.Board',{
 	extend: 'Ext.view.View',
 	alias: ['widget.forums-board','widget.forums-forum-list'],
@@ -26,19 +37,4 @@ Ext.define('NextThought.view.forums.Board',{
 			]}
 		]
 	})
-
-	/**
-	 * https://docs.google.com/a/nextthought.com/presentation/d/18qyM3011F_AXjwAPGpE-94DPKmuPPPnKQ0EepyAoXmQ/edit#slide=id.g9418ebe4_053
-	 *
-	 * I'm not sure there will be a visual component mapping to a list of boards. I think we will simply list the
-	 * contents of all viewable boards as a flat list of Forums. So this container will be nearly symbolic to its
-	 * relation to the models.
-	 *
-	 * This will contain a flattened list of Forums.
-	 *
-	 * When a user selects one we will add that view (Forum) onto the stack, suppressing this one. (The stack and impl
-	 * to be handled in the controller)
-	 */
-
-
 });
