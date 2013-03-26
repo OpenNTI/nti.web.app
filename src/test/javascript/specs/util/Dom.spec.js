b72396b50a28226d08ed7c02085b8e8fb93dbfb5
Andrew Ligon <andrew.ligon@nextthought.com>
2013-03-26 21:00:02 +0000

    Basic tests for the Dom util
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@17591 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/javascript/specs/util/Dom.spec.js
+++ src/test/javascript/specs/util/Dom.spec.js
@@ -2,37 +2,74 @@ describe("Dom.js Tests",function(){
 	var testBody, DomUtil;
 	
 	beforeEach(function(){
-		DomUtil = NTITestUtils.newInstanceOfSingleton(NextThought.util.dom);
+		DomUtil = NTITestUtils.newInstanceOfSingleton(NextThought.util.Dom);
 		testBody = document.createElement('div');
 		document.body.appendChild(testBody);
 	});
 
 	afterEach(function(){
-		document.remove(testBody);
+		document.body.removeChild(testBody);
 	});
 
-	describe("getVideosFromDom",function(){
-		var result, videoObject = {
+	it("getVideosFromDom",function(){
+		var result, videoObject = [{
 			'url' : 'www.youtube.com',
 			'thumbnail' : 'image.jpeg'
-		};
-		videoContainer = document.createElement('object');
-		videoContainer.className("videoContainer");
-		video = document.createElement('object');
-		video.className('naqvideo');
-		firstParam = document.createElement('param');
+		}],
+		videoContainer = document.createElement('object'),
+		video = document.createElement('object'),
+		firstParam = document.createElement('param'),
+		secondParam = document.createElement('param');
+		
+		videoContainer.className = "videoContainer";
+		video.className = 'naqvideo';
 		firstParam.name = 'url';
 		firstParam.value = 'www.youtube.com';
-		secondParam = document.createElement('param');
 		secondParam.name = 'thumbnail';
 		secondParam.value = 'image.jpeg';
 
+		
+
 		video.appendChild(firstParam);
 		video.appendChild(secondParam);
-		videoContainer.appendChild(video); 
+		videoContainer.appendChild(video);
+		testBody.appendChild(videoContainer); 
 
-		result = DomUtil.getVideoFromDom(videoContainer);
-
-		expect(result).toBe(videoObject);
+		result = DomUtil.getVideosFromDom(videoContainer);
+		expect(result).toEqual(videoObject);
 	});
-})
+
+	it("getImagesFromDom",function(){
+		var result, imgObject =[{
+				url : 'full.png'
+			}],
+			imgContainer = document.createElement('span');
+			img = document.createElement('img');
+
+			img.setAttribute('data-nti-image-full','full.png');
+			img.setAttribute('data-nti-image-half','half.png');
+			img.setAttribute('data-nti-image-quarter','quarter.png');
+			img.setAttribute('data-nti-image-size','full');
+			img.setAttribute('src','half.png');
+
+			imgContainer.appendChild(img);
+			testBody.appendChild(imgContainer);
+
+			result = DomUtil.getImagesFromDom(imgContainer);
+			expect(result).toEqual(imgObject);
+
+	});
+
+	it("adjustLinks",function(){
+		var linkContainer = document.createElement('div'),
+			link = document.createElement('a');
+
+			link.setAttribute('href','www.google.com');
+
+			linkContainer.appendChild(link);
+			testBody.appendChild(linkContainer);
+
+			DomUtil.adjustLinks(document.body,"www.facebook.com");
+			expect(link.target).toBe("_blank");
+	});
+});
