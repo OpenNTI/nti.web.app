29eb65926f8492b6a1b7eb3155293f7c100debe1
Chris Utz <chris.utz@nextthought.com>
2013-03-31 21:59:06 +0000

    Start testing we actually create the search components we expect.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@17799 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/javascript/specs/controller/Search.spec.js
+++ src/test/javascript/specs/controller/Search.spec.js
@@ -17,4 +17,40 @@ describe('Search Controller Tests', function(){
 			expect(controller.mimeToXType('application/vnd.nextthought.forums.communityheadlinetopic')).toBe('search-result-forums-communityheadlinetopic');
 		});
 	});
+
+	describe('component for hit', function(){
+
+		function createHit(mime){
+			return NextThought.model.Hit.create({'MimeType': 'application/vnd.nextthought.'+mime});
+		}
+
+		function componentFromCfg(cfg){
+			cfg = Ext.apply(cfg, {fillInData: Ext.emptyFn});
+			return Ext.ComponentManager.create(cfg);
+		}
+
+		it('book, content, and highlights all get the old search result', function(){
+			var hits = [createHit('note'), createHit('highlight'), createHit('bookcontent')];
+
+			Ext.Array.each(hits, function(hit){
+				var cmp = componentFromCfg(controller.componentConfigForHit(hit));
+
+				expect(cmp).toBeTruthy();
+				expect(cmp.$className).toBe('NextThought.view.menus.search.Result');
+				cmp.destroy();
+			});
+		});
+
+		xit('chat gets it\'s own component', function(){
+
+		});
+
+		xit('Topic and topic comments share a component', function(){
+
+		});
+
+		xit('Blogs and blog comment share a component', function(){
+
+		});
+	});
 });
