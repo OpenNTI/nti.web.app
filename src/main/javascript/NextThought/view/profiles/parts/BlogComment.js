6b2043d088fb8aec5d894bc225d5c6a1795836b1
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-04-01 17:04:48 +0000

    blog comments are not bookmarkable (per design)
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@17807 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/profiles/parts/BlogComment.js
+++ src/main/javascript/NextThought/view/profiles/parts/BlogComment.js
@@ -15,7 +15,7 @@ Ext.define('NextThought.view.profiles.parts.BlogComment',{
 	ui: 'forum-comment',
 
 	renderTpl: Ext.DomHelper.markup([
-		{ cls: 'controls', cn:[{cls:'favorite'},{cls:'like'}]},
+		{ cls: 'controls', cn:[{cls:'favorite-spacer'},{cls:'like'}]},
 		{ cls: 'avatar', style: { backgroundImage: 'url({avatarURL});'}},
 		{ cls: 'wrap', 'data-commentid':'{ID}', cn:[
 			{ cls: 'meta', cn: [
@@ -43,7 +43,6 @@ Ext.define('NextThought.view.profiles.parts.BlogComment',{
 		avatarEl: '.avatar',
 		ctrlEl: '.controls',
 		liked: '.controls .like',
-		favorites: '.controls .favorite',
 		editEl: '.foot .edit',
 		deleteEl: '.foot .delete',
 		flagEl:'.foot .flag',
