5fa6d81ac82cd4a974ca3f3303604153bc53a4a8
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-04-08 20:22:10 +0000

    take out my temp debug stuff.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@18013 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/account/activity/Popout.js
+++ src/main/javascript/NextThought/view/account/activity/Popout.js
@@ -74,9 +74,7 @@ Ext.define('NextThought.view.account.activity.Popout',{
 		me.callParent(arguments);
 		me.mon(me.el,'click',function(e){e.stopPropagation();},me);
 
-		if(!$AppConfig.debug){
-			me.on('blur',me.destroy,me);
-		}
+		me.on('blur',me.destroy,me);
 
 		Ext.defer(function(){
 			me.mon(me.el.up('body'),{
