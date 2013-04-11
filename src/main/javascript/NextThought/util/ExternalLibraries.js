7baceea4377552be88fcb8d039c533f763929516
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-04-11 22:52:50 +0000

    lets "block" until external libraries are loaded.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@18163 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/util/ExternalLibraries.js
+++ src/main/javascript/NextThought/util/ExternalLibraries.js
@@ -1,32 +1,65 @@
-Ext.define('NextThought.util.ExternalLibraries',{
-	singleton: true,
-	loadExternalLibraries: function (libs){
-		if(window.libsLoaded){return;}
+/*
+ * WARNING: DO NOT mimic this file.  This is a very very special pattern where we are taking advantage Ext's
+ * classloader.
+ *
+ * We are using the fact that the framework will not call the application's "launch" function until all requires are
+ * satisfied.  So we are using the define as a make-shift callback/event emitter.
+ *
+ * Once all external libraries are loaded we define the class...which then lets the Ext.Loader finish dependency loads.
+ *
+ * this expects a libs.js in jsonp format. The function name is "loadExternalLibraries".
+ *
+ * The libraries arg should be an array of objects.
+ * Each object will have a single key (the library name) and a value.
+ *
+ * The Value can be either a url string or an object, where the url is assigned to the url key, and a waitFor may be defined (it must define a symbole in the global space to wait for it to exist) then a callback can be defined on the key "cb".
+ */
+
+(function(global){
+
+	function finish(){ Ext.define('NextThought.util.ExternalLibraries',{}); }
+
+	function loadExternalLibraries(libs){
+		var rec = [];
+
+		function maybeFinish(name){
+			if(name){ delete rec[name]; }
+			if(Object.keys(rec).length===0){
+				finish();
+			}
+		}
+
+		if(global.libsLoaded){return;}
 		Ext.each(libs, function(o){
 			Ext.Object.each(o,function waiting(name,v){
 				var cb = v && v.cb,
 					url = (v && v.url) || v,
 					waitFor = (v && v.waitFor) || false;
 
-				if(waitFor && !window[waitFor]){
+				rec[name] = true;
+
+				if(waitFor && !global[waitFor]){
 					Ext.defer(waiting,10,this,[name,v]);
 					return;
 				}
 
 				Ext.Loader.loadScript({
 					url: url,
-					onError:function(){console.error(name+' failed to load');},
-					onLoad:function(){ Ext.callback(cb); }
+					onError:function(){maybeFinish(name); console.error(name+' failed to load');},
+					onLoad:function(){ maybeFinish(name); Ext.callback(cb); /*console.debug(name+' finshed loading');*/ }
 				});
 			});
 		});
-		window.libsLoaded = true;
+		maybeFinish();//if no libraries are defined, lets go ahead and finish.
+		global.libsLoaded = true;
 	}
 
-
-},function(){
-	window.loadExternalLibraries = this.loadExternalLibraries;
-	if(!window.libsLoaded){
+	if(!global.libsLoaded){
+		global.loadExternalLibraries = loadExternalLibraries;
 		Ext.Loader.loadScript('javascript/libs.js');
 	}
-});
+	else {
+		finish();
+	}
+
+}(window));
