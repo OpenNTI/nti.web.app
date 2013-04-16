59bace11162a3285dd8a59039e49c4f541ca9634
Chris Utz <chris.utz@nextthought.com>
2013-04-16 15:54:23 +0000

    Basic purchase window ( no good view but it provides the container).  Closing logic and an event to show the purchasable window.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@18269 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/controller/Store.js
+++ src/main/javascript/NextThought/controller/Store.js
@@ -11,15 +11,28 @@ Ext.define('NextThought.controller.Store', {
 	],
 
 	views: [
+		'NextThought.view.store.PurchaseWindow'
 	],
 
 	refs: [
-		{ ref: 'navigationMenu', selector: 'navigation-menu'}
+		{ ref: 'navigationMenu', selector: 'navigation-menu'},
+		{ ref: 'purchaseWindow', selector: 'purchase-window'}
 	],
 
 	init: function(){
 		this.callParent(arguments);
 		this.application.on('session-ready', this.onSessionReady, this);
+
+		this.listen({
+			component:{
+				'purchase-window button[action=cancel]': {
+					'click': this.purchaseWindowCancel
+				},
+				'purchase-window' : {
+					'beforeclose': this.maybeClosePurchaseWindow
+				}
+			}
+		});
 	},
 
 	onSessionReady: function(){
@@ -36,10 +49,21 @@ Ext.define('NextThought.controller.Store', {
 	/**
 	 * Show the detail/purchase view for the given purchasable
 	 *
-	 * @param purchasable The NTIID or Purchasable object to show
+	 * @param purchasable The Purchasable object to show
 	 */
 	showPurchasable: function(purchasable){
+		var win;
 
+		//If we are currently showing a purchase window
+		//don't show another one, we shouldn't be able to get into this state
+		if(this.getPurchaseWindow()){
+			console.error('Purhcase already in progress.  How did you manage this', win);
+			return null;
+		}
+
+		win = NextThought.view.store.PurchaseWindow.create({purchasable: purchasable});
+		win.show();
+		return win;
 	},
 
 
@@ -48,12 +72,12 @@ Ext.define('NextThought.controller.Store', {
 	 * Validates the given coupone code for the provided purchasable item.
 	 *
 	 * @param cmp The owner cmp
-	 * @param purchasable The Purchasable object to validate the coupon code with
+	 * @param purchaseDesc an object containing the Purchasable,
 	 * @param coupon The coupon code to validate
 	 * @param success The success callback called if the provided coupone is valid
 	 * @param failure The failure callback called if we are unable to validate the coupon for any reason
 	 */
-	validateCouponCode: function(cmp, purchasable, coupon, success, failure){
+	validateCouponCode: function(cmp, purchaseDesc, coupon, success, failure){
 
 	},
 
@@ -72,7 +96,7 @@ Ext.define('NextThought.controller.Store', {
 	/**
 	 *
 	 * @param cmp the owner cmp
-	 * @param purchaseDesc an object containing the Purchasable, Quantity and card information
+	 * @param purchaseDesc an object containing the Purchasable, Quantity and PaymentInfo
 	 * @param token a stripe payment token from createPurchase
 	 * @param success the callback called when the purchase has completed succesfully
 	 * @param failure the callback for error conditions
@@ -84,9 +108,39 @@ Ext.define('NextThought.controller.Store', {
 	/**
 	 * Handler for canceling an in progress purchase.
 	 *
-	 * @param cmp the owner cmp
+	 * @param btn the button triggering the cancel
 	 */
-	cancelPurchase: function(cmp){
 
+	purchaseWindowCancel: function(btn){
+		btn.up('window').close();
+	},
+
+	maybeClosePurchaseWindow: function(win){
+		//Detect this by looking at what stage in the process we are in.
+		//I.E its safe to close from the detail view, but not once we have started
+		//a purchase process
+		var destructive = true;
+
+		if(!win.forceClosing && destructive){
+
+			/*jslint bitwise: false*/ //Tell JSLint to ignore bitwise opperations
+			Ext.Msg.show({
+				msg: 'This will cancel your current purchase.',
+				buttons: Ext.MessageBox.OK | Ext.MessageBox.CANCEL,
+				icon: 'warning-red',
+				buttonText: {'ok': 'caution:Cancel my purchase'},
+				title: 'Are you sure?',
+				fn: function(str){
+					if(str === 'ok'){
+						win.forceClosing = true;
+						win.close();
+						delete win.forceClosing;
+					}
+				}
+			});
+
+			return false;
+		}
+		return true;
 	}
 });
---
+++ src/main/javascript/NextThought/view/store/PurchaseWindow.js
@@ -0,0 +1,55 @@
+Ext.define('NextThought.view.store.PurchaseWindow', {
+	extend: 'NextThought.view.Window',
+	alias: 'widget.purchase-window',
+
+	cls:'purchase-window',
+	width: 695,
+	height: 640,
+	layout: 'fit',
+	modal: true,
+	header: true,
+	dockedItems:{
+		xtype: 'container',
+		dock:'bottom',
+		ui: 'footer',
+		height:55,
+		baseCls: 'nti-window',
+		layout: {
+			type: 'hbox',
+			align: 'stretchmax'
+		},
+		defaults:{
+			cls: 'footer-region',
+			xtype: 'container',
+			flex: 1,
+			layout: 'hbox'
+		},
+		items:[{
+			layout: {type:'hbox', pack:'end'},
+			defaults: { xtype:'button', ui:'blue', scale:'large'},
+			items:[
+				{text: 'Cancel',  action: 'cancel', ui: 'secondary' },
+				{text: 'Redeem Code', cls:'.x-btn-blue-large', action: 'redeem'},
+				{text: 'Buy!', cls:'.x-btn-blue-large', action: 'buy'}
+			]
+		}]
+	},
+
+	initComponent: function(){
+		this.callParent(arguments);
+		this.on('show', this.addCustomMask, this);
+		this.on('close', this.removeCustomMask, this);
+	},
+
+	addCustomMask: function(){
+		var mask = this.zIndexManager.mask;
+		mask.addCls('nti-black-clear');
+	},
+
+	removeCustomMask: function(){
+		var mask = this.zIndexManager.mask;
+		if(mask){
+			mask.removeCls('nti-black-clear');
+		}
+	}
+});
