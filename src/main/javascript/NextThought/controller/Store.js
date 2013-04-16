50696b374e4d3746aa00ed33e67fce3430eb46ad
Chris Utz <chris.utz@nextthought.com>
2013-04-16 05:44:41 +0000

    Stub some handlers in the controller that will likely be needed.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@18264 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/controller/Store.js
+++ src/main/javascript/NextThought/controller/Store.js
@@ -28,5 +28,62 @@ Ext.define('NextThought.controller.Store', {
 		store.on('load', function(){ app.finishInitializeTask(token); }, this, {single: true});
 		store.proxy.url = $AppConfig.service.getPurchasableItemURL();
 		store.load();
+	},
+
+	/**
+	 * Show the detail/purchase view for the given purchasable
+	 *
+	 * @param purchasable The NTIID or Purchasable object to show
+	 */
+	showPurchasable: function(purchasable){
+
+	},
+
+
+	/**
+	 *
+	 * Validates the given coupone code for the provided purchasable item.
+	 *
+	 * @param cmp The owner cmp
+	 * @param purchasable The Purchasable object to validate the coupon code with
+	 * @param coupon The coupon code to validate
+	 * @param success The success callback called if the provided coupone is valid
+	 * @param failure The failure callback called if we are unable to validate the coupon for any reason
+	 */
+	validateCouponCode: function(cmp, purchasable, coupon, success, failure){
+
+	},
+
+	/**
+	 * Called to generate a stripe payment token from purchase information
+	 *
+	 * @param cmp the owner cmp
+	 * @param purchaseDesc an object containing the Purchasable, Quantity and card information
+	 * @param success The success callback called with the generated stripe token
+	 * @param failure The failure callback called if the token generation fails
+	 */
+	createPurchase: function(cmp, purchaseDesc, success, failure){
+
+	},
+
+	/**
+	 *
+	 * @param cmp the owner cmp
+	 * @param purchaseDesc an object containing the Purchasable, Quantity and card information
+	 * @param token a stripe payment token from createPurchase
+	 * @param success the callback called when the purchase has completed succesfully
+	 * @param failure the callback for error conditions
+	 */
+	submitPurchase: function(cmp, purchaseDesc, token, success, failure){
+
+	},
+
+	/**
+	 * Handler for canceling an in progress purchase.
+	 *
+	 * @param cmp the owner cmp
+	 */
+	cancelPurchase: function(cmp){
+
 	}
 });
