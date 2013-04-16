7967bd03c4e5f5d1c7b7498ba4211895f6b99b36
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-04-16 16:55:20 +0000

    make purchasable items re-clickable.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@18277 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/store/menus/Collection.js
+++ src/main/javascript/NextThought/view/store/menus/Collection.js
@@ -2,6 +2,14 @@ Ext.define('NextThought.view.store.menus.Collection',{
 	extend: 'NextThought.view.menus.navigation.Collection',
 	alias: 'widget.purchasable-collection',
 
+	listeners: {
+		select: function(selModel,record){
+			//allow reselect since we don't style the selected state, this has no
+			// visual effect other than the ability to click on it again
+			selModel.deselect(record);
+		}
+	},
+
 	menuItemTpl: Ext.DomHelper.markup({
 		cls: 'stratum item', 'data-qtip': '{Title}', cn:[
 			{ tag:'img', src: Ext.BLANK_IMAGE_URL, cls:'bookcover', style: {
