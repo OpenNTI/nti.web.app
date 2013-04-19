540736939cf9a38550e53c96b4d24324bf5624a6
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-04-19 00:20:49 +0000

    lets see if this helps.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@18422 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/annotations/note/Panel.js
+++ src/main/javascript/NextThought/view/annotations/note/Panel.js
@@ -16,6 +16,7 @@ Ext.define('NextThought.view.annotations.note.Panel',{
 
 	ui: 'nt',
 	cls: 'note-container',
+	componentLayout: 'autocomponent',
 	layout: 'auto',
 	defaultType: 'note-panel',
 	childEls: ['body'],
