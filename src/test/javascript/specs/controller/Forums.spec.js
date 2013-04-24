9bf9b57c8a3ebc5d1977d32634de1b5d061d50fd
Sean Jones <sean.jones@nextthought.com>
2013-04-24 21:04:56 +0000

    Fix and improve Forum navigation unit tests.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@18622 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/javascript/specs/controller/Forums.spec.js
+++ src/test/javascript/specs/controller/Forums.spec.js
@@ -1,9 +1,12 @@
 describe('Forums Controller Tests', function(){
 
-	var controller, mockObjects;
+	var controller, mockObjects, navController, oldViewPort;
 
 	beforeEach(function(){
 		controller = app.getController('Forums');
+		navController = app.getController('Navigation');
+		oldViewPort = navController.viewport;
+		navController.viewport = { activateView: Ext.emptyFn};
 
 		mockObjects = {
 			'communityBoard1': Ext.create('NextThought.model.forums.CommunityBoard', {
@@ -12,21 +15,40 @@ describe('Forums Controller Tests', function(){
 
 			'communityTopic1': Ext.create('NextThought.model.forums.CommunityHeadlineTopic', {
 				NTIID: 'communityTopic1'
+			}),
+			'book1': Ext.create('NextThought.model.PageInfo', {
+				NTIID: 'book1'
 			})
 		};
 	});
 
-	xdescribe('Test navigateToForumContent', function(){
+	afterEach(function(){
+		navController.viewport = oldViewPort;
+	});
+
+	describe('Test navigateToForumContent', function(){
 		it('Expects going to the Board \'communityBoard1\'', function(){
 			spyOn(controller, 'navigateToForumContent').andCallThrough();
+			spyOn(controller, 'presentTopic');
 			controller.navigateToForumContent(mockObjects['communityBoard1']);
 			expect(controller.navigateToForumContent).toHaveBeenCalledWith(mockObjects['communityBoard1']);
+			expect(controller.presentTopic).toHaveBeenCalledWith(mockObjects['communityBoard1']);
 		});
 
 		it('Expects going to the Topic \'communityTopic1\'', function(){
 			spyOn(controller, 'navigateToForumContent').andCallThrough();
+			spyOn(controller, 'presentTopic');
 			controller.navigateToForumContent(mockObjects['communityTopic1']);
 			expect(controller.navigateToForumContent).toHaveBeenCalledWith(mockObjects['communityTopic1']);
+			expect(controller.presentTopic).toHaveBeenCalledWith(mockObjects['communityTopic1']);
+		});
+
+		it('Expects to not go to the content \'book1\'', function(){
+			spyOn(controller, 'navigateToForumContent').andCallThrough();
+			spyOn(controller, 'presentTopic');
+			controller.navigateToForumContent(mockObjects['book1']);
+			expect(controller.navigateToForumContent).toHaveBeenCalledWith(mockObjects['book1']);
+			expect(controller.presentTopic).not.toHaveBeenCalled();
 		});
 	});
 });
