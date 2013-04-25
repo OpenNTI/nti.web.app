8c35c9a47c63ea041002bb56e9f943783926f1e8
Chris Utz <chris.utz@nextthought.com>
2013-04-25 00:14:54 +0000

    Comment the approach for resolving meta info to be smarter about 403s.  Refactor a bit.  No changed behaviour yet.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@18637 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/cache/LocationMeta.js
+++ src/main/javascript/NextThought/cache/LocationMeta.js
@@ -21,50 +21,91 @@ Ext.define('NextThought.cache.LocationMeta', {
 			return;
 		}
 
-        this.loadMeta(ntiid, function(){
-            return  Ext.callback(callback, scope, [this.getValue(ntiid)]);
+        this.loadMeta(ntiid, function(meta){
+            return  Ext.callback(callback, scope, [meta]);
         });
     },
 
 
+	createAndCacheMeta: function(ntiid, pi, metaId){
+		var assessmentItems = [],
+			theId = metaId || pi.getId(),
+			meta = LocationProvider.getLocation(theId),
+			me = this;
+
+		function buildPath(s, root){
+			var p = s.split('/'); p.splice(-1,1,'');
+			p = p.join('/');
+			//trim off the root if its present
+			return p.replace(new RegExp(RegExp.escape(root)+'$'),'');
+		}
+
+		if(!meta){
+			return null;
+		}
+
+		if(!metaId){
+			assessmentItems = pi.get('AssessmentItems') || [];
+		}
+
+		this.meta[theId] = meta;
+		this.ids[ntiid] = theId;
+
+		meta.baseURI = buildPath(pi.getLink('content'),meta.root);
+		meta.absoluteContentRoot = meta.baseURI + meta.root;
+
+		//Also yank out any assessment items and cache them by id.  Note
+		//right now this only works because there is a one-to-one question to
+		//PageInfo mapping.  If I recall that is happening on the server now also
+		//but is probably temporary. IE mashups probably break this
+		Ext.each(assessmentItems, function(assessmentItem){
+			me.ids[assessmentItem.getId()] = theId;
+		});
+
+		return meta;
+	},
+
+	/**
+	 * Load meta data for the provided ntiid.  These should be content ids here.
+	 * We can get almost all the meta data we need out of the toc (which is nice for the sample content use case)
+	 * however, we need one pesky piece of data off the pageInfo.  We need the contentRoot so we know where icons and content
+	 * is actually rooted.  Luckily (at least right now) a book lives entirely in one place. I.E. the contentRoot of
+	 * the root element is currently the same as any of its descendant's contentRoot.  We rely on this assumption
+	 * to make resolving meta data for content you don't technically have access to (but have part of the book) work.
+	 *
+	 * We start by resolving the ntiid as a PageInfo.  If this succeeds snag meta out of the library, apply the content
+	 * root, cache, and return.  If the PageInfo request 403s it may be content we can get meta info out of the library
+	 * for, we just can't access the content.  So, see if we can find our ntiid in the library.  If we find it, use
+	 * that meta data but resolve the root PageInfo to get the contentRoot.  Note this relies on an assmuption that entire
+	 * units of content are served from the same place.  This assumption probably needs to go away but we seem dependent on it
+	 * now.  Because of this assumption these results should still be cachable in memory.
+	 * If resolving the PageInfo for the root fails we are SOL and have to return nothing.
+	 *
+	 * It is possible that we have an
+	 * assessment question id (or similar) that is in a book we can get meta data for, but we don't have an explicit listing
+	 * of it in the library.  We rely on some assumptions of how the question ids are generated to try and identify their
+	 * containing book.  If we find one that looks good, we return the meta for the root of the book.  Note in this case
+	 * we must not cache the result by the id we were initially looking up.
+	 *
+	 * @param ntiid the ntiid we want content metadata for
+	 * @param cb completion callback.  If meta can be determined it will be the first arg
+	 */
     loadMeta: function(ntiid, cb) {
 		var me = this;
 
-	    function buildPath(s, root){
-            var p = s.split('/'); p.splice(-1,1,'');
-		    p = p.join('/');
-			//trim off the root if its present
-		    return p.replace(new RegExp(RegExp.escape(root)+'$'),'');
-        }
-
         function pageIdLoaded(pi){
-			var assessmentItems = pi.get('AssessmentItems') || [],
-				theId = pi.getId(),
-				meta = LocationProvider.getLocation(theId);
-
+			var meta = this.createAndCacheMeta(ntiid, pi);
 			if(!meta){
 				fail.call(this);
-				return;
 			}
-
-            this.meta[theId] = meta;
-            this.ids[ntiid] = theId;
-
-	        meta.baseURI = buildPath(pi.getLink('content'),meta.root);
-			meta.absoluteContentRoot = meta.baseURI + meta.root;
-
-			//Also yank out any assessment items and cache them by id.  Note
-			//right now this only works because there is a one-to-one question to
-			//PageInfo mapping.  If I recall that is happening on the server now also
-			//but is probably temporary. IE mashups probably break this
-			Ext.each(assessmentItems, function(assessmentItem){
-				me.ids[assessmentItem.getId()] = theId;
-			});
-
-            Ext.callback(cb, this);
+            Ext.callback(cb, this, [meta]);
         }
 
-        function fail(){
+        function fail(req, resp){
+			if(resp && resp.status === 403){
+				console.log('Unauthorized when requesting page info', ntiid);
+				return;
+			}
             console.error('fail', arguments);
             Ext.callback(cb, this);
         }
