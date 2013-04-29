3aa41a718ba20016acc0b83edbd78cda59cf4dd1
Chris Utz <chris.utz@nextthought.com>
2013-04-29 23:03:57 +0000

    CompositeLite doesn't implement allowContextMenu so we have to iterate.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@18837 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/store/purchase/History.js
+++ src/main/javascript/NextThought/view/store/purchase/History.js
@@ -1,52 +1,58 @@
-Ext.define('NextThought.view.store.purchase.History',{
+Ext.define('NextThought.view.store.purchase.History', {
 	extend: 'NextThought.view.store.purchase.DetailView',
 	alias: 'widget.purchase-history',
 
 	ui: 'purchase-history-panel',
 
-	renderTpl:Ext.DomHelper.markup({cls:'loading', cn:{html:'Loading...'}}),
+	renderTpl: Ext.DomHelper.markup({cls: 'loading', cn: {html: 'Loading...'}}),
 
-	errorTpl:Ext.DomHelper.createTemplate({cls:'loading problem', cn:{html:'Oops!'}}),
+	errorTpl: Ext.DomHelper.createTemplate({cls: 'loading problem', cn: {html: 'Oops!'}}),
 
 	historyTpl: new Ext.XTemplate(Ext.DomHelper.markup({
 		tag: 'table',
-		cn:[{
-			tag: 'thead', cn:{ tag:'tr', cn:[
-				{tag: 'th', cls: 'key', cn:{tag: 'span', html: 'Licence or Activation Key'}},
-				{tag: 'th', cls: 'qty', cn:{tag: 'span', html: 'Usage / Qty'}},
-				{tag: 'th', cls: 'tot', cn:{tag: 'span', html: 'Total'}}
+		cn: [
+			{
+				tag: 'thead', cn: { tag: 'tr', cn: [
+				{tag: 'th', cls: 'key', cn: {tag: 'span', html: 'Licence or Activation Key'}},
+				{tag: 'th', cls: 'qty', cn: {tag: 'span', html: 'Usage / Qty'}},
+				{tag: 'th', cls: 'tot', cn: {tag: 'span', html: 'Total'}}
 			]}
-		},{
-			tag: 'tbody', cn:{ tag: 'tpl', 'for':'.', cn:{ tag:'tr', cls:'{type}', cn:[
-				{tag: 'td', cls: 'key', cn:[{tag: 'span', html: '{key}'},{html:'{Action} {Time:date("F j, Y")}'}]},
-				{tag: 'td', cls: 'qty', cn:{tag: 'span', html: '{usage} / {Order.Quantity}'}},
-				{tag: 'td', cls: 'tot', cn:{tag: 'span', html: '{Price}'}}
+			},
+			{
+				tag: 'tbody', cn: { tag: 'tpl', 'for': '.', cn: { tag: 'tr', cls: '{type}', cn: [
+				{tag: 'td', cls: 'key', cn: [
+					{tag: 'span', html: '{key}'},
+					{html: '{Action} {Time:date("F j, Y")}'}
+				]},
+				{tag: 'td', cls: 'qty', cn: {tag: 'span', html: '{usage} / {Order.Quantity}'}},
+				{tag: 'td', cls: 'tot', cn: {tag: 'span', html: '{Price}'}}
 			]}}
-		}]
+			}
+		]
 	})),
 
 	ordinal: 'history',
 
-	setupRenderData: function(){
+	setupRenderData: function () {
 		var req = {
-				url: this.record.getLink('history'),
-				scope: this,
-				success: this.applyHistory,
-				failure: this.failedToLoadHistoy
-			};
+			url: this.record.getLink('history'),
+			scope: this,
+			success: this.applyHistory,
+			failure: this.failedToLoadHistoy
+		};
 
 		Ext.Ajax.request(req);
 	},
 
 
-	applyHistory: function(resp){
-		if(!this.rendered){
-			this.on('afterrender',Ext.bind(this.applyHistory,arguments),this,{single:true});
+	applyHistory: function (resp) {
+		if (!this.rendered) {
+			this.on('afterrender', Ext.bind(this.applyHistory, arguments), this, {single: true});
 			return;
 		}
 
 		var data = [], obj = Ext.decode(resp.responseText) || {};
-		Ext.each(ParseUtils.parseItems(obj.Items||''),function(h){
+		Ext.each(ParseUtils.parseItems(obj.Items || ''), function (h) {
 			var d = h.getData(), q;
 			d.Order = (d.Order && d.Order.getData()) || {};
 			d.Pricing = (d.Pricing && d.Pricing.getData()) || {};
@@ -61,13 +67,13 @@ Ext.define('NextThought.view.store.purchase.History',{
 
 			console.log(d);
 
-			if(!Ext.isEmpty(d.InvitationCode)){
+			if (!Ext.isEmpty(d.InvitationCode)) {
 				d.type = 'bulk';
 				d.key = d.InvitationCode;
 				d.usage = q - (q >= 0 ? d.RemainingInvitations : q);
 			}
 
-			if(d.RedemptionCode){
+			if (d.RedemptionCode) {
 				d.Action = 'Redeemed';
 				d.Time = d.RedemptionTime;
 				d.Price = 'n/a';
@@ -76,14 +82,15 @@ Ext.define('NextThought.view.store.purchase.History',{
 			data.push(d);
 		});
 
-		this.historyTpl.overwrite(this.el,data);
-		this.el.select('tr.bulk td:first-of-type span')
-				.allowContextMenu()
-				.selectable();
+		this.historyTpl.overwrite(this.el, data);
+		this.el.select('tr.bulk td:first-of-type span').each(function (i) {
+			i.allowContextMenu();
+			i.selectable();
+		});
 	},
 
 
-	failedToLoadHistoy: function(){
+	failedToLoadHistoy: function () {
 		console.error(arguments);
 		this.errorTpl.overwrite(this.el);
 		this.up('[showError]').showError('Could not load purchase history. Please try again later.')
