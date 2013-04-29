ed6a510a5224a2949de82e826130e768788e282b
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-04-29 19:30:42 +0000

    update buy notice
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@18825 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/mixins/store-feature/Purchasable.js
+++ src/main/javascript/NextThought/mixins/store-feature/Purchasable.js
@@ -1,11 +1,15 @@
 Ext.define('NextThought.mixins.store-feature.Purchasable',{
 
-	needsPurchaseTpl: new Ext.XTemplate(Ext.DomHelper.markup({ cls: 'missing-context-info', cn:[
-		{ cls:'bookcover', style: {backgroundImage: 'url({Icon})'} },
-		{ cls:'meta', cn:[
-			{ cls: 'title', html: '{Title}'},
-			{ cls: 'byline', html: 'By {[values.Author||values.Provider]}'},
-			{ cls: 'buy-this', cn:{ cls: 'button', html: 'Buy' } }
+	needsPurchaseTpl: new Ext.XTemplate(Ext.DomHelper.markup([
+		{ cls:'missing-context-msg', html:'Your free sample does not include access to this content.' },
+		{ cls: 'missing-context-info', cn:[
+			{ cls:'bookcover', style: {backgroundImage: 'url({Icon})'} },
+			{ cls:'price', html:'{[NTIFormat.currency((values.price||values.Amount), values.Currency)]}' },
+			{ cls:'meta', cn:[
+				{ cls: 'title', html: '{Title}'},
+				{ cls: 'byline', html: 'By {[values.Author||values.Provider]}'},
+				{ cls: 'buy-this', cn:{ cls: 'button', html: 'Buy Now' } }
+			]}
 		]}
-	] }))
+	]))
 });
