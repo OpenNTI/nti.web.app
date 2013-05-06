7837e7df859fbaf62a1c618c9efd4fe7e29985aa
Phil Dow <phil.dow@nextthought.com>
2013-05-06 17:54:58 +0000

    prevening layout manager from forcing tab panel back into original dom location
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@18991 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/profiles/TabPanel.js
+++ src/main/javascript/NextThought/view/profiles/TabPanel.js
@@ -4,6 +4,8 @@ Ext.define('NextThought.view.profiles.TabPanel',{
 
 	plain: true,
 	ui: 'profile',
+	
+	componentLayout: 'profiletabpanel',
 
 	tabBar: {
 		plain: true,
@@ -13,4 +15,19 @@ Ext.define('NextThought.view.profiles.TabPanel',{
 		defaults: { plain: true, ui: 'profile-tab' }
 	}
 
+});
+
+// prevent the layout manager from moving the tab panel back into
+// its original dom pos. because we're changing it in
+// NextThought.view.profiles.Panel # handleScrollHeaderLock
+
+Ext.define('NextThought.layout.component.ProbileTabPanelLayout', {
+	extend: 'Ext.layout.component.Dock',
+	alias: 'layout.profiletabpanel',
+	type: 'profiletabpanel',
+
+	isValidParent: function(item, target, position) {
+		if (target.hasCls('x-panel-profile')) return true;
+		else return this.callParent(arguments);
+	}
 });
