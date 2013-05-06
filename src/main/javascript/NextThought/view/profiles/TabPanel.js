c15dfc75feef177f3dcc4351b706342d5bc81670
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-05-06 19:13:49 +0000

    clean up docs and if/else
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@18992 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/profiles/TabPanel.js
+++ src/main/javascript/NextThought/view/profiles/TabPanel.js
@@ -17,17 +17,31 @@ Ext.define('NextThought.view.profiles.TabPanel',{
 
 });
 
-// prevent the layout manager from moving the tab panel back into
-// its original dom pos. because we're changing it in
-// NextThought.view.profiles.Panel # handleScrollHeaderLock
-
+/**
+ * We need to prevent the layout manager from moving the tab panel back into its original dom position because we're
+ * changing it in {@link NextThought.view.profiles.Panel#handleScrollHeaderLock}
+ *
+ * @see NextThought.view.profiles.Panel#handleScrollHeaderLock
+ * @private Do not use this class anywhere else, it is very specific to THIS tab panel.
+ */
 Ext.define('NextThought.layout.component.ProbileTabPanelLayout', {
 	extend: 'Ext.layout.component.Dock',
 	alias: 'layout.profiletabpanel',
 	type: 'profiletabpanel',
 
+	/**
+	 * The crux. The layout decideds to re-home the tabbar element if this method returns false.
+	 *
+	 * @param item {Ext.Component/HTMLElement/Ext.Element}
+	 * @param target {Element/Ext.Element}
+	 * @param position {Object} Ignored by this override
+	 * @returns {boolean}
+	 */
 	isValidParent: function(item, target, position) {
-		if (target.hasCls('x-panel-profile')) return true;
-		else return this.callParent(arguments);
+		if (target.hasCls('x-panel-profile')) {
+			return true;
+		}
+
+		return this.callParent(arguments);
 	}
 });
