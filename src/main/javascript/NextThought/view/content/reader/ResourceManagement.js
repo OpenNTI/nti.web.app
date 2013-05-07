a15f0d249de3fe3d18c62530cfeb21fe843a4a1e
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-05-07 17:27:28 +0000

    swap out dead YouTube videos with a message.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@19008 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/content/reader/ResourceManagement.js
+++ src/main/javascript/NextThought/view/content/reader/ResourceManagement.js
@@ -22,17 +22,39 @@ Ext.define('NextThought.view.content.reader.ResourceManagement', function(){
 Ext.define('NextThought.view.content.reader.ResourceManager',{
 	alias: 'reader.resourceManager',
 
+	YOU_TUBE_API_KEY: 'YT',
+	YOU_TUBE_IFRAME_QUERY: 'iframe[src*="youtube.com"]',
+	YOU_TUBE_BLOCKED_TPL: Ext.DomHelper.createTemplate({
+		cls: 'youtube blocked video',
+		html: 'YouTube appears to be blocked by your connection.'
+	}),
+
 
 	constructor: function(reader){
 		this.reader = reader;
-		reader.on('set-content','manage',this);
+		reader.on('set-content','manage',this,{delay:1});
 	},
 
 
 	manage: function(reader, rawContent){
+		this.manageYouTubeVideos();
+	},
 
-		console.log('Do Management stuff');
 
+	manageYouTubeVideos: function(){
+		var d,items, tpl = this.YOU_TUBE_BLOCKED_TPL;
+
+		if(window[this.YOU_TUBE_API_KEY] !== undefined){
+			return;
+		}
+
+		d = this.reader.getDocumentElement();
+		items = d.querySelectorAll(this.YOU_TUBE_IFRAME_QUERY);
+
+		Ext.each(items,function(i){
+			tpl.insertBefore(i);
+			Ext.fly(i).remove();
+		});
 	}
 
 });
