4b708c35aef66259332a03973303844d6c0aa747
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-05-31 21:01:40 +0000

    some plumbing for chat dock
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@19754 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/chat/Dock.js
+++ src/main/javascript/NextThought/view/chat/Dock.js
@@ -1,12 +1,16 @@
 Ext.define('NextThought.view.chat.Dock',{
 	extend: 'Ext.panel.Panel',
 	alias: 'widget.chat-dock',
+	id: 'chat-dock',//there should be ONLY ONE instance of this.
 
 	title: 'Chats',
 	collapsible: true,
 
 //	ui: 'chat-dock',
-	cls: 'chat-dock'
-
+	cls: 'chat-dock',
+	defaultType: 'panel',
+	defaults: {
+		html: 'foo'
+	}
 
 });
--- src/main/javascript/NextThought/view/chat/WindowManager.js
+++ src/main/javascript/NextThought/view/chat/WindowManager.js
@@ -3,16 +3,98 @@ Ext.define('NextThought.view.chat.WindowManager',{
 	registry: [],
 	buttonMap: {},
 
-	constructor: function(){
 
+	constructor: function(){
+		var me = this;
+		console.log('Chat Window Manager inited');
+
+		//Ensure we have a reference to the dock
+		if(!me.getDock()){
+			Ext.ComponentManager.onAvailable('chat-dock',function(){
+				Ext.defer(me.getDock,1,me);
+			});
+		}
+
+		return this.callParent(arguments);
+	},
+
+
+	getDock: function(){
+		if(!this.dock){
+			this.dock = Ext.getCmp('chat-dock');
+			if(this.dock){
+				this.dock.getItemWithAssociatedWindow = function(win){
+					return this.items.findBy(function(i){
+						return win && win.id && (i.associatedWindowId === win.id);
+					});
+				};
+
+				//Add items that were skipped
+				Ext.Object.each(this.registry,this.addToDock,this);
+			}
+		}
+
+		return this.dock;
+	},
+
+
+	addToDock: function(windowId, window){
+		var dock = this.getDock(),
+			dockedItem;
+		if(!dock) { return; }
+
+		if(dock.getItemWithAssociatedWindow(window)){
+			console.warn('WARN: duplicate dock add');
+			return;
+		}
+
+		dockedItem = dock.add({ associatedWindowId: windowId, associatedWindow: window });
+		if(dockedItem){
+			window.dockedItemRef = dockedItem;
+		}
 	},
 
 
 	register: function(win){
+		if(!win || !win.id){
+			return;
+		}
+
+		this.registry[win.id] = win;
+
+		this.addToDock(win.id,win);
+
+		win.on('destroy','unregister',this);
 	},
 
 
 	unregister: function(win){
+		if(!win || !win.id){
+			return;
+		}
+
+		var dock = this.getDock(),
+			id = win.id,
+			w = this.registry[id],
+			ref = w && w.dockedItemRef;
+
+		delete this.registry[id];
+
+		if(w !== win){
+			console.warn('WARN: unregistered an instance with an id of another instance!');
+		}
+
+		if(!ref && dock){
+			ref = dock.getItemWithAssociatedWindow(win);
+		}
+
+		if(ref){
+			ref.destroy();
+		}
+		else if(!dock){
+			console.warn('WARN: this should not happen');
+		}
 	}
+
 
 });
