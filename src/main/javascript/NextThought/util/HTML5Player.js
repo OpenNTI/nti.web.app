a7f26eda41dd608f1ffeb30613a9e55c59f63059
Sean Jones <sean.jones@nextthought.com>
2013-06-03 13:57:30 +0000

    Add a very basic HTML5 media player utility.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@19774 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/main/javascript/NextThought/util/HTML5Player.js
@@ -0,0 +1,62 @@
+Ext.define('NextThought.util.HTML5Player',{
+
+	constructor: function(){
+		this.el = arguments[0].el.dom;
+	},
+
+	getCurrentTime: function(){
+		return this.el.currentTime;
+	},
+
+	getPlayerState: function(){
+		var playerState = -1;
+
+		if (this.el.paused){
+			playerState = 2;
+		}
+		else if (this.el.ended){
+			playerState = 0;
+		}
+		else if (this.el.readyState === 2 || this.el.readyState === 3){
+			playerState = 3;
+		}
+		else{
+			playerState = 1;
+		}
+
+		return playerState;
+	},
+
+	load: function(source, offset){
+		var sourceTpl = Ext.DomHelper.createTemplate({tag: 'source', src: '{src}', type: '{type}'});
+		// Remove any sources that may be there
+		this.el.innerHTML = '';
+
+		sourceTpl.append(this.el, {src: source[0], type: 'video/mp4'});
+		sourceTpl.append(this.el, {src: source[1], type: 'video/webm'});
+
+		this.el.load();
+
+		if (offset < 0.0){
+			this.seek(offset);
+		}
+	},
+
+	'play': function(){
+		this.el.play();
+	},
+
+	pause: function(){
+		this.el.pause();
+	},
+
+	seek: function(offset){
+		this.el.currentTime = offset;
+	},
+
+	stop: function(){
+		// Remove the current sources and trigger a load to free the used memory
+		this.el.innerHTML = '';
+		this.el.load();
+	}
+});
