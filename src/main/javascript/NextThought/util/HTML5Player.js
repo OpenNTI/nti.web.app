f14251a2d5b2f78eea15193aa44412e628befc35
Sean Jones <sean.jones@nextthought.com>
2013-06-06 16:39:25 +0000

    Update HTML5Player to use the ExtJS conventions for 'el' and 'dom', improve the safety of the loading and seeking code, and add a cleanup method.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@19946 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/util/HTML5Player.js
+++ src/main/javascript/NextThought/util/HTML5Player.js
@@ -1,26 +1,27 @@
 Ext.define('NextThought.util.HTML5Player',{
 
-	constructor: function(){
-		this.el = arguments[0].el.dom;
+	constructor: function(args){
+		this.el = Ext.get(args.el);
+		this.dom = this.el.dom;
 	},
 
 //	SAJ: The HTML5 player code is part of the browser and is always ready to load a media source.
 	isReady: true,
 
 	getCurrentTime: function(){
-		return this.el.currentTime;
+		return this.dom.currentTime;
 	},
 
 	getPlayerState: function(){
 		var playerState = -1;
 
-		if (this.el.paused){
+		if (this.dom.paused){
 			playerState = 2;
 		}
-		else if (this.el.ended){
+		else if (this.dom.ended){
 			playerState = 0;
 		}
-		else if (this.el.readyState === 2 || this.el.readyState === 3){
+		else if (this.dom.readyState === 2 || this.dom.readyState === 3){
 			playerState = 3;
 		}
 		else{
@@ -31,35 +32,50 @@ Ext.define('NextThought.util.HTML5Player',{
 	},
 
 	load: function(source, offset){
-		var sourceTpl = Ext.DomHelper.createTemplate({tag: 'source', src: '{src}', type: '{type}'});
+		var sourceTpl = Ext.DomHelper.createTemplate({tag: 'source', src: '{src}', type: '{type}'}),
+			dom = this.dom;
 		// Remove any sources that may be there
-		this.el.innerHTML = '';
+		if( dom.innerHTML ){
+			dom.innerHTML = '';
+			dom.load();
+		}
 
-		sourceTpl.append(this.el, {src: location.protocol+source[0], type: 'video/mp4'});
-		sourceTpl.append(this.el, {src: location.protocol+source[1], type: 'video/webm'});
+		sourceTpl.append(dom, {src: location.protocol+source[0], type: 'video/mp4'});
+		sourceTpl.append(dom, {src: location.protocol+source[1], type: 'video/webm'});
 
-		this.el.load();
+		dom.load();
 
 		if (offset > 0.0){
-			this.el.addEventListener('loadedmetadata',function(){this.currentTime = offset;});
+			this.el.on('loadedmetadata',function(){dom.currentTime = offset;}, this, {single: true});
 		}
 	},
 
 	'play': function(){
-		this.el.play();
+		this.dom.play();
 	},
 
 	pause: function(){
-		this.el.pause();
+		this.dom.pause();
 	},
 
 	seek: function(offset){
-		this.el.currentTime = offset;
+		var dom = this.dom;
+		if (dom.readyState === 0){
+			this.el.on('loadedmetadata',function(){dom.currentTime = offset;}, this, {single: true});
+		}
+		else{
+			this.dom.currentTime = offset;
+		}
 	},
 
 	stop: function(){
 		// Remove the current sources and trigger a load to free the used memory
-		this.el.innerHTML = '';
-		this.el.load();
+		this.dom.innerHTML = '';
+		this.dom.load();
+	},
+
+	cleanup: function(){
+		this.stop();
+		this.el.clearListeners();
 	}
 });
