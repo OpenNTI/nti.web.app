9a6b78f907b2aecaa76b704a981c13fa6c0b192f
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2013-06-26 20:17:28 +0000

    Date object expects month to be from 0-11 not 1-12.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@20518 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/account/coppa/upgraded/MonthPicker.js
+++ src/main/javascript/NextThought/view/account/coppa/upgraded/MonthPicker.js
@@ -26,18 +26,18 @@ Ext.define('NextThought.view.account.coppa.upgraded.MonthPicker',{
 		this.store = new Ext.data.Store({
 			fields:['name', 'id'],
 			data: [
-				{id:'1', name:'January'},
-				{id:'2', name:'February'},
-				{id:'3', name:'March'},
-				{id:'4', name:'April'},
-				{id:'5', name:'May'},
-				{id:'6', name:'June'},
-				{id:'7', name:'July'},
-				{id:'8', name:'August'},
-				{id:'9', name:'September'},
-				{id:'10', name:'October'},
-				{id:'11', name:'November'},
-				{id:'12', name:'December'}
+				{id:'0', name:'January'},
+				{id:'1', name:'February'},
+				{id:'2', name:'March'},
+				{id:'3', name:'April'},
+				{id:'4', name:'May'},
+				{id:'5', name:'June'},
+				{id:'6', name:'July'},
+				{id:'7', name:'August'},
+				{id:'8', name:'September'},
+				{id:'9', name:'October'},
+				{id:'10', name:'November'},
+				{id:'11', name:'December'}
 			]
 		});
 		this.callParent(arguments);
