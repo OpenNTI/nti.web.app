49f39d7bc44308e59009f40f140be33253520d53
David Rice <david.rice@nextthought.com>
2013-07-04 17:57:00 +0000

    Implemented an fsm to handle various touch interactions with content
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@20716 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/content/reader/Touch.js
+++ src/main/javascript/NextThought/view/content/reader/Touch.js
@@ -11,7 +11,15 @@ Ext.define('NextThought.view.content.reader.Touch', {
         SCROLL_MAX_SPEED: 200,
         SCROLL_THRESHOLD_SPEED: 1,
         TAP_TIME: 2,
-        TAP_HOLD_TIME: 1000
+        TAP_THRESHOLD: 15,
+        TAP_HOLD_TIME: 1000,
+        STATE: {
+            NONE: 0,
+            DOWN: 1,
+            SCROLLING: 2,
+            SELECTING: 3,
+            DRAGGING: 4
+        }
     },
 
     /**
@@ -54,47 +62,102 @@ Ext.define('NextThought.view.content.reader.Touch', {
             reader = this.reader,
             scroll = reader.getScroll(),
             dom = reader.getEl().dom,
-            anotherTouchStarted,
-            startY,
+            state = s.STATE.NONE,
+            initialTime,
+            initialY,
+            lastY,
             //current movement delta
             vel;
 
+        function withinTapThreshold() {
+            return Math.abs(lastY-initialY) < s.TAP_THRESHOLD;
+        }
+
         dom.addEventListener('touchstart', function(e) {
             e.preventDefault();
-            anotherTouchStarted = true;
 
-            startY = e.touches[0].pageY;
+            // Only start a new touch if all touches are off
+            if (state !== s.STATE.NONE)
+                return;
+            state = s.STATE.DOWN;
+
+            initialTime = Date.now();
+            initialY = e.touches[0].pageY;
+            lastY = initialY;
             vel = 0;
 
-            setTimeout(function() {
-                // TODO: check for long press
-                var isLongPress = false;
-                if (isLongPress) {
+            console.log('touchStart');
 
+            // Or just assume single
+            setTimeout(function() {
+                if (state === s.STATE.DOWN) {
+                    // TODO: check element at position for text (selecting) or drag/drop question (dragging)
+                    console.log('long press');
+                    vel=0;
                 }
+
             }, s.TAP_HOLD_TIME);
         }, false);
 
         dom.addEventListener('touchmove', function(e) {
             e.preventDefault();
 
-            var touch = e.touches[0];
-            vel = startY - touch.pageY;
-            startY = touch.pageY;
-            scroll.by(vel);
+            console.log('touchMove '+(lastY-initialY));
+
+            if (state === s.STATE.DOWN) {
+                scrollMove();
+                if (!withinTapThreshold())
+                    state = s.STATE.SCROLLING;
+            }
+            else if (state === s.STATE.SCROLLING) {
+                scrollMove();
+            }
+            else if (state === s.STATE.SELECTING) {
+                // TODO: Update Selection
+            }
+            else if (state === s.STATE.DRAGGING) {
+                // TODO: Update Dragged element
+            }
+            else
+                console.warn('Unknown touch state on touchMove!');
+
+            function scrollMove() {
+                var touch = e.touches[0];
+                vel = lastY - touch.pageY;
+                lastY = touch.pageY;
+                scroll.by(vel);
+            }
+
         }, false);
 
         dom.addEventListener('touchend', function(e){
             e.preventDefault();
 
             var startLt0 = vel<0,
-                lastUpdateTime = Date.now();
+                lastUpdateTime = Date.now(),
+                tempState = state;
+            state = s.STATE.NONE;
 
-            anotherTouchStarted = false;
+            console.log('touchEnd');
 
-            // Cap the ending velocity at the max speed
-            if (Math.abs(vel) > s.SCROLL_MAX_SPEED)
-                vel = startLt0? -s.SCROLL_MAX_SPEED : s.SCROLL_MAX_SPEED;
+            if (tempState === s.STATE.DOWN) {
+                // TODO: Send tap event
+            }
+            else if (tempState === s.STATE.SCROLLING) {
+                // Cap the ending velocity at the max speed
+                if (Math.abs(vel) > s.SCROLL_MAX_SPEED)
+                    vel = startLt0? -s.SCROLL_MAX_SPEED : s.SCROLL_MAX_SPEED;
+                kineticScroll();
+            }
+            else if (tempState === s.STATE.SELECTING) {
+                // TODO: Update Selection
+            }
+            else if (tempState === s.STATE.DRAGGING) {
+                // TODO: Update Dragged element
+            }
+            else
+                console.warn('Unknown touch state on touchEnd!');
+
 
             function kineticScroll() {
                 var lt0 = vel< 0,
@@ -107,7 +170,8 @@ Ext.define('NextThought.view.content.reader.Touch', {
                 // and hasn't changed directions
                 aboveThreshold = (lt0 ? -vel : vel) > s.SCROLL_THRESHOLD_SPEED;
                 sameDirection = startLt0 === lt0;
-                if ( aboveThreshold && sameDirection && !anotherTouchStarted ){
+
+                if ( aboveThreshold && sameDirection && state === s.STATE.NONE ){
                     // Apply friction in the opposite movement direction
                     // based on the time passed for smoother movement
                     vel+= (lt0 ? s.SCROLL_FRICTION : -s.SCROLL_FRICTION)*deltaTime;
@@ -116,7 +180,8 @@ Ext.define('NextThought.view.content.reader.Touch', {
                     setTimeout(kineticScroll, s.SCROLL_TIME_STEP);
                 }
             }
-            kineticScroll();
         });
+
     }
+
 });
