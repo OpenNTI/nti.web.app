e42013573c088e239a1fa727306a02fd0ad46887
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-07-16 14:39:57 +0000

    fix isDirty for greg
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@20987 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/model/Base.js
+++ src/main/javascript/NextThought/model/Base.js
@@ -854,11 +854,26 @@ Ext.define('NextThought.model.Base', {
 		//that has the same id.  If it isn't the exact record call the function
 		//fname on it with the provided args
 		Ext.data.StoreManager.each(function(s){
-			var recById = s.getById(rec.getId());
+			var recById = s.getById(rec.getId()), fnHook = function(){return false;};
+
+			//This record has been filtered out and may potentially throw an error if we attempt to call store group
+			// functions. So, we let this record update and the store think its ungroupped while it updates.
+			if(recById && !s.data.contains(recById) && s.isGrouped()) {
+				s.isGrouped = fnHook;
+			}
 
 			//Ok we found one and it isn't the same object
 			if(recById && rec !== recById && rec.get('MimeType') === recById.get('MimeType') && Ext.isFunction(recById[fname])){
-				recById[fname].apply(recById, args);
+				try{
+					recById[fname].apply(recById, args);
+				}
+				catch(e){
+					console.warn(e.message);
+				}
+			}
+
+			if(s.isGrouped === fnHook){
+				delete s.isGrouped;//restore the default
 			}
 		});
 
