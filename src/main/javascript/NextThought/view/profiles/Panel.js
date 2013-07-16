69d3e9315c901caf42fbc86a9303fc96580a209d
Andrew Ligon <andrew.ligon@nextthought.com>
2013-07-16 16:58:11 +0000

    Only align the editor if its visible, and fade it out when you scroll down.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@21000 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/profiles/Panel.js
+++ src/main/javascript/NextThought/view/profiles/Panel.js
@@ -234,8 +234,21 @@ Ext.define('NextThought.view.profiles.Panel',{
 			el, height;
 
 		function alignEditor(ed){
-			if(ed && ed.rendered){
+			var top, o;
+			if(ed && ed.rendered && ed.isVisible()){
 				ed.realign();
+
+				top = ed.el.getY();
+
+				if(top > 165){
+					o = 1;
+				}else if(top > 135){
+					o = (top - 135)/30;
+				}else{
+					o = 0;
+				}
+
+				ed.el.setStyle({'opacity': o.toFixed(2)});
 			}
 		}
 
