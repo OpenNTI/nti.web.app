d247c586e9fbbff90cd9290b600e5eb56355b164
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-07-18 01:20:20 +0000

    checkpoint. Discussions showing. Click handler registered to print "Hello" in console.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@21099 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/course/overview/Discussion.js
+++ src/main/javascript/NextThought/view/course/overview/Discussion.js
@@ -1,4 +1,71 @@
 Ext.define('NextThought.view.course.overview.Discussion',{
 	extend: 'Ext.Component',
-	alias: 'widget.course-overview-discussion'
+	alias: 'widget.course-overview-discussion',
+
+	ui: 'course',
+	cls: 'overview-discussion',
+
+	renderTpl: Ext.DomHelper.markup([
+		{ cls: 'image', style:{backgroundImage:'url({icon})'}},
+		{ cls: 'meta', cn:[
+			{ cls: 'label', html: '{label}'},
+			{ cls: 'title', html: '{title}'},
+			{ cls: 'comments', html:'{comments:plural("Comment")}'}
+		]}
+	]),
+
+
+	listeners: {
+		click:{
+			element: 'el',
+			fn: 'onClick'
+		}
+	},
+
+
+	constructor: function(config){
+		var n = config.node,
+			i = config.locationInfo;
+
+		config.data = {
+			title: n.getAttribute('title'),
+			icon: getURL(i.root+n.getAttribute('icon')),
+			ntiid: n.getAttribute('ntiid'),
+			label: n.getAttribute('label'),
+			comments: 0
+		};
+
+		this.callParent([config]);
+	},
+
+
+	beforeRender: function(){
+		this.callParent(arguments);
+		this.renderData = Ext.apply(this.renderData||{},this.data);
+
+		$AppConfig.service.getObject(this.data.ntiid,
+				this.onBoardResolved,
+				this.onBoardResolveFailure,
+				this,
+				true
+		);
+	},
+
+
+	onBoardResolved: function(topic){
+		this.data.comments = topic.get('PostCount');
+		if(this.rendered){
+			this.renderTpl.overwrite(this.el,this.data);
+		}
+	},
+
+
+	onBoardResolveFailure: function(){
+		console.warn('Could not load the topic object to show the comment count.');
+	},
+
+
+	onClick: function(){
+		console.debug('hello');
+	}
 });
