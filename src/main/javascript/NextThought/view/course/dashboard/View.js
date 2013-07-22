6f3bc1d14b31bd64ff86ab52d6d76fe4f674af3f
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-07-22 23:30:11 +0000

    some base styles & blocks. Now to implement a sorting/priority system to pack the grid tight.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@21243 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/course/dashboard/View.js
+++ src/main/javascript/NextThought/view/course/dashboard/View.js
@@ -2,6 +2,59 @@ Ext.define('NextThought.view.course.dashboard.View',{
 	extend: 'Ext.container.Container',
 	alias: 'widget.course-dashboard',
 
+	ui: 'course',
+	cls: 'course-dashboard-container',
+	layout: 'auto',
+
+	items:{
+		layout: 'auto',
+		ui: 'course',
+		cls: 'course-dashboard',
+		xtype: 'container',
+		defaultType: 'box',
+		items:[
+			{ cls:'grid-item col-4 row-2', cols:4, rows:2 },
+			{ cls:'grid-item col-1 row-2', cols:1, rows:2 },
+			{ cls:'grid-item col-2 row-1', cols:2, rows:1 },
+			{ cls:'grid-item col-2 row-1', cols:2, rows:1 },
+			{ cls:'grid-item col-1 row-1', cols:1, rows:1 },
+			{ cls:'grid-item col-1 row-1', cols:1, rows:1 },
+//			{ cls:'grid-item col-1 row-4', cols:1, rows:4 },
+			{ cls:'grid-item col-1 row-1', cols:1, rows:1 },
+			{ cls:'grid-item col-1 row-1', cols:1, rows:1 },
+			{ cls:'grid-item col-1 row-1', cols:1, rows:1 }
+		]
+	},
+
+
+	//Used to test sorting
+	shuffle: function shuffle(array) {
+	  var m = array.length, t, i;
+
+	  // While there remain elements to shuffle…
+	  while (m) {
+	    // Pick a remaining element…
+	    i = Math.floor(Math.random() * m--);
+
+	    // And swap it with the current element.
+	    t = array[m];
+	    array[m] = array[i];
+	    array[i] = t;
+	  }
+
+	  return array;
+	},
+
+	constructor: function(config){
+
+		config.items = Ext.clone(this.items);
+		this.shuffle(config.items.items);
+
+
+		this.callParent(arguments);
+	},
+
+
 	onNavigateComplete: function(pageInfo){
 		if(!pageInfo.isPartOfCourse()){
 			this.removeAll(true);
