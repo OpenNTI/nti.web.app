2bea01f791fd6ba74c93f5abd2b8db94986992b6
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-07-25 20:19:22 +0000

    our first tile :) Videos. The Current, and remaining videos.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@21325 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/course/dashboard/tiles/Videos.js
+++ src/main/javascript/NextThought/view/course/dashboard/tiles/Videos.js
@@ -4,19 +4,37 @@ Ext.define('NextThought.view.course.dashboard.tiles.Videos',{
 
 	statics: {
 
-		getTileFor: function(date, courseNode, locationInfo, currentCourseNode, nextCourseNode){
+		getTileFor: function(date, courseTocNode, locationInfo, courseRecord /*, ...don't care about the rest of the args*/){
 			var f = 'object[mimeType$=ntivideo]',
 				DQ = Ext.DomQuery,
-				r = currentCourseNode && currentCourseNode.getChildren();
+				videos = [],
+				store = courseRecord && courseRecord.store, r, i, len;
 
-			r = DQ.filter(r||[],f);
+			function addDate(r){
+				return function(n){
+					if( r ){
+						//this will only be referenced within THIS class/file. WARNING: VERY PRIVATE
+						n.NTCourseNode = r;
+					}
+					videos.push(n);
+				};
+			}
 
-			if(Ext.isEmpty(r)){
-				return;
+			if(store){
+				i = store.indexOf(courseRecord);
+				len = store.getCount();
+				for(i; i<len; i++){
+					r = store.getAt(i);
+					Ext.each(DQ.filter(r.getChildren()||[],f),addDate(r));
+				}
+			}
+
+			if(Ext.isEmpty(videos)){
+				return null;
 			}
 
 			//We have videos
-			return this.create({sources: r, locationInfo: locationInfo});
+			return this.create({sources: videos, locationInfo: locationInfo});
 		}
 
 	},
@@ -24,6 +42,46 @@ Ext.define('NextThought.view.course.dashboard.tiles.Videos',{
 	config: {
 		cols: 4,
 		rows: 2,
-		weight: 10
+		weight: 10,
+		sources: [],
+		locationInfo: null
+	},
+
+
+	//Effectively disable, I know we aren't really deprecating them, but rather just blocking them.
+	add:Ext.deprecated('This component is not meant to be handled like a container.'),
+	remove:Ext.deprecated('This component is not meant to be handled like a container.'),
+
+
+	afterRender: function(){
+		this.callParent(arguments);
+
+		var items = [],
+			l = this.getLocationInfo();
+
+		Ext.each(this.getSources(),function(s){
+			var rec = s.NTCourseNode;
+			delete s.NTCourseNode;
+			items.push({node:s, locationInfo: l, courseRecord: rec});
+		});
+
+		this.content = Ext.widget({
+			xtype: 'course-overview-video-section',
+			playerWidth: 533,
+			renderTo: this.el,
+			items: items,
+			cls: 'dashboard-videos',
+			leaveCurtain: true,
+			tpl:Ext.DomHelper.markup({ tag: 'tpl', 'for':'.', cn: [{
+					cls: 'video-row',
+					cn: [
+						{ cls: 'poster', style: { backgroundImage:'url({poster})'} },
+						{ cls: 'meta', cn:[
+							{ cls:'date', html: '{date:date("l, F j")}' },
+							{ cls:'label', html: '{label}', 'data-qtip':'{label}' }
+						]}
+					]
+				}]})
+		});
 	}
 });
