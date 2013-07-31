5de2534a0d0438f90825d2272cf66c46553b60a5
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-07-31 15:46:08 +0000

    start filling in most active user widget
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@21614 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/course/dashboard/tiles/MostActive.js
+++ src/main/javascript/NextThought/view/course/dashboard/tiles/MostActive.js
@@ -5,7 +5,11 @@ Ext.define('NextThought.view.course.dashboard.tiles.MostActive',{
 	statics: {
 
 		getTileFor: function(effectiveDate, course, locationInfo, courseNodeRecord){
-			return null;//this.create({lastModified: courseNodeRecord.get('date'),locationInfo: locationInfo});
+			return this.create({
+				lastModified: courseNodeRecord.get('date'),
+				locationInfo: locationInfo,
+				courseNodeRecord: courseNodeRecord
+			});
 		}
 
 	},
@@ -14,7 +18,78 @@ Ext.define('NextThought.view.course.dashboard.tiles.MostActive',{
 		cols: 1,
 		rows: 2,
 		weight: 9
+	},
+
+
+	initComponent: function(){
+		this.callParent(arguments);
+		this.view = this.add({
+			xtype: 'dataview',
+			cls:'active-users',
+			ui: 'tile',
+
+			store: 'ext-empty-store',
+
+			overItemCls: 'x-item-over',
+			itemSelector:'.user',
+			tpl: Ext.DomHelper.markup({
+				tag: 'tpl', 'for':'.', cn: [{
+					cls: 'user',
+					'data-qtip': '{displayName}',
+					style: {
+						backgroundImage: 'url({avatarURL})'
+					}
+				}]
+			})
+		});
+
+
+		this.loadSummary();
+	},
+
+
+	loadSummary: function(){
+		var rec = this.getCourseNodeRecord(),
+			pi = rec.get('pageInfo'),
+			req;
+
+		if(!pi){
+			rec.listenForFieldChange('pageInfo','loadSummary',this,true);
+			return;
+		}
+
+		req = {
+			url: pi.getLink('TopUserSummaryData'),
+			scope: this,
+			success: this.onSummaryLoad,
+			failure: this.onSummaryFailed
+		};
+
+		Ext.Ajax.request(req);
+	},
+
+
+	onSummaryFailed: function(){
+		console.error('Summary failed to load:',arguments);
+	},
+
+
+	onSummaryLoad: function(resp){
+		var json = Ext.decode(resp.responseText,true) || {},
+			users = [], me = this;
+
+		function pluckUsers(i){ if(i){users.push(i.Username);} }
+
+		Ext.each(json.Items||[],pluckUsers);
+
+		UserRepository.getUser(users,function(u){
+			me.store = Ext.data.Store({
+				model: 'NextThought.model.User',
+				proxy: 'memory',
+				data: u
+			});
+			me.view.bindStore(me.store);
+			me.view.getSelectionModel().select(0);
+		});
 	}
-
-
 });
