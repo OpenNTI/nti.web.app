49a5fcf16eecfd630b73b92ee6815aee0043a850
Chris Utz <chris.utz@nextthought.com>
2013-08-02 06:00:09 +0000

    One more place.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@21796 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/course/dashboard/tiles/TopDiscussions.js
+++ src/main/javascript/NextThought/view/course/dashboard/tiles/TopDiscussions.js
@@ -91,7 +91,12 @@ Ext.define('NextThought.view.course.dashboard.tiles.TopDiscussions',{
 
 
 	onItemClicked: function(view, rec /*, dom, index, event, eOpts*/){
-		this.fireEvent('navigate-to-href',this,rec.getId());
+		if(!this.topic){
+			alert('An error occurred showing this discussion.');
+		}
+		else{
+			this.fireEvent('navigate-to-course-discussion', this.locationInfo.ContentNTIID, this.topic.get('ContainerId'), this.topic.getId());
+		}
 	},
 
 
@@ -116,6 +121,7 @@ Ext.define('NextThought.view.course.dashboard.tiles.TopDiscussions',{
 			this.onForumResolved(topic);
 			return;
 		}
+		this.topic = topic;
 
 		$AppConfig.service.getObject(
 				topic.get('ContainerId'),
