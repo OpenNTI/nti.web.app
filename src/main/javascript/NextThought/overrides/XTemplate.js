384d133abcc7de1500cdef8f2c92b8f658efb206
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-08-02 18:10:39 +0000

    add hook in to templates for chris
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@21817 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/overrides/XTemplate.js
+++ src/main/javascript/NextThought/overrides/XTemplate.js
@@ -14,3 +14,23 @@ Ext.define('NextThought.overrides.XTemplate',{
 		}
 	};
 });
+
+
+Ext.override(Ext.XTemplateCompiler,{
+
+    myStringsRe: /\{{3}((?!\{\{\{|\}\}\}).+?\}?)\}{3}/g,
+
+	parse: function(str){
+        var t = this.myStringsRe.exec(str);
+        if(t){
+            console.debug('(Override)XTemplateCompiler#parse():', t);
+            str = str.replace(this.myStringsRe,function(m,key){
+                console.log('Looking up String...',key);
+                //return lookup from strings
+                return m;
+            });
+        }
+        return this.callParent([str]);
+    }
+
+});
