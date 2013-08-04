dc9f4339188c260d619c0a9a0b2337ae4eb616b7
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-08-04 19:38:07 +0000

    hook up XTemplates to NTIStrings
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@21866 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/overrides/XTemplate.js
+++ src/main/javascript/NextThought/overrides/XTemplate.js
@@ -1,20 +1,7 @@
 Ext.define('NextThought.overrides.XTemplate',{
 	override: 'Ext.XTemplate',
-	applySubtemplate: function(id, values){ return Ext.XTemplate.subs[id].apply(values); }
-},function(){
-	var c = this;
-	c.registerSubtemplate = function(name,tpl){
-		c.subs = c.subs || {};
-		if(tpl && !(tpl instanceof Ext.XTemplate)){
-			tpl = new Ext.XTemplate(tpl);
-		}
-		c.subs[name] = tpl;
-		if(!tpl){
-			delete c.subs[name];
-		}
-	};
+	requires:['NextThought.util.Globals']
 });
-
 
 Ext.override(Ext.XTemplateCompiler,{
 
@@ -23,11 +10,11 @@ Ext.override(Ext.XTemplateCompiler,{
 	parse: function(str){
         var t = this.myStringsRe.exec(str);
         if(t){
-            console.debug('(Override)XTemplateCompiler#parse():', t);
+            //console.debug('(Override)XTemplateCompiler#parse():', t);
             str = str.replace(this.myStringsRe,function(m,key){
-                console.log('Looking up String...',key);
-                //return lookup from strings
-                return m;
+                //console.log('Looking up String...',key);
+	            //return lookup from strings
+                return Globals.getExternalizedString(key,m);
             });
         }
         return this.callParent([str]);
