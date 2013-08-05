1e698cabf1fedf52c553aec0ae02c83032ef8e0b
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-08-05 22:34:07 +0000

    cache as much as we can...Regex's are expensive to make, and so are functions.  Keep them around.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@21974 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/overrides/XTemplate.js
+++ src/main/javascript/NextThought/overrides/XTemplate.js
@@ -7,15 +7,25 @@ Ext.override(Ext.XTemplateCompiler,{
 
     myStringsRe: /\{{3}((?!\{\{\{|\}\}\}).+?\}?)\}{3}/g,
 
+	myReplacementFn: (function(){
+		//cache the regex and function so its not creating them on the fly each time
+		var escapRe = /(\{|\})/gm;
+
+		function escapeFn(n,c){ return '&#'+c.charCodeAt(0)+';'; }
+
+		return function(m,key){
+			var def = {},
+				s = Globals.getExternalizedString(key, def);
+			//Its written like this to prevent executing throwaway work.
+			// Only calculate the escaped key if the default token is returend.
+			return s !== def ? s : m.replace(escapRe,escapeFn);
+        };
+	}()),
+
 	parse: function(str){
         var t = this.myStringsRe.exec(str);
-        if(t){
-            //console.debug('(Override)XTemplateCompiler#parse():', t);
-            str = str.replace(this.myStringsRe,function(m,key){
-                //console.log('Looking up String...',key);
-	            //return lookup from strings
-                return Globals.getExternalizedString(key,m);
-            });
+        if( t ){
+            str = str.replace(this.myStringsRe,this.myReplacementFn);
         }
         return this.callParent([str]);
     }
