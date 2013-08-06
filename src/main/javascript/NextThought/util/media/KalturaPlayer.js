ff498bedabc4465fb1eff212d8ac9e6b3382c2e0
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-08-06 18:53:15 +0000

    Kaltura SUPER sandboxed... now works everywhere. *facepalm*
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@22041 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/util/media/KalturaPlayer.js
+++ src/main/javascript/NextThought/util/media/KalturaPlayer.js
@@ -6,7 +6,7 @@ Ext.define('NextThought.util.media.KalturaPlayer',{
 		kind:'video',
 		type: 'kaltura',
 		valid: function(){
-			return Boolean(window.kWidget);
+			return true;
 		}
 	},
 
@@ -18,13 +18,58 @@ Ext.define('NextThought.util.media.KalturaPlayer',{
 		observable: 'Ext.util.Observable'
 	},
 
-	playerTpl: Ext.DomHelper.createTemplate({
-		name: '{id}', id: '{id}', 'style': 'width:{width}px;height:{height}px;'
+	PLAYER_TPL: Ext.DomHelper.createTemplate({
+		name: '{id}',
+		id: '{id}',
+		style: {
+			width:'{width}px',
+			height:'{height}px'
+		},
+		cn: [{//Globals.loadScript(location.protocol+"//html5.kaltura.org/js");
+			tag:'iframe',
+			id: '{id}-iframe',
+			name: '{id}-iframe',
+			src: Ext.SSL_SECURE_URL,
+			frameBorder: 0,
+            scrolling: 'no',
+			seamless: true,
+			width:'{width}',
+			height:'{height}',
+			style:{
+				zIndex: 1,
+				overflow: 'hidden',
+				width:'{width}px',
+				height:'{height}px'
+			}
+		}]
 	}),
+
+
+	PLAYER_BODY_TPL: Ext.DomHelper.createTemplate([
+		'<!DOCTYPE html>',
+		{tag:'html', lang:'en', cn:[
+			{tag:'head',cn:[
+				{tag:'script', type:'text/javascript', src:('{basePath}resources/lib/jQuery-1.8.0min.js')},
+				{tag:'script', type:'text/javascript', src:(location.protocol+'//html5.kaltura.org/js')},
+				{tag:'script', type:'text/javascript', html:'\n{code}\n'},
+				{tag:'style', type:'text/css', cn:[
+					'body, html { margin: 0; padding: 0; }'
+				]}
+			]},
+			{tag:'body', cn:{
+				id: '{id}', name: '{id}', style: {
+					width:'{width}px',
+					height:'{height}px'
+				}
+			}}]
+		}
+	]),
+
 
 	// We need to externalize these values since they relate to our Kaltura Account.  This is okay for OU since they are paying for it.
 	PARTNER_ID: '1500101',
 	UICONF_ID: '15491291',
+
 
 	constructor: function(config){
 		this.mixins.observable.constructor.call(this);
@@ -37,122 +82,132 @@ Ext.define('NextThought.util.media.KalturaPlayer',{
 		this.height = config.height;
 		this.currentPosition = 0;
 		this.currentState = -1;
+		this.isReady = false;
 
+		this.handleMessage = Ext.bind(this.handleMessage,this);
 		this.playerSetup();
 	},
 
+
 	playerSetup: function(){
-		var kWidget = window.kWidget,
-			me = this,
-			mw = window.mw;
-		this.globals.currentTime = this.getGlobalHandler('kaltura', this.id, 'currentTime', me.playerUpdatePlayheadHandler, me);
-		this.globals.doPlay = this.getGlobalHandler('kaltura', this.id, 'doPlay', me.playerDoPlayHandler, me);
-		this.globals.doPause = this.getGlobalHandler('kaltura', this.id, 'doPause', me.playerDoPauseHandler, me);
-		this.globals.playerPlayEnd = this.getGlobalHandler('kaltura', this.id, 'playerPlayEnd', me.playerPlayEndHandler, me);
-		this.globals.mediaLoadError = this.getGlobalHandler('kaltura', this.id, 'mediaLoadError', me.playerErrorHandler, me);
-		this.globals.mediaError = this.getGlobalHandler('kaltura', this.id, 'mediaError', me.playerErrorHandler, me);
-		this.isReady = false;
+		var doc,
+			data = {
+				id: this.id,
+				height: this.height,
+				width: this.width,
+				basePath: location.protocol+'//'+location.host+location.pathname,
+				code: this.buildWrapperCode()
+			};
+
 
 //		Inject Kaltura Player HTML
-		this.playerTpl.append(this.parentEl, {id: this.id, height: this.height, width: this.width});
+		this.el = this.PLAYER_TPL.append(
+				this.parentEl, data, true);
+		this.iframe = this.el.down('iframe');
+
+		window.addEventListener("message", this.handleMessage, false);
+		doc = this.getPlayerContextDocument();
+		doc.open();
+		doc.write(this.PLAYER_BODY_TPL.apply(data));
+		doc.close();
+
 		console.log(this.id);
-		this.el = Ext.get(this.id);
-
-		// Force HTML5 player over Flash player
-		mw.setConfig( 'KalturaSupport.LeadWithHTML5', true );
-		// Allow AirPlay
-		mw.setConfig('EmbedPlayer.WebKitAllowAirplay', true);
-		// Do not rewrite video tags willy-nilly
-		mw.setConfig( 'EmbedPlayer.RewriteSelector', false );
-
-		kWidget.embed({
-			targetId: me.id,
-			wid: "_"+ me.PARTNER_ID,
-			uiconf_id: me.UICONF_ID,
-			flashvars: {
-				"externalInterfaceDisabled": false,
-				"autoPlay": true
-			},
-			params:{
-				wmode: 'transparent'
-			},
-			readyCallback: Ext.Function.createBuffered(me.playerReady, 500, me)
-		});
-
 	},
 
-	getGlobalHandler: function (prefix, playerID, fnName, fn, scope, oneShot){
-		var name = '__'+prefix+playerID+fnName;
-		window[name] = function(){
-			Ext.callback(fn,scope,arguments);
-			if(oneShot){
-				delete window[name];
-			}
-		};
-		return name;
+
+	handleMessage: function(event){
+		//console.debug('Message:',event);
+
+		var filter = /^kalturaplayer\./i,
+			eventData = event.data || {},
+			eventName = eventData.event||'',
+			handlerName;
+
+		if(!filter.test(eventName) && this.id === eventData.id ){
+			return;
+		}
+
+		eventName = eventName.replace(filter,'');
+		handlerName = eventName+'Handler';
+
+		if(/error/i.test(eventName)){
+			this.playerErrorHandler(eventName, eventData);
+			return;
+		}
+
+		if(!Ext.isFunction(this[handlerName])){
+			console.warn('Player does not handle: '+handlerName);
+			return;
+		}
+
+		this[handlerName](eventData);
 	},
 
-	playerReady: function(){
-		var me = this;
-		this.player = Ext.getDom(this.id);
-		this.isReady = true;
-		this.player.addJsListener('playerUpdatePlayhead', me.globals.currentTime);
-		this.player.addJsListener('doPlay', me.globals.doPlay);
-		this.player.addJsListener('doPause', me.globals.doPause);
-		this.player.addJsListener('playerPlayEnd', me.globals.playerPlayEnd);
-		this.player.addJsListener('mediaLoadError', me.globals.mediaLoadError);
-		this.player.addJsListener('mediaError', me.globals.mediaError);
-		this.fireEvent('player-ready', 'kaltura');
+
+	sendMessage: function(type,name,data){
+		this.getPlayerContext().postMessage({
+			type: type,
+			name: name,
+			data: data
+		},'*');
 	},
 
-	playerUpdatePlayheadHandler: function(data){
-		this.currentPosition = data;
+
+	sendCommand: function(name,data){
+		this.sendMessage('command',name,data);
 	},
 
-	playerDoPlayHandler: function(){
-		console.warn(this.currentState);
-		this.currentState = 1;
-		this.fireEvent('player-event-play', 'kaltura');
+
+	buildWrapperCode: function(){
+		var code = [],
+			me = this;
+
+		function resolve(m, k){
+			return me[k] || m;
+		}
+
+		code.push(this.playerCode.inject.toString());
+		code.push('$(inject);');
+
+		return code.join('\n').replace(/%([^%]+)%/gm,resolve);
 	},
 
-	playerDoPauseHandler: function(){
-		console.warn(this.currentState);
-		this.currentState = 2;
-		this.fireEvent('player-event-pause', 'kaltura');
+
+	getPlayerContext: function(){
+		var iframe = Ext.getDom(this.iframe);
+		return iframe && (iframe.contentWindow || window.frames[iframe.name]);
 	},
 
-	playerPlayEndHandler: function(){
-		this.stop();
-		this.currentState = -1;
-		this.fireEvent('player-event-ended', 'kaltura');
+
+	getPlayerContextDocument: function(){
+		var doc = this.playerDocument || (Ext.getDom(this.iframe).contentDocument || this.getPlayerContext().document);
+		if( doc && !doc.body ){
+			doc.body = doc.getElementsByTagName('body')[0];
+		}
+		this.playerDocument = doc;
+		return doc;
 	},
 
-	playerErrorHandler: function(){
-		console.error('kaltura error ' + arguments);
-		this.fireEvent('player-error', 'kaltura');
-	},
 
-	getCurrentTime: function(){
-		return this.currentPosition;
-	},
+	getCurrentTime: function(){ return this.currentPosition; },
 
-	getPlayerState: function(){
-		return this.currentState;
-	},
+
+	getPlayerState: function(){ return this.currentState; },
+
 
 	load: function(source, offset){
 		var kalturaData;
 
 		source = Ext.isArray(source) ? source[0] : source;
 
-		this.player.sendNotification('cleanMedia');
+		this.sendCommand('cleanMedia');
 
 		this.currentSource = source;
 		this.currentStartAt = offset;
 
 		kalturaData = source.split(':');
-		console.log(kalturaData, source, offset);
-		this.player.sendNotification('changeMedia', {entryId: kalturaData[1]});
+		//console.log(kalturaData, source, offset);
+		this.sendCommand('changeMedia', {entryId: kalturaData[1]});
 		this.currentPosition = 0;
 		this.currentState = -1;
 
@@ -162,39 +217,159 @@ Ext.define('NextThought.util.media.KalturaPlayer',{
 
 	},
 
-	play: function(){
-		this.player.sendNotification('doPlay');
-	},
 
-	pause: function(){
-		this.player.sendNotification('doPause');
-	},
+	play: function(){ this.sendCommand('doPlay'); },
+
+
+	pause: function(){ this.sendCommand('doPause'); },
+
+
+	stop: function(){ this.sendCommand('doStop'); },
+
 
 	seek: function(offset){
 		this.currentStartAt = offset;
-		this.player.sendNotification('doSeek', offset);
+		this.sendCommand('doSeek', offset);
 	},
 
-	stop: function(){
-		this.player.sendNotification('doStop');
-	},
 
 	cleanup: function(){
-		var el = Ext.get(this.id),
-			key;
-		this.stop();
+		var el = Ext.get(this.id);
 		this.isReady = false;
 		el.clearListeners();
-		this.player.sendNotification('cleanMedia');
-		kWidget.destroy(this.id);
-		Ext.destroy(el);
-		for (key in this.globals){
-			if (this.globals.hasOwnProperty(key)){
-				delete window[this.globals[key]];
-			}
-		}
-	}
+		Ext.destroy(el);//because we're shelling this thing into an iframe, we can just destroy it.
+	},
 
-}, function(){
-	Globals.loadScript(location.protocol+"//html5.kaltura.org/js");
+
+
+	readyHandler: function(){
+		this.isReady = true;
+		this.fireEvent('player-ready', 'kaltura');
+	},
+
+
+	playerUpdatePlayheadHandler: function(data){
+		this.currentPosition = data;
+	},
+
+
+	doPlayHandler: function(){
+		console.warn(this.currentState);
+		this.currentState = 1;
+		this.fireEvent('player-event-play', 'kaltura');
+	},
+
+
+	doPauseHandler: function(){
+		console.warn(this.currentState);
+		this.currentState = 2;
+		this.fireEvent('player-event-pause', 'kaltura');
+	},
+
+
+	playerPlayEndHandler: function(){
+		this.stop();
+		this.currentState = -1;
+		this.fireEvent('player-event-ended', 'kaltura');
+	},
+
+
+	playerErrorHandler: function(){
+		console.error('kaltura error ', arguments);
+		this.fireEvent('player-error', 'kaltura');
+	},
+
+
+	playerCode:{
+
+		inject: function inject(){
+
+			function send(event,data){
+				//console.log('Event: '+event, playerId, data);
+				host.postMessage({
+					event: 'kalturaplayer.'+event,
+					id: playerId,
+					data: data
+				},'*');
+			}
+
+			function ensureReady(){
+				if(!ensureReady.called){
+					ensureReady.called = true;
+					setTimeout(function(){send('ready');},1);
+				}
+			}
+
+			function makeHandler(name){
+				ensureReady();
+
+				var newName = '__'+name,
+					seen = {};
+
+				window[newName] =  function(id){
+					seen[name] = (seen[name]||0) + 1;
+
+					if(/error/i.test(name) && seen[name] < 2){
+						console.warn('Ignoring intial erros about no source',name, arguments);
+						return;
+					}
+
+					send(name,Array.prototype.slice.call(arguments));
+				};
+				return newName;
+			}
+
+			function playerReady(){
+				var player = document.getElementById(playerId),
+					events = ['playerUpdatePlayhead','doPlay','doPause','playerPlayEnd','mediaLoadError','mediaError'],
+					i = events.length - 1;
+
+				for(i; i; i--){
+					player.addJsListener(events[i],makeHandler(events[i]));
+				}
+
+				setTimeout(ensureReady,5000);
+			}
+
+
+			function handleMessage(event){
+				var eventData = event.data,
+					player = document.getElementById(playerId);
+
+				//console.debug('From '+playerId+', to app:', eventData);
+				player.sendNotification(eventData.name,eventData.data);
+			}
+
+
+			window.addEventListener('message', handleMessage, false);
+
+			// Force HTML5 player over Flash player
+			mw.setConfig( 'KalturaSupport.LeadWithHTML5', true );
+			// Allow AirPlay
+			mw.setConfig('EmbedPlayer.WebKitAllowAirplay', true);
+			// Do not rewrite video tags willy-nilly
+			mw.setConfig( 'EmbedPlayer.RewriteSelector', false );
+
+			//mw.setConfig('debug', true);
+
+			window.playerId = '%id%';
+			window.host = window.top;
+
+			kWidget.embed({
+				targetId: playerId,
+				wid: '_%PARTNER_ID%',
+				uiconf_id: '%UICONF_ID%',
+				flashvars: {
+					"externalInterfaceDisabled": false,
+					"autoPlay": false
+				},
+				params:{
+					wmode: 'transparent'
+				},
+				readyCallback: playerReady
+			});
+		}
+
+
+	}
 });
