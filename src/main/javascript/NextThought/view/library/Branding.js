badfdfd91e5954ff45924ffe37cf09bba258b4ee
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-08-09 22:02:43 +0000

    stub branding styles
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@22303 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/library/Branding.js
+++ src/main/javascript/NextThought/view/library/Branding.js
@@ -1,6 +1,27 @@
 Ext.define('NextThought.view.library.Branding',{
 	extend: 'Ext.Component',
-	alias: 'widget.library-branding-box'
+	alias: 'widget.library-branding-box',
+
+	ui: 'branding',
+	cls: 'branding',
+
+	renderTpl: Ext.DomHelper.markup([
+		{cls:'logo', 'data-qtip':'{logo-alt-text}'},
+		{cls:'box',cn:[
+			{ cls: 'flourish1' },
+			{ cls: 'flourish2' },
+			{ cls:'title', html:'{title}' },
+			{ cls:'message', html:'{message}', cn:{ cls:'ellipsis', cn:[{},{},{}] }}
+		]}
+	]),
 
 
+	beforeRender: function(){
+		this.callParent(arguments);
+		this.renderData = Ext.apply(this.renderData||{},{
+			'logo-alt-text': getString('library:branding logo-alt-text',' '),
+			'title': getString('library:branding message-title',' '),
+			'message': getString('library:branding message',' ')
+		});
+	}
 });
