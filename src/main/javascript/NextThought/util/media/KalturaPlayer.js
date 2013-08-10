0f009c1e519cea0664e6c9a4fb406cd4c8d5fb6c
Sean Jones <sean.jones@nextthought.com>
2013-08-10 17:28:39 +0000

    Try a different URL for the Kaltura javascript. This URL is heavily tied to our account and is not a long term solution. A long term solution is to self host the open source version of the player code.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@22326 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/util/media/KalturaPlayer.js
+++ src/main/javascript/NextThought/util/media/KalturaPlayer.js
@@ -50,7 +50,7 @@ Ext.define('NextThought.util.media.KalturaPlayer',{
 		{tag:'html', lang:'en', cn:[
 			{tag:'head',cn:[
 				{tag:'script', type:'text/javascript', src:('{basePath}resources/lib/jQuery-1.8.0min.js')},
-				{tag:'script', type:'text/javascript', src:('http://html5.kaltura.org/js')},
+				{tag:'script', type:'text/javascript', src:('https://cdnapisec.kaltura.com/p/1500101/sp/150010100/embedIframeJs/uiconf_id/15491291/partner_id/1500101')},
 				{tag:'script', type:'text/javascript', html:'\n{code}\n'},
 				{tag:'style', type:'text/css', cn:[
 					'body, html { margin: 0; padding: 0; }'
