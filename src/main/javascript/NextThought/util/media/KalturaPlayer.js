bc65a6b2425f802a44414bd6b2a6136f1cddf3d8
Sean Jones <sean.jones@nextthought.com>
2013-08-10 21:26:58 +0000

    Yet another URL for the Kaltura JS.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@22343 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/util/media/KalturaPlayer.js
+++ src/main/javascript/NextThought/util/media/KalturaPlayer.js
@@ -50,7 +50,7 @@ Ext.define('NextThought.util.media.KalturaPlayer',{
 		{tag:'html', lang:'en', cn:[
 			{tag:'head',cn:[
 				{tag:'script', type:'text/javascript', src:('{basePath}resources/lib/jQuery-1.8.0min.js')},
-				{tag:'script', type:'text/javascript', src:('https://cdnapisec.kaltura.com/p/1500101/sp/150010100/embedIframeJs/uiconf_id/15491291/partner_id/1500101')},
+				{tag:'script', type:'text/javascript', src:(location.protocol+'//cdnapisec.kaltura.com/html5/html5lib/v1.8.9/mwEmbedLoader.php')},
 				{tag:'script', type:'text/javascript', html:'\n{code}\n'},
 				{tag:'style', type:'text/css', cn:[
 					'body, html { margin: 0; padding: 0; }'
