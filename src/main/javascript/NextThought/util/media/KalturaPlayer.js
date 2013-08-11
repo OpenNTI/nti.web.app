4d3e1921f86ed5bc6079bc9a53a69a64b40b3853
Sean Jones <sean.jones@nextthought.com>
2013-08-11 04:36:09 +0000

    Downgrade Kaltura versions. The 1.8.X series is behaving differently than the 1.7.X series.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@22405 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/util/media/KalturaPlayer.js
+++ src/main/javascript/NextThought/util/media/KalturaPlayer.js
@@ -50,7 +50,7 @@ Ext.define('NextThought.util.media.KalturaPlayer',{
 		{tag:'html', lang:'en', cn:[
 			{tag:'head',cn:[
 				{tag:'script', type:'text/javascript', src:('{basePath}resources/lib/jQuery-1.8.0min.js')},
-				{tag:'script', type:'text/javascript', src:(location.protocol+'//cdnapisec.kaltura.com/html5/html5lib/v1.8.9/mwEmbedLoader.php')},
+				{tag:'script', type:'text/javascript', src:(location.protocol+'//cdnapisec.kaltura.com/html5/html5lib/v1.7.0.5/mwEmbedLoader.php')},
 				{tag:'script', type:'text/javascript', html:'\n{code}\n'},
 				{tag:'style', type:'text/css', cn:[
 					'body, html { margin: 0; padding: 0; }'
