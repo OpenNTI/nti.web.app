d9a8cc4e2808fde15d8c6e96ab30195fdb75c9ee
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-08-12 13:42:17 +0000

    make natural layout work like before, but don't die if there is a problem.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@22589 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/layout/component/Natural.js
+++ src/main/javascript/NextThought/layout/component/Natural.js
@@ -4,22 +4,24 @@ Ext.define('NextThought.layout.component.Natural', {
 	type: 'natural',
 
 
-//	beginLayout: function (ownerContext) {
-//        this.callParent(arguments);
-//		if(!ownerContext.bodyContext){
-//			ownerContext.bodyContext = ownerContext.getEl('el');
-//		}
-//    },
+	beginLayout: function (ownerContext) {
+        this.callParent(arguments);
+		if(!ownerContext.bodyContext){
+			try{
+				ownerContext.bodyContext = ownerContext.getEl('el');
+			}catch(e){
+				ownerContext.bodyContent = null;
+			}
+		}
+    },
 
 
 	publishInnerWidth: function(ownerContext, width){
-//		return ownerContext.bodyContext.setWidth(width, false);
-		return ownerContext.setWidth(width, false);
+		return (ownerContext.bodyContext || ownerContext).setWidth(width, false);
 	},
 
 
 	publishInnerHeight: function (ownerContext, height) {
-//		return ownerContext.bodyContext.setHeight(height, false);
-		return ownerContext.setHeight(height, false);
+		return (ownerContext.bodyContext || ownerContext).setHeight(height, false);
 	}
 });
