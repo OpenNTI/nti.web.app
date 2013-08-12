15c7d3e5140134ac564c2421e4d23c2f2da053b0
Andrew Ligon <andrew.ligon@nextthought.com>
2013-08-12 00:35:49 +0000

    Added helper functions on the CommunityBoard model, to get the course (if any) that owns it.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@22465 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/model/forums/CommunityBoard.js
+++ src/main/javascript/NextThought/model/forums/CommunityBoard.js
@@ -3,5 +3,25 @@ Ext.define('NextThought.model.forums.CommunityBoard',{
 
 	fields: [
 		{name:'title', type:'auto', persist:false}
-	]
+	],
+
+	getRelatedCourse: function(){
+		return this.course || this.findCourse();
+	},
+
+	belongsToCourse: function(){
+		return !!this.getRelatedCourse();
+	},
+
+	findCourse: function(){
+		var me = this;
+
+		Library.courseStore.each(function(title){
+			if(me.getId() == title.getBoard()){
+				me.course = title;
+			}
+		});
+
+		return me.course || null;
+	}
 });
