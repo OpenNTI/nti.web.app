163d918c7bc9b158bad4a1752c5a1f180cdd2992
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-08-12 15:15:09 +0000

    put something (the text from Aarons design) in the message box for the library branding
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@22603 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/resources/strings/platform.ou.edu/strings.js
+++ src/main/resources/strings/platform.ou.edu/strings.js
@@ -1,3 +1,6 @@
 window.NTIStrings = window.NTIStrings || {};
 
 NTIStrings['Available for Purchase'] = 'Available Courses';
+NTIStrings['library:branding logo-alt-text'] = 'The University of Oklahoma';
+NTIStrings['library:branding message-title'] = 'Online Learning Initiative';
+NTIStrings['library:branding message'] = 'The University of Oklahoma is proud to announce an exciting new partnership with NextThought!';
