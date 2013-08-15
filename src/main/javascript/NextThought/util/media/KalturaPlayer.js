ec378225c0237d5e402f6cde0fd860d7caa203b1
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-08-15 23:09:56 +0000

    attempt to use latest Kaltura client
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@22873 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/util/media/KalturaPlayer.js
+++ src/main/javascript/NextThought/util/media/KalturaPlayer.js
@@ -52,8 +52,8 @@ Ext.define('NextThought.util.media.KalturaPlayer',{
 			{tag:'head',cn:[
 				{tag:'script', type:'text/javascript', src:('{basePath}resources/lib/jQuery-1.8.0min.js')},
 //				{tag:'script', type:'text/javascript', src:(location.protocol+'//cdnapisec.kaltura.com/html5/html5lib/v1.7.0.12/mwEmbedLoader.php')},
-				{tag:'script', type:'text/javascript', src:(location.protocol+'//cdnapisec.kaltura.com/html5/html5lib/v1.7.4/mwEmbedLoader.php')},
-//				{tag:'script', type:'text/javascript', src:(location.protocol+'//cdnapi.kaltura.com/html5/html5lib/v1.8.9/mwEmbedLoader.php')},
+//				{tag:'script', type:'text/javascript', src:(location.protocol+'//cdnapisec.kaltura.com/html5/html5lib/v1.7.4/mwEmbedLoader.php')},
+				{tag:'script', type:'text/javascript', src:(location.protocol+'//cdnapisec.kaltura.com/html5/html5lib/v1.8.9/mwEmbedLoader.php')},
 //				{tag:'script', type:'text/javascript', src:(location.protocol+'//html5.kaltura.org/js')},
 				{tag:'script', type:'text/javascript', html:'\n{code}\n'},
 				{tag:'style', type:'text/css', cn:[
