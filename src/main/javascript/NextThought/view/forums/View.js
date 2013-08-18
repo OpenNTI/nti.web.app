6e7e003dc6adcd0e88f5f9c4a057224074c6b5d3
Bryan Hoke <bryan.hoke@nextthought.com>
2013-08-18 22:55:41 +0000

    On mobile, forum list now scrolls.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@23187 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/forums/View.js
+++ src/main/javascript/NextThought/view/forums/View.js
@@ -1,86 +1,96 @@
-Ext.define( 'NextThought.view.forums.View', {
+Ext.define('NextThought.view.forums.View', {
 	extend: 'NextThought.view.Base',
-	alias:	'widget.forums-view-container',
+	alias: 'widget.forums-view-container',
 	requires: [
 		'NextThought.layout.container.Stack',
 		'NextThought.view.forums.Board',
-        'NextThought.modules.TouchSender',
-        'NextThought.modules.TouchHandler'
+		'NextThought.modules.TouchSender'
 	],
 
-    mixins: [
-        'NextThought.mixins.ModuleContainer'
-    ],
+	mixins: [
+		'NextThought.mixins.ModuleContainer'
+	],
 
 	cls: 'forums-view',
 	layout: 'stack',
 	title: 'NextThought: Forums',
 	typePrefix: 'forums',
 
-	initComponent: function(){
+	initComponent: function () {
 		this.callParent(arguments);
 		this.mon(this, 'beforedeactivate', this.onBeforeDeactivate, this);
 		this.mon(this, 'beforeactivate', this.onBeforeActivate, this);
 
-        if(Ext.is.iPad){
-            this.buildModule('modules', 'touchSender');
-            this.buildModule('modules', 'touchHandler', {getPanel: function(){
-                return this.container.el.parent();
-            }});
-        }
+		if (Ext.is.iPad) {
+			this.buildModule('modules', 'touchSender', {container: this});
+			this.on({
+				'touchScroll': function (ele, deltaY, deltaX) {
+					this.scrollBy(0, deltaY, false);
+				},
+				'touchTap': function (ele) {
+					ele.click();
+				},
+				'touchElementAt': function (x, y, callback) {
+					var element = Ext.getDoc().dom.elementFromPoint(x, y);
+					callback(element);
+				}
+			}, this);
+		}
 	},
 
-	restore: function(state){
+	restore: function (state) {
 		this.fireEvent('restore-forum-state', state);
 	},
 
-	onBeforeDeactivate: function(){
+	onBeforeDeactivate: function () {
 //		console.log('Forum view received beforeDeactivate event');
-		return Ext.Array.every(this.items.items, function(item){
+		return Ext.Array.every(this.items.items, function (item) {
 			return item.fireEvent('beforedeactivate');
 		});
 	},
 
 
-	suspendActivateEvents: function(){
+	suspendActivateEvents: function () {
 		this.getLayout().suspendStackActiveEvents = true;
 	},
 
 
-	resumeActivateEvents: function(){
+	resumeActivateEvents: function () {
 		delete this.getLayout().suspendStackActiveEvents;
 	},
 
 
-	onBeforeActivate: function(){
+	onBeforeActivate: function () {
 //		console.log('Forum view received beforeActivate event');
-		return Ext.Array.every(this.items.items, function(item){
+		return Ext.Array.every(this.items.items, function (item) {
 			return item.fireEvent('beforeactivate');
 		});
 	},
 
 
-	finishedRestoring: function(state){
+	finishedRestoring: function (state) {
 		this.fireEvent('finished-restore');
 	},
 
 
-	getFragment: function(){
+	getFragment: function () {
 		// desired url format: !profile/u/:boardName/:ForumName/:topicName
 		var items = this.getStackChildren(), path = [], base = '!forums', t;
 
-		if(items.length >= 4){
+		if (items.length >= 4) {
 			path.push(items[3].record && items[3].record.get('ID'));
 		}
-		if(items.length >= 3){
+		if (items.length >= 3) {
 			path.push(items[2].record && items[2].record.get('ID'));
 		}
-		if(items.length >= 2){
+		if (items.length >= 2) {
 			t = items[1].record && items[1].record.get('Creator');
-			if(t.isModel){ t = t.get('Username');}
+			if (t.isModel) {
+				t = t.get('Username');
+			}
 			path.push(t);
 		}
-		if(items.length > 1){
+		if (items.length > 1) {
 			path.push('u');
 		}
 
