495cb5a9d97d225465dedf96f89850fb4c502181
Chris Utz <chris.utz@nextthought.com>
2013-08-19 11:12:59 +0000

    Wording for super duper secret request alias change.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@23277 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/resources/strings/strings.js
+++ src/main/resources/strings/strings.js
@@ -6,15 +6,15 @@ window.NTIStrings = window.NTIStrings || {};
 NTIStrings['default_scoreboard_message'] = 'Results';
 
 NTIStrings['Apologies'] = 'Our Apologies!';
-NTIStrings['Cannot load page.'] = 'Cannot load page. Please try again later.'
+NTIStrings['Cannot load page.'] = 'Cannot load page. Please try again later.';
 
-NTIStrings['forum_sharing_label'] = 'Share with all students.'
+NTIStrings['forum_sharing_label'] = 'Share with all students.';
 
-NTIStrings['contact_us_title'] = 'Contact Us...'
-NTIStrings['contact_us_message'] = 'Please use the form below to share your comments, report an issue, or suggest new features.  If you need help or have a question about the features, please take a look at the NextThought Help Center.  We may already have content there to help you.'
+NTIStrings['contact_us_title'] = 'Contact Us...';
+NTIStrings['contact_us_message'] = 'Please use the form below to share your comments, report an issue, or suggest new features.  If you need help or have a question about the features, please take a look at the NextThought Help Center.  We may already have content there to help you.';
 
-NTIStrings['alias_request_title'] = 'Request Alias Change...'
-NTIStrings['alias_request_message'] = 'Please use the form below to request an alias change for your account.  Please let us know the desired alias and the reason you are requesting this change.  We will review the request and let you know when the request has been processed.'
+NTIStrings['alias_request_title'] = 'Request Name Change';
+NTIStrings['alias_request_message'] = 'Please use the form below to request a change in the way your name is displayed. Submit the name you would like to use and the reason for making this change.';
 
 NTIStrings['no_contact_header'] = 'No Contacts Yet';
 NTIStrings['no_contact_sub'] = 'Click search to find some.';
