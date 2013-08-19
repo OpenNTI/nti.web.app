f64ca29be449f9d3c91a3b4ece5e481803f5232e
Chris Utz <chris.utz@nextthought.com>
2013-08-19 11:28:39 +0000

    Mas wording para OU
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@23280 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/resources/strings/platform.ou.edu/strings.js
+++ src/main/resources/strings/platform.ou.edu/strings.js
@@ -4,3 +4,5 @@ NTIStrings['Available for Purchase'] = 'Available Courses';
 NTIStrings['library:branding logo-alt-text'] = 'The University of Oklahoma';
 NTIStrings['library:branding message-title'] = 'Online Learning Initiative';
 NTIStrings['library:branding message'] = 'The University of Oklahoma is proud to announce an exciting new partnership with NextThought!';
+
+NTIStrings['reset-password-not-allowed-subtext'] = 'You are not allowed to change your password at this time.  Please contact OU Information Technology at 325-Help for assistance.';
