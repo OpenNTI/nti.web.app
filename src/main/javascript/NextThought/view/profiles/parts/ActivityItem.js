d85a8fd765069e9183d37040296ad503da2d07a0
Bryan Hoke <bryan.hoke@nextthought.com>
2013-08-21 23:32:47 +0000

    Code reformat.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@23443 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/profiles/parts/ActivityItem.js
+++ src/main/javascript/NextThought/view/profiles/parts/ActivityItem.js
@@ -1,4 +1,4 @@
-Ext.define('NextThought.view.profiles.parts.ActivityItem',{
+Ext.define('NextThought.view.profiles.parts.ActivityItem', {
 	extend: 'NextThought.view.annotations.note.Panel',
 	requires: [
 		'NextThought.mixins.note-feature.GetLatestReply',
@@ -40,96 +40,98 @@ Ext.define('NextThought.view.profiles.parts.ActivityItem',{
 	},
 
 
-	initComponent: function(){
-		if(!this.record || !this.record.isModel){
+	initComponent: function () {
+		if (!this.record || !this.record.isModel) {
 			Ext.Error.raise('We need a record for this component');
 		}
 		this.callParent(arguments);
 	},
 
 
-	setRecord: function(record){
+	setRecord: function (record) {
 		this.callParent(arguments);
 		this.maybeFillIn();
 	},
 
 
-	updateFromRecord: function(){
+	updateFromRecord: function () {
 		this.callParent(arguments);
 	},
 
 
-	afterRender: function(){
+	afterRender: function () {
 		this.callParent(arguments);
 
-		if(this.commentsEl && this.commentsEl.dom){
-			this.mon(this.commentsEl, 'click', this.clickedRevealAllReplies,this);
+		if (this.commentsEl && this.commentsEl.dom) {
+			this.mon(this.commentsEl, 'click', this.clickedRevealAllReplies, this);
 		}
 
-		if(this.replyButton && this.replyButton.dom){
-			this.mon( this.replyButton, 'click', this.clickedRevealAllReplies, this);
+		if (this.replyButton && this.replyButton.dom) {
+			this.mon(this.replyButton, 'click', this.clickedRevealAllReplies, this);
 		}
 
 		this.mon(this.editEl, 'click', 'onEdit', this);
-		this.mon( this.deleteEl, 'click', this.onDelete, this);
-		this.mon( this.contextWrapEl, 'click', this.goToObject, this);
-		this.on( 'reveal-replies', this.clickedRevealAllReplies);
+		this.mon(this.deleteEl, 'click', this.onDelete, this);
+		this.mon(this.contextWrapEl, 'click', this.goToObject, this);
+		this.on('reveal-replies', this.clickedRevealAllReplies);
 
 		//NOTE: We run into a case where a reply to one of our replies doesn't trigger opening the reply editor.
 		// It was mainly a timing issue, so now listen to add events.
-		this.on('add', function(cmp, child){
-			if( this.replyToId && child.record && child.record.getId() === this.replyToId){
+		this.on('add', function (cmp, child) {
+			if (this.replyToId && child.record && child.record.getId() === this.replyToId) {
 				Ext.defer(this.maybeOpenReplyEditor, 1, this);
 			}
 		});
 	},
 
 
-	addAdditionalRecordListeners: function(record){
+	addAdditionalRecordListeners: function (record) {
 		this.callParent(arguments);
-		this.mon( record, 'convertedToPlaceholder', this.destroy, this);
+		this.mon(record, 'convertedToPlaceholder', this.destroy, this);
 	},
 
 
-	removeAdditionalRecordListeners: function(record){
+	removeAdditionalRecordListeners: function (record) {
 		this.callParent(arguments);
-		this.mun( record, 'convertedToPlaceholder', this.destroy, this);
+		this.mun(record, 'convertedToPlaceholder', this.destroy, this);
 	},
 
 
-	onBeforeAdd: function(cmp){
+	onBeforeAdd: function (cmp) {
 		this.callParent(arguments);
-		if(!this.isExpanded()){
-			if(this.items.last()){
+		if (!this.isExpanded()) {
+			if (this.items.last()) {
 				this.items.last().destroy();
 			}
 		}
 	},
 
 
-	updateCount: function(){
-		if(this.commentsEl){
+	updateCount: function () {
+		if (this.commentsEl) {
 			var c = this.record.getReplyCount() || 0;
 			console.log('count was update to: ', c);
 			this.commentsEl.update(Ext.Format.plural(c, 'comment'));
 		}
 	},
 
-	clickedRevealAllReplies: function(){
-		this.mun( this.replyButton, 'click', this.clickedRevealAllReplies, this);
-		if(!this.commentsEl){ return; }
+	clickedRevealAllReplies: function () {
+		this.mun(this.replyButton, 'click', this.clickedRevealAllReplies, this);
+		if (!this.commentsEl) {
+			return;
+		}
 		delete this.commentsEl;
 
 		this.shouldShowReplies();
 	},
 
 
-	shouldShowReplies: function(){
+	shouldShowReplies: function () {
 		this.suspendLayouts();
 		this.removeAll();
 		this.resumeLayouts();
 
-		Ext.defer(function(){
+		Ext.defer(function () {
 			Ext.suspendLayouts();
 			this.addReplies(this.record.children);
 			Ext.resumeLayouts(true);
@@ -137,7 +139,7 @@ Ext.define('NextThought.view.profiles.parts.ActivityItem',{
 	},
 
 
-	onDelete: function(){
+	onDelete: function () {
 		var me = this;
 		/*jslint bitwise: false*/ //Tell JSLint to ignore bitwise opperations
 		Ext.Msg.show({
@@ -147,8 +149,8 @@ Ext.define('NextThought.view.profiles.parts.ActivityItem',{
 			icon: 'warning-red',
 			buttonText: {'ok': 'Delete'},
 			title: 'Are you sure?',
-			fn: function(str){
-				if(str === 'ok'){
+			fn: function (str) {
+				if (str === 'ok') {
 					me.record.destroy();
 					me.adjustRootsReferenceCount(me.record);
 				}
@@ -157,34 +159,38 @@ Ext.define('NextThought.view.profiles.parts.ActivityItem',{
 	},
 
 
-	isExpanded: function(){ return !this.commentsEl; },
+	isExpanded: function () {
+		return !this.commentsEl;
+	},
 
-	setRecordTitle: function(){
-		function callback(snip, value){
-			if(snip && snip !== value){
-				me.subjectEl.set({'data-qtip':value});
+
+	setRecordTitle: function () {
+		function callback(snip, value) {
+			if (snip && snip !== value) {
+				me.subjectEl.set({'data-qtip': value});
 			}
 			me.subjectEl.update(snip || 'Subject');
-			if(!snip){
+			if (!snip) {
 				me.subjectEl.addCls('no-subject');
 				me.name.addCls('no-subject');
 			}
 		}
 
-		var me  = this;
+		var me = this;
 		me.record.resolveNoteTitle(callback);
 	},
 
 
-
-	maybeFillIn: function(){
+	maybeFillIn: function () {
 		var me = this,
 			D = Ext.dom.Element.DISPLAY,
 			subject,
 			loaded = me.loaded,
-			onScreen = loaded || (me.el && me.el.first().isOnScreenRelativeTo(Ext.get('profile'),{bottom:1000}));
+			onScreen = loaded || (me.el && me.el.first().isOnScreenRelativeTo(Ext.get('profile'), {bottom: 1000}));
 
-		if(loaded || !onScreen){return;}
+		if (loaded || !onScreen) {
+			return;
+		}
 
 		me.loaded = true;
 
@@ -196,7 +202,7 @@ Ext.define('NextThought.view.profiles.parts.ActivityItem',{
 		me.editEl.setVisibilityMode(D);
 		me.footEl.setVisibilityMode(D);
 
-		if(isMe(me.record.get('Creator'))){
+		if (isMe(me.record.get('Creator'))) {
 			me.flagEl.hide();
 		}
 		else {
@@ -205,12 +211,12 @@ Ext.define('NextThought.view.profiles.parts.ActivityItem',{
 			me.flagEl.addCls('last');
 		}
 
-		LocationMeta.getMeta(me.record.get('ContainerId'),me.setLocation,me);
-		if(me.root){
+		LocationMeta.getMeta(me.record.get('ContainerId'), me.setLocation, me);
+		if (me.root) {
 			me.contextEl.show();
 			me.contextEl.mask('Loading...');
-			me.loadContext(function(){
-				if( me.contextEl ){
+			me.loadContext(function () {
+				if (me.contextEl) {
 					me.contextEl.unmask();
 				}
 			});
@@ -218,34 +224,34 @@ Ext.define('NextThought.view.profiles.parts.ActivityItem',{
 	},
 
 
-	onEdit: function(){
+	onEdit: function () {
 		this.callParent(arguments);
 		this.footEl.hide();
 	},
 
 
-	activateReplyEditor: function(){
+	activateReplyEditor: function () {
 		this.callParent(arguments);
 		this.addCls('has-active-editor');
 	},
 
 
-	deactivateReplyEditor: function(){
+	deactivateReplyEditor: function () {
 		this.callParent(arguments);
 		this.removeCls('has-active-editor');
 		this.footEl.show();
 	},
 
 
-	setContext: function(){
+	setContext: function () {
 		this.callParent(arguments);
-		if( this.context ){
+		if (this.context) {
 			this.context.select('iframe').remove();
 		}
 	},
 
 
-	loadContext: function(fin){
+	loadContext: function (fin) {
 		var me = this,
 			r = me.record,
 			cid = r.get('ContainerId'),
@@ -253,14 +259,14 @@ Ext.define('NextThought.view.profiles.parts.ActivityItem',{
 			C = ContentUtils,
 			metaHandled = true;
 
-		function parse(content){
+		function parse(content) {
 			var dom = C.parseXML(C.fixReferences(content, metaInfo.absoluteContentRoot));
-			me.setContext(dom,dom);
+			me.setContext(dom, dom);
 		}
 
-		function error(req,resp){
-			function filter(item){
-				if(item instanceof NextThought.model.Course){
+		function error(req, resp) {
+			function filter(item) {
+				if (item instanceof NextThought.model.Course) {
 					return Boolean(item.getLink('enroll'));
 				}
 				return true;
@@ -271,24 +277,24 @@ Ext.define('NextThought.view.profiles.parts.ActivityItem',{
 				ntiid = req && req.ntiid,
 				p = ContentUtils.purchasableForContentNTIID(ntiid, filter);
 
-			if(resp.status === 403){
+			if (resp.status === 403) {
 				me.handlePurchasable(p, el);
 				return;
 			}
 
 			metaHandled = false;
 
-			ContentUtils.findContentObject(cid, function(obj, meta){
+			ContentUtils.findContentObject(cid, function (obj, meta) {
 				//TOOD need a generic framework for various objects here
-				if(obj && /ntivideo/.test(obj.mimeType || obj.MimeType)){
+				if (obj && /ntivideo/.test(obj.mimeType || obj.MimeType)) {
 					var src, sources, contextEl;
 					console.log('Need to set context being video', obj);
-					if(meta){
+					if (meta) {
 						me.setLocation(meta);
 						//me.locationEl.update(meta.getPathLabel());
-						if(me.context){
+						if (me.context) {
 							contextEl = me.context.up('.context');
-							if(contextEl){
+							if (contextEl) {
 								contextEl.addCls('video-context');
 							}
 							me.context.setHTML('');
@@ -296,7 +302,7 @@ Ext.define('NextThought.view.profiles.parts.ActivityItem',{
 
 						sources = obj.sources;
 
-						if(!Ext.isEmpty(sources)){
+						if (!Ext.isEmpty(sources)) {
 							src = sources.first().thumbnail;
 						}
 
@@ -306,19 +312,20 @@ Ext.define('NextThought.view.profiles.parts.ActivityItem',{
 								tag: 'img',
 								cls: 'video-thumbnail',
 								src: src
-							}]);
+							}
+						]);
 					}
 				}
-				else{
-					if(resp.status === 404){
-						if(p){
+				else {
+					if (resp.status === 404) {
+						if (p) {
 							me.handlePurchasable(p, el);
 							Ext.callback(fin);
 							return;
 						}
 
 						meta = ContentUtils.getLocation(ntiid);
-						if(meta){
+						if (meta) {
 							me.locationEl.update(meta.getPathLabel());
 							me.context.update(meta.location && meta.location.getAttribute('desc'));
 							Ext.callback(fin);
@@ -332,52 +339,52 @@ Ext.define('NextThought.view.profiles.parts.ActivityItem',{
 		}
 
 
-		LocationMeta.getMeta(cid, function(meta){
+		LocationMeta.getMeta(cid, function (meta) {
 			metaInfo = meta;
 
-			function maybeFin(){
-				if(metaHandled){
+			function maybeFin() {
+				if (metaHandled) {
 					Ext.callback(fin);
 				}
 			}
 
-			C.spider(cid,maybeFin,parse,error);
+			C.spider(cid, maybeFin, parse, error);
 		}, me);
 	},
 
 
-	handlePurchasable: function(purchasable, el){
+	handlePurchasable: function (purchasable, el) {
 		var me = this,
 			tpl = me.needsActionTplMap[purchasable.get('MimeType')];
 
 		me.requiresPurchase = true;
 		me.purchasable = purchasable;
 		el.removeCls('content-callout').addCls('purchase');
-		if(tpl){
+		if (tpl) {
 			me[tpl].overwrite(el, purchasable.getData(), true);
 		}
 
 		Ext.DomHelper.append(me.getEl(), {
 			cls: 'purchasable-mask',
-			style: {top: (me.itemEl.getY() - me.el.getY())+'px'}
+			style: {top: (me.itemEl.getY() - me.el.getY()) + 'px'}
 		});
 	},
 
 
-	goToObject: function(){
+	goToObject: function () {
 		var rec = this.record,
 			cid;
 
 		//Show purchase window if we're purchase-able
-		if(this.requiresPurchase){
+		if (this.requiresPurchase) {
 			this.fireEvent('show-purchasable', this, this.purchasable);
 			return;
 		}
 
 		//If we are a placholder find a reply to navigate to
-		if(!rec || rec.placeholder){
-			Ext.each(this.down('[record]'), function(cmp){
-				if(cmp.record && !cmp.record.placholder){
+		if (!rec || rec.placeholder) {
+			Ext.each(this.down('[record]'), function (cmp) {
+				if (cmp.record && !cmp.record.placholder) {
 					rec = cmp.record;
 					return false; //break
 				}
@@ -387,48 +394,57 @@ Ext.define('NextThought.view.profiles.parts.ActivityItem',{
 
 		cid = rec ? rec.get('ContainerId') : null;
 
-		if(rec && cid){
+		if (rec && cid) {
 			this.fireEvent('navigation-selected', cid, rec, null);
 		}
 	},
 
 
-	setLocation: function(meta){
-		if(!meta){return;}
+	setLocation: function (meta) {
+		if (!meta) {
+			return;
+		}
 
-		if(!this.rendered){
-			this.on('afterrender',Ext.bind(this.setLocation,this,arguments),this,{single:true});
+		if (!this.rendered) {
+			this.on('afterrender', Ext.bind(this.setLocation, this, arguments), this, {single: true});
 			return;
 		}
 
 		this.locationEl.update(meta.getPathLabel());
 		this.locationIcon.setStyle({
-			backgroundImage: Ext.String.format('url({0})',meta.getIcon())
+			backgroundImage: Ext.String.format('url({0})', meta.getIcon())
 		});
 
 		this.locationEl.hover(
-				function(){Ext.fly(this).addCls('over');},
-				function(){Ext.fly(this).removeCls('over');});
+			function () {
+				Ext.fly(this).addCls('over');
+			},
+			function () {
+				Ext.fly(this).removeCls('over');
+			});
 
 		this.locationEl.on('click',
-				Ext.bind(this.fireEvent,this,['navigation-selected', meta.NTIID, null, null]));
+			Ext.bind(this.fireEvent, this, ['navigation-selected', meta.NTIID, null, null]));
 	}
 
 
-},function(){
+}, function () {
 
 	this.prototype.renderTpl = Ext.DomHelper.markup([
 		{
 			cls: 'note profile-activity-item',
-			cn:[
-				{ cls: 'content-callout', cn:[
+			cn: [
+				{ cls: 'content-callout', cn: [
 					{ cls: 'icon' },
-					{ cn:[
+					{ cn: [
 						{ cls: 'location link'},
-						{ cls: 'context', cn: [{tag: 'canvas'},{cls: 'text'}] }
+						{ cls: 'context', cn: [
+							{tag: 'canvas'},
+							{cls: 'text'}
+						] }
 					]}
 				]},
-				{ cls:'item', cn:[
+				{ cls: 'item', cn: [
 					{ cls: 'avatar' },
 					{ cls: 'controls', cn: [
 						{ cls: 'favorite-spacer' },
@@ -453,14 +469,17 @@ Ext.define('NextThought.view.profiles.parts.ActivityItem',{
 							{ cls: 'delete', html: 'Delete' }
 
 						]
-					}]
+					}
+				]
 				}
 			]
-		},{
+		},
+		{
 			id: '{id}-body',
 			cls: 'note-replies',
-			cn:['{%this.renderContainer(out,values)%}']
-		},{
+			cn: ['{%this.renderContainer(out,values)%}']
+		},
+		{
 			cls: 'respond', cn: {
 			cn: [
 				{
@@ -475,10 +494,7 @@ Ext.define('NextThought.view.profiles.parts.ActivityItem',{
 });
 
 
-
-
-
-Ext.define('NextThought.view.profiles.parts.ActivityItemReply',{
+Ext.define('NextThought.view.profiles.parts.ActivityItemReply', {
 	extend: 'NextThought.view.annotations.note.Panel',
 	requires: ['NextThought.util.Content'],
 	alias: 'widget.profile-activity-item-reply',
@@ -492,41 +508,47 @@ Ext.define('NextThought.view.profiles.parts.ActivityItemReply',{
 		deleteEl: '.reply-options .delete'
 	},
 
-	afterRender: function(){
+	afterRender: function () {
 		var D = Ext.dom.Element.DISPLAY;
 		this.flagEl.setVisibilityMode(D);
 		this.editEl.setVisibilityMode(D);
 		this.deleteEl.setVisibilityMode(D);
 
-		try{
-			if(!this.up('profile-activity-item').isExpanded()){
-				this.mon( this.replyButton, 'click', this.shouldRevealReplies, this);
+		try {
+			if (!this.up('profile-activity-item').isExpanded()) {
+				this.mon(this.replyButton, 'click', this.shouldRevealReplies, this);
 			}
 		}
-		catch(e){
+		catch (e) {
 			console.warn('ActivityItemReply was not in an ActivityItem');
 		}
 
 		this.callParent(arguments);
-		this.mon( this.deleteEl, 'click', this.onDelete, this);
-		this.mon( this.editEl, 'click', this.onEdit, this);
+		this.mon(this.deleteEl, 'click', this.onDelete, this);
+		this.mon(this.editEl, 'click', this.onEdit, this);
 	},
 
-	shouldRevealReplies: function(){
-		this.mun( this.replyButton, 'click', this.shouldRevealReplies, this);
+	shouldRevealReplies: function () {
+		this.mun(this.replyButton, 'click', this.shouldRevealReplies, this);
 
 		var activityItem = this.up('profile-activity-item');
-		if(!activityItem || activityItem.isExpanded()){ return; }
+		if (!activityItem || activityItem.isExpanded()) {
+			return;
+		}
 		activityItem.replyToId = this.record.getId();
 		activityItem.fireEvent('reveal-replies');
 	},
 
-	setRecord: function(){
+	setRecord: function () {
 		this.callParent(arguments);
 
-		if(!this.rendered){return;}
+		if (!this.rendered) {
+			return;
+		}
 
-		if(isMe(this.record.get('Creator'))){ this.flagEl.hide(); }
+		if (isMe(this.record.get('Creator'))) {
+			this.flagEl.hide();
+		}
 		else {
 			this.editEl.hide();
 			this.deleteEl.hide();
@@ -534,37 +556,42 @@ Ext.define('NextThought.view.profiles.parts.ActivityItemReply',{
 		}
 	}
 
-},function(){
+}, function () {
 
-	this.prototype.renderTpl = Ext.DomHelper.markup([{
-		cls: 'reply profile-activity-reply-item',
-		cn: [
-			{ cls: 'avatar' },
-			{ cls: 'meta', cn: [
-				{ cls: 'controls', cn: [
-					{ cls: 'favorite-spacer' },
-					{ cls: 'like' }]},
-				{ tag: 'span', cls: 'name' },' ',
-				{ tag: 'span', cls: 'time' }
-			]},
-			{ cls: 'body' },
-			{ cls: 'respond',
-				cn: [
-					{
-						cls: 'reply-options',
-						cn: [
-							{ cls: 'reply', html: 'Reply' },
-							{ cls: 'edit', html: 'Edit' },
-							{ cls: 'flag', html: 'Report' },
-							{ cls: 'delete', html: 'Delete' }
-						]
-					}
-				]
-			}
-		]
-	},{
-		id: '{id}-body',
-		cls: 'note-replies',
-		cn:['{%this.renderContainer(out,values)%}']
-	}]);
+	this.prototype.renderTpl = Ext.DomHelper.markup([
+		{
+			cls: 'reply profile-activity-reply-item',
+			cn: [
+				{ cls: 'avatar' },
+				{ cls: 'meta', cn: [
+					{ cls: 'controls', cn: [
+						{ cls: 'favorite-spacer' },
+						{ cls: 'like' }
+					]},
+					{ tag: 'span', cls: 'name' },
+					' ',
+					{ tag: 'span', cls: 'time' }
+				]},
+				{ cls: 'body' },
+				{ cls: 'respond',
+					cn: [
+						{
+							cls: 'reply-options',
+							cn: [
+								{ cls: 'reply', html: 'Reply' },
+								{ cls: 'edit', html: 'Edit' },
+								{ cls: 'flag', html: 'Report' },
+								{ cls: 'delete', html: 'Delete' }
+							]
+						}
+					]
+				}
+			]
+		},
+		{
+			id: '{id}-body',
+			cls: 'note-replies',
+			cn: ['{%this.renderContainer(out,values)%}']
+		}
+	]);
 });
