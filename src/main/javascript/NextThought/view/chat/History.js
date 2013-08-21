181189f644daa1d367dc41809adfa9b7d95c33b2
Bryan Hoke <bryan.hoke@nextthought.com>
2013-08-21 19:51:45 +0000

    Code reformat.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@23430 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/chat/History.js
+++ src/main/javascript/NextThought/view/chat/History.js
@@ -1,4 +1,4 @@
-Ext.define('NextThought.view.chat.History',{
+Ext.define('NextThought.view.chat.History', {
 	extend: 'Ext.panel.Panel',
 	alias: 'widget.chat-history',
 	id: 'chat-history',//there should be ONLY ONE instance of this.
@@ -17,21 +17,21 @@ Ext.define('NextThought.view.chat.History',{
 	defaultType: 'chat-history-item',
 	border: false,
 
-	initComponent: function(){
+	initComponent: function () {
 		this.callParent(arguments);
 		this.initializeStore();
 	},
 
 
-	initializeStore: function(){
-		if(NextThought.store.PageItem.prototype.proxy.url === 'tbd'){
-			Ext.defer(this.initializeStore,100,this);
+	initializeStore: function () {
+		if (NextThought.store.PageItem.prototype.proxy.url === 'tbd') {
+			Ext.defer(this.initializeStore, 100, this);
 			return;
 		}
 
 
 		function mergeChatsFilter(item) {
-			var o = (item.get('Contributors')||[]).slice(),
+			var o = (item.get('Contributors') || []).slice(),
 				caller = mergeChatsFilter.caller || {},
 				seen = caller.seenOccupants || [];
 
@@ -39,7 +39,7 @@ Ext.define('NextThought.view.chat.History',{
 
 			o.sort();
 			o = o.join('|');
-			if(Ext.Array.contains(seen,o)){
+			if (Ext.Array.contains(seen, o)) {
 				return false;
 			}
 			seen.push(o);
@@ -47,9 +47,9 @@ Ext.define('NextThought.view.chat.History',{
 		}
 
 
-		var s = NextThought.store.PageItem.create({ filters:[ mergeChatsFilter ], pageSize: 100 });
-			
-		s.proxy.extraParams = Ext.apply(s.proxy.extraParams||{},{
+		var s = NextThought.store.PageItem.create({ filters: [ mergeChatsFilter ], pageSize: 100 });
+
+		s.proxy.extraParams = Ext.apply(s.proxy.extraParams || {}, {
 			sortOn: 'createdTime',
 			sortOrder: 'descending',
 			accept: [
@@ -58,7 +58,7 @@ Ext.define('NextThought.view.chat.History',{
 			].join(',')
 		});
 
-		this.mon(s,{
+		this.mon(s, {
 			scope: this,
 			load: 'storeLoaded',
 			add: 'storeLoaded'
@@ -69,14 +69,14 @@ Ext.define('NextThought.view.chat.History',{
 	},
 
 
-	onAdded: function(ownerCt){
+	onAdded: function (ownerCt) {
 		this.callParent(arguments);
-		this.relayEvents(ownerCt,['peek']);
+		this.relayEvents(ownerCt, ['peek']);
 	},
 
 
-	getStore: function(){
-		if(!this.store){
+	getStore: function () {
+		if (!this.store) {
 			this.initializeStore();
 		}
 
@@ -84,8 +84,8 @@ Ext.define('NextThought.view.chat.History',{
 	},
 
 
-	prefetchNext: function() {
-		if(!this.rendered){
+	prefetchNext: function () {
+		if (!this.rendered) {
 			Ext.log.warn('Not rendered yet');
 			return;
 		}
@@ -93,53 +93,59 @@ Ext.define('NextThought.view.chat.History',{
 			more = this.el.down('.more'),
 			body = this.ownerCt.el && this.ownerCt.el.down('#chat-dock-body');
 
-		if(more){
-			if(this.loadingMore){
+		if (more) {
+			if (this.loadingMore) {
 				this.loadingMore = false;
 				more.unmask();
-			}else{
+			} else {
 				more.mask('Loading');
 				this.loadingMore = true;
-				this.lastScroll = (body)? body.getScroll().top : Infinity;
+				this.lastScroll = (body) ? body.getScroll().top : Infinity;
 			}
 		}
 
 		if (!links) {
-			if(more){more.unmask();}
+			if (more) {
+				more.unmask();
+			}
 			return;
 		}
 
 
-		if(links && links['batch-next'] && !s.isLoading()){
+		if (links && links['batch-next'] && !s.isLoading()) {
 			s.clearOnPageLoad = false;
-			s.getProxy().buildUrl = function(){return links['batch-next'];};
+			s.getProxy().buildUrl = function () {
+				return links['batch-next'];
+			};
 			s.nextPage();
-		}else{
-			if(more){ more.unmask(); }
+		} else {
+			if (more) {
+				more.unmask();
+			}
 		}
 	},
 
 
-	storeLoaded: function(s){
-		if(!this.rendered){
-			this.on('afterrender',Ext.bind(this.storeLoaded,this,arguments),this,{single:true});
+	storeLoaded: function (s) {
+		if (!this.rendered) {
+			this.on('afterrender', Ext.bind(this.storeLoaded, this, arguments), this, {single: true});
 			return;
 		}
 
 
 		var body, l = s.batchLinks;
 		this.removeAll(true);
-		this.add(Ext.Array.map(s.getRange(),function(a){
+		this.add(Ext.Array.map(s.getRange(), function (a) {
 			return {record: a, store: s};
 		}));
 
-		if( this.items.length< 1 || !(l && l['batch-next'])){
+		if (this.items.length < 1 || !(l && l['batch-next'])) {
 			this.prefetchNext();
 		}
 
-		if(l && l['batch-next']){
+		if (l && l['batch-next']) {
 			this.add({
-				xtype:'box',
+				xtype: 'box',
 				autoEl: {
 					cls: 'more',
 					html: 'Load More'
@@ -153,7 +159,7 @@ Ext.define('NextThought.view.chat.History',{
 				}
 			});
 		}
-		else if(this.items.length < 1){
+		else if (this.items.length < 1) {
 			this.add({
 				xtype: 'box',
 				autoEl: {
@@ -163,9 +169,11 @@ Ext.define('NextThought.view.chat.History',{
 			});
 		}
 
-		if(this.loadingMore){
+		if (this.loadingMore) {
 			body = this.ownerCt.el.down('#chat-dock-body');
-			if(body){ body.scrollTo('top',this.lastScroll); }
+			if (body) {
+				body.scrollTo('top', this.lastScroll);
+			}
 			this.loadingMore = false;
 		}
 	}
@@ -173,9 +181,7 @@ Ext.define('NextThought.view.chat.History',{
 });
 
 
-
-
-Ext.define('NextThought.view.chat.HistoryItem',{
+Ext.define('NextThought.view.chat.HistoryItem', {
 	extend: 'Ext.Component',
 	alias: 'widget.chat-history-item',
 	cls: 'chat-dock-item',
@@ -183,13 +189,13 @@ Ext.define('NextThought.view.chat.HistoryItem',{
 
 	renderTpl: Ext.DomHelper.markup([
 		{cls: 'avatars {avatarCls}', cn: [
-			{cls: 'img1 avatar', style:{backgroundImage: '{img1}'} },
-			{cls: 'img2 avatar', style:{backgroundImage: '{img2}'} },
-			{cls: 'img3 avatar', style:{backgroundImage: '{img3}'} },
-			{cls: 'img4 avatar', style:{backgroundImage: '{img4}'} }
+			{cls: 'img1 avatar', style: {backgroundImage: '{img1}'} },
+			{cls: 'img2 avatar', style: {backgroundImage: '{img2}'} },
+			{cls: 'img3 avatar', style: {backgroundImage: '{img3}'} },
+			{cls: 'img4 avatar', style: {backgroundImage: '{img4}'} }
 		]},
 		{cls: 'wrap', cn: [
-			{cls: 'names {namesCls}', html:'{names}', 'data-count':'{count}'},
+			{cls: 'names {namesCls}', html: '{names}', 'data-count': '{count}'},
 			{cls: 'status', html: '{date:date("n/j/Y")} &middot; {duration}'}
 		]}
 	]),
@@ -205,39 +211,39 @@ Ext.define('NextThought.view.chat.HistoryItem',{
 	},
 
 
-	beforeRender: function(){
+	beforeRender: function () {
 		this.callParent(arguments);
 		this.fillInInformation();
 	},
 
 
-	afterRender: function(){
+	afterRender: function () {
 		this.callParent(arguments);
-		this.mon(this.el,'click','onClick',this);
+		this.mon(this.el, 'click', 'onClick', this);
 	},
 
 
-	onClick: function(e){
+	onClick: function (e) {
 		e.stopEvent();
 		this.getTranscriptsForOccupants();
 	},
 
 
-	getTranscriptsForOccupants: function(){
+	getTranscriptsForOccupants: function () {
 		var records = [],
 			store = this.store,
-			occupants = (this.record.get('Contributors')||[]).slice(),
+			occupants = (this.record.get('Contributors') || []).slice(),
 			length = occupants.length;
 
 
 		//Lets just assume that we have all of 'em in the map for now. (there is no way to query for these objects so
 		// paging them in is not really in the cards for now.)
-		store.snapshot.each(function(obj){
-			var list = (obj.get('Contributors')||[]),
-					len = list.length;
+		store.snapshot.each(function (obj) {
+			var list = (obj.get('Contributors') || []),
+				len = list.length;
 
-			if(length === len){
-				if(Ext.Array.intersect(occupants,list).length === length){
+			if (length === len) {
+				if (Ext.Array.intersect(occupants, list).length === length) {
 					records.push(obj);
 				}
 			}
@@ -248,7 +254,7 @@ Ext.define('NextThought.view.chat.HistoryItem',{
 	},
 
 
-	fillInInformation: function(){
+	fillInInformation: function () {
 		var me = this,
 			record = me.record,
 			roomInfo = record.get('RoomInfo'),
@@ -261,31 +267,31 @@ Ext.define('NextThought.view.chat.HistoryItem',{
 			isGroup = occ.length > 2;
 
 
-		data.duration = TimeUtils.getDurationText(started,ended);
+		data.duration = TimeUtils.getDurationText(started, ended);
 		data.date = started;
 
 		function fill(users) {
 			var userCount = 1;
 
-			if(Ext.isEmpty(users)){
-				console.error('This room info did not have any occupants',roomInfo);
+			if (Ext.isEmpty(users)) {
+				console.error('This room info did not have any occupants', roomInfo);
 				usernames.push('Empty');
 			}
 
-			Ext.each(users,function(u){
-				if(!isMe(u)){
-					if(userCount <= 4){
-						if(userCount > 1){
+			Ext.each(users, function (u) {
+				if (!isMe(u)) {
+					if (userCount <= 4) {
+						if (userCount > 1) {
 							data.avatarCls = 'quad';
-							if( me.rendered ){
+							if (me.rendered) {
 								me.avatarsEl.addCls(data.avatarCls);
 							}
 						}
 
 						//if(!isGroup){
-						data['img'+userCount] = 'url('+u.get('avatarURL')+')';
-						if( me.rendered ){
-							me['img'+userCount].setStyle({backgroundImage: data['img'+userCount]});
+						data['img' + userCount] = 'url(' + u.get('avatarURL') + ')';
+						if (me.rendered) {
+							me['img' + userCount].setStyle({backgroundImage: data['img' + userCount]});
 						}
 						userCount++;
 						//}
@@ -299,30 +305,30 @@ Ext.define('NextThought.view.chat.HistoryItem',{
 			});
 
 			//blank out the rest
-			for(userCount; userCount <= 4; userCount++){
-				delete data['img'+userCount];
-				if( me.rendered ){
-					me['img'+userCount].setStyle({backgroundImage: undefined});
+			for (userCount; userCount <= 4; userCount++) {
+				delete data['img' + userCount];
+				if (me.rendered) {
+					me['img' + userCount].setStyle({backgroundImage: undefined});
 				}
 			}
 
-			Ext.apply(data,{
+			Ext.apply(data, {
 				names: usernames.join(', '),
 				count: usernames.length
 			});
 
-			if(!me.rendered){
-				me.renderData = Ext.apply(me.renderData||{},data);
+			if (!me.rendered) {
+				me.renderData = Ext.apply(me.renderData || {}, data);
 				return;
 			}
 
-			me.namesEl.update(data.names).set({'data-count':data.count});
-			if(usernames.length > 1){
+			me.namesEl.update(data.names).set({'data-count': data.count});
+			if (usernames.length > 1) {
 				me.namesEl.addCls('overflown');
 			}
 		}
 
 
-		UserRepository.getUser(Ext.Array.merge(occ, contributors),fill);
+		UserRepository.getUser(Ext.Array.merge(occ, contributors), fill);
 	}
 });
