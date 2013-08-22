37e682e16413407e1922dac27fc8f8581483a305
Bryan Hoke <bryan.hoke@nextthought.com>
2013-08-22 01:06:52 +0000

    On mobile, content navigation menus now behave like they do on the desktop. Some reformatting.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@23446 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/menus/JumpTo.js
+++ src/main/javascript/NextThought/view/menus/JumpTo.js
@@ -1,93 +1,90 @@
 Ext.define('NextThought.view.menus.JumpTo', {
-    extend: 'Ext.menu.Menu',
-    alias: 'widget.jump-menu',
-    ui: 'nt',
-    plain: true,
-    showSeparator: false,
-    shadow: false,
-    frame: false,
-    border: false,
-    hideMode: 'display',
-    minWidth: 200,
-    maxWidth: 500,
+	extend: 'Ext.menu.Menu',
+	alias: 'widget.jump-menu',
+	ui: 'nt',
+	plain: true,
+	showSeparator: false,
+	shadow: false,
+	frame: false,
+	border: false,
+	hideMode: 'display',
+	minWidth: 200,
+	maxWidth: 500,
 
-    layout: {
-        type: 'vbox',
-        align: 'stretch',
-        overflowHandler: 'Scroller'
-    },
+	layout: {
+		type: 'vbox',
+		align: 'stretch',
+		overflowHandler: 'Scroller'
+	},
 
-    defaults: {
-        ui: 'jumpto-menuitem',
-        plain: true
-    },
+	defaults: {
+		ui: 'jumpto-menuitem',
+		plain: true
+	},
 
-    cls: 'jump-menu',
+	cls: 'jump-menu',
 
-    initComponent: function() {
-        var me = this;
-        this.callParent(arguments);
-        this.on('click', this.handleClick, this);
-    },
-
-
-    handleClick: function(menu, item) {
-        if (!item || !item.ntiid) {
-            return;
-        }
-
-        if (item.rememberLastLocation) {
-            this.fireEvent('set-last-location-or-root', item.ntiid);
-            return;
-        }
-
-        this.fireEvent('set-location', item.ntiid);
-    },
+	initComponent: function () {
+		var me = this;
+		this.callParent(arguments);
+		this.on('click', this.handleClick, this);
+	},
 
 
-    afterRender: function() {
-        this.callParent(arguments);
-        this.mon(this, {
-            scope: this,
-            'mouseleave': this.startHide,
-            'mouseenter': this.stopHide
-        });
-    },
+	handleClick: function (menu, item) {
+		if (!item || !item.ntiid) {
+			return;
+		}
+
+		if (item.rememberLastLocation) {
+			this.fireEvent('set-last-location-or-root', item.ntiid);
+			return;
+		}
+
+		this.fireEvent('set-location', item.ntiid);
+	},
 
 
-    startShow: function(el, align, offset) {
-        this.stopHide();
-        if (Ext.is.iPad) {
-            // Want it to be more responsive to taps
-            this.showBy(el, align, offset);
-        }
-        else {
-            this.showTimeout = Ext.defer(this.showBy, 750, this, [el,  align, offset]);
-        }
-    },
+	afterRender: function () {
+		this.callParent(arguments);
+		if (!Ext.is.iPad) { // iPad doesn't need/want these
+			this.mon(this, {
+				scope: this,
+				'mouseleave': this.startHide,
+				'mouseenter': this.stopHide
+			});
+		}
+	},
 
 
-    stopShow: function() {
-        this.startHide();
-        clearTimeout(this.showTimeout);
-    },
+	startShow: function (el, align, offset) {
+		this.stopHide();
+		if (Ext.is.iPad) {
+			// Want it to be more responsive to taps
+			this.showBy(el, align, offset);
+		}
+		else {
+			this.showTimeout = Ext.defer(this.showBy, 750, this, [el, align, offset]);
+		}
+	},
 
 
-    startHide: function() {
-        var me = this;
-        me.stopHide();
-        if (Ext.is.iPad) {
-            // So stray clicks will hide the menu and the next click on the nav-bar item will show the menu
-            me.showing = false;
-            me.leaveTimer = setTimeout(function() { me.hide(); }, 0);
-        }
-        else {
-            me.leaveTimer = setTimeout(function() { me.hide(); }, 500);
-        }
-    },
+	stopShow: function () {
+		this.startHide();
+		clearTimeout(this.showTimeout);
+	},
 
 
-    stopHide: function() {
-        clearTimeout(this.leaveTimer);
-    }
+	startHide: function () {
+		var me = this;
+		me.stopHide();
+		me.leaveTimer = setTimeout(function () {
+			me.hide();
+		}, 500);
+	},
+
+
+	stopHide: function () {
+		clearTimeout(this.leaveTimer);
+	}
 });
