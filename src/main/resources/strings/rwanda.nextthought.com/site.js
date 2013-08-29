415dba012a9b308f31407f7069158dccbb5bdab1
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2013-08-29 22:22:48 +0000

    add site specific stuff for CMU Rwanda.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@23792 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/main/resources/strings/rwanda.nextthought.com/site.js
@@ -0,0 +1,3 @@
+//NTIStrings['library:branding logo-alt-text'] = 'Carnegie Mellon University';
+//NTIStrings['library:branding message-title'] = 'Online Learning Initiative';
+//NTIStrings['library:branding message'] = 'Carnegie Mellon University is proud to announce an exciting new partnership with NextThought!';
