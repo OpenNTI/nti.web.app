4d6f89cd89d9dea1d813c21ccdf2a416b6c87162
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-09-05 20:51:30 +0000

    the beginnings of a better framework for touchmove handling.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@24029 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/Main.js
+++ src/main/javascript/NextThought/view/Main.js
@@ -30,7 +30,7 @@ Ext.define('NextThought.view.Main', {
 		this.hidden = Boolean(NextThought.phantomRender);
 		this.callParent(arguments);
 
-		if (Ext.is.iPad) {
+		if (Ext.is.iOS) {
 			document.ontouchstart = this.onTouchStart;
 
 			document.ontouchmove = this.onTouchMove;
@@ -41,27 +41,31 @@ Ext.define('NextThought.view.Main', {
 
 
 	onTouchStart: function (e) {
-		var touch = e.touches[0];
+		var touch = e.touches[0],
+			mouseEnterEvent,
+			mouseOverEvent,
+			mouseDownEvent;
+
 		if (!touch) {
 			return;
 		}
 
 		// Dispatch mouseenter
-		var mouseEnterEvent = document.createEvent('MouseEvents');
+		mouseEnterEvent = document.createEvent('MouseEvents');
 		mouseEnterEvent.initMouseEvent('mouseenter', true, true, window,
 			0, touch.screenX, touch.screenY, touch.clientX, touch.clientY,
 			false, false, false, false, 0, null);
 		touch.target.dispatchEvent(mouseEnterEvent);
 
 		// Dispatch mouseover
-		var mouseOverEvent = document.createEvent('MouseEvents');
+		mouseOverEvent  = document.createEvent('MouseEvents');
 		mouseOverEvent.initMouseEvent('mouseover', true, true, window,
 			0, touch.screenX, touch.screenY, touch.clientX, touch.clientY,
 			false, false, false, false, 0, null);
 		touch.target.dispatchEvent(mouseOverEvent);
 
 		// Dispatch mousedown
-		var mouseDownEvent = document.createEvent('MouseEvents');
+		mouseDownEvent = document.createEvent('MouseEvents');
 		mouseDownEvent.initMouseEvent('mousedown', true, true, window,
 			1, touch.screenX, touch.screenY, touch.clientX, touch.clientY,
 			false, false, false, false, 0, null);
@@ -70,7 +74,12 @@ Ext.define('NextThought.view.Main', {
 
 
 	onTouchMove: function (e) {
-		var touch = e.touches[0];
+		var touch = e.touches[0],
+			mouseMoveEvent, changedTouch,
+			mouseLeaveEvent,
+			mouseOutEvent,
+			mouseEnterEvent;
+
 		if (!touch) {
 			return;
 		}
@@ -78,31 +87,31 @@ Ext.define('NextThought.view.Main', {
 		e.preventDefault();
 
 		// Dispatch mousemove
-		var mouseMoveEvent = document.createEvent('MouseEvents');
+		mouseMoveEvent = document.createEvent('MouseEvents');
 		mouseMoveEvent.initMouseEvent('mousemove', true, true, window,
 			1, touch.screenX, touch.screenY, touch.clientX, touch.clientY,
 			false, false, false, false, 0, null);
 		touch.target.dispatchEvent(mouseMoveEvent);
 
 		// Dispatch leave/enter events if moving onto a different element
-		var changedTouch = e.changedTouches[0];
+		changedTouch = e.changedTouches[0];
 		if (changedTouch && changedTouch.target !== touch.target) {
 			// Dispatch mouseleave
-			var mouseLeaveEvent = document.createEvent('MouseEvents');
+			mouseLeaveEvent = document.createEvent('MouseEvents');
 			mouseLeaveEvent.initMouseEvent('mouseleave', true, true, window,
 				0, changedTouch.screenX, changedTouch.screenY, changedTouch.clientX, changedTouch.clientY,
 				false, false, false, false, 0, null);
 			changedTouch.target.dispatchEvent(mouseLeaveEvent);
 
 			// Dispatch mouseout
-			var mouseOutEvent = document.createEvent('MouseEvents');
+			mouseOutEvent = document.createEvent('MouseEvents');
 			mouseOutEvent.initMouseEvent('mouseout', true, true, window,
 				0, changedTouch.screenX, changedTouch.screenY, changedTouch.clientX, changedTouch.clientY,
 				false, false, false, false, 0, null);
 			changedTouch.target.dispatchEvent(mouseOutEvent);
 
 			// Dispatch mouseenter
-			var mouseEnterEvent = document.createEvent('MouseEvents');
+			mouseEnterEvent = document.createEvent('MouseEvents');
 			mouseEnterEvent.initMouseEvent('mouseenter', true, true, window,
 				0, touch.screenX, touch.screenY, touch.clientX, touch.clientY,
 				false, false, false, false, 0, null);
@@ -112,26 +121,30 @@ Ext.define('NextThought.view.Main', {
 
 
 	onTouchEnd: function (e) {
-		var touch = e.changedTouches[0];
+		var touch = e.changedTouches[0],
+			mouseUpEvent,
+			mouseLeaveEvent,
+			mouseOutEvent;
+
 		if (!touch) {
 			return;
 		}
 		// Dispatch mouseup
-		var mouseUpEvent = document.createEvent('MouseEvents');
+		mouseUpEvent = document.createEvent('MouseEvents');
 		mouseUpEvent.initMouseEvent('mouseup', true, true, window,
 			1, touch.screenX, touch.screenY, touch.clientX, touch.clientY,
 			false, false, false, false, 0, null);
 		touch.target.dispatchEvent(mouseUpEvent);
 
 		// Dispatch mouseleave
-		var mouseLeaveEvent = document.createEvent('MouseEvents');
+		mouseLeaveEvent = document.createEvent('MouseEvents');
 		mouseLeaveEvent.initMouseEvent('mouseleave', true, true, window,
 			0, touch.screenX, touch.screenY, touch.clientX, touch.clientY,
 			false, false, false, false, 0, null);
 		touch.target.dispatchEvent(mouseLeaveEvent);
 
 		// Dispatch mouseout
-		var mouseOutEvent = document.createEvent('MouseEvents');
+		mouseOutEvent = document.createEvent('MouseEvents');
 		mouseOutEvent.initMouseEvent('mouseout', true, true, window,
 			0, touch.screenX, touch.screenY, touch.clientX, touch.clientY,
 			false, false, false, false, 0, null);
@@ -141,7 +154,6 @@ Ext.define('NextThought.view.Main', {
 
 	afterRender: function () {
 		this.callParent(arguments);
-
 		var map = {
 			width: 'right',
 			height: 'bottom',
@@ -178,46 +190,160 @@ Ext.define('NextThought.view.Main', {
 
 		this.identity = this.sidebar.add({xtype: 'identity'});
 
-		if (Ext.is.iPad) {
-			var me = this,
-				optWindow;
+		if (Ext.is.iOS) {
+			this.setupTablet();
+		}
+	},
 
-			// Prevent two-finger panning
-			Ext.getDoc().on('gesturestart', function (e) {
-				e.preventDefault();
-			});
 
-			/*If user rotates to portrait, display screen saying to rotate it.
-			 * if they rotate back to landscape, destroy screen*/
-			window.addEventListener('orientationchange', function () {
-				if (optWindow) {
-					optWindow.destroy();
-					optWindow = null;
+	setupTablet: function(){
+		var me = this,
+			o = { height: '100%', width: '100%', overflow: 'hidden', webkitOverflowScrolling: 'none' },
+			cache = {};
+
+		function touchEnd(e){
+			touchHandler.lastEvent = null;
+			scrollLock(e);
+		}
+
+		function touchHandler(e){
+			var t = e.getTarget('.scrollable'),
+				el,xy, lastXY = touchHandler.lastEvent,
+				styles;
+
+			function testRange(e, direction, positionName, maxName){
+				var scroll = 'scroll'+(maxName||'Height'),
+					max = 'client'+(maxName||'Height'),
+					pos = 'scroll'+(positionName||'Top');
+				// The user is scrolling up, and the element is already scrolled to top
+				// OR
+				// The user is scrolling down, and the element is already scrolled to bottom
+				if((direction > 0 && t[pos] <= 0) || (direction < 0 && t[pos] >= (t[scroll] - t[max]))) {
+					e.preventDefault();
 				}
-				if (Math.abs(window.orientation) !== 90) {
-					optWindow = me.createPortraitOrientationScreen();
-					var iframe = optWindow.el.down('iframe');
-					iframe.el.dom.contentWindow.addEventListener('touchstart', function (e) {
-						e.preventDefault();
-					});
-					optWindow.show();
-				}
-			}, true);
+			}
 
+			if(t){
+				e.stopPropagation();
+				xy = e.getXY();
+				el = Ext.get(t);
+				if(!cache[el.id]){
+					cache[el.id] = el.getStyle(['overflow-x','overflow-y']);
+				}
+
+				styles = cache[el.id];
+				if(styles['overflow-x'] === 'hidden'){
+					t.scrollLeft = 0;
+					testRange(e, xy[1] - lastXY[1], 'Top', 'Height');
+				}
+
+				if(styles['overflow-y'] === 'hidden'){
+					t.scrollTop = 0;
+					testRange(e, xy[0] - lastXY[0], 'Left', 'Width');
+				}
+
+				touchHandler.lastEvent = e.getXY();
+			}
+			else {
+				//e.stopEvent();
+				delete touchHandler.lastEvent;
+			}
+		}
+
+
+		function scrollLock(e){
+			var el, styles, t = e.getTarget('.scrollable');
+			if(!t){return;}
+
+			el = Ext.get(t);
+			if(!cache[el.id]){
+				cache[el.id] = el.getStyle(['overflow-x','overflow-y']);
+			}
+			styles = cache[el.id];
+			if(styles['overflow-x'] === 'hidden'){
+				t.scrollLeft = 0;
+			}
+
+			if(styles['overflow-y'] === 'hidden'){
+				t.scrollTop = 0;
+			}
+
+		}
+
+
+		function touchStart(e) {
+			var t = e.getTarget('.scrollable');
+			if(!t){
+				return;
+			}
+
+			touchHandler.lastEvent = e.getXY();
+
+			if (t.scrollTop === 0) {
+				t.scrollTop = 1;
+			} else if (t.scrollHeight === t.scrollTop + t.offsetHeight) {
+				t.scrollTop -= 1;
+			}
+		}
+
+		Ext.getDoc().first('html').setStyle(o);
+		Ext.getBody().setStyle(o);
+
+
+		// Prevent two-finger panning
+		Ext.getDoc().swallowEvent('gesturestart',true)
+					.swallowEvent('touchmove',true);
+
+		// based on http://stackoverflow.com/a/14244680/823158 and http://stackoverflow.com/a/9417931/823158
+		Ext.getBody().on({
+			touchmove: touchHandler,
+			touchend: touchEnd,
+			touchcancel: touchEnd,
+			touchstart: touchStart
+		});
+
+		window.onscroll = function(){window.scrollTo(0,0);};
+		window.onresize = function() { Ext.getBody().setWidth(window.innerWidth).setHeight(window.innerHeight); };
+
+		this.lockOrientation();
+	},
+
+
+	lockOrientation: function(){
+		var optWindow, iframe;
+
+		/*If user rotates to portrait, display screen saying to rotate it.
+		 * if they rotate back to landscape, destroy screen*/
+		window.addEventListener('orientationchange', function () {
+			var iframe;
+
+			if (optWindow) {
+				optWindow.destroy();
+				optWindow = null;
+			}
 			if (Math.abs(window.orientation) !== 90) {
-				optWindow = this.createPortraitOrientationScreen();
-				var iframe = optWindow.el.down('iframe');
+				optWindow = me.createPortraitOrientationScreen();
+				iframe = optWindow.el.down('iframe');
 				iframe.el.dom.contentWindow.addEventListener('touchstart', function (e) {
 					e.preventDefault();
 				});
-
 				optWindow.show();
 			}
+		}, true);
 
-			document.body.onorientationchange = function () {
-				window.scrollTo(0, 0);
-			};
+		if (Math.abs(window.orientation) !== 90) {
+			optWindow = this.createPortraitOrientationScreen();
+			iframe = optWindow.el.down('iframe');
+			iframe.el.dom.contentWindow.addEventListener('touchstart', function (e) {
+				e.preventDefault();
+			});
+
+			optWindow.show();
 		}
+
+		document.body.onorientationchange = function () {
+			window.scrollTo(0, 0);
+		};
 	},
 
 
