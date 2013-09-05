8a531004723b423dca49934f1ddaf80893340b5b
Bryan Hoke <bryan.hoke@nextthought.com>
2013-09-05 17:58:50 +0000

    On mobile, questions no longer block content scrolling.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@24022 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/assessment/Question.js
+++ src/main/javascript/NextThought/view/assessment/Question.js
@@ -180,6 +180,13 @@ Ext.define('NextThought.view.assessment.Question', {
 		}, this, {single: true});
 		this.getTargetEl().addCls('indexed-content');
 		this.syncTop();
+
+		if (Ext.is.iPad) {
+			// Absorb event for scrolling
+			this.getEl().dom.addEventListener('touchmove', function (e) {
+				e.stopPropagation();
+			});
+		}
 	},
 
 
