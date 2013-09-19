62983940bf1fee5af299cc80a8630b9b062edd43
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-09-19 15:32:39 +0000

    delegation isn't needed anymore on those components
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@24497 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/reader/Panel.js
+++ src/main/javascript/NextThought/view/reader/Panel.js
@@ -22,7 +22,7 @@ Ext.define('NextThought.view.reader.Panel',{
 			align: 'stretch'
 		},
 		items: [
-			{ xtype: 'content-toolbar', hidden: true, delegate:[ 'content-view-container reader-content' ]},
+			{ xtype: 'content-toolbar', hidden: true },
 			{ xtype: 'reader-content', flex: 1 }
 		]
 	},{
