c49ffefbef760ac869a0dc7941311471d4d45d1d
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-10-11 04:33:30 +0000

    checkpoint: dragging enabled, and styled. Next: drop.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@25479 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/assessment/input/Matching.js
+++ src/main/javascript/NextThought/view/assessment/input/Matching.js
@@ -2,7 +2,14 @@ Ext.define('NextThought.view.assessment.input.Matching',{
 	extend: 'NextThought.view.assessment.input.Base',
 	alias: 'widget.question-input-matchingpart-v3',
 
+	requires: [
+		'Ext.dd.DragZone',
+		'Ext.dd.DropZone',
+		'Ext.dd.StatusProxy'
+	],
 
+
+	//<editor-fold desc="Setup & Config">
 	inputTpl: Ext.DomHelper.markup([
 		{ cls: 'terms', cn:{ 'tag': 'tpl', 'for': 'terms', cn: [{
 			cls: 'target term drag', 'data-match': '{[xindex]}', cn: [
@@ -58,19 +65,98 @@ Ext.define('NextThought.view.assessment.input.Matching',{
 		el = ownerMain.getInsertionEl();
 		this.injectionSource.appendTo(el);
 		//ownerMain.updateLayout();
+
+
+		this.setupDragZones();
+		this.setupDropZones();
+	},
+	//</editor-fold>
+
+
+	//<editor-fold desc="Drag & Drop">
+	getDragProxy: function(){
+		var proxy = this.dragProxy;
+
+		if (!proxy) {
+			proxy = this.dragProxy = new Ext.dd.StatusProxy({
+				cls: 'dd-matching-proxy-ghost',
+				id: this.id + '-drag-status-proxy',
+				constrain: true
+				//repair : Ext.emptyFn <--to help debug
+			});
+		}
+		return proxy;
 	},
 
 
+	setupDragZones: function() {
+		var me = this;
+
+		this.injectionSource.select('.drag').each(function(el) {
+			var cfg,
+				dragData,
+				dragZone;
+
+			cfg = {
+				animRepair: true,
+				proxy: me.getDragProxy(),
+
+				getDragData: function(e) {
+					var sourceEl = e.getTarget('.drag', 10), d;
+					if (sourceEl) {
+						d = sourceEl.cloneNode(true);
+						d.id = Ext.id();
+						return (dragData = {
+							sourceEl: sourceEl,
+							repairXY: Ext.fly(sourceEl).getXY(),
+							ddel: d,
+							matchOrdinal: sourceEl.getAttribute('data-match')
+						});
+					}
+				},
+
+				getRepairXY: function() {
+					return this.dragData.repairXY;
+				},
+
+				onStartDrag: function() {
+					var el = this.getProxy().getDragEl(),
+						dx = Math.floor(el.getWidth()/2),
+						dy = -Math.floor(el.getHeight()/2);
+
+					// Center drag and drop proxy on cursor pointer
+					this.setDelta(dx, dy);
+
+					Ext.fly(dragData.sourceEl).addCls('dragging');
+				},
+
+
+				afterValidDrop: function(){
+					Ext.fly(dragData.sourceEl).removeCls('dragging');
+				},
+
+
+				afterInvalidDrop: function(){
+					Ext.fly(dragData.sourceEl).removeCls('dragging');
+				}
+			};
+
+			dragZone = new Ext.dd.DragZone(el, cfg);
+		});
+	},
+
+
+	setupDropZones: function() {
+
+	},
+	//</editor-fold>
+
+
+	//<editor-fold desc="Grading">
 	getValue: function() {},
 
 
-	getSolutionContent: function(part) {},
-
-
-	editAnswer: function() {},
-
-
-	mark: function() {},
+	//	mark: function() {},
 
 
 	markCorrect: function() {
@@ -81,16 +167,26 @@ Ext.define('NextThought.view.assessment.input.Matching',{
 	markIncorrect: function() {
 		this.callParent();
 	},
+	//</editor-fold>
 
 
+	//<editor-fold desc="UI State">
 	reset: function() {
 		this.callParent();
 	},
 
 
+	editAnswer: function() {},
+
+
+	getSolutionContent: function(part) {},
+
+
 	hideSolution: function() {
 		this.callParent();
 	},
+	//</editor-fold>
+
 
 }, function(){
 	if (isFeature('v3matching')) {
