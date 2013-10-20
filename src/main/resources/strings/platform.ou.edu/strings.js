e67ced0dc4af53c0ac811b8dc6f49a6de2ab104c
Chris Utz <chris.utz@nextthought.com>
2013-10-20 22:58:10 +0000

    Strings entry for coming soon.  Having trouble getting this tested locally
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@25914 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/resources/strings/platform.ou.edu/strings.js
+++ src/main/resources/strings/platform.ou.edu/strings.js
@@ -1,6 +1,7 @@
 window.NTIStrings = window.NTIStrings || {};
 
 NTIStrings['Available for Purchase'] = 'Available Courses';
+NTIStrings['Coming Soon'] = 'Upcoming Courses';
 NTIStrings['library:branding logo-alt-text'] = 'The University of Oklahoma';
 NTIStrings['library:branding message-title'] = 'Online Learning Initiative';
 NTIStrings['library:branding message'] = 'The University of Oklahoma is proud to announce an exciting new partnership with NextThought!';
