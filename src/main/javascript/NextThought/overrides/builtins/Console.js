edbf4c8d7cbcbc476c86bef385a0a424ca851b2d
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-10-28 18:31:13 +0000

    allow the log to be disabled.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@26317 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/overrides/builtins/Console.js
+++ src/main/javascript/NextThought/overrides/builtins/Console.js
@@ -1,9 +1,11 @@
-Ext.define('NextThought.overrides.builtins.Console', {});
-(function() {
+Ext.define('NextThought.overrides.builtins.Console', function() {
+
+	//make sure console exists
 	Ext.applyIf(window, {
 		console: { log: Ext.emptyFn }
 	});
 
+	//make sure console functions exist.
 	Ext.applyIf(window.console, {
 		debug: console.log,
 		info: console.log,
@@ -16,4 +18,41 @@ Ext.define('NextThought.overrides.builtins.Console', {});
 		time: Ext.emptyFn,
 		timeEnd: Ext.emptyFn
 	});
-}());
+
+
+	var fns = ['log', 'debug', 'info', 'warn', 'error', 'group', 'trace', 'groupCollapsed', 'groupEnd', 'time', 'timeEnd'],
+		originalLogFns = {};
+
+	//capture the originals...
+	Ext.copyTo(originalLogFns, console, fns, true);
+
+
+	function disableLogging() {
+		var l = fns.length - 1, fn = function() {};
+		for (l; l >= 0; l--) {
+			if (fns[l]) {
+				console[fns[l]] = fn;
+			}
+		}
+	}
+
+
+	function enableLogging() {
+		var l = fns.length - 1;
+		for (l; l >= 0; l--) {
+			if (fns[l]) {
+				console[fns[l]] = originalLogFns[fns[l]];
+			}
+		}
+	}
+
+
+	console.enable = enableLogging;
+	console.disable = disableLogging;
+
+	if (!$AppConfig.enableLogging) {
+		console.disable();
+	}
+
+	return {};
+});
