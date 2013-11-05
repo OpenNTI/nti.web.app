f67b2cd2992722429878fcdff6ca15a85912019b
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-11-05 18:22:55 +0000

    this can't go here. Breaks mathcounts. (still working on a good place to do this, but this will let mathcounts work)
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@26643 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/controller/Reader.js
+++ src/main/javascript/NextThought/controller/Reader.js
@@ -148,20 +148,7 @@ Ext.define('NextThought.controller.Reader', {
 
 
 	setLocation: function(ntiid) {
-		var r = this.getContentReader(),
-			ntiidStr = Ext.isString(ntiid)? ntiid : ntiid
-					&& (ntiid.getId && ntiid.getId());
-
-		ntiidStr = ContentUtils.getLineage(ntiidStr).pop();
-
-		if(!Library.getTitle(ntiidStr)){//getTitle requires the ROOT ntiid.
-
-			//we are being asked to switch to something which we no longer
-			//have in the library.  We may have recently have lost access to it.
-			//Maybe its a purchasable we can prompt them with
-			this.fireEvent('unauthorized-navigation', this, ntiidStr);
-			return false;
-		}
+		var r = this.getContentReader();
 
 		if (this.fireEvent('show-view', 'content', true) === false) {
 			return false;
