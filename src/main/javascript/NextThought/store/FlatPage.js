10e9b92b53e38305937b911ea73680fa8143ecb9
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-11-06 23:31:55 +0000

    fix whitespace
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@26724 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/store/FlatPage.js
+++ src/main/javascript/NextThought/store/FlatPage.js
@@ -16,9 +16,9 @@ Ext.define('NextThought.store.FlatPage', {
 
 	sorters: [
 		{
-		//	property : 'line',
-		//	direction: 'ASC'
-		//},{
+			//	property : 'line',
+			//	direction: 'ASC'
+			//},{
 			property: 'CreatedTime',
 			direction: 'DESC'
 		}
@@ -26,24 +26,24 @@ Ext.define('NextThought.store.FlatPage', {
 	filters: [
 		{ id: 'nochildren', filterFn: function(r) { return !r.parent;}},
 		{ id: 'no-private-notes', filterFn: function(r) {
-			return !isFeature('notepad')
-					|| r.get('Class') !== 'Note'
-					|| (r.get('sharedWith').length
-					|| !Ext.isEmpty(r.get('title')));
+			return !isFeature('notepad') ||
+				   r.get('Class') !== 'Note' ||
+				   (r.get('sharedWith').length ||
+					!Ext.isEmpty(r.get('title')));
 		}}
 	],
 
 
-	remove: function(record,isMove,silent) {
+	remove: function(record, isMove, silent) {
 		var r = record || [],
-			args = Array.prototype.slice.call(arguments);
+				args = Array.prototype.slice.call(arguments);
 
 		if (!Ext.isArray(r)) {
 			r = [r];
 		}
 
 		if (isMove) {
-			Ext.each(r, function(r,i,a) {
+			Ext.each(r, function(r, i, a) {
 				if (r.placeholder) {
 					console.log('>>???');
 					a.splice(i, 1); }
@@ -80,7 +80,7 @@ Ext.define('NextThought.store.FlatPage', {
 		if (Ext.Array.contains(otherStore.$boundToFlat || [], this)) { return; }
 
 
-		function remove(s,rec) {
+		function remove(s, rec) {
 			var f;
 			if (!Ext.isEmpty(rec)) {
 				f = me.filters.getRange();
@@ -97,70 +97,70 @@ Ext.define('NextThought.store.FlatPage', {
 		}
 
 
-		function add(s,rec) {
+		function add(s, rec) {
 
-      function doesRecordPassFilters(rec) {
-        return Ext.Array.every(currentFilters, function(f) {
-          if (f.filterFn) { return f.filterFn.apply(f, [rec]); }
-          return true;
-        });
-      }
+			function doesRecordPassFilters(rec) {
+				return Ext.Array.every(currentFilters, function(f) {
+					if (f.filterFn) { return f.filterFn.apply(f, [rec]); }
+					return true;
+				});
+			}
 
 
-      function addMe(r) {
-        var i = me.findExact('NTIID', r.get('NTIID'));
-        if (!r || !(r instanceof NextThought.model.Note)) { return; }
+			function addMe(r) {
+				var i = me.findExact('NTIID', r.get('NTIID'));
+				if (!r || !(r instanceof NextThought.model.Note)) { return; }
 
-        if (i !== -1 && r !== me.getAt(i)) {
-          console.warn('DUPLICATE NTIID', r, me.getAt(i));
-          return;
-        }
+				if (i !== -1 && r !== me.getAt(i)) {
+					console.warn('DUPLICATE NTIID', r, me.getAt(i));
+					return;
+				}
 
-        if (!r.parent) {
-          //If the rec passes current filters, add it.
-          if (doesRecordPassFilters(r)) {
-            me.add(r);//add one at a time to get insertion sort.
-          }
-          else {
-            // If the lineFilter is set on the flatPage store,
-            // wait until we set the line property on the new rec,
-            // then check it and add it.
-            r.addObserverForField(me, 'line', function() {
-              me.suspendEvents(false);
-              me.add(r);
-              me.filter(me.filters.getRange());
-              me.resumeEvents();
-            }, {single: true});
-          }
-        }
-      }
+				if (!r.parent) {
+					//If the rec passes current filters, add it.
+					if (doesRecordPassFilters(r)) {
+						me.add(r);//add one at a time to get insertion sort.
+					}
+					else {
+						// If the lineFilter is set on the flatPage store,
+						// wait until we set the line property on the new rec,
+						// then check it and add it.
+						r.addObserverForField(me, 'line', function() {
+							me.suspendEvents(false);
+							me.add(r);
+							me.filter(me.filters.getRange());
+							me.resumeEvents();
+						}, {single: true});
+					}
+				}
+			}
 
 			var placeholders = Ext.Array.filter(s.getItems(), function(r) {return r.placeholder && !r.parent;}),
-				records = ((rec && (Ext.isArray(rec) ? rec : [rec])) || []).concat(placeholders),
-          currentFilters = me.filters.getRange();
+					records = ((rec && (Ext.isArray(rec) ? rec : [rec])) || []).concat(placeholders),
+					currentFilters = me.filters.getRange();
 
 			Ext.each(records, addMe);
 		}
 
-		function load(s,rec) {
-      var placeholders = Ext.Array.filter(s.getItems(), function(r) {return r.placeholder && !r.parent;}),
-          records = ((rec && (Ext.isArray(rec) ? rec : [rec])) || []).concat(placeholders), me = this;
+		function load(s, rec) {
+			var placeholders = Ext.Array.filter(s.getItems(), function(r) {return r.placeholder && !r.parent;}),
+					records = ((rec && (Ext.isArray(rec) ? rec : [rec])) || []).concat(placeholders), me = this;
 
-      Ext.each(records, function(r) {
-        var i = me.find('NTIID', r.get('NTIID'), 0, false, true, true);
-        if (!r || !(r instanceof NextThought.model.Note)) { return; }
+			Ext.each(records, function(r) {
+				var i = me.find('NTIID', r.get('NTIID'), 0, false, true, true);
+				if (!r || !(r instanceof NextThought.model.Note)) { return; }
 
-        if (i !== -1 && r !== me.getAt(i)) {
-          me.removeAt(i);
-        }
+				if (i !== -1 && r !== me.getAt(i)) {
+					me.removeAt(i);
+				}
 
-        if (!r.parent) {
-          me.add(r);//add one at a time to get insertion sort.
-        }
-      });
+				if (!r.parent) {
+					me.add(r);//add one at a time to get insertion sort.
+				}
+			});
 			me.filter();//TEST THIS: Have filtered items been seen in the list and we not notice?? Or has it
 			// "just worked" and now that I'm adding a new filter I'm just now noticing it?
-    }
+		}
 
 		monitors = me.mon(otherStore, {
 			scope: me,
