e61fab7c48ba17a546eb4dc41fe202df729890a2
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-11-15 03:16:46 +0000

    wait on the IE9 message :(
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@27136 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/Main.js
+++ src/main/javascript/NextThought/view/Main.js
@@ -191,14 +191,14 @@ Ext.define('NextThought.view.Main', {
 		});
 
 		//<editor-fold desc="IE9 message">
-		if (Ext.isIE9m) {
-			setTimeout(function() {
-				Ext.widget('message-bar', {
-					renderTo: Ext.getBody(),
-					messageType: 'ie9',
-					message: 'Internet Explorer 9 will not be supported begining January 2014. Please upgrade your browser.'
-				});
-			},1500);//ease in
+//		if (Ext.isIE9m) {
+//			setTimeout(function() {
+//				Ext.widget('message-bar', {
+//					renderTo: Ext.getBody(),
+//					messageType: 'ie9',
+//					message: 'Internet Explorer 9 will no longer be supported after __. Please upgrade your browser.'
+//				});
+//			},1500);//ease in
 		}
 		//</editor-fold>
 
