11c8040960ef651602f8996106dff294deb916cf
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-12-04 18:55:14 +0000

    style for left panel
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@27690 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/courseware/assessment/Navigation.js
+++ src/main/javascript/NextThought/view/courseware/assessment/Navigation.js
@@ -2,7 +2,7 @@ Ext.define('NextThought.view.courseware.assessment.Navigation', {
 	extend: 'Ext.view.View',
 	alias: 'widget.course-assessment-navigation',
 
-	ui: 'course',
+	ui: 'course-assessment',
 	cls: 'nav-outline scrollable',
 	preserveScrollOnRefresh: true,
 
@@ -17,27 +17,40 @@ Ext.define('NextThought.view.courseware.assessment.Navigation', {
 	},
 
 
-	getTargetEl: function() {
-		return this.frameBodyEl;
-	},
-
-
+	getTargetEl: function() { return this.frameBodyEl; },
 	overItemCls: 'over',
 	itemSelector: '.outline-row',
 	tpl: new Ext.XTemplate(
 			Ext.DomHelper.markup(
 					{ tag: 'tpl', 'for': '.', cn: [
 						{ cls: 'outline-row', 'data-qtip': '{label:htmlEncode}', cn: [
-							{ cls: 'label', html: '{label}'},
-							{ cls: 'notifications' }
+							{ tag: 'tpl', 'if': 'count', cn: { cls: 'count', html: '{count}' } },
+							{ cls: 'label', html: '{label:htmlEncode}'}
 						]}
 					]}), {
 				//template functions
 			}),
 
-	clear: function() {
-		this.bindStore('ext-empty-store');
-	},
+
+	store: new Ext.data.Store({
+		fields: [
+			{name: 'id', type: 'string'},
+			{name: 'label', type: 'string'},
+			{name: 'count', type: 'int', defaultValue: 0},
+			{name: 'type', type: 'string', defaultValue: 'view'},//or filter
+			{name: 'mapping', type: 'string'}
+		],
+		data: [
+			{id: 'activity', label: 'Activity & Notifications', mapping: '', count: 6},
+			{id: 'assignments', label: 'Assignments', mapping: '' },
+			{id: 'grades', label: 'Grades and Performance', mapping: '' }
+		]
+	}),
+
+
+	//clear: function() {
+	//	this.bindStore('ext-empty-store');
+	//},
 
 
 	setTitle: function(title) {
