d7826f2559edab94abbf35a71ef453b7dee379a1
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-12-04 18:54:57 +0000

    butt up the nav view to the main view in the assessment view
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@27685 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/courseware/assessment/View.js
+++ src/main/javascript/NextThought/view/courseware/assessment/View.js
@@ -6,7 +6,7 @@ Ext.define('NextThought.view.courseware.assessment.View', {
 	],
 
 
-	navigation: {xtype: 'box', cls: 'make-white'},
+	navigation: {xtype: 'box', cls: 'make-white', margin: '0 0 10 0', override: true},
 	body: {xtype: 'box', cls: 'make-white'}
 
 });
