56a51b21672c13da564dd67b8dfd1989d910a720
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-12-04 18:55:00 +0000

    have to add fancy scroll so the view fits in the container :/
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@27686 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/courseware/assessment/View.js
+++ src/main/javascript/NextThought/view/courseware/assessment/View.js
@@ -2,11 +2,19 @@ Ext.define('NextThought.view.courseware.assessment.View', {
 	extend: 'NextThought.view.navigation.AbstractPanel',
 	alias: 'widget.course-assessment',
 	ui: 'course-assessment',
+
 	requires: [
 	],
 
+	mixins: {
+		customScroll: 'NextThought.mixins.CustomScroll'
+	},
 
 	navigation: {xtype: 'box', cls: 'make-white', margin: '0 0 10 0', override: true},
 	body: {xtype: 'box', cls: 'make-white'}
 
+	initComponent: function() {
+		this.callParent(arguments);
+		this.initCustomScrollOn('content');
+	}
 });
