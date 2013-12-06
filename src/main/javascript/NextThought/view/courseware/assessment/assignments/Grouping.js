e8be775339db83db69fe7b9a7f234e7c37f7aec9
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-12-06 02:06:02 +0000

    add a component to group assignment views
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@27768 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/main/javascript/NextThought/view/courseware/assessment/assignments/Grouping.js
@@ -0,0 +1,36 @@
+Ext.define('NextThought.view.courseware.assessment.assignments.Grouping', {
+	extend: 'Ext.container.Container',
+	alias: 'widget.course-assessment-assignment-group',
+	ui: 'course-assessment',
+	cls: 'assignment-group',
+
+	requires: [
+		'NextThought.layout.component.Natural'
+	],
+
+	layout: 'auto',
+	componentLayout: 'natural',
+	childEls: ['body'],
+	getTargetEl: function() { return this.body; },
+
+	renderTpl: Ext.DomHelper.markup([
+		{ cls: 'subtitle', html: '{subtitle}' },
+		{ cls: 'title', html: '{title}' },
+		{ id: '{id}-body', cls: 'body', cn: ['{%this.renderContainer(out,values)%}'] }
+	]),
+
+
+	config: {
+		subTitle: '',
+		title: ''
+	},
+
+
+	initComponent: function() {
+		this.callParent(arguments);
+		this.renderData = Ext.apply(this.renderData || {}, {
+			subtitle: this.getSubTitle(),
+			title: this.getTitle()
+		});
+	}
+});
--- src/main/javascript/NextThought/view/courseware/assessment/assignments/View.js
+++ src/main/javascript/NextThought/view/courseware/assessment/assignments/View.js
@@ -2,10 +2,12 @@ Ext.define('NextThought.view.courseware.assessment.assignments.View', {
 	extend: 'Ext.container.Container',
 	alias: 'widget.course-assessment-assignments',
 	requires: [
-		'NextThought.view.courseware.assessment.assignments.FilterBar'
+		'NextThought.view.courseware.assessment.assignments.FilterBar',
+		'NextThought.view.courseware.assessment.assignments.Grouping'
 	],
 
 	items: [
-		{xtype: 'course-assessment-assignments-filterbar'}
+		{xtype: 'course-assessment-assignments-filterbar'},
+		{xtype: 'course-assessment-assignment-group', title: '1. About Geology', subTitle: 'August 19'}
 	]
 });
