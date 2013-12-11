918a7ca156cf152cfe8601805d2d6493ce69e258
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-12-11 20:28:02 +0000

    remove stop gap workaround from last night
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@27998 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/model/assessment/AssignmentSubmission.js
+++ src/main/javascript/NextThought/model/assessment/AssignmentSubmission.js
@@ -1,6 +1,5 @@
 Ext.define('NextThought.model.assessment.AssignmentSubmission', {
 	extend: 'NextThought.model.Base',
-	mimeType: 'application/vnd.nextthought.assessment.ssignmentsubmission',
 	fields: [
 		{name: 'assignmentId', type: 'string'},
 		{name: 'parts', type: 'arrayItem'}
