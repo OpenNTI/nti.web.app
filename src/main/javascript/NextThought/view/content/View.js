69bc89b4105ca2b3db87ce3bc78e4ba92ffaa079
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-12-17 05:42:52 +0000

    relabel for aaron.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@28262 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/content/View.js
+++ src/main/javascript/NextThought/view/content/View.js
@@ -62,7 +62,7 @@ Ext.define('NextThought.view.content.View', {
 		{label: 'Lessons', viewId: 'course-book?'},
 		{label: 'Discussions', viewId: 'course-forum'},
     //		{label: 'Notebook', viewId: ''},
-		{label: 'Your Progress', viewId: 'course-assessment'},
+		{label: 'Assignments', viewId: 'course-assessment'},
 		{label: 'Course Info', viewId: 'course-info'}
 	],
 
