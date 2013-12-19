3659dbe4eaa9989ff244b3071c0eeafebd02f552
Andrew Ligon <andrew.ligon@nextthought.com>
2013-12-19 00:52:43 +0000

    add assignment group by drop down menu and styles
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@28358 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/courseware/assessment/assignments/FilterBar.js
+++ src/main/javascript/NextThought/view/courseware/assessment/assignments/FilterBar.js
@@ -8,7 +8,7 @@ Ext.define('NextThought.view.courseware.assessment.assignments.FilterBar', {
 		{ cls: 'third dropmenu', cn: [
 			{ cls: 'label', html: 'All Assignments' }
 		] },
-		{ cls: 'third dropmenu', cn: [
+		{ cls: 'third dropmenu groupBy', cn: [
 			{ cls: 'label', html: 'By Lesson' }
 		] },
 		{ cls: 'third search', cn: [
@@ -17,6 +17,71 @@ Ext.define('NextThought.view.courseware.assessment.assignments.FilterBar', {
 		] }
 	]),
 
+	renderSelectors:{
+		groupEl: '.groupBy'
+	},
+
+
+	afterRender: function(){
+		this.callParent(arguments);
+		this.currentGrouping = 'lesson';
+
+		this.createGroupByMenu();
+
+		this.mon(this.groupEl,'click','showGroupByMenu');
+	},
+
+	
+	showGroupByMenu: function(){
+		this.groupByMenu.showBy(this.groupEl, 'tl-tl', this.groupByMenu.offset);
+	},
+
+
+	createGroupByMenu: function(){
+		var type = this.currentGrouping,
+			items = [
+				{ text: 'By Lesson', groupBy: 'lesson', checked: type === 'lesson'},
+				{ text: 'By Due Date', groupBy: 'due', checked: type === 'due'},
+				{ text: 'By Completion', grouBy: 'completion', checked: type === 'completion'}
+			];
+
+		this.groupByMenu = Ext.widget('menu',{
+			ui: 'nt',
+			cls: 'group-by-menu',
+			plain: true,
+			shadow: false,
+			width: 257,
+			frame: false,
+			border: false,
+			ownerCmp: this,
+			offset: [0,0],
+			defaults: {
+				ui: 'nt-menuitem',
+				xtype: 'menucheckitem',
+				group: 'groupByOptions',
+				cls: 'group-by-option',
+				height: 50,
+				plain: true,
+				listeners: {
+					scope: this,
+					'checkchange': 'switchOrdering'
+				}
+			},
+			items: items
+		});
+	},
+
+
+	switchOrdering: function(item){
+		var offset = item.getOffsetsTo(this.groupByMenu),
+			x = offset && offset[1];
+
+		this.groupEl.el.down('.label').update(item.text);
+
+		this.groupByMenu.offset = [0,-x];
+
+		this.currentGrouping = item.groupBy;
+	},
 
 
 	getShowType: function() {
