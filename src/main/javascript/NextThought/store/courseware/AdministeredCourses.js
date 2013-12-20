6f714253e929e484f6f067bff1893bdda9db4c33
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-12-20 06:10:50 +0000

    some how not defining a new proxy corrupts the base class when this subclass sets a new model. :P Workaround by redefining the proxy.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@28419 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/store/courseware/AdministeredCourses.js
+++ src/main/javascript/NextThought/store/courseware/AdministeredCourses.js
@@ -1,4 +1,32 @@
 Ext.define('NextThought.store.courseware.AdministeredCourses', {
 	extend: 'NextThought.store.courseware.EnrolledCourses',
-	model: 'NextThought.model.courseware.CourseInstanceAdministrativeRole'
+
+	model: 'NextThought.model.courseware.CourseInstanceAdministrativeRole',
+	proxy: {
+		type: 'ajax',
+		timeout: 3600000,//hour
+		headers: {
+			'Accept': 'application/vnd.nextthought.collection+json',
+			'Content-Type': 'application/json'
+		},
+		url: 'tbd',
+		reader: {
+			type: 'json',
+			root: 'Items'
+		},
+
+		noCache: false,
+
+		//Don't send any params with this store load.
+		groupParam: undefined,
+		groupDirectionParam: undefined,
+		sortParam: undefined,
+		filterParam: undefined,
+		directionParam: undefined,
+		idParam: undefined,
+		//When we start paging, we will define these
+		pageParam: undefined,
+		startParam: undefined,
+		limitParam: undefined
+	}
 });
