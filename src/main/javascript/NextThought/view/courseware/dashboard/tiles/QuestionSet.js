6fca430b23b8a4a3136f0de91d194c5614f881ee
Andrew Ligon <andrew.ligon@nextthought.com>
2014-01-05 23:14:45 +0000

    Add an assignments tile to the dashboard.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@29044 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/courseware/dashboard/tiles/QuestionSet.js
+++ src/main/javascript/NextThought/view/courseware/dashboard/tiles/QuestionSet.js
@@ -5,8 +5,7 @@ Ext.define('NextThought.view.courseware.dashboard.tiles.QuestionSet', {
 	statics: {
 		getTileFor: function(effectiveDate, course, locationInfo, courseNodeRecord, finish) {
 			var DQ = Ext.DomQuery, me = this,
-				items = this.getChildrenNodes(courseNodeRecord),
-				c = [], req, containerId;
+				items = this.getChildrenNodes(courseNodeRecord);
 
 			items = DQ.filter(items || [], '[mimeType$=naquestionset]');
 
@@ -15,7 +14,24 @@ Ext.define('NextThought.view.courseware.dashboard.tiles.QuestionSet', {
 				return;
 			}
 
-			containerId = ContentUtils.getLineage(items[0].getAttribute('target-ntiid'))[1];
+			locationInfo.courseInstance.getAssignments()
+				.done(function(assignments){
+					var id = items[0].getAttribute('target-ntiid'),
+						assignment = assignments.getItem(id);
+
+					if (assignment) {
+						me.getTilesForAssignment(assignment, assignments, items, locationInfo, courseNodeRecord, finish);
+					} else {
+						me.getTileForAssessment(items, locationInfo, courseNodeRecord, finish);
+					}
+				})
+				.fail(function(reason){
+					console.error(reason);
+				});
+		},
+
+		getTileForAssessment: function(items, locationInfo, courseNodeRecord, finish){
+			var req, me = this, containerId = ContentUtils.getLineage(items[0].getAttribute('target-ntiid'))[1];
 
 			function findFirstUncompleted(ntiids, nodes) {
 				var node = nodes.shift();
@@ -28,7 +44,6 @@ Ext.define('NextThought.view.courseware.dashboard.tiles.QuestionSet', {
 				}else {
 					return node;
 				}
-
 			}
 
 			function findQuizSetNode(ntiid, nodes) {
@@ -45,12 +60,13 @@ Ext.define('NextThought.view.courseware.dashboard.tiles.QuestionSet', {
 				findQuizSetNode(ntiid, nodes);
 			}
 
-			function quizesLoaded(q, s, r) {
+			function assessmentsLoaded(q, s, r) {
 				var rec, nextItemNode, currentItemNode = null,
 					json = Ext.decode(r.responseText, true) || {},
 					totalQuizes = items.length, totalAttempts = 0;
 				
 				nextItemNode = findFirstUncompleted(Ext.Array.pluck(json.Items || [], 'questionSetId'), items.slice());
+				
 				if (!s || Ext.isEmpty(json.Items)) {
 					rec = null;
 				}else {
@@ -58,45 +74,23 @@ Ext.define('NextThought.view.courseware.dashboard.tiles.QuestionSet', {
 					currentItemNode = findQuizSetNode(rec.get('questionSetId'), items.slice());
 					totalAttempts = json.Items.length;
 				}
-
-				Service.getPageInfo(containerId, function(pageInfo){
-					var items = pageInfo.get('AssessmentItems') || [];
-
-					items = items.filter(function(item){
-						//if we don't have a rec, we don't have a questionSetId. If thats the case
-						//don't show the tile if any of the assessment items are assignments
-						if(rec){
-							return item.isAssignment && item.containsId(rec.get('questionSetId'));
-						}
-						return item.isAssignment;
-					});
-
-					if (items.length > 0) {
-						Ext.callback(finish);
-						return;
-					}
-
-					Ext.callback(finish, null, [me.create({
-						locationInfo: locationInfo,
-						latestAttempt: rec,
-						nextItemNode: nextItemNode,
-						currentItemNode: currentItemNode,
-						totalQuizes: totalQuizes,
-						totalAttempts: totalAttempts,
-						lessonStartDate: courseNodeRecord.get('startDate'),
-						lastModified: courseNodeRecord.get('date')
-					})]);
-				}, function(){
-					Ext.callback(finish);
-				})
-
 				
+				Ext.callback(finish, null, [me.create({
+					locationInfo: locationInfo,
+					latestAttempt: rec,
+					nextItemNode: nextItemNode,
+					currentItemNode: currentItemNode,
+					totalQuizes: totalQuizes,
+					totalAttempts: totalAttempts,
+					lessonStartDate: courseNodeRecord.get('startDate'),
+					lastModified: courseNodeRecord.get('date'),
+					isAssignment: false
+				})]);				
 			}
 
-			
 			req = {
 				url: Service.getContainerUrl(containerId, 'UniqueMinMaxSummary'),
-				scope: this,
+				scope: me,
 				method: 'GET',
 				params: {
 					//accept: NextThought.model.assessment.AssessedQuestionSet.mimeType,
@@ -104,10 +98,37 @@ Ext.define('NextThought.view.courseware.dashboard.tiles.QuestionSet', {
 					sortOrder: 'ascending',
 					attribute: 'questionSetId'
 				},
-				callback: quizesLoaded
+				callback: assessmentsLoaded
 			};
 
 			Ext.Ajax.request(req);
+		},
+
+		getTilesForAssignment: function(assignment, assignments, items, locationInfo, courseNodeRecord, finish){
+			function getNextAssignment(a, as){
+				var next, difference;
+
+				as.get('Items').forEach(function(item){
+					var due = item.get('availableBeginning'),
+						diff = due && (a.get('availableBeginning') - due);
+
+					if ( diff && diff > 0 && diff < difference) {
+						next = item;
+						difference = diff;
+					}
+				});
+			}
+
+
+			Ext.callback(finish, null, [this.create({
+				locationInfo: locationInfo,
+				nextItemNode: getNextAssignment(assignment, assignments),
+				currentItemNode: assignment,
+				totalQuizes: items.length,
+				lessonStartDate: courseNodeRecord.get('startDate'),
+				lastModified: courseNodeRecord.get('date'),
+				isAssignment: true
+			})]);
 		}
 	},
 
@@ -121,8 +142,10 @@ Ext.define('NextThought.view.courseware.dashboard.tiles.QuestionSet', {
 	defaultType: 'course-dashboard-tiles-question-set-view',
 
 	constructor: function(configs) {
+		var xtype = configs.isAssignment? 'course-dashboard-tiles-assignment-view' : 'course-dashboard-tiles-question-set-view';
+
 		configs.items = [
-			{xtype: 'container', defaultType: this.defaultType, items: [
+			{xtype: 'container', defaultType: xtype, items: [
 				{
 					nextItemNode: configs.nextItemNode,
 					currentItemNode: configs.currentItemNode,
@@ -130,7 +153,8 @@ Ext.define('NextThought.view.courseware.dashboard.tiles.QuestionSet', {
 					totalQuizes: configs.totalQuizes,
 					totalAttempts: configs.totalAttempts,
 					lessonStartDate: configs.lessonStartDate,
-					lessonEndDate: configs.lastModified
+					lessonEndDate: configs.lastModified,
+					isAssignment: true
 				 }
 			]}
 		];
@@ -274,5 +298,98 @@ Ext.define('NextThought.view.courseware.dashboard.widget.QuestionSetView', {
 			}, this);*/
 		}
 	}
+
+}, function(){
+	Ext.define('NextThought.view.courseware.dashboard.widget.AssignmentView', {
+		extend: 'NextThought.view.courseware.dashboard.widget.QuestionSetView',
+		alias: 'widget.course-dashboard-tiles-assignment-view',
+
+		cls: 'question-set-view non-empty assignment',
+
+		headerTpl: {
+			xtype: 'component',
+			cls: 'header',
+			header: true,
+			renderTpl: Ext.DomHelper.markup([
+				{ cls: 'tile-title', html: 'Assignment'}
+			])
+		},
+
+		lastAttemptTpl: {
+			xtype: 'container',
+			cls: 'attempt',
+			attempt: true,
+			items: [
+				{
+					xtype: 'component',
+					renderTpl: Ext.DomHelper.markup(
+						[
+							{ cls: 'title' },
+							{ cls: 'meta', cn: [
+								{ tag: 'span', cls: 'question-count'},
+								{ tag: 'span', cls: 'date'}
+							]}
+						]
+					)
+				}
+			]
+		},
+
+
+		afterRender: function(){
+			this.callParent(arguments);
+
+			this.assignmentId = this.currentItemNode.getId();
+			this.fireEvent('has-been-submitted', this);
+			this.setLatestAssignmentAttempt(false, null, this.currentItemNode, this.nextItemNode);
+		},
+
+
+		setHistory: function(history){
+			if(!history){
+				console.warn('No history');
+			}
+
+			var submission = history.get('Submission'),
+				completed = (submission && submission.get('CreatedTime')) || new Date(),
+				due = this.assignment && this.assignment.get('availableEnding');
+
+			this.setLatestAssignmentAttempt(true, completed > due, this.currentItemNode, this.nextItemNode);
+		},
+
+
+		setLatestAttempt: function(){},
+
+		
+		setLatestAssignmentAttempt: function(attempted, late, assignment, next){
+			var me = this,
+				lastAttempt = me.down('[attempt]'),
+				el = lastAttempt.el,
+				title = el.down('.title'),
+				count = el.down('.meta .question-count'),
+				date = el.down('.meta .date'),
+				go = me.el.down('.next'),
+				parts = assignment && assignment.get('parts')[0],
+				questionSet = parts && parts.get('question_set'),
+				questions = questionSet && questionSet.get('questions');
+
+			title.update(assignment.get('title'));
+			
+			if (questions) {  
+				count.update(Ext.util.Format.plural(questions.length, 'question'));
+			}
+
+			if(attempted){
+				go.update('Review');
+			} else {
+				go.update('Start');
+			}
+
+			this.mon(go, 'click', function(){
+				me.fireEvent('navigate-to-assignment', assignment.getId());
+			}, {single: true});
+
+		}
+	});
 
 });
