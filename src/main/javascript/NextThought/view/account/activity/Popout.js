be487cae18f4f7301297a6b52e750d99aa528393
Zach McWilliams <zach.mcwilliams@nextthought.com>
2014-01-10 22:24:43 +0000

    mobile - popout scroll comments and correct popout height
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@29862 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/account/activity/Popout.js
+++ src/main/javascript/NextThought/view/account/activity/Popout.js
@@ -207,6 +207,34 @@ Ext.define('NextThought.view.account.activity.Popout', {
 					}
 				});
 
+                if(Ext.is.iOS){
+                    //Make the comments in popouts scrollable
+                    if(pop.el.down('.replies')){
+                        pop.el.down('.replies').addCls('scrollable');
+                    }
+
+                    //If popout decides to change position when typing, put it back
+                    if(pop.el.down('.content')){
+                        var me = this;
+                        pop.el.down('.respond').down('.content').on('focus',function(){
+                            Ext.defer(function(){
+                                pop.firstY = pop.getY();
+                            },100);
+                        });
+                        pop.el.down('.respond').down('.content').on('keydown',function(){
+                            window.clearTimeout(me.popYTimeout);
+                            me.timeElapsed = 0;
+                            me.popYTimeout = window.setInterval(function(){
+                                if((pop.getY() != pop.firstY) || me.timeElapsed >= 900){
+                                    pop.setY(pop.firstY);
+                                    window.clearInterval(me.popYTimeout);
+                                }
+                                me.timeElapsed += 100;
+                            },100);
+                        });
+                    }
+                }
+
 				if (viewRef) {
 					if (viewRef.cancelPopupTimeout) {
 						pop.mon(pop, 'mouseover', viewRef.cancelPopupTimeout, viewRef);
