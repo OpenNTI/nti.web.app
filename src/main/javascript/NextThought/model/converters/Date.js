a07f3c025106d57a69a3c90f230a4088b0fadb6b
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-01-13 08:22:39 +0000

    drop Z in dates and let the -00:00 specify GMT
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@30302 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/model/converters/Date.js
+++ src/main/javascript/NextThought/model/converters/Date.js
@@ -4,9 +4,14 @@ Ext.define('NextThought.model.converters.Date', {
 }, function() {
 	Ext.data.Types.ISODATE = {
 		convert: function(v) {
+			if (v && v[v.length - 1] === 'Z') {
+				v = v.substr(0, v.length - 1);
+			}
+
 			var fmt = 'Y-m-d\\TH:i:sP',
 			//we know most of the time the date will be a timezone-less sting... so concat the GMT zone and parse strict.
 				d = Ext.Date.parse(v + '-00:00', fmt, true);
+
 			//if the parse strict fails, d will be undefined, and we will parse again without the concat.
 			return d || Ext.Date.parse(v, fmt, true);
 		},
