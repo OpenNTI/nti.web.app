e82843e817b2d0f43c883c826de7dfb5bac6960b
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-01-13 06:50:07 +0000

    relock for prod
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@30279 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/model/courseware/CourseCatalogEntry.js
+++ src/main/javascript/NextThought/model/courseware/CourseCatalogEntry.js
@@ -19,7 +19,9 @@ Ext.define('NextThought.model.courseware.CourseCatalogEntry', {
 		{ name: 'Title', type: 'string', persist: false },
 		{ name: 'Video', type: 'string', persist: false },
 
-		{ name: 'Preview', type: 'bool' },
+		{ name: 'Preview', type: 'Synthetic', fn: function() {
+			return this.get('StartDate') > Ext.Date.parse('2014-01-01', 'Y-m-d');
+		} },
 
 		{ name: 'icon', type: 'string', mapping: 'LegacyPurchasableIcon' }, //small
 		{ name: 'thumbnail', type: 'string', mapping: 'LegacyPurchasableThumbnail' }, //small/medium
