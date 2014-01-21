cd9330a147ef8a4baedc9ed6f5378435c101eff9
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-01-21 17:16:32 +0000

    Ext’s garbage collector didn’t catch exceptions when trying to access dom node properties. Take their implementation, and catch exceptions, to ensure we cleanup caches. (and not spam the error reporter every 30seconds)
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@31022 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/overrides/Ext.js
+++ src/main/javascript/NextThought/overrides/Ext.js
@@ -10,14 +10,14 @@ Ext.define('NextThought.overrides.Ext', {
 	 * @param cfg {Object} The values to apply to the object in the first arg.
 	 * @return {*}
 	 */
-	applyRecursively: function applyRecursively(o,cfg) {
+	applyRecursively: function applyRecursively(o, cfg) {
 		if (!o) {return o;}
 
 		if (Ext.isArray(o)) {
-			Ext.each(o, function(v,i,a) { a[i] = applyRecursively(v, cfg); });
+			Ext.each(o, function(v, i, a) { a[i] = applyRecursively(v, cfg); });
 		}
 		else if (Ext.isObject(o)) {
-			Ext.Object.each(o, function(k,v) { o[k] = applyRecursively(v, cfg); });
+			Ext.Object.each(o, function(k, v) { o[k] = applyRecursively(v, cfg); });
 			o = Ext.apply(o, cfg);
 		}
 
@@ -27,7 +27,9 @@ Ext.define('NextThought.overrides.Ext', {
 
 },function() {
 
-	var get = Ext.getElementById;
+	var EC = Ext.cache,
+		Element = Ext.dom.Element,
+		get = Ext.getElementById;
 
 	/**
 	 *  This is not to be confused with document.getElementById.  Ext has a periodic task that checks that cached
@@ -56,25 +58,88 @@ Ext.define('NextThought.overrides.Ext', {
 	 *  Copy function garbageCollect() from ext/src/dom/Element.js as a starting point, then restart the 30second interval.
 	 */
 	Ext.getElementById = function(id) {
-			var el = get.apply(this, arguments);
+		var el = get.apply(this, arguments);
 
-			function testFrame(frame) {
-				var win = frame.contentWindow || window.frames[frame.name],
-					src;
-				try {
-					src = Globals.HOST_PREFIX_PATTERN.exec(frame.getAttribute('src'));
-					if (src[3] === document.domain || src[1] === 'javascript') {
-						el = win.document.getElementById(id) || false;
+		function testFrame(frame) {
+			var win = frame.contentWindow || window.frames[frame.name],
+				src;
+			try {
+				src = Globals.HOST_PREFIX_PATTERN.exec(frame.getAttribute('src'));
+				if (src[3] === document.domain || src[1] === 'javascript') {
+					el = win.document.getElementById(id) || false;
+				}
+			//for iframes where we cannot access its content(Cross Origin Content) ignore.
+			} catch (e) { swallow(e); }
+			return !el;
+		}
+
+		if (!el) {
+			Ext.each(document.getElementsByTagName('iframe'), testFrame);
+		}
+
+		return el;
+	};
+
+
+	//do our own GC... (mostly copied verbatim, added lint fixes and try/catch)
+	function garbageCollect() {
+		if (!Ext.enableGarbageCollector) {
+			clearInterval(Element.collectorThreadId);
+		} else {
+			var eid,
+				d,
+				o,
+				t, clean;
+
+			for (eid in EC) {
+				if (EC.hasOwnProperty(eid)) {
+					clean = false;
+					o = EC[eid];
+
+					// Skip document and window elements
+					if (o.skipGarbageCollection) {
+						continue;
 					}
-				//for iframes where we cannot access its content(Cross Origin Content) ignore.
-				} catch (e) { swallow(e); }
-				return !el;
-			}
 
-			if (!el) {
-				Ext.each(document.getElementsByTagName('iframe'), testFrame);
-			}
+					d = o.dom;
 
-			return el;
-		};
+					try {
+						clean = !d || (!d.parentNode || (!d.offsetParent && !Ext.getElementById(eid)));
+					} catch (e) {
+						clean = true;
+					}
+
+					if (clean) {
+						if (Ext.enableListenerCollection) {
+							Ext.EventManager.removeAll(d);
+						}
+						delete EC[eid];
+					}
+				}
+			}
+			// Cleanup IE Object leaks
+			if (Ext.isIE) {
+				t = {};
+				for (eid in EC) {
+					if (EC.hasOwnProperty(eid)) {
+						t[eid] = EC[eid];
+					}
+				}
+				EC = Ext.cache = t;
+			}
+		}
+	}
+
+	function takeOverGC() {
+		if (!Element.collectorThreadId) {
+			setTimeout(takeOverGC, 1000);
+			return;
+		}
+
+		clearInterval(Element.collectorThreadId);//stop Ext's Garbage collector...
+		//then do our own...
+		Element.collectorThreadId = setInterval(garbageCollect, 30000);
+	}
+
+	takeOverGC();
 });
