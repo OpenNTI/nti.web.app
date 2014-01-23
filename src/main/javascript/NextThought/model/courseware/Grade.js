dbef8dfa7e674ed6674e964981db9546628c8c11
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-01-23 01:32:47 +0000

    round the float on grades
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@31253 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/model/courseware/Grade.js
+++ src/main/javascript/NextThought/model/courseware/Grade.js
@@ -3,7 +3,18 @@ Ext.define('NextThought.model.courseware.Grade', {
 
 	fields: [
 		{name: 'Username', type: 'string'},
-		{name: 'value', type: 'string'},
+		{name: 'value', type: 'string', convert: function(v) {
+			var n;
+			if (typeof v === 'number') {
+				n = v.toFixed(1);
+				if (n.split('.')[1] === '0') {
+					n = v.toFixed(0);
+				}
+				v = n;
+			}
+
+			return v;
+		}},
 		{name: 'AssignmentId', type: 'string'}
 	]
 });
