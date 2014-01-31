0330f903cab6ea877c079b937c417608a28248fa
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-01-31 03:21:13 +0000

    add an empty state for forums.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@31937 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/forums/Root.js
+++ src/main/javascript/NextThought/view/forums/Root.js
@@ -11,8 +11,16 @@ Ext.define('NextThought.view.forums.Root', {
 	cls: 'forum-list',
 	itemSelector: '.forum-list-item',
 
+	emptyText: Ext.DomHelper.markup({
+		cls: 'empty-state', cn: [
+			{cls: 'header', html: getString('forums.empty_boards_header')},
+			{cls: 'sub', html: getString('forums.empty_boards_sub')}
+		]
+	}),
+	deferEmptyText: false,
+
 	listeners: {
-		select: function(selModel,record) {
+		select: function(selModel, record) {
 			//allow reselect since we don't style the selected state, this has no
 			// visual effect other than the ability to click on it again
 			selModel.deselect(record);
