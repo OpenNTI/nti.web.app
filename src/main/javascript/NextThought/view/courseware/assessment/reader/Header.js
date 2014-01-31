b51f702dc179473f9ca67bf8163c79a32726e4b9
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-01-31 03:06:26 +0000

    don't show the student's view as submitted/late if they just got a grade.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@31934 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/courseware/assessment/reader/Header.js
+++ src/main/javascript/NextThought/view/courseware/assessment/reader/Header.js
@@ -66,7 +66,7 @@ Ext.define('NextThought.view.courseware.assessment.reader.Header', {
 			completed = submission && submission.get('CreatedTime'),
 			overdue;
 
-		if (!history || !history.get('Submission')) {
+		if (!history || !submission || (submission.get('parts') || []) === 0) {
 			this.removeCls('submitted');
 			this.updateLayout();
 			return;
