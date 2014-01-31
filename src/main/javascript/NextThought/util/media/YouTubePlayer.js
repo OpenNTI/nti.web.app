ffc46a4b5632e49e28ea6300463176cebe5f29e3
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-01-31 08:19:07 +0000

    grr.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@31979 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/util/media/YouTubePlayer.js
+++ src/main/javascript/NextThought/util/media/YouTubePlayer.js
@@ -30,6 +30,7 @@ Ext.define('NextThought.util.media.YouTubePlayer', {
 		this.player = null;
 		this.width = config.width;
 		this.height = config.height;
+		this.parent = config.parent;
 
 		this.playerSetup();
 	},
