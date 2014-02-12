11660d1d92c78a14e84385676943cf97b6a48217
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-02-12 21:21:43 +0000

    stop the one function that tanks us. monitor the owning grid & its parents for activation so we can force a re-layout.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@32846 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/overrides/grid/plugin/BufferedRenderer.js
+++ src/main/javascript/NextThought/overrides/grid/plugin/BufferedRenderer.js
@@ -1,32 +1,59 @@
 Ext.define('NextThought.overrides.grid.plugin.BufferedRenderer', {
 	override: 'Ext.grid.plugin.BufferedRenderer',
 
-	onViewRefresh: function() {
-		console.debug('Buffered View Refresh?', arguments);
+	/*onViewRefresh: function() {
+		console.debug('BufferedRenderer: View Refresh?', arguments);
 		if (this.grid.isVisible(true)) {
 			this.callParent(arguments);
 		} else {
 			console.debug('droped refresh, not visible');
 		}
-	},
+	},*/
 
 
-	onViewResize: function() {
-		console.debug('Buffered View Resize?', arguments);
+	/*onViewResize: function() {
+		console.debug('BufferedRenderer: View Resize?', arguments);
 		if (this.grid.isVisible(true)) {
 			this.callParent(arguments);
 		} else {
 			console.debug('droped view resize, not visible');
 		}
-	},
+	},*/
 
 
 	renderRange: function() {
-		console.debug('Buffered render range?', arguments);
+		console.debug('BufferedRenderer: renderRange?', arguments);
 		if (this.grid.isVisible(true)) {
 			this.callParent(arguments);
 		} else {
-			console.debug('droped renderRange, not visible');
+			console.debug('BufferedRenderer: droped renderRange, not visible');
 		}
+	},
+
+
+	init: function(grid) {
+		this.callParent(arguments);
+		this.__monitorActivation(grid);
+	},
+
+
+	__monitorActivation: function(grid) {
+		function monitorCardChange(cmp, me) {
+			var c = cmp.up('{isOwnerLayout("card")}');
+			me = me || cmp;
+			if (c) {
+				me.mon(c, {
+					buffer: 1,
+					show: function() {
+						if (grid.isVisible(true)) {
+ 							grid.updateLayout({defer: false});
+						}
+					}
+				});
+				monitorCardChange(c, me);
+			}
+		}
+
+		monitorCardChange(grid);
 	}
 });
