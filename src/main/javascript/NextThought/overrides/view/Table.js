c8c1614c30ee11340e6bd93f624a58a1e0fc5e2f
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-02-24 19:02:34 +0000

    catch some errors that might be thrown with our buffered stores
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@33526 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/main/javascript/NextThought/overrides/view/Table.js
@@ -0,0 +1,13 @@
+Ext.define('NextThought.overrides.view.Table', {
+	override: 'Ext.view.Table',
+
+	getRecord: function() {
+		try {
+		//buffered stores may throw an error if we try to access an item that has
+		// been paged out... lets not blow a gasket in this senario.
+			return this.callParent(arguments);
+		} catch (e) {
+			return null;
+		}
+	}
+});
