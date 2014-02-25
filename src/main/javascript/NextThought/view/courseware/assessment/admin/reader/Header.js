8156303fa2078ead9b395713cc1864bc6f0d7e41
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-02-25 18:27:47 +0000

    doh passed the wrong args
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@33621 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/courseware/assessment/admin/reader/Header.js
+++ src/main/javascript/NextThought/view/courseware/assessment/admin/reader/Header.js
@@ -62,6 +62,6 @@ Ext.define('NextThought.view.courseware.assessment.admin.reader.Header', {
 
 	goTo: function(rec) {
 		var v = this.parentView;
-		Ext.defer(v.fireGoToAssignment, 1, v, [rec]);
+		Ext.defer(v.fireGoToAssignment, 1, v, [v, rec]);
 	}
 });
