2c4f92dac7c8062adce1d691cc81776d304c3ac9
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-03-04 02:23:09 +0000

    some cleanup
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@34081 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/form/fields/SimpleTextField.js
+++ src/main/javascript/NextThought/view/form/fields/SimpleTextField.js
@@ -1,210 +1,202 @@
 Ext.define('NextThought.view.form.fields.SimpleTextField', {
-  extend: 'Ext.Component',
-  alias: 'widget.simpletext',
+	extend: 'Ext.Component',
+	alias: 'widget.simpletext',
 
-  cls: 'textbox-base',
+	cls: 'textbox-base',
 
-  renderTpl: Ext.DomHelper.markup([
-    { tag: 'input', type: '{type}', placeholder: '{placeholder}' },
-    { tag: 'span', cls: 'clear' }
-  ]),
+	renderTpl: Ext.DomHelper.markup([
+		{ tag: 'input', type: '{type}', placeholder: '{placeholder}' },
+		{ tag: 'span', cls: 'clear' }
+	]),
 
-  renderSelectors: {
-    inputEl: 'input',
-    clearEl: '.clear'
-  },
+	renderSelectors: {
+		inputEl: 'input',
+		clearEl: '.clear'
+	},
 
-  silentIsValid: true,
-
-  constructor: function(config) {
-    delete config.autoEl;
-    delete config.renderTpl;
-    delete config.renderSelectors;
-    this.callParent(arguments);
-  },
+	config: {
+		inputType: 'text',
+		placeholder: '',
+		validator: null
+	},
 
 
-  initComponent: function() {
-    this.renderData = {
-      type: this.inputType || 'text',
-      placeholder: this.placeholder
-    };
-  },
+	silentIsValid: true,
+
+	constructor: function(config) {
+		delete config.autoEl;
+		delete config.renderTpl;
+		delete config.renderSelectors;
+		this.callParent(arguments);
+	},
 
 
-  setError: function() {
-    var e = this.inputEl;
-    e.removeCls('error');//if there is an animation, we have to remove the class before it will play again.
-    e.addCls('error');
-    //this.removeCls('error');
-    //this.addCls('error');
-
-  },
+	initComponent: function() {
+		this.renderData = {
+			type: this.getInputType(),
+			placeholder: this.getPlaceholder()
+		};
+	},
 
 
-  clearValue: function(silent) {
-    var e = this.inputEl;
-    e.dom.value = '';
-    e.removeCls('error');
-    this.clearEl.hide();
-    this.keyPressed(new Ext.EventObjectImpl());
-    if (!silent) {
-      this.focus();
-      this.fireEvent('clear');
-    }
-  },
+	setError: function() {
+		//if there is an animation, we have to remove the class before it will play again.
+		this.inputEl.removeCls('error').addCls('error');
+	},
 
 
-  reset: function() {
-    this.clearValue(true);
-  },
+	clearValue: function(silent) {
+		var e = this.inputEl;
+		e.dom.value = '';
+		e.removeCls('error');
+		this.clearEl.hide();
+		this.keyPressed(new Ext.EventObjectImpl());
+		if (!silent) {
+			this.focus();
+			this.fireEvent('clear');
+		}
+	},
 
 
-  getValue: function() {
-    return this.inputEl.getValue();
-  },
+	reset: function() { this.clearValue(true); },
 
 
-  setValue: function(v) {
-    this.update(v);
-  },
-
-  maybeTagEmpty: function(v) {
-    this.inputEl[(Ext.isEmpty(v) ? 'add' : 'remove') + 'Cls']('empty');
-    this[(Ext.isEmpty(v) ? 'add' : 'remove') + 'Cls']('empty');
-  },
-
-  update: function(v) {
-    this.inputEl.dom.value = v;
-    this.maybeTagEmpty(v);
-    if (!this.readOnly) {
-      this.clearEl[v !== '' ? 'show' : 'hide']();
-    }
-    //this.handleBlur();
-  },
+	getValue: function() { return this.inputEl.getValue(); },
 
 
-  afterRender: function() {
-    this.callParent(arguments);
-    var e = this.inputEl;
-    this.maybeTagEmpty();
+	setValue: function(v) { this.update(v); },
 
-    if (this.readOnly) {
-      e.set({readonly: 'readonly'});
-      this.clearEl.hide();
-    }
+	__maybeTagEmpty: function(v) {
+		var a = (Ext.isEmpty(v) ? 'add' : 'remove') + 'Cls',
+			c = 'empty';
+		this.inputEl[a](c);
+		this[a](c);
+	},
 
-    this.mon(e, {
-      scope: this,
-      keyup: this.keyPressed,
-      contextmenu: function(e) {
-        e.stopPropagation();
-      } //allow context on simple texts
-    });
-    this.mon(e, Ext.EventManager.getKeyEvent(), this.keyDown, this);
-
-	if(this.inputEl){
-		this.inputEl.on('paste', this.onPaste, this);
-	}
-
-    this.mon(this.clearEl, 'click', function() {
-      this.clearValue();
-    }, this);
-    this.lastValue = this.getValue();
-  },
+	update: function(v) {
+		this.inputEl.dom.value = v;
+		this.__maybeTagEmpty(v);
+		if (!this.readOnly) {
+			this.clearEl[v !== '' ? 'show' : 'hide']();
+		}
+		//this.handleBlur();
+	},
 
 
-  getFocusEl: function() {
-    return this.inputEl;
-  },
+	afterRender: function() {
+		this.callParent(arguments);
+		var e = this.inputEl,
+			monitors = {
+				scope: this,
+				keyup: 'keyPressed',
+				contextmenu: function(e) { e.stopPropagation(); } //allow context menu
+			};
+
+		monitors[Ext.EventManager.getKeyEvent()] = 'keyDown';
 
 
-  specialKeys: {
-    27: true,	//Ext.EventObject.prototype.ESC
-    8: true,	//Ext.EventObject.prototype.BACKSPACE
-    46: true	//Ext.EventObject.prototype.DELETE
-  },
+		this.__maybeTagEmpty();
+
+		if (this.readOnly) {
+			e.set({readonly: 'readonly'});
+			this.clearEl.hide();
+		}
+
+		this.mon(e, monitors);
+		this.mon(this.clearEl, 'click', this.clearValue.bind(this, false));
+
+		if (this.inputEl) {
+			this.inputEl.on('paste', 'onPaste', this);
+		}
+
+		this.lastValue = this.getValue();
+	},
 
 
-  keyDown: function(event) {
-    var k = event.getKey();
-
-    //We need this to fit in more tightly with Ext's Field interface.
-    if (event.isSpecialKey()) {
-      this.fireEvent('specialkey', this, new Ext.EventObjectImpl(event));
-    }
-
-    if (this.specialKeys[k]) {
-      if (k === event.ESC) {
-        if (this.inputEl.dom.value === '') {
-          return;
-        }
-        this.clearValue();
-      }
-      event.stopPropagation();
-      this.keyPressed(event);
-    }
-  },
+	getFocusEl: function() { return this.inputEl; },
 
 
-  onPaste: function(e){
-	  var me = this;
-	  if (!me.pasteTask) {
-		  me.pasteTask = new Ext.util.DelayedTask(me.keyPressed, me, [e]);
-	  }
-	  // since we can't get the paste data, we'll give the area a chance to populate
-	  me.pasteTask.delay(1);
-  },
+	specialKeys: {
+		27: true,	//Ext.EventObject.prototype.ESC
+		8: true,	//Ext.EventObject.prototype.BACKSPACE
+		46: true	//Ext.EventObject.prototype.DELETE
+	},
 
 
-  keyPressed: function(event) {
-    var e = this.inputEl,
-            k = event.getKey && event.getKey(),
-            v = this.getValue(),
-            c = this.clearEl;
+	keyDown: function(event) {
+		var k = event.getKey();
 
-    if (!this.readOnly) {
-      c[v ? 'show' : 'hide']();
-    }
+		//We need this to fit in more tightly with Ext's Field interface.
+		if (event.isSpecialKey()) {
+			this.fireEvent('specialkey', this, new Ext.EventObjectImpl(event));
+		}
 
-    e.removeCls('error');
-
-    if (k && (k === event.ENTER || k === event.ESC)) {
-      this.fireEvent('commit', v, this);
-    }
-
-    if (this.lastValue !== v) {
-      this.lastValue = v;
-      this.maybeTagEmpty(v);
-      this.fireEvent('changed', v, this);
-    }
-  },
+		if (this.specialKeys[k]) {
+			if (k === event.ESC) {
+				if (this.inputEl.dom.value === '') {
+					return;
+				}
+				this.clearValue();
+			}
+			event.stopPropagation();
+			this.keyPressed(event);
+		}
+	},
 
 
-  validate: function(silent) {
-    var valid,
-        val = this.getValue() || '';
-
-    if (!silent) {
-      this.inputEl.removeCls('error');
-    }
-    valid = (this.allowBlank === false) ? (val.length >= (this.minLength || 1)) : true;
-
-    if (valid && this.validator) {
-      //The docs say validator should return true or an error string, both of which are truthy here :/
-      valid = Boolean(this.validator(val));
-    }
-
-    if (!silent && !valid) {
-      this.setError();
-    }
-
-    return valid;
-  },
+	onPaste: function(e) {
+		var me = this;
+		if (!me.pasteTask) {
+			me.pasteTask = new Ext.util.DelayedTask(me.keyPressed, me, [e]);
+		}
+		// since we can't get the paste data, we'll give the area a chance to populate
+		me.pasteTask.delay(1, null, null, null);
+	},
 
 
-  isValid: function() {
-    return this.validate(this.silentIsValid);
-  }
+	keyPressed: function(event) {
+		var e = this.inputEl, v = this.getValue(), c = this.clearEl,
+			k = event.getKey && event.getKey();
+
+		if (!this.readOnly) {
+			c[v ? 'show' : 'hide']();
+		}
+
+		e.removeCls('error');
+
+		if (k && (k === event.ENTER || k === event.ESC)) {
+			this.fireEvent('commit', v, this);
+		}
+
+		if (this.lastValue !== v) {
+			this.lastValue = v;
+			this.__maybeTagEmpty(v);
+			this.fireEvent('changed', v, this);
+		}
+	},
+
+
+	validate: function(silent) {
+		var valid, val = this.getValue() || '';
+
+		if (!silent) {
+			this.inputEl.removeCls('error');
+		}
+		valid = (this.allowBlank === false) ? (val.length >= (this.minLength || 1)) : true;
+
+		if (valid && this.validator) {
+			//The docs say validator should return true or an error string, both of which are truthy here :/
+			valid = Boolean(this.validator(val));
+		}
+
+		if (!silent && !valid) {
+			this.setError();
+		}
+
+		return valid;
+	},
+
+
+	isValid: function() { return this.validate(this.silentIsValid); }
 });
 
