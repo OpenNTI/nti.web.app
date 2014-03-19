5f997e688b817fc844f3997ad5f3e545fb8f7030
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-03-19 01:45:13 +0000

    this should cleanup some of the erroronious deprecation warnings. (some were very redundant)
    Also, make "Deffered" an official Promise type. (wher you can make it without a job callback) I don't like it. But here you go.  Heed the warnings in the comments.
    
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@35091 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/util/Promise.js
+++ src/main/javascript/NextThought/util/Promise.js
@@ -2,12 +2,12 @@
 window.Promise = null;
 
 //TODO: use native promises asap
-window.Promise = window.Promise || (function(Global) {
+Promise = window.Promise || (function(global) {
 
 	//<editor-fold desc="Private shared methods">
 	function then(onFulfilled, onRejected) {
 		var chain = onFulfilled && onFulfilled.then ? onFulfilled : null,
-			promise = chain || PromiseFactory.make(),
+			promise = chain || new Promise(WHEN_THEN),
 			me = this;
 
 		if (chain) {
@@ -103,22 +103,23 @@ window.Promise = window.Promise || (function(Global) {
 	}
 	//</editor-fold>
 
-	var nextId = 1, Cls, p, State = { PENDING: 0, FULFILLED: 1, REJECTED: 2 };
+	var WHEN_THEN = {},
+		nextId = 1, Cls, p, State = { PENDING: 0, FULFILLED: 1, REJECTED: 2 };
 
 	Cls = function(worker) {
 		this.id = (nextId++);
 		this.state = State.PENDING;
 
-		if (worker) {
+		if (worker && worker.call) {
 			try {
-				worker.call(Global, fulfill.bind(this), reject.bind(this));
+				worker.call(global, fulfill.bind(this), reject.bind(this));
 			} catch (e) {
 				reject.call(this, e);
 			}
-		} else {
+		} else if (worker !== WHEN_THEN) {
 			//The spec expects the constructor of a promise to take a callback that will do the work... our implementation was not so contained. We have more
 			// of a Deffered promise model. Where we get an empty Promise object and externally fulfill/reject it.
-			console.debug('No callback This invocation will break with native Promises');
+			console.error('No callback This invocation will break with native Promises');
 		}
 	};
 
@@ -133,6 +134,9 @@ window.Promise = window.Promise || (function(Global) {
 Ext.applyIf(Promise.prototype, {
 	done: function(fn) { this.validateHandler(fn); return this.then(fn); },
 	fail: function(fn) { this.validateHandler(fn); return this.then(undefined, fn); },
+	//the proper name is 'catch', however JSLint has a problem with that (reserved word and all) so lets keep using "fail".
+	//this is only here to be compliant to the api.
+	'catch': function(fn) { return this.fail.apply(this, arguments); },
 	always: function(fn) {this.validateHandler(fn); return this.then(fn, fn); },
 	validateHandler: function(fn) { if (typeof fn !== 'function') { throw new TypeError('Expected a function'); } },
 
@@ -155,97 +159,114 @@ Ext.applyIf(Promise, {
 
 
 /**
- * The standard calls this "all"...
- * TODO: refactor to name this "all" and only define it if it is not already defined.
+ * Deffered promise.
+ * I guess there _MIGHT_ be some cases where we want this pattern. :/ I don't like it. But here you go.
+ *
+ * This will make a promise that you may or may NOT be able to keep.
+ *    -- There is no guarantee that fulfill or reject will get called.
+ *
+ * This version of the promise is no better than the callback-hell model.  Keep in mind that
+ * Deffered's do not force execution of their promise, the are not Guaranteed to resolve.
+ *
+ * I strongly recommend examining your code and your structure before commiting to using this as a final solution.
+ */
+Ext.define('Deffered', {
+	extend: 'Promise',
+
+	constructor: function() {
+		var p = this;
+
+		function wtf(f, r) {
+			p.fulfill = function(value) {f(value);};
+			p.reject = function(reason) {r(reason);};
+		}
+
+		this.callParent([wtf]);
+	}
+});
+
+
+/**
+ *
+ * @param {Promise[]} promises
+ * @type {Function}
  * @return {Promise}
  */
-Promise.pool = function() {
+Promise.all = Promise.all || function(promises) {
 	// get promises
-	var promises = [].slice.call(arguments, 0),
-		values = [],
-		state = 'fulfill',
-		toGo = promises.length, i,
-	// promise to return
-		promise = PromiseFactory.make();
-
-
-	if (Object.prototype.toString.call(promises[0]) === '[object Array]') {
-		promises = promises[0];
-		toGo = promises.length;
-	}
+	var values = [],
+		toGo = promises.length, i;
 
 	values.length = promises.length;
 
+	return new Promise(function(fulfill, reject) {
+		var state = fulfill;
 
-	// whenever a promise completes
-	function checkFinished() {
-		// check if all the promises have returned
-		if (toGo) {
-			return;
-		}
-		// set the state with all values if all are complete
-		promise[state](values);
-	}
-
-	function prime(index) {
-		var p = promises[index];
-
-		if (!p || !p.then) {//handle falsy/non-promise @ index
-			toGo--;
-			checkFinished();
-			return;
+		// whenever a promise completes
+		function checkFinished() {
+			// check if all the promises have returned
+			if (toGo) {
+				return;
+			}
+			// set the state with all values if all are complete
+			state(values);
 		}
 
-		p.then(function(value) {
-			// on success
-			values[index] = value;
-			toGo--;
-			checkFinished();
-		}, function(value) {
-			// on error
-			values[index] = value;
-			toGo--;
-			// set error state
-			state = 'reject';
-			checkFinished();
-		});
-	}
+		function prime(index) {
+			var p = promises[index];
 
-	checkFinished();//handle empty array.
+			function done(value) {
+				values[index] = value;
+				toGo--;
+				checkFinished();
+			}
 
-	// whenever a promise finishes check to see if they're all finished
-	for (i = 0; i < promises.length; i++) {
-		prime(i);
-	}
+			if (!p || !p.then) {//handle falsy/non-promise @ index
+				done(p);
+				return;
+			}
 
-	// promise at the end
-	return promise;
+			p.then(done,// on success
+					function(v) {
+						state = reject;
+						done(v);
+					});
+		}
+
+		checkFinished();//handle empty array.
+
+		// whenever a promise finishes check to see if they're all finished
+		for (i = 0; i < promises.length; i++) {
+			prime(i);
+		}
+	});
 };
 
 
+/**
+ * Not sure we need this flexibility. Most the places we call this use the Array arg.
+ *
+ * @param {...Promise|Promise[]} var_args
+ * @return {Promise}
+ */
+Promise.pool = function(var_args) {
+	var waitOn = var_args;
+	if (Object.prototype.toString.call(var_args) !== '[object Array]') {
+		waitOn = Array.prototype.slice.call(arguments);
+	}
+
+	return Promise.all(waitOn);
+};
 
 
 Ext.define('NextThought.util.Promise', {
 	singleton: true,
 
 	make: function() {
-		function clean(i) {
-			delete p.fulfill; delete o.fulfill;
-			delete p.reject; delete o.reject;
-			p.state = i; o.state = i;
-		}
-
-		var o = {state: 0},
-		//because we used a "deferred" promise model (fulfullment/rejection happens externally) we have to construct it and bring the fulfull/reject
-		// functions to the surface.
-			p = new Promise(function(f, r) {
-				o.fulfill = function(value) {f(value); clean(1);};
-				o.reject = function(reason) {r(reason); clean(2);};
-			});
-
-		Ext.applyIf(p, o);
 		console.deprecated('[Bad Practice!] Promises should be not made without a guarantee. See this line.');
 		/**
+		 * Do NOT just replace .make() with "new Deffered()". Actually take the time to evaluate.
+		 *
 		 * Prferred pattern ex:
 		 * new Promise(function (fulfill, reject) {//the Guarantee
 		 *     //do something async here
@@ -257,7 +278,7 @@ Ext.define('NextThought.util.Promise', {
 		 *          .fail(reject);
 		 * });
 		 */
-		return p;
+		return new Deffered();
 	}
 
 }, function() { window.PromiseFactory = this; });
