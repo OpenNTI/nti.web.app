f4d52b530da0c0b63abf350de43a6044ee7b18ef
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-03-22 16:14:34 +0000

    pie progress, just need to make the legend now
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@35359 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/chart/Pie.js
+++ src/main/javascript/NextThought/chart/Pie.js
@@ -14,8 +14,22 @@ Ext.define('NextThought.chart.Pie', {
 
 	config: {
 		title: '',
-		color: '#40b450',
-		pixelDensity: 2
+		colors: ['#40b450', /*'#b8b8b8',*/ '#3fb3f6', '#F35252'],
+		pixelDensity: 2,
+		series: [
+			{value: 12, label: 'foo'},
+			{value: 30, label: 'bar'},
+			{value: 23, label: 'baz'}
+		]
+	},
+
+
+	updateSeries: function(v) {
+		function sum(a, v) { return a + v.value; }
+		function deg(v) { return (v.value / total) * 360; }
+
+		var total = v && v.reduce && v.reduce(sum, 0);
+		this.data = (v && v.map && v.map(deg)) || [];
 	},
 
 
@@ -23,6 +37,7 @@ Ext.define('NextThought.chart.Pie', {
 		this.addCls('pie');
 		this.callParent(arguments);
 		this.renderData.title = this.getTitle();
+		this.updateSeries(this.getSeries());
 	},
 
 
@@ -45,33 +60,69 @@ Ext.define('NextThought.chart.Pie', {
 	redraw: function() {
 		if (!this.context) {return;}
 		this.context.canvas.width += 0; //set the canvas dirty and make it clear on next draw.
-		this.drawCircle();
+		this.drawPie();
 	},
 
-	drawCircle: function() {
+
+	degreesToRadians: function(degrees) { return (degrees * Math.PI) / 180; },
+
+
+	sumTo: function(i) {
+		var sum = 0, j = 0;
+		for (j; j < i; j++) {
+			sum += this.data[j];
+		}
+		return sum;
+	},
+
+
+	drawSegment: function(i) {
 		var ctx = this.context,
-				stroke = this.canvas.width * (1 / 112),
-				centerX = this.canvas.width / 2,
-				centerY = this.canvas.height / 2,
-				radius = this.canvas.width / 4;
+			radius = Math.floor(this.canvas.width / 4),
+
+			startingAngle = this.degreesToRadians(this.sumTo(i)),
+			arcSize = this.degreesToRadians(this.data[i]),
+			endingAngle = startingAngle + arcSize;
+
+		ctx.save();
+
+		ctx.beginPath();
+		ctx.moveTo(0, 0);
+
+		ctx.arc(0, 0, radius, startingAngle, endingAngle, false);
+		ctx.arc(0, 0, radius * 0.5, endingAngle, startingAngle, true);
+
+		ctx.closePath();
+
+		ctx.fillStyle = this.colors[i % this.colors.length];
+		ctx.fill();
+
+		ctx.restore();
+	},
+
+
+	drawPie: function() {
+		var ctx = this.context,
+			centerX = this.canvas.width / 2,
+			centerY = this.canvas.height / 2,
+			len = this.data.length, i = 0;
 
 		ctx.save();
 		try {
 			ctx.setTransform(1, 0, 0, 1, 0, 0);
 			ctx.translate(centerX, centerY);
 			ctx.rotate(-Math.PI / 2);
-			ctx.beginPath();
-			ctx.arc(0, 0, radius, 0, (2 * Math.PI) * (79 / 100), false);
-			ctx.lineWidth = stroke;
-			ctx.fillStyle = this.getColor();
-			ctx.strokeStyle = this.getColor();
 
-			//ctx.shadowColor = '#ddd';
-			//ctx.shadowBlur = 5;
-			//ctx.shadowOffsetX = 1;
-			//ctx.shadowOffsetY = 1;
+			ctx.shadowColor = '#ddd';
+			ctx.shadowBlur = 5;
+			ctx.shadowOffsetX = 1;
+			ctx.shadowOffsetY = 1;
 
-			ctx.stroke();
+			for (i; i < len; i++) {
+				this.drawSegment(i);
+			}
+
+
 		} finally {
 			ctx.restore();
 		}
