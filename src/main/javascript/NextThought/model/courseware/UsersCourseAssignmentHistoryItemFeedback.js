0c8ed33725d20bb194fcef4ae41c7ba33dc31f18
Andrew Ligon <andrew.ligon@nextthought.com>
2014-04-04 22:49:30 +0000

    Add helpers to get the feedback container, the submission, and the course off of a feedback item.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@36550 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/model/courseware/UsersCourseAssignmentHistoryItemFeedback.js
+++ src/main/javascript/NextThought/model/courseware/UsersCourseAssignmentHistoryItemFeedback.js
@@ -9,5 +9,63 @@ Ext.define('NextThought.model.courseware.UsersCourseAssignmentHistoryItemFeedbac
 		{ name: 'GroupingField', mapping: 'Last Modified', type: 'groupByTime', persist: false, affectedBy: 'Last Modified'},
 		{name: 'assignmentName', type: 'string', persist: false},
 		{name: 'assignmentContainer', type: 'string', persist: false}
-	]
+	],
+
+
+	getFeedbackContainerURL: function() {
+		var href = this.get('href');
+
+		return href && href.split('/').slice(0, -1).join('/');
+	},
+
+	getSubmissionURL: function() {
+		var href = this.get('href');
+
+		return href && href.split('/').slice(0, -2).join('/');
+	},
+
+	getFeedbackContainer: function() {
+		var url = this.getFeedbackContainerURL();
+
+		if (this.containerPromise) {
+			return this.containerPromise;
+		}
+
+		this.containerPromise = Service.request(url)
+			.done(function(resp) {
+				return ParseUtils.parseItems(resp)[0];
+			});
+
+		return this.containerPromise;
+	},
+
+
+	getSubmission: function() {
+		var me = this,
+			url = me.getSubmissionURL();
+
+		if (me.submissionPromise) {
+			return me.submissionPromise;
+		}
+
+		me.submissionPromise = Service.request(url)
+			.done(function(resp) {
+				var submission = ParseUtils.parseItems(resp)[0];
+
+				if (!me.containerPromise) {
+					Promise.resovle(submission.get('Feedback'));
+				}
+
+				return submission;
+			});
+
+		return me.submissionPromise;
+	},
+
+
+	getCourse: function() {
+		var catalogEntry = CourseWareUtils.courseForNtiid(this.get('AssignmentId'));
+
+		return CourseWareUtils.findCourseBy(catalogEntry.findByMyCourseInstance());
+	}
 });
