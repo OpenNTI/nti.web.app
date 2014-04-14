e193d47b701426bd19aa1581b0d43e193404f5d3
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-04-14 22:35:45 +0000

    update verbage for email setting.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@37189 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/account/settings/Preferences.js
+++ src/main/javascript/NextThought/view/account/settings/Preferences.js
@@ -26,7 +26,7 @@ Ext.define('NextThought.view.account.settings.Preferences', {
 			{
 				tag: 'span',
 				cls: 'not-ready nti-checkbox email',
-				html: 'Send emails periodically of interesting changes.',
+				html: 'Send me email notifications about activity I may have missed.',
 				tabIndex: 0,
 				role: 'button',
 				'aria-role': 'button',
