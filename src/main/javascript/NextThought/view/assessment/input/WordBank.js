34a2e8b5a3a4c626773e29456646b63540773b4a
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-04-18 17:06:47 +0000

    style input boxes
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@37482 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/assessment/input/WordBank.js
+++ src/main/javascript/NextThought/view/assessment/input/WordBank.js
@@ -15,6 +15,9 @@ Ext.define('NextThought.view.assessment.input.WordBank', {
 	inputTpl: Ext.DomHelper.markup({ cls: 'fill-in', html: '{lineWithBlank}' }),
 
 
+	blankTpl: Ext.DomHelper.createTemplate({ tag: 'span', cls: 'blank drop target', html: '&nbsp;' }),
+
+
 	renderSelectors: {
 		wordBankEl: '.wordbank-ct'
 	},
@@ -30,10 +33,20 @@ Ext.define('NextThought.view.assessment.input.WordBank', {
 
 	afterRender: function() {
 		this.callParent(arguments);
-		var wordbank = this.part.get('wordbank');
+		var blanks,
+			wordbank = this.part.get('wordbank');
 		if (wordbank) {
 			this.wordbank = Ext.widget({xtype: 'assessment-components-wordbank', record: this.part, renderTo: this.wordBankEl});
 		}
+
+		blanks = this.inputBox.query('input.nqablankfield');//TODO: input[type="blank"]
+
+		blanks.forEach(this.setupBlank.bind(this));
+	},
+
+
+	setupBlank: function(input) {
+		this.blankTpl.insertAfter(input, input.dataset);
 	},
 
 
