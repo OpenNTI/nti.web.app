fe492b9180d46feec95ea4e4e1a8a452d8699565
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-05-09 14:58:14 +0000

    add spacer for overview
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@38712 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/main/javascript/NextThought/view/courseware/overview/parts/Spacer.js
@@ -0,0 +1,10 @@
+Ext.define('NextThought.view.courseware.overview.parts.Spacer', {
+	extend: 'Ext.Component',
+	alias: [
+		'widget.course-overview-spacer',
+		'widget.course-overview-nticourseoverviewspacer'
+	],
+
+	ui: 'course',
+	cls: 'overview-spacer'
+});
