d2bc70e250935b46c4f32a4a721f51ea0ecbc609
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-05-14 19:14:12 +0000

    blank out open course widget strings (add 'you are registered' text)
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@39048 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/resources/strings/alibra.nextthought.com/strings.js
+++ src/main/resources/strings/alibra.nextthought.com/strings.js
@@ -8,11 +8,11 @@ NTIStrings['NextThought.view.courseware.info.parts.Description.hours'] = ' ';
 NTIStrings['course-info.description-widget.open-enrolled'] = ' ';
 NTIStrings['course-info.description-widget.for-no-credit'] = '';
 
-NTIStrings['course-info.open-course-widget.heading'] = ' ';
-NTIStrings['course-info.open-course-widget.message'] = ' ';
-NTIStrings['course-info.open-course-widget.free-to-anyone'] = ' ';
-NTIStrings['course-info.open-course-widget.not-for-credit'] = ' ';
-NTIStrings['course-info.open-course-widget.registered'] = ' ';
+NTIStrings['course-info.open-course-widget.heading'] = '';
+NTIStrings['course-info.open-course-widget.message'] = '';
+NTIStrings['course-info.open-course-widget.free-to-anyone'] = '';
+NTIStrings['course-info.open-course-widget.not-for-credit'] = '';
+NTIStrings['course-info.open-course-widget.registered'] = 'You are registered.';
 
 NTIStrings['course-info.course-supoprt.phone'] = '';
 NTIStrings['course-info.course-supoprt.link1.Label'] = 'Submit Request';
