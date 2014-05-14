90c2a2556eb88e27fec0d28688bccec5b2ace8c2
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-05-14 20:58:12 +0000

    our pre-localization strings were not copied into the RU file.  These entries need to be added to the master list.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@39082 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/resources/locales/ru/LC_MESSAGES/NextThoughtWebApp.js
+++ src/main/resources/locales/ru/LC_MESSAGES/NextThoughtWebApp.js
@@ -1,3 +1,47 @@
+NTIStrings['UnresolvedUsernamePattern'] = 'username';
+
+NTIStrings['default_scoreboard_message'] = 'Results';
+
+NTIStrings['Apologies'] = 'Our Apologies!';
+NTIStrings['Cannot load page.'] = 'Cannot load page. Please try again later.';
+
+NTIStrings['forum_sharing_label'] = 'Share with all students.';
+
+NTIStrings['forums.empty_boards_header'] = 'Oh no! It\'s empty!';
+NTIStrings['forums.empty_boards_sub'] = 'There are no forum boards yet.';
+
+NTIStrings['contact_us_title'] = 'Contact Us...';
+NTIStrings['contact_us_message'] = 'Please use the form below to share your comments, report an issue, or suggest new features.  If you need help or have a question about the features, please take a look at the NextThought Help Center.  We may already have content there to help you.';
+
+NTIStrings['alias_request_title'] = 'Request Name Change';
+NTIStrings['alias_request_message'] = 'Please use the form below to request a change in the way your name is displayed. Submit the name you would like to use and the reason for making this change.';
+
+NTIStrings['no_contact_header'] = 'No Contacts Yet';
+NTIStrings['no_contact_sub'] = 'Click search to find some.';
+NTIStrings['no_list_header'] = 'No Lists Yet';
+NTIStrings['no_list_sub'] = 'Click create list to add one.';
+NTIStrings['no_group_header'] = 'No Groups Yet';
+NTIStrings['no_group_sub'] = 'Click create or join to add some.';
+
+NTIStrings['course-enrollment-enroll'] = 'Get Started';
+
+
+NTIStrings['course-info.open-course-widget.heading'] = '';
+NTIStrings['course-info.open-course-widget.message'] = '';
+NTIStrings['course-info.open-course-widget.free-to-anyone'] = '';
+NTIStrings['course-info.open-course-widget.not-for-credit'] = '';
+NTIStrings['course-info.open-course-widget.registered'] = 'You are registered.';
+NTIStrings['course-info.description-widget.for-no-credit'] = '';
+NTIStrings['course-info.description-widget.open-enrolled'] = '';
+
+NTIStrings['course-info.course-supoprt.photo'] = 'resources/images/platform.ou.edu/tech_support.jpg';
+
+NTIStrings['enrollment.enroll.header'] = 'Congratulations!';
+NTIStrings['enrollment.enroll.description'] = 'Your enrollment was successful. This course is open, and you can start participating right away. Visit My Courses to access your materials and get started today.';
+NTIStrings['enrollment.unenroll.header'] = 'Goodbye!';
+NTIStrings['enrollment.unenroll.description'] = 'You are no longer enrolled in this course.';
+
+
 window.NTIStrings = window.NTIStrings || {};
 NTIStrings['forum_creation_text'] = 'To create a new forum fill out the information below.  We\'ll create a new forum for you with the provided title and description';
 NTIStrings['forum_edit_text'] = 'To change the forum\'s title or description, edit the fields below.';
