9cb87dad267702b140168f24f20e90387c7d4552
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-05-14 21:22:55 +0000

    use no-space space...empty strings trigger fallback code.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@39086 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/resources/strings/alibra.nextthought.com/strings.js
+++ src/main/resources/strings/alibra.nextthought.com/strings.js
@@ -3,17 +3,17 @@ window.NTIStrings = window.NTIStrings || {};
 
 //NTIStrings['course-info.course-supoprt.photo'] = 'resources/images/platform.ou.edu/tech_support.jpg';
 
-NTIStrings['NextThought.view.courseware.info.parts.Description.hours'] = ' ';
+NTIStrings['NextThought.view.courseware.info.parts.Description.hours'] = '\u200B';
 
-NTIStrings['course-info.description-widget.open-enrolled'] = ' ';
-NTIStrings['course-info.description-widget.for-no-credit'] = '';
+NTIStrings['course-info.description-widget.open-enrolled'] = '\u200B';
+NTIStrings['course-info.description-widget.for-no-credit'] = '\u200B';
 
-NTIStrings['course-info.open-course-widget.heading'] = '';
-NTIStrings['course-info.open-course-widget.message'] = '';
-NTIStrings['course-info.open-course-widget.free-to-anyone'] = '';
-NTIStrings['course-info.open-course-widget.not-for-credit'] = '';
+NTIStrings['course-info.open-course-widget.heading'] = '\u200B';
+NTIStrings['course-info.open-course-widget.message'] = '\u200B';
+NTIStrings['course-info.open-course-widget.free-to-anyone'] = '\u200B';
+NTIStrings['course-info.open-course-widget.not-for-credit'] = '\u200B';
 NTIStrings['course-info.open-course-widget.registered'] = 'You are registered.';
 
-NTIStrings['course-info.course-supoprt.phone'] = '';
+NTIStrings['course-info.course-supoprt.phone'] = '\u200B';
 NTIStrings['course-info.course-supoprt.link1.Label'] = 'Submit Request';
 NTIStrings['course-info.course-supoprt.link1.URL'] = 'mailto:support@nextthought.com';
