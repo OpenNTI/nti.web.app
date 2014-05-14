252f40cf86d58e3326156f4f0a972953a886d629
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-05-14 16:32:07 +0000

    add strings for alibra
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@39023 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/main/resources/strings/alibra-alpha.nextthought.com
@@ -0,0 +1,1 @@
+alibra.nextthought.com
---
+++ src/main/resources/strings/alibra.nextthought.com/strings.js
@@ -0,0 +1,13 @@
+window.NTIStrings = window.NTIStrings || {};
+
+
+//NTIStrings['course-info.course-supoprt.photo'] = 'resources/images/platform.ou.edu/tech_support.jpg';
+
+NTIStrings['NextThought.view.courseware.info.parts.Description.hours'] = ' ';
+
+NTIStrings['course-info.description-widget.open-enrolled'] = ' ';
+NTIStrings['course-info.description-widget.for-no-credit'] = '';
+
+NTIStrings['course-info.course-supoprt.phone'] = '';
+NTIStrings['course-info.course-supoprt.link1.Label'] = 'Submit Request';
+NTIStrings['course-info.course-supoprt.link1.URL'] = 'support@nextthought.com';
