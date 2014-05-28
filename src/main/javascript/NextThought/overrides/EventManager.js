09966223da5c6668987e305bf9a40f83b09a63cf
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-05-28 21:40:20 +0000

    These events may through NPEs in IE10 when using elements across Iframe boundaries.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@39985 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/overrides/EventManager.js
+++ src/main/javascript/NextThought/overrides/EventManager.js
@@ -1,19 +1,36 @@
 Ext.define('NextThought.overrides.EventManager', function() {
 
-	var normalizeEventExt = Ext.EventManager.normalizeEvent;
+	var EM = Ext.EventManager,
+		normalizeEventExt = EM.normalizeEvent;
 
-	Ext.EventManager.xnormalizeEvent = function(eventName, fn) {
-		if (arguments.length > 2) {
-			console.error('i didnt account for this');
-		}
-		var nomEventName = eventName;
-		if (eventName === 'animationend') {
-			nomEventName = Ext.supports.CSS3TransitionEnd.replace(/transitionend/, eventName);
-			console.debug('listinging on animationEnd', eventName);
-		}
+	function makeSafe(fn) {
+		return function() {
+			try {
+				return fn.apply(this, arguments);
+			} catch (e) {
+				return null;
+			}
+		};
+	}
 
-		return normalizeEventExt.call(this, [nomEventName, fn]);
-	};
+	Ext.apply(EM, {
+		xnormalizeEvent: function(eventName, fn) {
+			if (arguments.length > 2) {
+				console.error('i didnt account for this');
+			}
+			var nomEventName = eventName;
+			if (eventName === 'animationend') {
+				nomEventName = Ext.supports.CSS3TransitionEnd.replace(/transitionend/, eventName);
+				console.debug('listinging on animationEnd', eventName);
+			}
+
+			return normalizeEventExt.call(this, [nomEventName, fn]);
+		},
+
+		getEventCache: makeSafe(EM.getEventCache),
+		getEventListenerCache: makeSafe(EM.getEventListenerCache),
+		handleSingleEvent: makeSafe(EM.handleSingleEvent)
+	});
 
 	return {};
 });
