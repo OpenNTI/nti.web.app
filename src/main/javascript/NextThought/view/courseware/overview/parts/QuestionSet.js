c4c53ddbd1bb5f46798a51530a15198b21f664cb
Andrew Ligon <andrew.ligon@nextthought.com>
2014-06-13 20:24:10 +0000

    Add the time stamp to the assignment widgets in the course overview
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@41368 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/courseware/overview/parts/QuestionSet.js
+++ src/main/javascript/NextThought/view/courseware/overview/parts/QuestionSet.js
@@ -98,7 +98,7 @@ Ext.define('NextThought.view.courseware.overview.parts.QuestionSet', {
 
 		var score = this.down('assessment-score'),
 			tally = this.down('assessment-tally'),
-			format = 'l, F j',
+			format = 'l, F j g:i a',
 			opens = assignment.get('availableBeginning'),
 			date = assignment.get('availableEnding'),
 			day = date && (new Date(date.getTime())).setHours(0, 0, 0, 0),
@@ -203,7 +203,7 @@ Ext.define('NextThought.view.courseware.overview.parts.QuestionSet', {
 			this.addCls('late');
 		}
 		tally[late ? 'setRedText' : 'setGreyText'](getFormattedString('NextThought.view.courseware.overview.parts.QuestionSet.completed', {
-			date: Ext.Date.format(completed, 'l, F j')
+			date: Ext.Date.format(completed, 'l, F j g:i a')
 		}));
 	},
 
