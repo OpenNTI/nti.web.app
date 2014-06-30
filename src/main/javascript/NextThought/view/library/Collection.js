d277467f89e35cfaa7e79f390d5025e75c4908a3
Andrew Ligon <andrew.ligon@nextthought.com>
2014-06-30 20:23:33 +0000

    Ellipsis the course titles to 40 characters
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@42326 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/library/Collection.js
+++ src/main/javascript/NextThought/view/library/Collection.js
@@ -8,11 +8,11 @@ Ext.define('NextThought.view.library.Collection', {
 	rowSpan: 1,
 
 	tpl: Ext.DomHelper.markup([
-		// { cls: 'stratum collection-name', 'aria-label': '{name} {count} items', 'role': 'heading', cn: {
-		// 	'aria-hidden': 'true', cn: [
-		// 		'{name}', {cls: 'count', 'aria-hidden': 'true', html: '{count}'}
-		// 	]
-		// }},
+		//{ cls: 'stratum collection-name', 'aria-label': '{name} {count} items', 'role': 'heading', cn: {
+		//	'aria-hidden': 'true', cn: [
+		//		'{name}', {cls: 'count', 'aria-hidden': 'true', html: '{count}'}
+		//	]
+		//}},
 		{ cls: 'grid', 'role': 'group', 'aria-label': '{name}', cn: {
 			tag: 'tpl', 'for': 'items', cn: ['{entry}']}
 		}
@@ -27,8 +27,8 @@ Ext.define('NextThought.view.library.Collection', {
 			{ tag: 'tpl', 'if': 'sample', cn: { cls: 'sample', 'data-qtip': 'Sample' }}, //store - sample flag
 			{ cls: 'meta', 'aria-hidden': 'true', cn: [
 				{ cls: 'courseName', html: '{courseName}' },  //course name/id
-				{ tag: 'tpl', 'if': 'title', cn: { cls: 'title', html: '{title:ellipsis(50)}',//because multi-line text won't honor ellipsis css, manually do it.
-					'data-qtip': '{[values.title.length>50?Ext.String.htmlEncode(values.title):""]}' } },
+				{ tag: 'tpl', 'if': 'title', cn: { cls: 'title', html: '{title:ellipsis(40)}',//because multi-line text won't honor ellipsis css, manually do it.
+					'data-qtip': '{[values.title.length>40?Ext.String.htmlEncode(values.title):""]}' } },
 				{ tag: 'tpl', 'if': 'author', cn: { cls: 'author', html: '{author}',
 					//it will likely be clipped if its longer than 20 chars, so add a tip if it is
 					'data-qtip': '{[values.author.length>20?Ext.String.htmlEncode(values.author):""]}' } },
