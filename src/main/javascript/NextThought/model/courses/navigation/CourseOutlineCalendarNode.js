a098b18ec1c9afe51e45f92ee931cf20b9861194
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-07-27 00:16:49 +0000

    add mime type
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@44277 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/model/courses/navigation/CourseOutlineCalendarNode.js
+++ src/main/javascript/NextThought/model/courses/navigation/CourseOutlineCalendarNode.js
@@ -1,3 +1,4 @@
 Ext.define('NextThought.model.courses.navigation.CourseOutlineCalendarNode', {
-	extend: 'NextThought.model.courses.navigation.CourseOutlineNode'
+	extend: 'NextThought.model.courses.navigation.CourseOutlineNode',
+	mimeType: 'application/vnd.nextthought.courses.courseoutlinecalendarnode'
 });
