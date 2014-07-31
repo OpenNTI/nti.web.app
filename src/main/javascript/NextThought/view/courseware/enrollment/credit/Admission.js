0fad9d1ca3a5c7eaa8f6c88c2175965878105b64
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-07-31 16:34:47 +0000

    128 was a spec error, its 18
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@44854 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/courseware/enrollment/credit/Admission.js
+++ src/main/javascript/NextThought/view/courseware/enrollment/credit/Admission.js
@@ -203,9 +203,9 @@
 								/*valueType: 'numeric',
 								valuePattern: [
 									{ '^\\d{0,10}$': '({{999}}) {{999}}-{{9999}}' },
-									{ '*': '{{' + ((new Array(128)).join('*')) + '}}' }
+									{ '*': '{{' + ((new Array(18)).join('*')) + '}}' }
 								],*/
-								valueValidation: /^.{1,128}$/,
+								valueValidation: /^.{1,18}$/,
 								placeholder: 'Primary Phone', required: true, size: 'large'}
 						]
 					},
