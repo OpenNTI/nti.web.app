c92109bfdfe142446801b949e735b877332c5187
Andrew Ligon <andrew.ligon@nextthought.com>
2014-08-05 19:49:08 +0000

    On 422 don't show the contact ou message.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@45366 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/courseware/enrollment/credit/Admission.js
+++ src/main/javascript/NextThought/view/courseware/enrollment/credit/Admission.js
@@ -613,41 +613,44 @@ Ext.define('NextThought.view.courseware.enrollment.credit.Admission', {
 			items = [];
 
 		if (json) {
-			items.push({
-				xtype: 'enrollment-credit-set',
-				inputs: [
+			fields.unshift({
+				name: 'rejected',
+				label: 'A Problem Occurred. Please Correct the Following:',
+				labelCls: 'error',
+				items: [
 					{
-						type: 'description',
-						text: json.Message,
-						cls: 'error-detail'
+						xtype: 'enrollment-credit-set',
+						inputs: [
+							{
+								type: 'description',
+								text: json.Message,
+								cls: 'error-detail'
+							}
+						]
+					}
+				]
+			});
+		} else {
+			fields.unshift({
+				name: 'rejected',
+				label: 'We are unable to confirm your eligibility to enroll through this process.',
+				labelCls: 'error',
+				items: [
+					{
+						xtype: 'enrollment-credit-set',
+						inputs: [
+							{
+								type: 'description',
+								text: 'Please contact the OU Admissions Office or resubmit your application.'
+							}
+						],
+						help: [
+							{text: 'OU Admissions Office', type: 'link', href: 'http://www.ou.edu/admissions.html', target: '_blank'}
+						]
 					}
 				]
 			});
 		}
-
-		items.push({
-			xtype: 'enrollment-credit-set',
-			inputs: [
-				{
-					type: 'description',
-					text: [
-						'Your request to earn college credit has been denied. ',
-						'If you believe there has been an error,',
-						'please contact the OU Admissions Office or resubmit your application.'
-					].join(' ')
-				}
-			],
-			help: [
-				{text: 'OU Admissions Office', type: 'link', href: 'http://www.ou.edu/admissions.html', target: '_blank'}
-			]
-		});
-
-		fields.unshift({
-			name: 'rejected',
-			label: 'A Problem Occurred. Please Correct the Following:',
-			labelCls: 'error',
-			items: items
-		});
 
 		this.add(fields);
 		this.fillInNations();
