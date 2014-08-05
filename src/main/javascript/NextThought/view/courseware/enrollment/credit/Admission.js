768c02f5776918c2ec8cbe6e18e6d5b6d534d5b2
Andrew Ligon <andrew.ligon@nextthought.com>
2014-08-05 20:26:38 +0000

    Don't cast years of oklahoma residence or sooner id to numbers
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@45376 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/courseware/enrollment/credit/Admission.js
+++ src/main/javascript/NextThought/view/courseware/enrollment/credit/Admission.js
@@ -181,7 +181,7 @@ Ext.define('NextThought.view.courseware.enrollment.credit.Admission', {
 					xtype: 'enrollment-credit-set',
 					label: 'Are you a resident of Oklahoma?',
 					inputs: [
-						{type: 'radio-group', name: 'years_of_oklahoma_residency', valType: 'number', required: true, omitIfBlank: true, allowEmptyInput: false, options: [
+						{type: 'radio-group', name: 'years_of_oklahoma_residency', /*valType: 'number',*/ required: true, omitIfBlank: true, allowEmptyInput: false, options: [
 							{text: 'Yes. I\'ve been a resident for {input} years.', value: 'input', inputWidth: 48},
 							{text: 'No.', value: 0}
 						]}
@@ -201,7 +201,7 @@ Ext.define('NextThought.view.courseware.enrollment.credit.Admission', {
 					xtype: 'enrollment-credit-set',
 					label: 'Have you ever attended the University of Oklahoma?',
 					inputs: [
-						{type: 'radio-group', name: 'sooner_id', required: true, omitIfBlank: true, allowEmptyInput: true, valType: 'number', options: [
+						{type: 'radio-group', name: 'sooner_id', required: true, omitIfBlank: true, allowEmptyInput: true, /*valType: 'number',*/ options: [
 							{text: 'Yes, and my Sooner ID was {input}', value: 'input', help: 'If you do not remember your Sooner ID number, please leave this field blank.'},
 							{text: 'No.', value: ''}
 						]}
