0a13d3e31593f8257cf9e4a8b4ff785b561a373e
Andrew Ligon <andrew.ligon@nextthought.com>
2014-09-30 20:35:56 +0000

    Open any links in the admission for that are not on the same host in a new tab
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@50197 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/courseware/enrollment/credit/parts/Description.js
+++ src/main/javascript/NextThought/view/courseware/enrollment/credit/parts/Description.js
@@ -16,5 +16,18 @@ Ext.define('NextThought.view.courseware.enrollment.credit.parts.Description', {
 		this.renderData = Ext.apply(this.renderData || {}, {
 			text: this.text
 		});
+	},
+
+
+	afterRender: function() {
+		this.callParent(arguments);
+
+		var anchors = this.el.query('a') || [];
+
+		anchors.forEach(function(anchor) {
+			if (anchor.host && anchor.host !== window.location.host) {
+				anchor.setAttribute('target', '_blank');
+			}
+		});
 	}
 });
