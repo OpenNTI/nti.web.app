9fb2c01d5782e6bd5ed82f3e58077f9794612331
Andrew Ligon <andrew.ligon@nextthought.com>
2014-12-02 17:54:57 +0000

    Add the timeline card to the over view and a window to view them in
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@54894 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/main/javascript/NextThought/view/courseware/overview/parts/Timeline.js
@@ -0,0 +1,37 @@
+Ext.define('NextThought.view.courseware.overview.parts.Timeline', {
+	extend: 'NextThought.view.cards.Card',
+	alias: 'widget.course-overview-ntitimeline',
+
+	requires: 'NextThought.ux.TimelineWindow',
+
+
+	initComponent: function() {
+		this.callParent(arguments);
+
+		var root = this.locationInfo.root,
+			width = this['suggested-width'],
+			height = this['suggested-height'];
+
+		height = height ? parseInt(height, 10) : -1;
+		width = width ? parseInt(width, 10) : -1;
+
+		this.data = {
+			thumbnail: root + this.icon,
+			description: this.desc,
+			title: this.label,
+			json: root + this.href,
+			desiredHeight: height,
+			desiredWidth: width
+		};
+	},
+
+	//always open this up in app
+	shouldOpenInApp: function() { return true; },
+
+	onCardClicked: function() {
+		var win = Ext.widget('timeline-window', this.data);
+
+
+		win.show();
+	}
+});
