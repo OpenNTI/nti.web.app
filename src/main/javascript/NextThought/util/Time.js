321b78b09c7602613af842aa19fba09b2bdb5542
Andrew Ligon <andrew.ligon@nextthought.com>
2014-12-15 22:03:54 +0000

    Add some function to the TimeUtils and test them
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@55710 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/util/Time.js
+++ src/main/javascript/NextThought/util/Time.js
@@ -1,6 +1,15 @@
 Ext.define('NextThought.util.Time', {
 	singleton: true,
 
+	DIVISORS: {
+		WEEKS: 7 * 24 * 60 * 60 * 1000, //millis / 1000 = seconds / 60 = minutes / 60 = hours / 24 = days / 7 = weeks
+		DAYS: 24 * 60 * 60 * 1000, //millis / 1000 = seconds / 60 = minutes / 60 = hours / 24 = days
+		HOURS: 60 * 60 * 1000, //millis / 1000 = seconds / 60 = minutes / 60 = hours
+		MINUTES: 60 * 1000, // millis / 1000 = seconds / 60 = minutes
+		SECONDS: 1000 //millis / 1000 = seconds
+	},
+
+
 	getTimeGroupHeader: function(time) {
 		var now = new Date(), t = time.getTime(),
 			oneDayAgo = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1),
@@ -96,6 +105,97 @@ Ext.define('NextThought.util.Time', {
 
 	getTimer: function(startTime, direction) {
 		return new this._timer(startTime, direction);
+	},
+
+
+	/**
+	 * Takes two dates and returns true if they are on the same day
+	 * @param  {Date}  a date to compare
+	 * @param  {Date}  b the other date to compare
+	 * @return {Boolean}   if they are on the same day
+	 */
+	isSameDay: function(a, b) {
+		//clone the dates so we don't affect what we were passed
+		a = new Date(a);
+		b = new Date(b);
+
+		//set the hours to 0 so they will both be at 12:00:00 at the start of the day
+		a.setHours(0, 0, 0, 0);
+		b.setHours(0, 0, 0, 0);
+
+		return a.getTime() === b.getTime();
+	},
+
+	/**
+	 * Takes a number of milliseconds and returns a string that is more similar
+	 * to how a human would say it (hopefully)
+	 *
+	 * ex. 2 weeks, 2 days, and 2 hours
+	 *
+	 * @param  {Number} millis         millis to convert
+	 * @param  {Number} numberOfUnits  How many units (weeks, days, hours, etc.) to include
+	 * @param  {Boolean} doNotPluralize whether or not to pluralize the units
+	 * @return {String}                the parsed string
+	 */
+	getNaturalDuration: function(millis, numberOfUnits, doNotPluralize) {
+		var units = [], lastItem, s,
+			weeks = parseInt(millis / this.DIVISORS.WEEKS, 10),
+			days = parseInt(millis / this.DIVISORS.DAYS, 10) % 7,
+			hours = parseInt(millis / this.DIVISORS.HOURS, 10) % 24,
+			minutes = parseInt(millis / this.DIVISORS.MINUTES, 10) % 60,
+			seconds = parseInt(millis / this.DIVISORS.SECONDS, 10) % 60;
+
+		function add(unit, label) {
+			units.push(doNotPluralize ? unit + ' ' + label : Ext.util.Format.plural(unit, label));
+		}
+
+		//if we have a unit and we haven't pushed the max number add it
+		if (weeks && units.length < numberOfUnits) {
+			add(weeks, 'week');
+		}
+
+		if (days && units.length < numberOfUnits) {
+			add(days, 'day');
+		} else if (weeks) {
+			//if there no days but there are weeks add an empty string to the units to keep the units we show
+			//from being too far about e.x. 2 weeks and 5 seconds
+			units.push('');
+		}
+
+		if (hours && units.length < numberOfUnits) {
+			add(hours, 'hour');
+		} else if (weeks || days) {
+			units.push('');
+		}
+
+		if (minutes && units.length < numberOfUnits) {
+			add(minutes, 'minute');
+		} else if (weeks || days || hours) {
+			units.push('');
+		}
+
+		if (seconds && units.length < numberOfUnits) {
+			add(seconds, 'second');
+		}
+
+		//filter out any empty strings we may have added
+		units = units.filter(function(val) {
+			return val;
+		});
+
+		if (units.length === 1) {
+			s = units[0];
+		} else if (units.length === 2) {
+			s = units.join(' and ');
+		} else {
+			lastItem = units.pop();
+
+			s = units.join(', ');
+
+			s += ', and ' + lastItem;
+		}
+
+		return s;
 	}
 
 },
@@ -125,28 +225,28 @@ function() {
 		}
 
 		function getRemainingSeconds() {
-			return parseInt(time / 1000) % 60;//milli / 1000 = seconds
+			return parseInt(time / 1000, 10) % 60;//milli / 1000 = seconds
 		}
 
 		function getRemainingMilliSeconds() {
 			return time % 1000;
 		}
 
- 		/**
- 		 * Add a callback to be called everytime the interval passes
- 		 *
- 		 *	Will be called with an object containing...
- 		 *	{
- 		 *		hours: int, //number of hours left
- 		 *		minutes: int, //number of minutes left after hours
- 		 *		seconds: int, //number of seconds left after minutes
- 		 *		milliseconds: int, //number of milliseconds left after seconds
- 		 *		remaining: int, //total number of milliseconds left
- 		 *	}
- 		 *
- 		 * @param  {Function} fn callback to be called
- 		 * @return {Object}      return this so calls can be chained
- 		 */
+		/**
+		 * Add a callback to be called every time the interval passes
+		 *
+		 *	Will be called with an object containing...
+		 *	{
+		 *		hours: int, //number of hours left
+		 *		minutes: int, //number of minutes left after hours
+		 *		seconds: int, //number of seconds left after minutes
+		 *		milliseconds: int, //number of milliseconds left after seconds
+		 *		remaining: int, //total number of milliseconds left
+		 *	}
+		 *
+		 * @param  {Function} fn callback to be called
+		 * @return {Object}      return this so calls can be chained
+		*/
 		this.tick = function(fn) {
 			tickFn = fn;
 
--- src/test/javascript/specs/util/Time.spec.js
+++ src/test/javascript/specs/util/Time.spec.js
@@ -1,56 +1,203 @@
-describe("Time Util Tests", function() {
-    describe ('Relative Time Tests', function(){
+describe('Time Util Tests', function() {
+	describe('Relative Time Tests', function() {
 
-        it('timeDifference 1s', function(){
-            var now = Ext.Date.now(),
-                then = now - 1000;
-            expect(TimeUtils.timeDifference(now, then)).toEqual('1 second ago');
-        });
+		it('timeDifference 1s', function() {
+			var now = Ext.Date.now(),
+				then = now - 1000;
+			expect(TimeUtils.timeDifference(now, then)).toEqual('1 second ago');
+		});
 
-        it('timeDifference 1m', function(){
-            var now = Ext.Date.now(),
-                then = now - 1000*60;
+		it('timeDifference 1m', function() {
+			var now = Ext.Date.now(),
+				then = now - 1000 * 60;
 
-            expect(TimeUtils.timeDifference(now, then)).toEqual('1 minute ago');
-        });
+			expect(TimeUtils.timeDifference(now, then)).toEqual('1 minute ago');
+		});
 
-        it('timeDifference 1h', function(){
-            var now = Ext.Date.now(),
-                then = now - 1000*60*60;
+		it('timeDifference 1h', function() {
+			var now = Ext.Date.now(),
+				then = now - 1000 * 60 * 60;
 
-            expect(TimeUtils.timeDifference(now, then)).toEqual('1 hour ago');
-        });
-        it('timeDifference 1d', function(){
-            var now = Ext.Date.now(),
-                then = now - 1000*60*60*24;
+			expect(TimeUtils.timeDifference(now, then)).toEqual('1 hour ago');
+		});
+		it('timeDifference 1d', function() {
+			var now = Ext.Date.now(),
+				then = now - 1000 * 60 * 60 * 24;
 
-            expect(TimeUtils.timeDifference(now, then)).toEqual('1 day ago');
-        });
-        it('timeDifference 2s', function(){
-            var now = Ext.Date.now(),
-                then = now - 2000;
+			expect(TimeUtils.timeDifference(now, then)).toEqual('1 day ago');
+		});
+		it('timeDifference 2s', function() {
+			var now = Ext.Date.now(),
+				then = now - 2000;
 
-            expect(TimeUtils.timeDifference(now, then)).toEqual('2 seconds ago');
-        });
+			expect(TimeUtils.timeDifference(now, then)).toEqual('2 seconds ago');
+		});
 
-        it('timeDifference 2m', function(){
-            var now = Ext.Date.now(),
-                then = now - 2000*60;
+		it('timeDifference 2m', function() {
+			var now = Ext.Date.now(),
+				then = now - 2000 * 60;
 
-            expect(TimeUtils.timeDifference(now, then)).toEqual('2 minutes ago');
-        });
+			expect(TimeUtils.timeDifference(now, then)).toEqual('2 minutes ago');
+		});
 
-        it('timeDifference 2h', function(){
-            var now = Ext.Date.now(),
-                then = now - 2000*60*60;
+		it('timeDifference 2h', function() {
+			var now = Ext.Date.now(),
+				then = now - 2000 * 60 * 60;
 
-            expect(TimeUtils.timeDifference(now, then)).toEqual('2 hours ago');
-        });
-        it('timeDifference 2d', function(){
-            var now = Ext.Date.now(),
-                then = now - 2000*60*60*24;
+			expect(TimeUtils.timeDifference(now, then)).toEqual('2 hours ago');
+		});
+		it('timeDifference 2d', function() {
+			var now = Ext.Date.now(),
+				then = now - 2000 * 60 * 60 * 24;
 
-            expect(TimeUtils.timeDifference(now, then)).toEqual('2 days ago');
-        });
+			expect(TimeUtils.timeDifference(now, then)).toEqual('2 days ago');
+		});
+	});
+
+	describe('sameDay test', function() {
+		it('isSameDay', function() {
+			var i, today,
+				now = new Date();
+
+			function randomTime() {
+				var date = new Date(now),
+					hour = getRandomInt(0, 23),
+					min = getRandomInt(0, 59),
+					sec = getRandomInt(0, 59);
+
+				date.setHours(hour, min, sec);
+
+				return date;
+			}
+
+			for (i = 0; i < 10; i++) {
+				today = randomTime();
+
+				console.log(now, today);
+
+				expect(TimeUtils.isSameDay(now, today)).toBeTruthy();
+			}
+		});
+	});
+
+	describe('getNaturalDuration tests', function() {
+		function getTime(weeks, days, hours, minutes, seconds) {
+			var s = 1000,
+				m = 60 * s,
+				h = 60 * m,
+				d = 24 * h,
+				w = 7 * d;
+
+			return (weeks * w) + (days * d) + (hours * h) + (minutes * m) + (seconds * s);
+		}
+
+		describe('Week tests', function() {
+			it('5 units, with plurals', function() {
+				var time = getTime(2, 2, 2, 2, 2),
+					t = TimeUtils.getNaturalDuration(time, 5);
+
+				expect(t).toEqual('2 weeks, 2 days, 2 hours, 2 minutes, and 2 seconds');
+			});
+
+			it('5 units, no days, no minutes, no plurals', function() {
+				var time = getTime(2, 0, 2, 0, 2),
+					t = TimeUtils.getNaturalDuration(time, 5, true);
+
+				expect(t).toEqual('2 week, 2 hour, and 2 second');
+			});
+
+			it('4 units, without plurals, no days', function() {
+				var time = getTime(2, 0, 2, 2, 2),
+					t = TimeUtils.getNaturalDuration(time, 4, true);
+
+				expect(t).toEqual('2 week, 2 hour, and 2 minute');
+			});
+
+			it('3 units, no days, no hours, with plurals', function() {
+				var time = getTime(2, 0, 0, 2, 2),
+					t = TimeUtils.getNaturalDuration(time, 3);
+
+				expect(t).toEqual('2 weeks');
+			});
+
+
+			it('2 units, with plurals', function() {
+				var time = getTime(2, 2, 2, 2, 2),
+					t = TimeUtils.getNaturalDuration(time, 2);
+
+				expect(t).toEqual('2 weeks and 2 days');
+			});
+
+			it('1 unit, with plurals', function() {
+				var time = getTime(2, 0, 0, 0, 0),
+					t = TimeUtils.getNaturalDuration(time, 1);
+
+				expect(t).toEqual('2 weeks');
+			});
+		});
+
+		describe('Day tests', function() {
+			it('5 units, with plurals', function() {
+				var time = getTime(0, 2, 2, 2, 2),
+					t = TimeUtils.getNaturalDuration(time, 5);
+
+				expect(t).toEqual('2 days, 2 hours, 2 minutes, and 2 seconds');
+			});
+
+			it('2 units, without plurals', function() {
+				var time = getTime(0, 2, 2, 2, 2),
+					t = TimeUtils.getNaturalDuration(time, 2, true);
+
+				expect(t).toEqual('2 day and 2 hour');
+			});
+		});
+
+		describe('Hour tests', function() {
+			it('5 units, with plurals', function() {
+				var time = getTime(0, 0, 2, 2, 2),
+					t = TimeUtils.getNaturalDuration(time, 5);
+
+				expect(t).toEqual('2 hours, 2 minutes, and 2 seconds');
+			});
+
+			it('2 units, with plurals', function() {
+				var time = getTime(0, 0, 2, 2, 2),
+					t = TimeUtils.getNaturalDuration(time, 2);
+
+				expect(t).toEqual('2 hours and 2 minutes');
+			});
+		});
+
+		describe('Minute tests', function() {
+			it('5 units, with plurals', function() {
+				var time = getTime(0, 0, 0, 2, 2),
+					t = TimeUtils.getNaturalDuration(time, 5);
+
+				expect(t).toEqual('2 minutes and 2 seconds');
+			});
+
+			it('2 units, with out plurals', function() {
+				var time = getTime(0, 0, 0, 2, 2),
+					t = TimeUtils.getNaturalDuration(time, 2, true);
+
+				expect(t).toEqual('2 minute and 2 second');
+			});
+		});
+
+		describe('Second tests', function() {
+			it('5 units, with plurals', function() {
+				var time = getTime(0, 0, 0, 0, 2),
+					t = TimeUtils.getNaturalDuration(time, 5);
+
+				expect(t).toEqual('2 seconds');
+			});
+
+			it('1 unit, with out plurals', function() {
+				var time = getTime(0, 0, 0, 0, 2),
+					t = TimeUtils.getNaturalDuration(time, 1, true);
+
+				expect(t).toEqual('2 second');
+			});
+		});
 	});
 });
