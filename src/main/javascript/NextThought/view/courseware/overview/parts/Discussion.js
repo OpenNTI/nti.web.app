15115c5b8c6d37f936128e4c7e67f06eca2b7062
Chris Utz <chris.utz@nextthought.com>
2015-01-09 00:38:19 +0000

    comment about why we are doing this.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@56888 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/courseware/overview/parts/Discussion.js
+++ src/main/javascript/NextThought/view/courseware/overview/parts/Discussion.js
@@ -68,7 +68,22 @@ Ext.define('NextThought.view.courseware.overview.parts.Discussion', {
 		var parsedId = ParseUtils.parseNTIID(ntiid),
 			bundle = this.getBundle(),
 			e;
-/*
+		/*
+		* Here is a hack.  The crux of which is to supply context about the subinstance we are in
+		* when we resolving a topic. We do this primarily for instructors who may instruct multiple
+		* subinstances that contain this discussion although strickly speaking it coudl happen for any
+		* account type if the account is enrolled in multiple subinstances of a course that contain
+		* the same named topic.  The issue is without the contexxt of the course we are in when the topic
+		* is selected on the overview the server as multiple topics to choose from (one for each section)
+		* and it is ambiguous as to which one to select.  Now the problem with this particular hack
+		& is that when we are in a section but trying to get to the root (because the topics are set up 
+		* in the root rather than the section) the provider id no longer matches the root and we 404.  In most
+		* cases the section is what contains the topic making this a non issue, but we now have courses where
+		* the topic only exists in the parent.  We need another way to pass the context of the such that we
+		* get the proper context in the event it is ambiguous.  While we have this in the context of a course (from
+		* the overview or content) we aren't going to have this in the stream right?  I think this manifests
+		* as course roulette but that is already a problem right?
+		*/
 		if (parsedId && (/^Topic:EnrolledCourse(Section|Root)$/).test(parsedId.specific.type)) {
 			if (bundle && bundle.getCourseCatalogEntry) {
 				e = bundle.getCourseCatalogEntry(); //this may not be filled in yet.
@@ -78,7 +93,7 @@ Ext.define('NextThought.view.courseware.overview.parts.Discussion', {
 				console.warn('Did not get the thing we were expecting...', bundle);
 			}
 		}
-*/
+
 		Service.getObject(ntiid, this.onTopicResolved, this.onTopicResolveFailure, this, true);
 	},
 
