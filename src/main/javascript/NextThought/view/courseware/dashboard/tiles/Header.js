4773afe66af5a87106d4ddcdd8ef70cdd2ffd948
Andrew Ligon <andrew.ligon@nextthought.com>
2015-01-12 02:41:45 +0000

    Style the week headers
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@57309 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/courseware/dashboard/tiles/Header.js
+++ src/main/javascript/NextThought/view/courseware/dashboard/tiles/Header.js
@@ -4,10 +4,12 @@ Ext.define('NextThought.view.courseware.dashboard.tiles.Header', {
 
 	cls: 'dashboard-header',
 
+	DATE_FORMAT: 'MMMM D',
+
 	renderTpl: Ext.DomHelper.markup([
 		{cls: 'label', cn: [
 			{tag: 'span', cls: 'start'},
-			'-',
+			' - ',
 			{tag: 'span', cls: 'end'}
 		]}
 	]),
@@ -35,7 +37,11 @@ Ext.define('NextThought.view.courseware.dashboard.tiles.Header', {
 			return;
 		}
 
-		this.startEl.update(week.start);
-		this.endEl.update(week.end);
+		this.startEl.update(this.formatDate(week.start));
+		this.endEl.update(this.formatDate(week.end));
+	},
+
+	formatDate: function(date) {
+		return moment(date).format(this.DATE_FORMAT);
 	}
 });
