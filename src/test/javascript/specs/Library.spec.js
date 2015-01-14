de6f248d0951024403f002b87b1a41a3d5607186
Moriah Donaldson <moriah.donaldson@nextthought.com>
2015-01-14 01:37:36 +0000

    Fix Library.spec test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@57591 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/test/javascript/specs/Library.spec.js
+++ src/test/javascript/specs/Library.spec.js
@@ -1,26 +1,28 @@
 describe('Library Store', function() {
 
 	beforeEach(function() {
-		Library.clearListeners(); //don't invoke the UI
-		Library.getStore().removeAll();
-		Library.load();
 
-		app.getController('CourseWare').onSessionReady();
 
-		waitsFor(
-			function() { return Library.loaded; },
-			'Library load never completed',
-			30000
-		);
+
 	});
 
-
-
-
-
-
 	it('should have titles (for tests)', function() {
-		expect(Library.getStore().count()).toBeGreaterThan(0);
+		runs(function() {
+			Library.clearListeners(); //don't invoke the UI
+			Library.getStore().removeAll();
+			Library.load();
+
+			app.getController('CourseWare').onSessionReady();
+		});
+
+		waitsFor(function() {
+
+			return Library.loaded;
+		}, 'Library load never completed', 30000);
+
+		runs(function() {
+			expect(Library.getStore().count()).toBeGreaterThan(0);
+		});
 	});
 
 });
