96d0ace6b355bee485d14fe2616f10308b76ce41
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2015-02-25 20:53:18 +0000

    Fix the minify-er. Remove a reserved words.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtWebApp/trunk@60215 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/javascript/NextThought/view/profiles/create/View.js
+++ src/main/javascript/NextThought/view/profiles/create/View.js
@@ -1,97 +1,83 @@
 Ext.define('NextThought.view.profiles.create.View', {
-    extend: 'Ext.Component',
-    alias: 'widget.profile-create-view',
+	extend: 'Ext.Component',
+	alias: 'widget.profile-create-view',
 
-    requires: [
-        'NextThought.view.profiles.About'
-    ],
-
-
-    mixins: {
-        EditingUser: 'NextThought.view.profiles.mixins.EditUserMixin'
-    },
-
-    layout: 'auto',
-
-    renderTpl: Ext.DomHelper.markup([
-        {
-            cls: 'fold', cn: [
-
-            { cls: 'field', cn: [
-                { cls: 'label', html: '{{{NextThought.view.profiles.About.name}}}' },
-                { cn: { tag: 'input', 'data-field': 'username', value: '{name}', class:'locked', 'placeholder': '{{{NextThought.view.profiles.About.name}}}' } },
-                { cls: 'error-msg', 'data-prop': 'username'}
-            ]},
-
-            { cls: 'field', cn: [
-                { cls: 'label', html: '{{{NextThought.view.profiles.About.location}}}' },
-                { cn: { tag: 'input', 'data-field': 'location', value: '{location}', 'placeholder': '{{{NextThought.view.profiles.About.location}}}' } },
-                { cls: 'error-msg', 'data-prop': 'location'}
-            ]},
-
-            { cls: 'field', cn: [
-                { cls: 'label', cn: { tag: 'span', 'data-field': 'affiliation', 'placeholder': '{{{NextThought.view.profiles.About.affiliation}}}' } },
-                { cn: { tag: 'input', 'data-field': 'affiliation', value: '{affiliation}', 'placeholder': '{{{NextThought.view.profiles.About.affiliation}}}' } },
-                { cls: 'error-msg', 'data-prop': 'affiliation'}
-            ]},
-
-            { cls: 'field', cn: [
-                { cls: 'label', cn: { tag: 'span', 'data-field': 'role', 'data-placeholder': '{{{NextThought.view.profiles.About.role}}}' } },
-                { cn: { tag: 'input', 'data-field': 'role', value: '{role}', 'placeholder': '{{{NextThought.view.profiles.About.role}}}' } },
-                { cls: 'error-msg', 'data-prop': 'role'}
-            ]},
-
-            { cls: 'field', cn: [
-                { cls: 'label', cn: { tag: 'span', 'data-field': 'about', 'data-placeholder': '{{{NextThought.view.profiles.About.write}}}' } },
-                { cn: { tag: 'textarea', 'data-field': 'about', value: '{about}', html:'{about}', 'placeholder': '{{{NextThought.view.profiles.About.write}}}' } },
-                { cls: 'error-msg', 'data-prop': 'about'}
-            ]}
-        ]},
-        { cls: 'error-msg' }
-    ]),
+	requires: [
+		'NextThought.view.profiles.About'
+	],
 
 
-    renderSelectors: {
-        pictureEl: '.picture-container',
-        nameEl: '.field input [data-field=name]',
-        locationEl: '.field input [data-field=location]',
-        affiliationEl: '.field input [data-field=affiliation]',
-        roleEl: '.field input [data-field=role]',
-        aboutEl: '.field [data-field=about]'
-    },
+	mixins: {
+		EditingUser: 'NextThought.view.profiles.mixins.EditUserMixin'
+	},
+
+	layout: 'auto',
+
+	renderTpl: Ext.DomHelper.markup([
+		{
+			cls: 'fold',
+			cn: [
+				{ cls: 'field', cn: [
+					{ tag: 'input', type: 'text', 'data-field': 'username', value: '{name}', cls: 'locked', 'placeholder': '{{{NextThought.view.profiles.About.name}}}' },
+					{ cls: 'error-msg', 'data-prop': 'username'}
+				]},
+
+				{ cls: 'field', cn: [
+					{ tag: 'input', type: 'text', 'data-field': 'location', value: '{location}', 'placeholder': '{{{NextThought.view.profiles.About.location}}}' },
+					{ cls: 'error-msg', 'data-prop': 'location'}
+				]},
+
+				{ cls: 'field', cn: [
+					{ tag: 'input', type: 'text', 'data-field': 'affiliation', value: '{affiliation}', 'placeholder': '{{{NextThought.view.profiles.About.affiliation}}}' },
+					{ cls: 'error-msg', 'data-prop': 'affiliation'}
+				]},
+
+				{ cls: 'field', cn: [
+					{ tag: 'input', type: 'text', 'data-field': 'role', value: '{role}', 'placeholder': '{{{NextThought.view.profiles.About.role}}}' },
+					{ cls: 'error-msg', 'data-prop': 'role'}
+				]},
+
+				{ cls: 'field', cn: [
+					{ tag: 'textarea', type: 'text', 'data-field': 'about', html: '{about}', 'placeholder': '{{{NextThought.view.profiles.About.write}}}' },
+					{ cls: 'error-msg', 'data-prop': 'about'}
+				]}
+			]
+		},
+		{ cls: 'error-msg' }
+	]),
+
+	initComponent: function() {
+		this.callParent(arguments);
+
+		this.on({
+			'save-edits': 'onSaveEdits',
+			'cancel-edits': 'onCancelEdits'
+		});
+	},
 
 
-    initComponent: function(){
-        this.callParent(arguments);
-
-        this.on({
-            'save-edits': 'onSaveEdits',
-            'cancel-edits': 'onCancelEdits'
-        });
-    },
-
-
-    beforeRender: function(){
-        this.callParent(arguments);
-
-        this.renderData = Ext.applyIf(this.renderData || {}, {
-            'name': this.user && this.user.getName(),
-            'location': this.user && this.user.get('location'),
-            'affiliation': this.user && this.user.get('affiliation'),
-            'role': this.user && this.user.get('role'),
-            'about': this.user && this.user.get('about')
-        });
-    },
+	beforeRender: function() {
+		this.callParent(arguments);
+		var user = this.user;
+		if (!user) {return;}
+		this.renderData = Ext.applyIf(this.renderData || {}, {
+			name: user.getName(),
+			location: user.get('location'),
+			affiliation: user.get('affiliation'),
+			role: user.get('role'),
+			about: user.get('about')
+		});
+	},
 
 
-    getEditableFields: function(){
-        return this.el && this.el.query('input[data-field]:not(.locked),textarea[data-field]');
-    },
+	getEditableFields: function() {
+		return this.el && this.el.query('input[data-field]:not(.locked),textarea[data-field]');
+	},
 
 
-    getValueForField: function(field){
-        var text = field && field.value;
+	getValueForField: function(field) {
+		var text = field && field.value;
 
-        return Ext.isEmpty(text) ? null : text;
-    }
+		return Ext.isEmpty(text) ? null : text;
+	}
 });
