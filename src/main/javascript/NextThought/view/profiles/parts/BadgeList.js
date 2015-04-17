b47d53b1b2d64d2f90bcd6213c05781327b798b1
Chris Utz <chris.utz@nextthought.com>
2015-04-17 14:14:04 -0500

    more wording tweaks

--- src/main/javascript/NextThought/view/profiles/parts/BadgeList.js
+++ src/main/javascript/NextThought/view/profiles/parts/BadgeList.js
@@ -175,7 +175,7 @@ Ext.define('NextThought.view.profiles.parts.BadgeList', {
 			}));
 
 			this.exportMenu.add(new Ext.Action({
-				text: 'Push to Mozilla BackPack',
+				text: 'Send to Mozilla BackPack',
 				handler: me.exportItemClicked.bind(me, record, itemEl),
 				itemId: 'export-backpack',
 				ui: 'nt-menuitem', plain: true
