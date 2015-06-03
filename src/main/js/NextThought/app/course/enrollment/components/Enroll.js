c7f92f07bf6c4bd59dd35dfd9fca5183809caabd
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2015-06-03 10:41:24 -0500

    add store enrollment components and parts.

---
+++ src/main/js/NextThought/app/course/enrollment/components/Enroll.js
@@ -0,0 +1,207 @@
+Ext.define('NextThought.app.course.enrollment.components.Enroll', {
+	extend: 'Ext.Component',
+	alias: 'widget.enrollment-enroll',
+
+	cls: 'enroll-for-credit-confirmation',
+
+	requires: [
+		'NextThought.app.course.enrollment.components.parts.DetailsTable'
+	],
+
+	buttonCfg: [
+		{name: getString('NextThought.view.courseware.enrollment.Enroll.ConttoPay'), action: 'goto-payment'}
+	],
+
+	renderTpl: Ext.DomHelper.markup([
+		{cls: 'message', cn: [
+			{cls: 'main {headerCls}', html: '{header}'},
+			{cls: 'text description', html: '{text}'},
+			{cls: 'text available', html: '{available}'},
+			{cls: 'confirm', html: '{confirm}'}
+		]},
+		{cls: 'details'},
+		{cls: 'subscribe-container', cn: [
+			{cls: 'subscribe', cn: [
+				{tag: 'input', id: '{id}-subscribe-check', type: 'checkbox', name: 'subscribe'},
+				{tag: 'label', cls: '{cls}', 'for': '{id}-subscribe-check', html: ''}
+			]},
+			{cls: 'legal'}
+		]}
+	]),
+
+
+	renderSelectors: {
+		enrollEl: '.enroll-now',
+		titleEl: '.main',
+		descriptionEl: '.description',
+		confirmEl: '.confirm',
+		availableEl: '.available',
+		detailsEl: '.details',
+		subscribeContainerEl: '.subscribe-container',
+		subscribeEl: '.subscribe input[name=subscribe]',
+		subscribeLabelEl: '.subscribe label',
+		subscribeLegalEl: '.subscribe-container .legal'
+	},
+
+
+	initComponent: function() {
+		this.callParent(arguments);
+
+		this.enableBubble('show-msg');
+	},
+
+
+	beforeRender: function() {
+		this.callParent(arguments);
+
+		var c = this.course;
+
+		this.renderData = Ext.apply(this.renderData || {}, {
+			headerCls: this.paymentfail ? 'error' : '',
+			header: this.paymentfail ?
+				getString('NextThought.view.courseware.enrollment.Enroll.PaymentProblems') :
+				getString('NextThought.view.courseware.enrollment.Enroll.AppAccepted'),
+			text: this.paymentfail ?
+				getString('NextThought.view.courseware.enrollment.Enroll.TryPayAgain') :
+				getString('NextThought.view.courseware.enrollment.Enroll.ThanksCreditOU'),
+			available: getFormattedString('NextThought.view.courseware.enrollment.Enroll.CreditAvailable', {title: c.getSemester()}),
+			confirm: getString('NextThought.view.courseware.enrollment.Enroll.ConfirmCourse')
+		});
+	},
+
+
+	afterRender: function() {
+		this.callParent(arguments);
+
+		this.detailsTable = Ext.widget('enrollment-details-table', {
+			course: this.course,
+			enrollmentOption: this.enrollmentOption,
+			renderTo: this.detailsEl
+		});
+
+		this.on('destroy', 'destroy', this.detailsTable);
+
+		this.beforeShow();
+	},
+
+
+	beforeShow: function() {
+		if (!this.rendered) { return; }
+
+		if (this.enrollmentOption.AllowVendorUpdates) {
+			this.subscribeLabelEl.update(getString('SubscribeToVendor') || 'Subscribe to updates.');
+			this.subscribeLegalEl.update(getString('SubscribeToVendorLegal'));
+			this.subscribeContainerEl.show();
+		} else {
+			this.subscribeContainerEl.hide();
+		}
+	},
+
+
+	getButtonCfg: function() {
+		return this.buttonCfg;
+	},
+
+
+	buttonClick: function(action) {
+		if (action === 'goto-payment') {
+			this.maybeSubmit();
+		}
+	},
+
+
+	stopClose: function() {
+		var me = this;
+
+		return new Promise(function(fulfill, reject) {
+			Ext.Msg.show({
+				msg: getString('NextThought.view.courseware.enrollment.Enroll.NotFinished'),
+				buttons: Ext.MessageBox.OK | Ext.MessageBox.CANCEL,
+				scope: this,
+				icon: 'warning-red',
+				buttonText: {
+					'ok': 'caution: ' +getString('NextThought.view.courseware.enrollment.Enroll.Leave'),
+					'cancel': getString('NextThought.view.courseware.enrollment.Enroll.Stay')
+				},
+				title: getString('NextThought.view.courseware.enrollment.Enroll.AreYouSure'),
+				fn: function(str) {
+					if (str === 'ok') {
+						fulfill();
+					} else {
+						reject();
+					}
+				}
+			});
+		});
+	},
+
+
+	showErrorMsg: function(json) {
+		if (json && json.Message) {
+			this.fireEvent('show-msg', json.Message, true, 5000);
+		} else {
+			this.fireEvent('show-msg', getString('NextThought.view.courseware.enrollment.Enroll.AlreadyEnrolled'), true, 5000);
+		}
+	},
+
+
+	showError: function(json) {
+		json = json || {};
+		json.title = getString('NextThought.view.courseware.enrollment.Enroll.PaymentProblems');
+		json.Message = json.Message || getString('NextThought.view.courseware.enrollment.Enroll.TryPayAgain');
+		json.ContactInformation = json.ContactInformation || getString('NextThought.view.courseware.enrollment.Enroll.HelpDesk');
+
+		this.titleEl.update(json.title);
+		this.titleEl.addCls('error');
+		this.availableEl.update('');
+		this.descriptionEl.update(json.Message);
+		this.confirmEl.update(json.ContactInformation);
+	},
+
+
+	maybeSubmit: function() {
+		var me = this,
+			minTime = wait(5000),
+			subscribe;
+
+		//if its there, visible, and checked
+		subscribe = this.subscribeEl && this.subscribeEl.isVisible() && this.subscribeEl.dom.checked;
+
+		me.addMask(getString('NextThought.view.courseware.enrollment.Enroll.ExternalPay'), 'navigation');
+
+		this.complete(this, {
+			subscribe: subscribe
+		})
+			.then(function(response) {
+				var json = Ext.JSON.decode(response, true);
+
+				if (json.href) {
+					minTime.then(function() {
+						window.location.href = json.href;
+					});
+				} else {
+					console.error('No href to redirect to...', response);
+					me.showErrorMsg();
+
+					minTime.then(function() {
+						me.error(me);
+						me.removeMask();
+					});
+				}
+			})
+			.fail(function(response) {
+				var json = Ext.JSON.decode(response, true);
+
+				console.error('Enroll and pay failed', response);
+
+				me.showErrorMsg(json);
+
+				me.showError(json);
+
+				minTime.then(function() {
+					me.error(me);
+					me.removeMask();
+				});
+			});
+	}
+});
---
+++ src/main/js/NextThought/common/form/fields/DateField.js
@@ -0,0 +1,117 @@
+Ext.define('NextThought.common.form.fields.DateField', {
+	extend: 'Ext.Component',
+	alias: 'widget.datefield',
+
+	requires: ['NextThought.common.form.fields.SearchComboBox'],
+
+	cls: 'datefield',
+
+	renderTpl: Ext.DomHelper.markup([
+		{cls: 'month'},
+		{tag: 'input', cls: 'day date-field-input', placeholder: 'Day', size: 2},
+		{tag: 'input', cls: 'year date-field-input', placeholder: 'Year', size: 4}
+	]),
+
+	renderSelectors: {
+		monthEl: '.month',
+		dayEl: '.day',
+		yearEl: '.year'
+	},
+
+
+	afterRender: function() {
+		this.callParent(arguments);
+
+
+		this.monthInput = Ext.widget('searchcombobox', {
+			options: [
+				{value: '0', text: 'January'},
+				{value: '1', text: 'February'},
+				{value: '2', text: 'March'},
+				{value: '3', text: 'April'},
+				{value: '4', text: 'May'},
+				{value: '5', text: 'June'},
+				{value: '6', text: 'July'},
+				{value: '7', text: 'August'},
+				{value: '8', text: 'September'},
+				{value: '9', text: 'October'},
+				{value: '10', text: 'November'},
+				{value: '11', text: 'December'}
+			],
+			emptyText: 'Month',
+			editable: false,
+			renderTo: this.monthEl
+		});
+
+		this.on('destroy', 'destroy', this.monthInput);
+
+		this.mon(this.dayEl, 'keydown', 'enforceNumber', DomUtils);
+		this.mon(this.yearEl, 'keydown', 'enforceNumber', DomUtils);
+
+		this.maybeChanged = Ext.Function.createBuffered(this.maybeChanged, 1, this);
+		this.mon(this.monthInput, 'select', 'maybeChanged');
+		this.mon(this.dayEl, 'keyup', 'maybeChanged');
+		this.mon(this.yearEl, 'keyup', 'maybeChanged');
+
+		if (this.date) {
+			this.setValue(this.date);
+		}
+	},
+
+
+	isEmpty: function() {
+		return Ext.isEmpty(this.yearEl.getValue() +
+						   this.dayEl.getValue() +
+						   this.monthInput.getValue());
+	},
+
+
+	isFullyAnswered: function() {
+		var q = this.hasBeenAnswered || (!Ext.isEmpty(this.yearEl.getValue()) &&
+										 !Ext.isEmpty(this.dayEl.getValue()) &&
+										 !Ext.isEmpty(this.monthInput.getValue()));
+
+		this.hasBeenAnswered = q;
+		return q;
+	},
+
+
+	maybeChanged: function onChange() {
+		var last = onChange.lastValue,
+			current = this.getValue();
+		if (last !== current && this.isFullyAnswered()) {
+			this.fireEvent('changed', current, last);
+		}
+	},
+
+
+	setValue: function(date) {
+		if (!this.rendered) {
+			this.date = date;
+			return;
+		}
+
+		var year = date && date.getFullYear(),
+			month = date && date.getMonth(),
+			day = date && date.getDate();
+
+		this.yearEl.dom.value = year;
+		this.dayEl.dom.value = day;
+
+		this.monthInput.setValue(month);
+	},
+
+
+	getValue: function() {
+		var year = parseInt(this.yearEl.getValue(), 10),
+			day = parseInt(this.dayEl.getValue(), 10),
+			month = parseInt(this.monthInput.getValue(), 10),
+			value = new Date(year, month, day);
+
+		if (day !== value.getDate() || month !== value.getMonth() || year !== value.getFullYear()) {
+			value = null;
+		}
+
+		return value;
+	}
+});
