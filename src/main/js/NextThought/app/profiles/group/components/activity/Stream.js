42ee40208f3cdd3c669f1de00fd1f5585f22bdc6
Chris Utz <chris.utz@nextthought.com>
2015-07-07 14:58:40 -0500

    add missing files

---
+++ src/main/js/NextThought/app/profiles/group/components/activity/Stream.js
@@ -0,0 +1,15 @@
+Ext.define('NextThought.app.profiles.group.components.activity.Stream', {
+	extend: 'NextThought.app.profiles.user.components.activity.Stream',
+	alias: 'widget.profile-group-activity-stream',
+
+	requires: ['NextThought.app.profiles.group.components.activity.parts.events.Created'],
+		   
+	initialWidgetConfig: function(){
+	   return { xtype: 'created-event', entity: this.user }
+	},
+		   
+	hasInitialWidget: function(){
+	   return !!this.down('created-event');
+	},
+	
+});
