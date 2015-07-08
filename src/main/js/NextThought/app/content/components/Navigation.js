558191cec64b2eb1666dd1e19e315d14af9fb853
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2015-07-08 02:03:43 -0500

    move common logic in a shareable place.

--- src/main/js/NextThought/app/content/components/Navigation.js
+++ src/main/js/NextThought/app/content/components/Navigation.js
@@ -1,55 +1,14 @@
 Ext.define('NextThought.app.content.components.Navigation', {
-	extend: 'Ext.Component',
+	extend: 'NextThought.common.components.Navigation',
 	alias: 'widget.content-navigation',
 
 	cls: 'content-navigation',
 
-	tabsTpl: new Ext.XTemplate(Ext.DomHelper.markup(
-		{tag: 'ul', cls: 'tabs', cn: [
-			{tag: 'tpl', 'for': 'tabs', cn: [
-				{
-					tag: 'li',
-					'data-index': '{#}',
-					'data-route': '{route}',
-					'data-subroute': '{subRoute}',
-					'data-title': '{title}',
-					'data-text': '{text}',
-					cls: 'tab{[values.active ? " active": ""]}',
-					html: '{text}'
-				}
-			]}
-		]}
-	)),
-
-	renderTpl: Ext.DomHelper.markup([
-		{cls: 'content-container', cn: [
-			{cls: 'content', cn: [
-				{cls: 'active-content', html: ''},
-				{cls: 'tab-container'},
-				{cls: 'active-tab'}
-			]}
-		]}
-	]),
-
-
-	renderSelectors: {
-		titleEl: '.content .active-content',
-		tabContainerEl: '.content .tab-container',
-		activeTabEl: '.content .active-tab'
-	},
-
-
 	afterRender: function() {
 		this.callParent(arguments);
 
-		this.mon(this.tabContainerEl, 'click', this.onTabClick.bind(this));
-
 		if (this.bundle) {
 			this.bundleChanged(this.bundle);
-		}
-
-		if (this.tabs) {
-			this.setTabs(this.tabs, true);
 		}
 	},
 
@@ -70,94 +29,5 @@ Ext.define('NextThought.app.content.components.Navigation', {
 			data = bundle.asUIData();
 
 		this.titleEl.update(data.title);
-	},
-
-	/**
-	 * Take an array of tabs to render in the navigation
-	 * tab config: {
-	 * 		route: String, //the name of the route this tab activates
-	 * 		title: String, //the title of the route this tab activates
-	 * 		text: String, //the name of the tab,
-	 * 		active: Boolean //if this is the active tab
-	 * }
-	 * @param {Array} tabs a list of tab configs to show
-	 */
-	setTabs: function(tabs) {
-		if (!this.rendered) {
-			this.tabs = tabs;
-			return;
-		}
-
-		var me = this,
-			container = me.tabContainerEl,
-			tabs, active;
-
-		function alignCurrentTab() {
-			//if for some reason the element was removed before we could call this
-			if (!me.activeTabEl) { return; }
-			active = active.getBoundingClientRect();
-			container = container.getBoundingClientRect();
-
-			me.activeTabEl.setStyle({
-				width: active.width + 'px',
-				left: active.left - container.left + 'px'
-			});
-		}
-
-		container.dom.innerHTML = '';
-
-		tabs = me.tabsTpl.append(container, {tabs: tabs}, true);
-		tabs = tabs && tabs.dom;
-		active = tabs && tabs.querySelector('.tab.active');
-		container = container && container.dom;
-
-		//if we are animating in, wait until we are finished to the the left will be correct
-		if (me.hasCls('showing')) {
-			wait(1500)
-				.then(alignCurrentTab);
-		} else {
-			alignCurrentTab();
-		}
-	},
-
-
-	updateRoute: function(route, subRoute) {
-		if (!this.rendered) {
-			this.on('afterrender', this.updateRoute.bind(this, route, subRoute));
-			return;
-		}
-
-		route = Globals.trimRoute(route);
-		subRoute = Globals.trimRoute(subRoute);
-
-		var tab = this.el.dom.querySelector('[data-route="' + route + '"]');
-
-		if (tab) {
-			if (subRoute) {
-				tab.setAttribute('data-subroute', subRoute);
-			} else {
-				tab.removeAttribute('data-subroute');
-			}
-		}
-	},
-
-
-	onTabClick: function(e) {
-		var tab = e.getTarget('.tab'),
-			route = tab && tab.getAttribute('data-route'),
-			subRoute = tab && tab.getAttribute('data-subroute');
-
-		route = Globals.trimRoute(route);
-		subRoute = Globals.trimRoute(subRoute);
-
-		if (!tab || !this.bodyView.onTabChange) { return; }
-
-		//if we are active and we have a subroute, that means the tab is not at
-		//its root so set the root route
-		if (tab.classList.contains('active') && subRoute) {
-			this.bodyView.onTabChange(tab.getAttribute('data-title'), route, tab);
-		} else {
-			this.bodyView.onTabChange(tab.getAttribute('data-title'), route + '/' + subRoute, tab);
-		}
 	}
 });
