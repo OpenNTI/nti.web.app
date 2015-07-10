bf48b8f61bdc65434c899cab188440cc27869e57
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2015-07-09 22:56:12 -0500

    cleanup.

--- src/main/js/NextThought/app/contacts/components/TabView.js
+++ src/main/js/NextThought/app/contacts/components/TabView.js
@@ -19,72 +19,8 @@ Ext.define('NextThought.app.contacts.components.TabView', {
 	ui: 'contacts',
 	cls: 'contact-sub-view',
 
-	// config: {
-	// 	bodyCls: '',
-	// 	bodyDefaultType: 'contacts-tabs-card',
-	// 	storeId: 'all-contacts-store',
-	// 	subType: 'contact',
-	// 	filterFn: undefined,
-	// 	bodyItems: null
-	// },
-
-
 	constructor: function(config) {
-		var type = config.subType || this.config.subType,
-			store;
-		// 		filter = config.filterFn || this.config.filterFn,
-		// 		bodyItems = config.bodyItems || this.config.bodyItems || [],
-		// 		emptyText = {
-		// 			cls: 'empty-state', cn: [
-		// 				{cls: 'header', html: '{{{no_' + type + '_header}}}'},
-		// 				{cls: 'sub', html: '{{{no_' + type + '_sub}}}'}
-		// 			]
-		// 		};
-
-		// if ('contact' !== type) {
-		// 	config.bodyDefaultType = config.bodyDefaultType || 'contacts-tabs-grouping';
-		// 	config.storeId = config.storeId || 'FriendsList';
-		// }
-
-		//We can't use the config's helper functions because those get created in the call to the parent...after which,
-		// these applications would become alot more tricky.
-		// this.applyConfigs('body', {
-		// 	defaultType: config.bodyDefaultType || this.config.bodyDefaultType,
-		// 	storeId: config.storeId || this.config.storeId,
-		// 	defaultInsertPoint: bodyItems.length,
-		// 	items: bodyItems,
-		// 	filter: filter,
-		// 	ui: 'contacts-' + type,
-		// 	cls: (config.bodyCls || this.config.bodyCls) + ' ' + type,
-		// 	emptyCmp: {
-		// 		xtype: 'box', emptyState: true,
-		// 		renderTpl: Ext.DomHelper.markup(emptyText)
-		// 	}
-		// });
-
-
-		// store = StoreUtils.newView(config.storeId || this.config.storeId);
-
-		// if (store.model === NextThought.model.User) {
-		// 	store.on('datachanged', 'injectLetterDividers', this);
-		// }
-
-		// if (Ext.isFunction(filter)) {
-		// 	store.filter(filter);
-		// }
-
-		// this.applyConfigs('navigation', {
-		// 	cls: type,
-		// 	subType: type,
-		// 	store: store,
-		// 	outlineLabel: config.outlineLabel || getString('contacts_all_' + type.toLowerCase())
-		// });
-
-
 		this.callParent(arguments);
-
-		this.on('destroy', 'clearListeners', store);
-
 		this.mon(this.navigation, 'contact-row-selected', 'scrollIntoView');
 	},
 
