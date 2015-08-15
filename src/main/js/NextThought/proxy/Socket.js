9866cf4587c0164fc2c386441d8f51ee1cc22d2b
Chris Utz <chris.utz@nextthought.com>
2015-08-15 18:50:59 -0500

    lets tweak the reconnection delay a bit

--- src/main/js/NextThought/proxy/Socket.js
+++ src/main/js/NextThought/proxy/Socket.js
@@ -103,7 +103,7 @@ Ext.define('NextThought.proxy.Socket', {
 		}
 
 		var me = this,
-			socket = io.connect(getURL()),
+			socket = io.connect(getURL(), {'reconnection delay': 3000}),
 			k;
 
 		if (this.isDebug && !socket.emit.chained) {
