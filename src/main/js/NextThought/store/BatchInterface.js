c045cf8e20fa2eb1fb0f861f9a8b56160cebdd9d
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2015-11-04 14:06:26 -0600

    Guard against null

--- src/main/js/NextThought/store/BatchInterface.js
+++ src/main/js/NextThought/store/BatchInterface.js
@@ -24,7 +24,7 @@ Ext.define('NextThought.store.BatchInterface', {
 		}
 
 		this.url = config.url;
-		this.params = config.params;
+		this.params = config.params || {};
 
 		this.batchSize = this.params.batchSize;
 
