959221bde77301dc9c292361b7d48917ba43497a
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2015-11-09 17:02:53 -0600

    Add individual email test.

--- src/test/javascript/specs/app/course/assessment/components/admin/Email.spec.js
+++ src/test/javascript/specs/app/course/assessment/components/admin/Email.spec.js
@@ -69,4 +69,49 @@ describe("Instructor Email Tests", function () {
 		expect(postRecord && postRecord.get('Body')).toBe(body);
 		expect(postRecord && postRecord.get('Subject')).toBe(title);
 	});
+
+
+	it('tests instructor to individual student email', function() {
+		var student = NextThought.model.User({
+				'realname': 'Test User1',
+				'NTIID': 'Foo-bar_User'
+			}),
+		 	emailRecord = NextThought.model.Email.create({
+				url: '/dataserver2/foo-bar-course/student/mail',
+				receiver: student,
+			}),
+			emailWin = Ext.create('NextThought.app.course.assessment.components.admin.email.Window', {
+				record: emailRecord,
+				renderTo: testBody
+			}), 
+			editor = emailWin.editor,
+			title = "Test Email Title",
+			body = "Test the body. Thanks.",
+			titleInputEl = editor.titleEl,
+			bodyInputEl = editor.titleEl,
+			c = editor.el.down('.content'),
+			e = {stopEvent: function(){}},
+			postRecord
+			b = document.createElement('div');
+
+		editor.presentSuccessMessage = function(){};
+
+		if (titleInputEl && titleInputEl.dom) {
+			titleInputEl.dom.value = title;	
+		}
+
+		b.innerText = body;
+		if (c && c.dom) {
+			c.dom.appendChild(b);
+		}
+
+		spyOn(editor.EmailActions, 'sendEmail').andCallFake(function(record){
+			postRecord = record;
+			return Promise.resolve();
+		});
+		
+		editor.onSave(e);
+		expect(postRecord && postRecord.get('Body')).toBe(body);
+		expect(postRecord && postRecord.get('Subject')).toBe(title);
+	});
 });
