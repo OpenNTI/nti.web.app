10a3665910cd51d27ec320d06a2d560a9b12a03e
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2015-12-03 22:25:39 -0600

    add a simpler time field.

--- src/main/js/NextThought/common/form/fields/DatePicker.js
+++ src/main/js/NextThought/common/form/fields/DatePicker.js
@@ -9,11 +9,8 @@ Ext.define('NextThought.common.form.fields.DatePicker', {
 			{ cls: 'date', html: '{date}', 'data-value': '{date}'}
 		]},
 		{tag: 'tpl', 'if': 'timePicker', cn: [
-			{cls: 'hour-container part', cn: [
-				{ cls: 'hour', name: 'hour', html: 'Hour'}
-			]},
-			{cls: 'minute-container part', cn: [
-				{ cls: 'minute', name: 'minute', html: 'Min'}
+			{cls: 'time-container', cn: [
+				{ tag: 'input', cls: 'time', html: '12 : 00'}
 			]},
 			{cls: 'meridiem-container part', cn: [
 				{ cls: 'meridiem', name: 'meridiem', html: 'AM', 'date-value': 'am'}
@@ -26,11 +23,9 @@ Ext.define('NextThought.common.form.fields.DatePicker', {
 	renderSelectors: {
 		dateContainerEl: '.date-container',
 		dateEl: '.date',
-		hourContainerEl: '.hour-container',
-		minuteContainerEl: '.minute-container',
+		timeContainerEl: '.time-container',
+		timeEl: '.time',
 		meridiemContainerEl: '.meridiem-container',
-		hourEl: '.hour-container .hour',
-		minuteEl: '.minute-container .minute',
 		meridiemEl: '.meridiem-container .meridiem'
 	},
 
@@ -53,18 +48,6 @@ Ext.define('NextThought.common.form.fields.DatePicker', {
 				'click': this.showDatePicker.bind(this)
 			});	
 		}
-		
-		if ( this.hourContainerEl) {
-			this.mon(this.hourContainerEl, {
-				'click': this.showHourPicker.bind(this)
-			});	
-		}
-
-		if (this.minuteContainerEl) {
-			this.mon(this.minuteContainerEl, {
-				'click': this.showMinutePicker.bind(this)
-			});	
-		}
 
 		if (this.meridiemContainerEl) {
 			this.mon(this.meridiemContainerEl, {
@@ -73,11 +56,9 @@ Ext.define('NextThought.common.form.fields.DatePicker', {
 		}
 
 		this.createDatePicker();
-		if (this.TimePicker) {
-			this.hourMenu = this.createHourMenu();	
-			this.minuteMenu = this.createMinuteMenu();
+		if (this.timeEl) {
+			this.timeEl.dom.addEventListener('keyup', this.timeChanged.bind(this));
 		}
-
 		this.setValue(this.defaultValue);
 	},
 
@@ -123,133 +104,7 @@ Ext.define('NextThought.common.form.fields.DatePicker', {
 	},
 
 
-	showHourPicker: function(){
-		if (!this.hourMenu) {
-			this.hourMenu = this.createHourMenu();
-		}
-
-		if (this.hourMenu.isVisible()) {
-			this.hourMenu.hide();
-		}
-		else {
-			this.hourMenu.showBy(this.hourContainerEl, 'tl-bl?');			
-		}
-	},
-
-
-	createHourMenu: function(){
-		var me = this,
-			menu = 
-				Ext.widget('menu', {
-					defaults: {
-						ui: 'nt-menuitem',
-						xtype: 'menucheckitem',
-						plain: true,
-						group: 'hour-group',
-						handler: function(item) {
-							me.handleHourMenuClick(item, item.up('.menu'));
-						}
-					},
-					width: 75,
-					items: [
-						{ text: '1', value: 1},
-						{ text: '2', value: 2},
-						{ text: '3', value: 3},
-						{ text: '4', value: 4},
-						{ text: '5', value: 5},
-						{ text: '6', value: 6},
-						{ text: '7', value: 7},
-						{ text: '8', value: 8},
-						{ text: '9', value: 9},
-						{ text: '10', value: 10},
-						{ text: '11', value: 11},
-						{ text: '12', value: 12}
-					]
-				});
-
-		this.on('destroy', menu.destroy.bind(menu));
-		return menu;
-	},
-
-	
-	handleHourMenuClick: function(item, menu) {
-		var targetEl = this.hourEl, item;
-		if (targetEl) {
-			targetEl.setHTML(item.text);
-			targetEl.dom.setAttribute('data-value', item.value);
-			if (this.minuteMenu) {
-				item = this.minuteMenu.items.getAt(0);
-				// Set the first value of the first item of the minute (0 minutes)
-				if (item) {
-					item.setChecked(true);
-					this.handleMinuteMenuClick(item);
-				}
-			}
-		}
-
-		// Broadcast the change.
-		if (this.formChanged){
-			this.formChanged();
-		}
-	},
-
-
-	showMinutePicker: function(){
-		if (!this.minuteMenu) {
-			this.minuteMenu = this.createMinuteMenu();
-		}
-
-		if (this.minuteMenu.isVisible()) {
-			this.minuteMenu.hide();
-		}
-		else {
-			this.minuteMenu.showBy(this.minuteContainerEl, 'tl-bl?');			
-		}
-	},
-
-
-	createMinuteMenu: function(){
-		var me = this,
-			menu = 
-				Ext.widget('menu', {
-					defaults: {
-						ui: 'nt-menuitem',
-						xtype: 'menucheckitem',
-						plain: true,
-						group: 'minute-group',
-						handler: function(item) {
-							me.handleMinuteMenuClick(item, item.up('.menu'));
-						}
-					},
-					width: 75,
-					items: [
-						{ text: '00', value: 0},
-						{ text: '05', value: 5},
-						{ text: '10', value: 10},
-						{ text: '15', value: 15},
-						{ text: '20', value: 20},
-						{ text: '25', value: 25},
-						{ text: '30', value: 30},
-						{ text: '35', value: 35},
-						{ text: '40', value: 40},
-						{ text: '45', value: 45},
-						{ text: '50', value: 50},
-						{ text: '55', value: 55}
-					]
-				});
-
-		this.on('destroy', menu.destroy.bind(menu));
-		return menu;
-	},
-
-
-	handleMinuteMenuClick: function(item, menu) {
-		var targetEl = this.minuteEl;
-		if (targetEl) {
-			targetEl.setHTML(item.text);
-			targetEl.dom.setAttribute('data-value', item.value);
-		}
-
+	timeChanged: function(){
 		// Broadcast the change.
 		if (this.formChanged){
 			this.formChanged();
@@ -324,35 +179,26 @@ Ext.define('NextThought.common.form.fields.DatePicker', {
 			m = 'pm';
 		}
 
+		// Get two digit format
+		hour = ("0" + hour).slice(-2);
+		minute = ("0" + minute).slice(-2);
+
 		this.onceRendered
 			.then(function() {
 				var b = Ext.Date.format(date, "n/j/Y"), 
 					dateOnly = new Date(b);
 
+				// Set date
 				me.dateEl.setHTML(dateString);
 				me.dateEl.dom.setAttribute('data-value', dateOnly);
 				if (me.datepicker) {
 					me.datepicker.setValue(dateOnly);
 				}
 
-				if (hour !== undefined && me.hourEl) {
-					me.hourEl.dom.setAttribute('data-value', hour);
-					// We don't want to set the displayed hour to 0. 
-					// Change what the user sees. 
-					// FIXME: This is a short term fix.
-					if (hour < 12 && hour === 0) {
-						hour = 12;
-					}
-					me.hourEl.setHTML(hour);
-				}
-				if (minute !== undefined && me.minuteEl) {
-					me.minuteEl.dom.setAttribute('data-value', minute);
-					if (minute === 0) {
-						minute = '00';
-					}
-					me.minuteEl.setHTML(minute);
-				}
+				// Set time
+				me.timeEl.dom.value = hour + ':' + minute;
 
+				// Set AM/PM
 				if (me.meridiemEl && m) {
 					me.meridiemEl.dom.setAttribute('data-value', m);
 					me.meridiemEl.setHTML(m.toUpperCase());
@@ -401,18 +247,16 @@ Ext.define('NextThought.common.form.fields.DatePicker', {
 
 	getTimeMilliseconds: function(){
 		var v, time,
-			hourVal = this.hourEl && this.hourEl.dom.getAttribute('data-value'),
-			minuteVal = this.minuteEl && this.minuteEl.dom.getAttribute('data-value'),
+			timeVal = this.timeEl && this.timeEl.dom.value,
 			meridiemVal = this.meridiemEl && this.meridiemEl.dom.getAttribute('data-value'),
-			hour = 0, minute = 0, t;
+			hour = 0, minute = 0, t, parts;
 
-		if (hourVal !== undefined) {
-			hour = parseInt(hourVal);
-		}
+		parts = (timeVal || "").split(':');
+		hour = parts[0];	
+		hour = parseInt(hour);
 
-		if (minuteVal !== undefined) {
-			minute = parseInt(minuteVal);
-		}
+		minute = parts[1];
+		minute = parseInt(minute);
 
 		if (meridiemVal === 'pm') {
 			hour = hour + 12;
