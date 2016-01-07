5dd6f06c1f7a7f69f965b7542bb1fdbc3b3f08b0
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2016-01-06 18:29:31 -0600

    use byline

--- src/main/js/NextThought/app/context/components/cards/RelatedWork.js
+++ src/main/js/NextThought/app/context/components/cards/RelatedWork.js
@@ -31,7 +31,7 @@ Ext.define('NextThought.app.context.components.cards.RelatedWork', {
 
 		this.renderData = Ext.applyIf(this.renderData || {}, {
 			title: this.content && this.content.get('label'),
-			author: this.content && this.content.get('Creator')
+			author: this.content && this.content.get('byline')
 		});
 	},
 
