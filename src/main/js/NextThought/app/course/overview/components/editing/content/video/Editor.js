14db4bc1a03101edf6658f8e80157e7e8eb07d52
Andrew Ligon <andrew.ligon@nextthought.com>
2016-01-14 10:42:59 -0600

    Turn back on the advanced flag for videos

--- src/main/js/NextThought/app/course/overview/components/editing/content/video/Editor.js
+++ src/main/js/NextThought/app/course/overview/components/editing/content/video/Editor.js
@@ -25,7 +25,7 @@ Ext.define('NextThought.app.course.overview.components.editing.content.video.Edi
 			return [
 				{
 					title: 'Pick a Video',
-					// advanced: true,
+					advanced: true,
 					category: 'Video',
 					iconCls: 'Video',
 					description: '',
