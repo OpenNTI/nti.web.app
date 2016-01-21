e5fd3099b3d1f036dc8a468f8f59b09a5d800efd
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2016-01-21 10:08:46 -0600

    Do not expose the links directly. The mixin will handle it.

--- src/main/js/NextThought/model/courses/CourseInstanceEnrollment.js
+++ src/main/js/NextThought/model/courses/CourseInstanceEnrollment.js
@@ -1,3 +1,9 @@
+/**
+ * TODO: This is a duplicate of the NextThought.model.courseware.CourseInstanceEnrollment
+ * However they currently expose fields named differently, we need to refactor this and use only one
+ * moving forward.
+ * 
+ */
 Ext.define('NextThought.model.courses.CourseInstanceEnrollment', {
 	extend: 'Ext.data.Model',
 
@@ -13,7 +19,6 @@ Ext.define('NextThought.model.courses.CourseInstanceEnrollment', {
 			return (r.raw.LegacyEnrollmentStatus === 'ForCredit' && v) || '';
 		}},
 		{name: 'Creator', type: 'singleItem', mapping: 'UserProfile' },
-		{name: 'LegacyEnrollmentStatus', type: 'string'},
-		{name: 'Links', type: 'auto'}
+		{name: 'LegacyEnrollmentStatus', type: 'string'}
 	]
 });
