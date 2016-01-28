b7f4cbe4a56f39368cd582b81562a6ebf2d246ad
Josh Birdwell <josh.birdwell@nextthought.com>
2016-01-28 15:16:39 -0600

    Checkpoint for audit logs

--- src/main/js/NextThought/app/course/overview/components/editing/Editor.js
+++ src/main/js/NextThought/app/course/overview/components/editing/Editor.js
@@ -6,7 +6,8 @@ Ext.define('NextThought.app.course.overview.components.editing.Editor', {
 		'NextThought.common.form.Form',
 		'NextThought.app.prompt.Actions',
 		'NextThought.app.course.overview.components.editing.Actions',
-		'NextThought.app.course.overview.components.editing.controls.Delete'
+		'NextThought.app.course.overview.components.editing.controls.Delete',
+		'NextThought.app.course.overview.components.editing.auditlog.Index'
 	],
 
 	saveText: 'Save',
@@ -123,6 +124,10 @@ Ext.define('NextThought.app.course.overview.components.editing.Editor', {
 		if (this.record) {
 			this.deleteBtn = this.addDeleteButton();
 		}
+
+		if (this.record && this.record.hasAuditLog && this.record.hasAuditLog()) {
+			this.addAuditLog();
+		}
 	},
 
 
@@ -176,6 +181,14 @@ Ext.define('NextThought.app.course.overview.components.editing.Editor', {
 				afterDelete: this.afterDelete.bind(this)
 			});
 		}
+	},
+
+
+	addAuditLog: function() {
+		return this.add({
+			xtype: 'overview-editing-audit-log',
+			record: this.record
+		});
 	},
 
 
