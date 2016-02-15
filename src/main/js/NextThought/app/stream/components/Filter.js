dedc38a075533e88bc193df6cd311ae3eb0fb8de
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2016-02-15 14:57:09 -0600

    Refactor the activity types selection.

--- src/main/js/NextThought/app/stream/components/Filter.js
+++ src/main/js/NextThought/app/stream/components/Filter.js
@@ -24,7 +24,7 @@ Ext.define('NextThought.app.stream.components.Filter', {
 
 	GROUP_TYPES: {
 		sort: new Ext.XTemplate(Ext.DomHelper.markup([
-			{cls: 'group {cls}', cn: [
+			{cls: 'group {cls} ', 'data-key': '{type}', cn: [
 				{cls: 'name', html: '{displayText}'},
 				{cls: 'select-wrapper', cn: [
 					{tag: 'select', name: '{name}', cn: [
@@ -42,10 +42,10 @@ Ext.define('NextThought.app.stream.components.Filter', {
 		])),
 
 		activity: new Ext.XTemplate(Ext.DomHelper.markup([
-			{cls: 'group {cls}', cn: [
+			{cls: 'group {cls}', 'data-key': '{type}', cn: [
 				{cls: 'name', html: '{displayText}'},
 				{tag: 'tpl', 'for': 'items', cn: [
-					{tag: 'label', cn: [
+					{tag: 'label', cls: 'group-item', 'data-value': '{type}', cn: [
 						{tag: 'input', type: 'checkbox'},
 						{tag: 'span', html: '{text}'}
 					]}
@@ -54,10 +54,10 @@ Ext.define('NextThought.app.stream.components.Filter', {
 		])),
 
 		modifier: new Ext.XTemplate(Ext.DomHelper.markup([
-			{cls: 'group {cls}', cn: [
+			{cls: 'group {cls}', 'data-key': '{type}', cn: [
 				{cls: 'name', html: '{text}'},
 				{tag: 'tpl', 'for': 'items', cn: [
-					{cls: '{cls}', html: '{text}'}
+					{cls: 'group-item {cls}', 'data-value': '{value}', html: '{text}'}
 				]}
 			]}
 		]))
@@ -90,16 +90,41 @@ Ext.define('NextThought.app.stream.components.Filter', {
 	addFilterGroup: function(group){
 		var type = group.type,
 			tpl = this.GROUP_TYPES[type],
-			me = this,
-			el = this.el;
+			el = this.el,
+			items = group.items, 
+			g = Ext.clone(group);
+
+		// Change the items to an array, if it's not.
+		if (group.items && !(group.items instanceof Array)) {
+			items = Object.keys(group.items).map(function(k){return group.items[k]});
+		}
 
 		if (tpl) {
-			tpl.append(el, group, true);
+			tpl.append(el, Ext.apply(g, {items: items}), true);
 		}
 
 		if (type === 'sort') {
 			this.__addModifierGroup(group);
 		}
+	},
+
+
+	setActiveFilters: function(filters) {
+		if (!this.rendered) { return; }
+
+		var me = this, dom = this.el.dom;
+
+		//Update the filterGroup
+		this.filterGroups = filters;
+
+		this.filterGroups.forEach(function(group) {
+			var type = group.type,
+				g = dom.querySelector('[data-key='+type+']');
+
+			if (group.setActiveItem) {
+				group.setActiveItem(g, group.activeItems);
+			}
+		});
 	},
 
 
@@ -168,6 +193,8 @@ Ext.define('NextThought.app.stream.components.Filter', {
 	handleClick: function(e) {
 		var group = e.getTarget('.group'),
 			item = e.getTarget('.group-item');
+
+		e.stopEvent();
 
 		group = group && group.getAttribute('data-key');
 		item = item && item.getAttribute('data-value');
