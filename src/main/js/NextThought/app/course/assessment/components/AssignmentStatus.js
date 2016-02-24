fd32e0b35419dd352ff4938d8002d4a32f0e8d97
Andrew Ligon <andrew.ligon@nextthought.com>
2016-02-24 11:21:51 -0600

    If the assignment has an edit link, make the status cmp toggle a menu

--- src/main/js/NextThought/app/course/assessment/components/AssignmentStatus.js
+++ src/main/js/NextThought/app/course/assessment/components/AssignmentStatus.js
@@ -2,10 +2,30 @@ Ext.define('NextThought.app.course.assessment.components.AssignmentStatus', {
 	extend: 'Ext.Component',
 	alias: 'widget.course-assignment-status',
 
+	statics: {
+		setActiveMenu: function(menu) {
+			this.activeMenu = menu;
+		},
+
+
+		getActiveMenu: function() {
+			return this.activeMenu;
+		},
+
+		closeActiveMenu: function() {
+			if (this.activeMenu && this.activeMenu.closeDueDateEditor) {
+				this.activeMenu.closeDueDateEditor();
+				delete this.activeMenu;
+			}
+		}
+	},
+
 	requires: [
-		'NextThought.app.course.assessment.AssignmentStatus'
+		'NextThought.app.course.assessment.AssignmentStatus',
+		'NextThought.app.course.assessment.components.editing.DueDate'
 	],
 
+	cls: 'assignment-status-container',
 
 	renderTpl: Ext.DomHelper.markup([
 		{cls: 'assignment-status'},
@@ -22,6 +42,17 @@ Ext.define('NextThought.app.course.assessment.components.AssignmentStatus', {
 	afterRender: function() {
 		this.callParent(arguments);
 
+		this.updateStatus();
+
+		if (this.assignment.getLink('edit')) {
+			this.addCls(['editable']);
+
+			this.mon(this.el, 'click', this.toggleDueDateEditor.bind(this));
+		}
+	},
+
+
+	updateStatus: function() {
 		var assignment = this.assignment,
 			history = this.history,
 			grade = history && history.get && history.get('Grade'),
@@ -35,5 +66,63 @@ Ext.define('NextThought.app.course.assessment.components.AssignmentStatus', {
 			});
 
 		this.statusEl.dom.innerHTML = status;
+	},
+
+
+	addDueDateEditor: function() {
+		this.dueDateEditor = new NextThought.app.course.assessment.components.editing.DueDate({
+			assignment: this.assignment,
+			onSave: this.onDueDateChanged.bind(this),
+			renderTo: this.menuContainer
+		});
+
+		this.on('destroy', this.dueDateEditor.destroy.bind(this.dueDateEditor));
+	},
+
+
+	showDueDateEditor: function() {
+		var me = this;
+
+		if (me.self.getActiveMenu() !== me) {
+			me.self.closeActiveMenu();
+		}
+
+		me.self.setActiveMenu(me);
+
+		if (!me.dueDateEditor) {
+			me.addDueDateEditor();
+		}
+
+		me.el.addCls('menu-open');
+
+		me.bodyClickMon = me.mon(Ext.getBody(), 'click', function(e) {
+			var el = e.getTarget('assignment-status-container');
+
+			if (!el || el !== me.el.dom) {
+				me.closeDueDateEditor();
+			}
+		});
+	},
+
+
+	closeDueDateEditor: function() {
+		this.el.removeCls('menu-open');
+		Ext.destroy(this.bodyClickMon);
+	},
+
+
+	toggleDueDateEditor: function(e) {
+		e.stopEvent();
+
+		if (this.el.hasCls('menu-open')) {
+			this.closeDueDateEditor();
+		} else {
+			this.showDueDateEditor();
+		}
+	},
+
+
+	onDueDateChanged: function() {
+		debugger;
 	}
 });
---
+++ src/main/js/NextThought/app/course/assessment/components/editing/DueDate.js
@@ -0,0 +1,11 @@
+Ext.define('NextThought.app.course.assessment.components.editing.DueDate', {
+	extend: 'Ext.container.Container',
+	alias: 'widget.course-assessment-duedate-editor',
+
+
+	layout: 'none',
+
+	items: [
+		{xtype: 'box', autoEl: {html: 'Due Date Editor'}}
+	]
+});
