c21fc12eb6e3fec3d9f14a132a232d633d01b28c
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-03-21 16:39:00 -0500

    Unit test to find missing classes

--- karma.conf.js
+++ karma.conf.js
@@ -3,5 +3,15 @@
 var baseConfig = require('nti-unittesting-clientside');
 
 module.exports = function (config) {
+	const webpack = baseConfig.webpack;
+	const externals = webpack.externals || [];
+
+	Object.assign(webpack, {externals});
+
+
+	externals.push({'extjs': 'Ext'});
+
+	baseConfig.files.unshift('src/main/resources/vendor/ext/ext-all.js');
+
 	config.set(baseConfig);
 };
