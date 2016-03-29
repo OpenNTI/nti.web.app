a0dce5ae3070cafa2a398bed6b36cb7f02904c3a
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-03-29 10:57:55 -0500

    move progress plugin into module

--- karma.conf.js
+++ karma.conf.js
@@ -1,18 +1,15 @@
 /*eslint no-var: 0 strict: 0*/
 'use strict';
 var baseConfig = require('nti-unittesting-clientside');
-var WebPack = require('webpack');
-var start = new Date();
-var chars = 0;
+var Progress = require('nti-unittesting-clientside/progress');
 
 module.exports = function (config) {
-	start = new Date();
 	const webpack = baseConfig.webpack;
 	const externals = webpack.externals || [];
 
 	Object.assign(webpack, {externals});
 
-	webpack.plugins.push(new WebPack.ProgressPlugin(progress));
+	webpack.plugins.push(Progress.getPlugin());
 
 	externals.push({'extjs': 'Ext'});
 
@@ -22,48 +19,3 @@ module.exports = function (config) {
 
 	config.set(baseConfig);
 };
-
-
-function progress (percentage, msg) {
-
-	if(percentage < 1) {
-		percentage = Math.floor(percentage * 100);
-		msg = percentage + '% ' + msg;
-		if(percentage < 100) {
-			msg = ' ' + msg;
-		}
-		if(percentage < 10) {
-			msg = ' ' + msg;
-		}
-	} else {
-		msg += '\n';
-	}
-
-	if ((new Date() - start) < 5000) {
-		return;
-	}
-
-	if (/emit/.test(msg)) {
-		msg = '';
-	}
-
-	goToLineStart(msg);
-	process.stderr.write(msg);
-}
-
-
-function goToLineStart (nextMessage) {
-	var str = '';
-
-	for(; chars > nextMessage.length; chars--) {
-		str += '\b \b';
-	}
-
-	chars = nextMessage.length;
-	for(let i = 0; i < chars; i++) {
-		str += '\b';
-	}
-	if(str) {
-		process.stderr.write(str);
-	}
-}
