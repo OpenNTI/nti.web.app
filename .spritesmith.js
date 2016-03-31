1f896404cee2bb119f0e1d8a3e959ff94d5acf8e
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-03-31 10:05:39 -0500

    change where _icons is created

--- .spritesmith.js
+++ .spritesmith.js
@@ -3,7 +3,7 @@
 module.exports = {
 	src: './src/main/resources/images/{icons,elements}/**/*.png',
 	destImage: './src/main/resources/images/sprite.png',
-	destCSS: './src/main/resources/scss/_icons.scss',
+	destCSS: './src/main/resources/scss/utils/_icons.scss',
 	imgPath: '../images/sprite.png',
 	padding: 2,
 	algorithm: 'binary-tree',
