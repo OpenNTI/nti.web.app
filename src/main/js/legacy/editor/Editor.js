3ab0d0a327ea2aa938383e9d27b98817f447bdb1
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-05-21 23:16:09 -0500

    this may prevent select box... but probably not.

--- src/main/js/legacy/editor/Editor.js
+++ src/main/js/legacy/editor/Editor.js
@@ -372,7 +372,7 @@ Ext.define('NextThought.editor.AbstractEditor', {
 		}
 
 		this.mon(Ext.getBody(), 'click', 'hidePopovers');
-		this.mon(Ext.getBody(), 'mscontrolselect', evt => (evt.preventDefault(), false));
+		document.execCommand( 'enableObjectResizing', false, false );
 
 		this.maybeEnableSave();
 
