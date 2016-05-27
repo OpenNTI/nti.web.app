a92b7624b734acf41cb9551317f0887d920b1a73
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-05-26 22:16:49 -0500

    Salt sprite url

--- .spritesmith.js
+++ .spritesmith.js
@@ -1,10 +1,13 @@
 'use strict';
+const timestamp = new Date().toISOString()
+	.replace(/[T:\ -]/g, '')
+	.replace(/\..+$/, '');
 
 module.exports = {
 	src: './src/main/resources/images/{icons,elements}/**/*.png',
 	destImage: './src/main/resources/images/sprite.png',
 	destCSS: './src/main/resources/scss/utils/_icons.scss',
-	imgPath: '../images/sprite.png',
+	imgPath: `../images/sprite.png?t=${timestamp}`,
 	padding: 2,
 	algorithm: 'binary-tree',
 	engine: 'pixelsmith'
