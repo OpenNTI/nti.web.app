de12fa3c89085a0556f740dc11ec55b5698d2108
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2016-05-26 19:04:04 -0500

    NTI-1150 : Add support for browser without pdf support.

--- src/main/js/legacy/app/contentviewer/components/attachment/Window.js
+++ src/main/js/legacy/app/contentviewer/components/attachment/Window.js
@@ -1,6 +1,7 @@
 const Ext = require('extjs');
 require('legacy/app/prompt/StateStore');
 require('./Panel');
+const Globals = require('legacy/util/Globals');
 
 
 module.exports = exports = Ext.define('NextThought.app.contentviewer.components.attachment.Window', {
@@ -27,7 +28,13 @@ module.exports = exports = Ext.define('NextThought.app.contentviewer.components.
 
 		this.record = data && data.record;
 		this.parentRecord = data && data.parentRecord;
-		this.addPreview();
+
+		if (this.hasBrowserSupport()) {
+			this.addPreview();
+		} else {
+			this.addUnsupportedForHref();
+		}
+
 	},
 
 
@@ -37,6 +44,60 @@ module.exports = exports = Ext.define('NextThought.app.contentviewer.components.
 			record: this.record,
 			parentRecord: this.parentRecord,
 			setSaveText: this.setSaveText.bind(this)
+		});
+	},
+
+
+	hasBrowserSupport: function () {
+		var nativeSupport = Globals.hasPDFSupport(),
+			version;
+		if (Ext.isGecko) {
+			version = /Firefox\/(\d+\.\d+)/.exec(navigator.userAgent)[1];
+			version = parseInt(version, 10);
+		}
+
+		if ((version && version <= 18) || (!nativeSupport && !Ext.isGecko)) {
+			return false;
+		}
+
+		return true;
+	},
+
+
+	addUnsupportedForHref: function () {
+		var href = this.record && this.record.get('url'),
+			anchorAttr = 'class=\'link\' target=\'_blank\'',
+			chrome = '<a ' + anchorAttr + ' href=\'http://www.google.com/chrome\'>Chrome,</a>',
+			safari = '<a ' + anchorAttr + ' href=\'http://www.apple.com/safari/download/\'>Safari,</a>',
+			ff = '<a ' + anchorAttr + ' href=\'http://www.getfirefox.com\'>Firefox,</a>',
+			ie = '<a ' + anchorAttr + ' href=\'http://www.microsoft.com/ie\'>Internet Explorer.</a>';
+
+
+		this.add({
+			xtype: 'box',
+			cls: 'content-card-target-container',
+			width: 560,
+			height: 640,
+			renderTpl: Ext.DomHelper.markup({
+				cls: 'no-support', cn: [
+					{cls: 'message', html: 'Your browser does not currently support viewing PDF files.'},
+					{cls: '', cn: [
+						{tag: 'a', cls: 'link', href: 'https://get.adobe.com/reader/', target: '_blank', html: 'Install Adobe Acrobat Reader '},
+						'or try the latest version of one of the following browsers:<br>',
+						chrome,
+						' ',
+						safari,
+						' ',
+						ff,
+						' ',
+						ie
+					]},
+					'<br>',
+					{cls: '', cn: [
+						{tag: 'a', cls: 'link', href: href, html: 'Download the PDF'}
+					]}
+				]
+			})
 		});
 	},
 
