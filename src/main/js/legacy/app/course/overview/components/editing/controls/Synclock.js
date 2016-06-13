58609464eeb3a83a5babf260243f6264a53edd81
Casper Shepard <caspershepard@Caspers-iMac.4pp.nextthought.com>
2016-06-13 14:27:13 -0500

    NTI 1202 - Fixes a problem with the preview always appearing unlocked.

--- src/main/js/legacy/app/course/overview/components/editing/controls/Synclock.js
+++ src/main/js/legacy/app/course/overview/components/editing/controls/Synclock.js
@@ -3,20 +3,22 @@ var Ext = require('extjs');
 module.exports = exports = Ext.define('NextThought.app.course.overview.components.editing.controls.Synclock', {
 	extend: 'Ext.Component',
 	alias: 'widget.overview-editing-controls-synclock',
-	cls: 'nt-sync-lock',
-
-	renderTpl: Ext.DomHelper.markup([
-		{cls: 'nt-sync-lock'}
-	]),
+	cls: 'nt-button synclock',
 
 	beforeRender: function () {
 		this.callParent(arguments);
 
-		const isLocked = this.record && this.record.hasLink('SyncUnlock');
+		let isLocked;
+
+		if (this.contents) {
+			isLocked = this.contents.data && this.contents.data.Links
+				&& this.contents.data.Links.hasLink('SyncUnlock');
+		} else {
+			isLocked = this.record && this.record.hasLink('SyncUnlock');
+		}
 
 		if (!Service.canDoAdvancedEditing() || !isLocked) {
 			this.hide();
-			return;
 		}
 	},
 
