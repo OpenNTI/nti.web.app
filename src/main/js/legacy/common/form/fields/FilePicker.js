e244c836d57404454c47a2cd331d1ffc1d17db66
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-07-01 16:55:42 -0500

    allow 'accepts' and 'filter' on the picker

--- src/main/js/legacy/common/form/fields/FilePicker.js
+++ src/main/js/legacy/common/form/fields/FilePicker.js
@@ -272,7 +272,10 @@ module.exports = exports = Ext.define('NextThought.common.form.fields.FilePicker
 		const bundle = StateStore.getInstance().getRootBundle();
 		const sourceID = bundle.getId();
 
-		ContentResources.selectFrom(sourceID)
+		const accept = x => !x.isFolder && (!this.acceptsContentFileFilter || this.acceptsContentFileFilter(x));
+		const filter = x => !this.contentFileFilter || this.contentFileFilter(x);
+
+		ContentResources.selectFrom(sourceID, accept, filter)
 			.then(file => {
 				this.currentFile = file.getID();
 				this.onFileChange(file);
