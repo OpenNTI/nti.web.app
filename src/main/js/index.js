c6aea05213e611ce8e91e790d8831e4f7899e957
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-07-07 20:40:22 -0500

    we were missing a bootstrap part.

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,2 +1,5 @@
 import 'babel-polyfill';//applies hooks into global
 import 'legacy'; //kick start the app (the core is still defined in extjs)
+import {addFeatureCheckClasses} from 'nti-lib-dom';
+
+addFeatureCheckClasses();
