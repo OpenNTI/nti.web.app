b4f1484adbc6d2de336e2ae69851ffebda43c6cf
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-07-08 00:22:19 -0500

    NTI-1349 - Wire up <ConflictResolutionHandler/>

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,5 +1,15 @@
 import 'babel-polyfill';//applies hooks into global
 import 'legacy'; //kick start the app (the core is still defined in extjs)
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import {addFeatureCheckClasses} from 'nti-lib-dom';
+import {ConflictResolutionHandler} from 'nti-web-commons';
 
 addFeatureCheckClasses();
+
+//once the all the app is react... this can just be a child of the root component... but for now, we'll mount it to a dummy div.
+ReactDOM.render(
+	<ConflictResolutionHandler/>,
+	document.body.appendChild(document.createElement('div')));
