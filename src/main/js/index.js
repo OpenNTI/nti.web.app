a814c1f3930d253e9f6e4619b8941575cc44ec2e
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-09-07 15:56:15 -0500

    explicitly init locale

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -6,7 +6,9 @@ import ReactDOM from 'react-dom';
 
 import {addFeatureCheckClasses} from 'nti-lib-dom';
 import {ConflictResolutionHandler} from 'nti-web-commons';
+import {init as initLocale} from 'nti-lib-locale';
 
+initLocale();
 addFeatureCheckClasses();
 
 //once the all the app is react... this can just be a child of the root component... but for now, we'll mount it to a dummy div.
