c4919cfbe0aeeeb28f7123026603106c19e6694d
Andrew Ligon <andrew.ligon@nextthought.com>
2016-09-27 17:53:25 -0500

    NTI-2277 Rely on the route activating to update the outline, and don't used the cached version

--- src/main/js/legacy/app/course/overview/components/View.js
+++ src/main/js/legacy/app/course/overview/components/View.js
@@ -57,7 +57,7 @@ module.exports = exports = Ext.define('NextThought.app.course.overview.component
 	},
 
 	onRouteActivate: function () {
-		this.updateOutline(this.isEditing);
+		this.updateOutline(this.isEditing, true);
 
 		this.alignNavigation();
 		this.isActive = true;
@@ -257,6 +257,8 @@ module.exports = exports = Ext.define('NextThought.app.course.overview.component
 			})
 			.then(me.navigation.setOutline.bind(me.navigation, bundle));
 
+		this.activeOutlineInterface = outlineInterface;
+
 		return outlineInterface;
 	},
 
@@ -311,7 +313,7 @@ module.exports = exports = Ext.define('NextThought.app.course.overview.component
 	},
 
 	__getRecord: function (id, record, editing, doNotCache) {
-		var outline = this.updateOutline(editing, doNotCache);
+		var outline = this.activeOutlineInterface || this.updateOutline(editing, doNotCache);
 
 		return outline.onceBuilt()
 			.then(builtOutline => {
