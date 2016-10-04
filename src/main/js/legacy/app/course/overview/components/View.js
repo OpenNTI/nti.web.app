53203ccc398c7e908eef22c708718fc4dd8399f7
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2016-10-04 18:00:29 -0500

    Fix NTI-2316: in editing mode, refresh the outline. Fix adding and navigating to new lessons or units.

--- src/main/js/legacy/app/course/overview/components/View.js
+++ src/main/js/legacy/app/course/overview/components/View.js
@@ -313,7 +313,8 @@ module.exports = exports = Ext.define('NextThought.app.course.overview.component
 	},
 
 	__getRecord: function (id, record, editing, doNotCache) {
-		var outline = this.activeOutlineInterface || this.updateOutline(editing, doNotCache);
+		var outline = editing ? this.updateOutline(editing, doNotCache) :
+								this.activeOutlineInterface || this.updateOutline(editing, doNotCache);
 
 		return outline.onceBuilt()
 			.then(builtOutline => {
