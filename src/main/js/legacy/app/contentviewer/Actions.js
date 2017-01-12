dc0e45cc43547bcb1e2f68071efb4a64566f8614
Andrew Ligon <andrew.ligon@nextthought.com>
2017-01-12 16:21:11 -0600

    NTI-2785 when building a fake page for a related work ref in the reader, if the href is an NTIID try to get a real page info for it, if not build the fake one

--- src/main/js/legacy/app/contentviewer/Actions.js
+++ src/main/js/legacy/app/contentviewer/Actions.js
@@ -14,57 +14,69 @@ module.exports = exports = Ext.define('NextThought.app.contentviewer.Actions', {
 
 	getRelatedWorkPageInfo: function (data, bundle) {
 		var ntiid = data.get ? data.get('NTIID') : data.NTIID,
+			href = data.get ? data.get('href') : data.href,
 			DH = Ext.DomHelper;
 
-		return ContentUtils.getLocation(ntiid, bundle)
-			.then(function (locations) {
-				var location = locations[0],
-					root = location && location.root,
-					postfix, pageInfo,
-					pageURI = encodeURIComponent('Pages(' + ntiid + ')'),
-					userURI = encodeURIComponent($AppConfig.username);
+		return new Promise((fulfill, reject) => {
+			if (ParseUtils.isNTIID(href)) {
+				Service.getPageInfo(href, void 0, void 0, void 0, bundle)
+					.then(x => fulfill(x))
+					.catch(() => reject());
+			} else {
+				reject();
+			}
+		}).catch(() => {
+			return ContentUtils.getLocation(ntiid, bundle)
+				.then(function (locations) {
+					var location = locations[0],
+						root = location && location.root,
+						postfix, pageInfo,
+						pageURI = encodeURIComponent('Pages(' + ntiid + ')'),
+						userURI = encodeURIComponent($AppConfig.username);
 
-				if (data.asDomData) {
-					data = data.asDomData(root || '');
-				}
+					if (data.asDomData) {
+						data = data.asDomData(root || '');
+					}
 
-				postfix = data.noTarget ? '' : '-target';
+					postfix = data.noTarget ? '' : '-target';
 
-				pageInfo = NextThought.model.PageInfo.create({
-					ID: ntiid,
-					NTIID: ntiid,
-					content: DH.markup([
-						{tag: 'head', cn: [
-							{tag: 'title', html: data.title},
-							{tag: 'meta', name: 'icon', content: data.thumbnail}
-						]},
-						{tag: 'body', cn: {
-							cls: 'page-contents no-padding',
-							cn: Ext.applyIf({
-								tag: 'object',
-								cls: 'nticard' + postfix,
-								type: 'application/vnd.nextthought.nticard' + postfix,
-								'data-ntiid': ntiid,
-								html: DH.markup([
-									{tag: 'img', src: data.thumbnail},
-									{tag: 'span', cls: 'description', html: data.description}
-								])
-							}, data.domSpec)
-						}}
-					]),
-					Links: [
-						{
-							Class: 'Link',
-							href: '/dataserver2/users/' + userURI + '/' + pageURI + '/UserGeneratedData',
-							rel: 'UserGeneratedData'
-						}
-					]
+					pageInfo = NextThought.model.PageInfo.create({
+						ID: ntiid,
+						NTIID: ntiid,
+						content: DH.markup([
+							{tag: 'head', cn: [
+								{tag: 'title', html: data.title},
+								{tag: 'meta', name: 'icon', content: data.thumbnail}
+							]},
+							{tag: 'body', cn: {
+								cls: 'page-contents no-padding',
+								cn: Ext.applyIf({
+									tag: 'object',
+									cls: 'nticard' + postfix,
+									type: 'application/vnd.nextthought.nticard' + postfix,
+									'data-ntiid': ntiid,
+									html: DH.markup([
+										{tag: 'img', src: data.thumbnail},
+										{tag: 'span', cls: 'description', html: data.description}
+									])
+								}, data.domSpec)
+							}}
+						]),
+						Links: [
+							{
+								Class: 'Link',
+								href: '/dataserver2/users/' + userURI + '/' + pageURI + '/UserGeneratedData',
+								rel: 'UserGeneratedData'
+							}
+						]
+					});
+
+					pageInfo.hideControls = true;
+
+					return pageInfo;
 				});
+		});
 
-				pageInfo.hideControls = true;
-
-				return pageInfo;
-			});
 	},
 
 
