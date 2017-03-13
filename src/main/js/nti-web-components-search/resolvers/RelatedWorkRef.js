6cd4bb714d29ef3d2113df257af6c8e2f7781fdf
Landon Sherwood <landonsherwood23@gmail.com>
2017-03-13 13:17:36 -0500

    Removed some unused logic for search results

--- src/main/js/nti-web-components-search/resolvers/RelatedWorkRef.js
+++ src/main/js/nti-web-components-search/resolvers/RelatedWorkRef.js
@@ -1,5 +1,3 @@
-import {getService} from 'nti-web-client';
-
 export default {
 	handles (targetMimeType) {
 		targetMimeType = targetMimeType.replace('application/vnd.nextthought.', '');
@@ -10,29 +8,5 @@ export default {
 		} else {
 			return false;
 		}
-	},
-
-	addByLine (name) {
-		const creator = `By ${name}`;
-
-		this.renderData.creator = creator;
-
-		if (this.rendered) {
-			this.creatorEl.update(creator);
-		}
-	},
-
-	resolveObject (hit) {
-		const byline = hit.get('byline');
-		const label = hit.get('label');
-
-		if (byline) {
-			this.addByLine(byline);
-		}
-
-		this.setTitle(label);
-
-		return getService()
-				.then(service => service.getObject(hit.NTIID));
-	},
+	}
 };
