c348f10653a54ac27fc5f5c04c3845b63fefe69c
Cody Crow <cody.crow@nextthought.com>
2017-03-16 16:08:14 -0500

    NTI-3086 Create gotoReadings prop (alias for onDelete)

--- src/main/js/legacy/app/content/content/Index.js
+++ src/main/js/legacy/app/content/content/Index.js
@@ -173,6 +173,15 @@ module.exports = exports = Ext.define('NextThought.app.content.content.Index', {
 					return;
 				}
 
+				const onDelete = () => {
+					this.currentBundle.updateFromServer()
+						.then(() => {
+							if (this.onDelete) {
+								this.onDelete();
+							}
+						});
+				};
+
 				this.editor = this.add({
 					xtype: 'react',
 					component: Editor,
@@ -184,14 +193,8 @@ module.exports = exports = Ext.define('NextThought.app.content.content.Index', {
 					onDidChange: () => {
 						this.currentBundle.updateContentPackage(packageId);
 					},
-					onDelete: () => {
-						this.currentBundle.updateFromServer()
-							.then(() => {
-								if (this.onDelete) {
-									this.onDelete();
-								}
-							});
-					}
+					onDelete: onDelete,
+					gotoResources: onDelete
 				});
 			})
 			.always(() => {
