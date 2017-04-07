7f82d9b9d6b96858c6df1f0de96ac43569fbd7c4
Landon Sherwood <landonsherwood23@gmail.com>
2017-04-07 15:18:40 -0500

    search results hits now appear at the same time

--- src/main/js/nti-web-components-search/components/Results.jsx
+++ src/main/js/nti-web-components-search/components/Results.jsx
@@ -5,28 +5,25 @@ import {
 	initComponent,
 	resolveTitle,
 	resolveFragments,
-	resolvePath,
-	resolveContainerID
+	resolvePath
 } from '../resolvers';
 
 import Hit from './Hit';
 
-function loadHitData (hit) {
-	const {getBreadCrumb} = this.props;
+function loadHitData (hit, getBreadCrumb) {
 
 	initComponent(hit);
-	return Promise.all(
+
+	return Promise.all([
 		resolveTitle(hit, getBreadCrumb),
 		resolveFragments(hit),
-		resolvePath(hit, getBreadCrumb),
-		resolveContainerID(hit)
-	).then((results) => {
+		resolvePath(hit, getBreadCrumb)
+	]).then((results) => {
 		return {
 			hit,
 			title: results[0],
 			fragments: results[1],
-			path: results[2],
-			containerID: results[3],
+			path: results[2]
 		};
 	});
 }
@@ -55,15 +52,14 @@ export default class SearchResults extends React.Component {
 	}
 
 	componentDidUpdate (prevProps, prevState) {
-		if(prevProps.showLoading !== this.props.showLoading) {
-			const {hits = []} = this.props;
-console.log(hits);
-			this.getHitData(hits);
+		if(prevProps.hits !== this.props.hits) {
+			const {hits = [], getBreadCrumb} = this.props;
+			this.getHitData(hits, getBreadCrumb);
 		}
 	}
 
-	getHitData (hits) {
-		Promise.all(hits.map(loadHitData))
+	getHitData (hits, getBreadCrumb) {
+		Promise.all(hits.map(hit => loadHitData(hit, getBreadCrumb)))
 		.then(results => {
 			this.setState({loaded: true, hits: results});
 		});
@@ -104,7 +100,7 @@ console.log(hits);
 		const {navigateToSearchHit} = this.props;
 
 		return (
-			<Hit hit={hit.hit} title={hit.title} key={index} fragments={hit.fragments} path={hit.path} containerID={hit.containerID} getBreadCrumb={hit.getBreadCrumb} navigateToSearchHit={navigateToSearchHit}/>
+			<Hit hit={hit.hit} title={hit.title} key={index} fragments={hit.fragments} path={hit.path} navigateToSearchHit={navigateToSearchHit}/>
 		);
 	}
 }
