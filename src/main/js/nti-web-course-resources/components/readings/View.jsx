e8c6c158c87d3948c947f7c44c2030fdb1ac16fd
Cody Crow <cody.crow@nextthought.com>
2017-04-10 16:37:56 -0500

    NTI-3117 Sort resource readings by chosen column

--- src/main/js/nti-web-course-resources/components/readings/View.jsx
+++ src/main/js/nti-web-course-resources/components/readings/View.jsx
@@ -17,36 +17,19 @@ function sortOnTitle (a, b) {
 	return (a.title || '').localeCompare(b.title);
 }
 
-const COLS = [
-	{
-		name: 'name',
-		className: 'name sort-inactive',
-		display: t('name'),
-		sortFn: sortOnTitle
-	},
-	{
-		name: 'publish',
-		className: 'publish sort-inactive',
-		display: t('publish'),
-		sortFn: (a, b) => {
-			const {isPublished:aPublished} = a;
-			const {isPublished:bPublished} = b;
+const tableClasses = {
+	className: 'course-resources-readings ascending',
+	headerClassName: 'course-resources-header',
+	bodyClassName: 'course-resources-body'
+};
 
-			return aPublished && !bPublished ? -1 : aPublished === bPublished ? sortOnTitle(a, b) : -1;
-		}
-	},
-	{
-		name: 'modified',
-		className: 'last-modified sort-inactive',
-		display: t('lastModified'),
-		sortFn: (a, b) => {
-			const aModified = a.getLastModified();
-			const bModified = b.getLastModified();
-
-			return aModified < bModified ? -1 : aModified === bModified ? 0 : -1;
-		}
-	}
-];
+const columnClasses = {
+	asc: 'sort-asc',
+	desc: 'sort-desc',
+	inactive: 'sort-inactive',
+	active: 'sort-active',
+	default: 'sort-default'
+};
 
 function getReadings (course) {
 	const {ContentPackageBundle} = course || {};
@@ -65,20 +48,55 @@ Readings.propTypes = {
 export default function Readings ({course, filter, gotoResource}) {
 	const readings = getReadings(course).filter((x) => filter(x.title));
 
-	const renderItem = (item) => {
+	const renderRow = (row, cols) => {
 		return (
-			<ListItem reading={item} gotoResource={gotoResource} />
+			<ListItem reading={row} gotoResource={gotoResource} columns={cols} />
 		);
 	};
 
+	const columns = [
+		{
+			name: 'name',
+			classes: Object.assign({
+				name: 'name'
+			}, columnClasses),
+			display: t('name'),
+			sortFn: sortOnTitle
+		},
+		{
+			name: 'publish',
+			classes: Object.assign({
+				name: 'publish'
+			}, columnClasses),
+			display: t('publish'),
+			sortFn: (a, b) => {
+				const {isPublished:aPublished} = a;
+				const {isPublished:bPublished} = b;
+
+				return aPublished && !bPublished ? 1 : aPublished === bPublished ? sortOnTitle(a, b) : -1;
+			}
+		},
+		{
+			name: 'modified',
+			classes: Object.assign({
+				name: 'last-modified'
+			}, columnClasses),
+			display: t('lastModified'),
+			sortFn: (a, b) => {
+				const aModified = a.getLastModified();
+				const bModified = b.getLastModified();
+
+				return aModified < bModified ? -1 : aModified === bModified ? 0 : 1;
+			}
+		}
+	];
+
 	return readings.length !== 0 ? (
 		<Table.ListTable
-			className="course-resources-readings ascending"
-			headerClassName="course-resources-header"
-			bodyClassName="course-resources-body"
-			items={readings}
-			renderItem={renderItem}
-			cells={COLS}
+			classes={tableClasses}
+			rows={readings}
+			columns={columns}
+			renderRow={renderRow}
 		/>
 	) : <EmptyState
 			header="This folder is empty."
