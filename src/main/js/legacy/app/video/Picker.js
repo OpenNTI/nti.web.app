d09ab99105c69d870c6ed52be731a3f28af93e74
Andrew Ligon <andrew.ligon@nextthought.com>
2017-06-16 12:38:31 -0500

    Checkpoint for creating a video

--- src/main/js/legacy/app/video/Picker.js
+++ src/main/js/legacy/app/video/Picker.js
@@ -1,8 +1,31 @@
 const Ext = require('extjs');
 
-const {EmbedInput} = require('nti-web-video');
+const {EmbedInput, createMediaSourceFromUrl, getCanonicalUrlFrom} = require('nti-web-video');
 const PromptStateStore = require('legacy/app/prompt/StateStore');
+const Video = require('legacy/model/Video');
+
+const SourceMimeType = 'application/vnd.nextthought.ntivideosource';
+
 require('legacy/overrides/ReactHarness');
+
+function createSource (media) {
+	return Promise.all([
+			media.getPoster(),
+			media.getThumbnail()
+		]).then(([poster, thumbnail]) => {
+			debugger;
+		});
+}
+
+function createVideo (media) {
+	return Promise.all([
+		createSource(media),
+		media.getTitle()
+	]).then(([sources, title]) => {
+
+		debugger;
+	});
+}
 
 module.exports = exports = Ext.define('NextThought.app.video.Picker', {
 	extend: 'Ext.container.Container',
@@ -22,8 +45,23 @@ module.exports = exports = Ext.define('NextThought.app.video.Picker', {
 
 		this.add({
 			xtype: 'react',
-			component: EmbedInput
+			component: EmbedInput,
+			onSelect: src => this.onSelect(src),
+			onDismiss: () => this.onDismiss()
 		});
+	},
+
+
+	onSelect (source) {
+		createMediaSourceFromUrl(getCanonicalUrlFrom(source))
+			.then((media) => {
+				return createVideo(media);
+			});
+	},
+
+
+	onDismiss () {
+		this.Prompt.doClose();
 	}
 }, function () {
 	PromptStateStore.register('video-picker', this);
