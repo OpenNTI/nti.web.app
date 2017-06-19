19f7116be0a80fbf19bfabca2356e7822d906f53
Andrew Ligon <andrew.ligon@nextthought.com>
2017-06-19 16:24:34 -0500

    Checkpoint for setting the video title

--- src/main/js/legacy/app/video/Picker.js
+++ src/main/js/legacy/app/video/Picker.js
@@ -1,30 +1,53 @@
 const Ext = require('extjs');
+const {getService} = require('nti-web-client');
 
-const {EmbedInput, createMediaSourceFromUrl, getCanonicalUrlFrom} = require('nti-web-video');
+const {EmbedInput, Editor, createMediaSourceFromUrl, getCanonicalUrlFrom} = require('nti-web-video');
 const PromptStateStore = require('legacy/app/prompt/StateStore');
 const Video = require('legacy/model/Video');
 
 const SourceMimeType = 'application/vnd.nextthought.ntivideosource';
 
+const Types = {
+	'kaltura': 'video/kaltura',
+	'youtube': 'video/youtube',
+	'vimeo': 'video/vimeo'
+};
+
 require('legacy/overrides/ReactHarness');
 
-function createSource (media) {
-	return Promise.all([
-			media.getPoster(),
-			media.getThumbnail()
-		]).then(([poster, thumbnail]) => {
-			debugger;
-		});
+function createSources ({service, source}) {
+	return [{
+		MimeType: SourceMimeType,
+		service,
+		source: [source],
+		type: [Types[service]]
+	}];
 }
 
-function createVideo (media) {
+function createVideoJSON (media) {
 	return Promise.all([
-		createSource(media),
+		createSources(media),
 		media.getTitle()
 	]).then(([sources, title]) => {
 
-		debugger;
+		return {
+			MimeType: Video.mimeType,
+			title,
+			sources
+		};
 	});
+}
+
+function createVideo (link, raw) {
+	if (!link) { return Promise.reject(); }
+
+	return getService()
+			.then((service) => service.postParseResponse(link, raw));
+}
+
+
+function getVideo (video) {
+	debugger;
 }
 
 module.exports = exports = Ext.define('NextThought.app.video.Picker', {
@@ -43,24 +66,71 @@ module.exports = exports = Ext.define('NextThought.app.video.Picker', {
 		this.Prompt.Header.hide();
 		this.Prompt.Footer.hide();
 
-		this.add({
+		const {video} = this.Prompt.data;
+
+		if (video) {
+			getVideo()
+				.then(video => this.editVideo(video));
+		} else {
+			this.createVideo();
+		}
+	},
+
+
+	getAssetLink () {
+		const {data} = this.Prompt;
+
+		return data && data.bundle && data.bundle.getLink('assets');
+	},
+
+
+	createPlaceholderVideo (raw) {
+		return {...raw, save: (...args) => this.onPlaceholderSaved(...args)};
+	},
+
+
+	onPlaceholderSaved () {
+		debugger;
+	},
+
+
+	editVideo (video) {
+		if (this.videoCreator) {
+			this.videoCreator.destroy();
+		}
+
+		this.videoEditor = this.add({
 			xtype: 'react',
-			component: EmbedInput,
-			onSelect: src => this.onSelect(src),
-			onDismiss: () => this.onDismiss()
+			component: Editor,
+			video,
+			onDone: () => this.onVideoEdit()
 		});
 	},
 
 
-	onSelect (source) {
-		createMediaSourceFromUrl(getCanonicalUrlFrom(source))
-			.then((media) => {
-				return createVideo(media);
-			});
+	createVideo () {
+		if (this.videoEditor) {
+			this.videoEditor.destroy();
+		}
+
+		this.videoCreator = this.add({
+			xtype: 'react',
+			component: EmbedInput,
+			onSelect: src => this.onNewVideoSelect(src),
+			onCancel: () => this.onCancel()
+		});
 	},
 
 
-	onDismiss () {
+	onNewVideoSelect (source) {
+		createMediaSourceFromUrl(getCanonicalUrlFrom(source))
+			.then(media => createVideoJSON(media))
+			.then(raw => this.createPlaceholderVideo(raw))
+			.then((placeholder) => this.editVideo(placeholder));
+	},
+
+
+	onCancel () {
 		this.Prompt.doClose();
 	}
 }, function () {
