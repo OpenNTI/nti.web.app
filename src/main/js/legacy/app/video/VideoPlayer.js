4f5ff7dcf4d00421ff22a16bbcb3674f615e3401
Andrew Ligon <andrew.ligon@nextthought.com>
2017-07-07 18:13:15 -0500

    Deactivate the other players when one activates

--- src/main/js/legacy/app/video/VideoPlayer.js
+++ src/main/js/legacy/app/video/VideoPlayer.js
@@ -1,5 +1,6 @@
 const Ext = require('extjs');
 const {getService} = require('nti-web-client');
+
 const {default: Video} = require('nti-web-video');
 
 require('legacy/overrides/ReactHarness');
@@ -8,6 +9,10 @@ require('legacy/overrides/ReactHarness');
 module.exports = exports = Ext.define('NextThought.app.video.VideoPlayer', {
 	extend: 'Ext.container.Container',
 	alias: 'widget.content-video-player',
+
+	mixins: {
+		instanceTracking: 'NextThought.mixins.InstanceTracking'
+	},
 
 	layout: 'none',
 	items: [],
@@ -38,46 +43,98 @@ module.exports = exports = Ext.define('NextThought.app.video.VideoPlayer', {
 		this.playerWidth = this.width || this.playerWidth;
 		this.playerHeight = this.getPlayerHeight();
 
-		getService()
-			.then((service) => service.getObject(this.video.getId()))
-			.then((video) => {
-				this.add({
-					xtype: 'container',
-					cls: 'video-wrapper',
-					layout: 'none',
-					items: [
-						{
-							xtype: 'react',
-							component: Video,
-							src: video,
-							width: this.playerWidth,
-							height: this.playerHeight,
-							onTimeUpdate: e => this.onTimeUpdate(e),
-							onSeeked: e => this.onSeeked(e),
-							onPlaying: e => this.onPlaying(e),
-							onPause: e => this.onPause(e),
-							onEnded: e => this.onEnded(e),
-							onError: e => this.onError(e)
-						}
-					]
-				})
-
-				this.videoPlayer = this.down('react');
-
-				this.commandQueue.forEach(x => x());
-
-				if (this.el) {
-					this.el.unmask();
-				}
-			});
-
+		this.trackThis();
 	},
 
 
 	afterRender () {
-		if (!this.videoPlayer) {
-			this.el.mask('Loading...');
+		this.callParent(arguments);
+
+		this.maybeActivatePlayer();
+
+		this.monitorCardChange();
+	},
+
+
+	getVideo () {
+		this.videoPromise = this.videoPromise || getService().then(service => service.getObject(this.video.getId()));
+
+		return this.videoPromise;
+	},
+
+
+	monitorCardChange () {
+		const monitor = (cmp) => {
+			const card = cmp.up('{isOwnerLayout("card")}');
+
+			if (card) {
+				this.mon(card, {
+					activate: () => this.maybeActivatePlayer(),
+					deactivate: () => this.deactivatePlayer()
+				});
+
+				monitor(card);
+			}
+		};
+
+		monitor(this);
+	},
+
+
+	maybeActivatePlayer () {
+		if (!this.isVisible(true)) { return; }
+
+		const otherInstances = this.getInstances();
+
+		otherInstances.forEach((instance) => {
+			if (instance !== this) {
+				instance.deactivatePlayer();
+			}
+		});
+
+		if (!this.isActive) {
+			this.activatePlayer();
 		}
+	},
+
+
+	activatePlayer () {
+		this.isActive = true;
+
+		this.getVideo()
+			.then((video) => {
+				if (this.videoPlayer) {
+					this.resumePlayback();
+					return;
+				}
+
+				this.videoPlayer = this.videoWrapper.add({
+					xtype: 'react',
+					component: Video,
+					src: video,
+					autoPlay: true,
+					width: this.playerWidth,
+					height: this.playerHeight,
+					onTimeUpdate: (e) => this.onTimeUpdate(e),
+					onSeeked: (e) => this.onSeeked(e),
+					onPlaying: (e) => this.onPlaying(e),
+					onPause: (e) => this.onPause(e),
+					onEnded: (e) => this.onEnded(e),
+					onError: (e) => this.onError(e)
+				});
+
+				this.commandQueue.forEach(command => command());
+
+				this.fireEvent('player-command-activate');
+			});
+	},
+
+
+	deactivatePlayer () {
+		delete this.isActive;
+
+		this.pausePlayback();
+		this.fireEvent('player-deactivated');
 	},
 
 
@@ -100,6 +157,8 @@ module.exports = exports = Ext.define('NextThought.app.video.VideoPlayer', {
 
 
 	resumePlayback () {
+		this.maybeActivatePlayer();
+
 		if (this.videoPlayer && this.videoPlayer.componentInstance) {
 			this.videoPlayer.componentInstance.play();
 		} else {
@@ -118,31 +177,31 @@ module.exports = exports = Ext.define('NextThought.app.video.VideoPlayer', {
 
 
 	onTimeUpdate (e) {
-		debugger;
+
 	},
 
 
 	onSeeked (e) {
-		debugger;
+
 	},
 
 
 	onPlaying (e) {
-		debugger;
+
 	},
 
 
 	onPause (e) {
-		debugger;
+
 	},
 
 
 	onEnded (e) {
-		debugger;
+
 	},
 
 
 	onError (e) {
-		debugger;
+
 	}
 });
