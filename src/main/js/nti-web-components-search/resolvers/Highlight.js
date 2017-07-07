7373cc87e808c4992f806ac202dde777bf525a56
Cody Crow <cody.crow@nextthought.com>
2017-07-07 13:57:50 -0500

    NTI-3554 Resolve titles for highlights

---
+++ src/main/js/nti-web-components-search/resolvers/Highlight.js
@@ -0,0 +1,12 @@
+import {getService} from 'nti-web-client';
+
+export default {
+	handles (targetMimeType) {
+		return targetMimeType === 'application/vnd.nextthought.highlight';
+	},
+
+	resolveObject (hit) {
+		const containerId = hit && hit.ContainerId || (hit.Containers || [])[0];
+		return getService().then(service => service.getObject(containerId));
+	}
+};
