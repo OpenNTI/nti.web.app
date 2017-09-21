295b7a1d1ec2dca45154687daac248f0ed9cda05
Cory Jones <cory.jones@nextthought.com>
2017-09-21 12:38:55 -0500

    NTI-4052 Handle ntivideorefs in published readings

---
+++ src/main/js/legacy/app/video/OverlayedPanelRef.js
@@ -0,0 +1,55 @@
+const Ext = require('extjs');
+
+const PlaylistItem = require('legacy/model/PlaylistItem');
+const VideoPosters = require('legacy/model/resolvers/VideoPosters');
+
+require('../contentviewer/overlay/Panel');
+require('./Video');
+
+
+module.exports = exports = Ext.define('NextThought.app.video.OverlayedPanelRef', {
+	extend: 'NextThought.app.video.OverlayedPanel',
+	alias: 'widget.overlay-video-ref',
+
+	constructor: function (config) {
+		this.callParent([config]);
+	},
+
+	createPlaylistItem: function (index) {
+		const id = this.data['ntiid'];
+		let item = PlaylistItem.fromDom(this.dom);
+		item.set('sources', index[id].sources);
+		item.set('NTIID', id);
+
+		return item;
+	},
+
+	getVideo: function (bundle, content) {
+		const id = this.data['ntiid'];
+
+		return Service.getObject(id).then((video) => {
+			return {
+				[id]: video.data
+			};
+		}).catch(() => {
+			return Promise.reject('Video no longer exists');
+		});
+	},
+
+	fillVideo: function (index) {
+		var me = this,
+			id = me.data['ntiid'],
+			source = index[id].sources[0],
+			poster = source.poster,
+			label = index[id].title;
+
+		if (poster) {
+			me.setBackground(poster, label);
+		} else {
+			VideoPosters.resolveForSource(source)
+				.then(function (imgs) {
+					me.setBackground(imgs.poster, label);
+				});
+		}
+	},
+});
