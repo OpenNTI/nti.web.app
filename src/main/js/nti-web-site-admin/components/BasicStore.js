ab1e40f08f7f5bb5967d681b5bf0f4f30383a116
Andrew Ligon <andrew.ligon@nextthought.com>
2017-11-08 16:02:15 +0700

    USe the connect decorator on the router

--- src/main/js/nti-web-site-admin/components/BasicStore.js
+++ src/main/js/nti-web-site-admin/components/BasicStore.js
@@ -1,23 +1,33 @@
 import EventEmitter from 'events';
 
+import React from 'react';
 import Connector from 'nti-lib-store-connector';
 
 const Instance = Symbol('Instance');
 
-export default class UserStore extends EventEmitter {
+export default class BasicStore extends EventEmitter {
 	static getInstance () {
-		this[Instance] = this[Instance] || new UserStore();
+		const Store = this;
+
+		this[Instance] = this[Instance] || new Store();
 
 		return this[Instance];
 	}
 
-	static connect (propMap) {
+	static connect (propMap, storeProp = 'store') {
 		const store = this.getInstance();
+		const extraProps = {
+			[storeProp]: store
+		};
 
 		return function (component) {
+			function StoreConnector (props) {
+				return React.createElement(component, {...extraProps, ...props});
+			}
+
 			return Connector.connect(
 				store,
-				component,
+				StoreConnector,
 				propMap
 			);
 		};
