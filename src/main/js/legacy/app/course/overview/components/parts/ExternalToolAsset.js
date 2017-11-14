7ff00ed028e016d036b9959c4ac521a22542c23e
Sheldon Smith <sheldon.smith@nextthought.com>
2017-11-13 19:41:06 -0500

    Attempt to further subclass Card based upon ContentLink.js

--- src/main/js/legacy/app/course/overview/components/parts/ExternalToolAsset.js
+++ src/main/js/legacy/app/course/overview/components/parts/ExternalToolAsset.js
@@ -1,14 +1,31 @@
 const Ext = require('extjs');
 
+const ExternalToolAsset = require('legacy/model/ExternalToolAsset');
+
 require('legacy/common/components/cards/Card');
-require('legacy/model/ExternalToolAsset');
+
 
 module.exports = exports = Ext.define('NextThought.app.course.overview.components.parts.ExternalToolAsset', {
 	extend: 'NextThought.common.components.cards.Card',
 
+	doNotRenderIcon: true,
+
+	renderTpl: Ext.DomHelper.markup([
+		{ cls: 'thumbnail', cn: [
+			{ cls: 'icon {extension} {iconCls}', style: 'background-image: url(\'{thumbnail}\');', cn: [
+				{tag: 'label', cls: 'extension', html: '{extension}'}
+			]}
+		]},
+		{ cls: 'meta', cn: [
+			{ cls: 'title', html: '{title}' },
+			{ cls: 'byline', html: '{{{NextThought.view.cards.Card.by}}}' },
+			{ cls: 'description', html: '{description}' }
+		]}
+	]),
+
 	constructor: function () {
 		this.callParent(arguments);
 
-	}
+	},
 
 });
