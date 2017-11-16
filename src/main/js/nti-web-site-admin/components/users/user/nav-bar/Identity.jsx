b1ede8770267d62ef1c9b70cf507b7fef48b16b7
Andrew Ligon <andrew.ligon@nextthought.com>
2017-11-16 16:50:22 -0600

    Add links to the user profile and email

--- src/main/js/nti-web-site-admin/components/users/user/nav-bar/Identity.jsx
+++ src/main/js/nti-web-site-admin/components/users/user/nav-bar/Identity.jsx
@@ -2,18 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Avatar, DisplayName} from 'nti-web-commons';
 import {LinkTo} from 'nti-web-routing';
+import {scoped} from 'nti-lib-locale';
+
+const DEFAULT_TEXT = {
+	profile: 'Profile',
+	email: 'Email'
+};
+
+const t = scoped('nti-site-admin.users.user.Identity', DEFAULT_TEXT);
 
 SiteAdminUserIdentity.propTypes = {
 	user: PropTypes.object
 };
 export default function SiteAdminUserIdentity ({user}) {
+	const {email} = user;
+
 	return (
 		<div className="site-admin-user-identity">
 			<Avatar entity={user} />
 			<DisplayName entity={user} />
-			<LinkTo.Object object={user}>
-				Go to Profile
-			</LinkTo.Object>
+			<div className="actions">
+				<span className="email">
+					{email && (
+						<a href={`mailto:${email}`}>
+							{t('email')}
+						</a>
+					)}
+				</span>
+				<LinkTo.Object className="profile" object={user}>
+					<i className="icon-goto" />
+					<span>{t('profile')}</span>
+				</LinkTo.Object>
+			</div>
 		</div>
 	);
 }
