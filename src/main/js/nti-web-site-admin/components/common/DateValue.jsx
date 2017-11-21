e4af8a09bcbbc95ceb83e9966edd914c9cc7c9cc
Cory Jones <cory.jones@nextthought.com>
2017-11-21 16:00:40 -0600

    Default to 'Never' if no last login for user

--- src/main/js/nti-web-site-admin/components/common/DateValue.jsx
+++ src/main/js/nti-web-site-admin/components/common/DateValue.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import {DateTime} from 'nti-web-commons';
+import {scoped} from 'nti-lib-locale';
 
 import Card from './Card';
+
+const DEFAULT_STRINGS = {
+	'noDate': 'Never'
+};
+
+const t = scoped('nti-web-site-admin.components.common.datevalue', DEFAULT_STRINGS);
 
 SiteAdminDateValue.propTypes = {
 	className: PropTypes.string,
@@ -17,7 +24,11 @@ export default function SiteAdminDateValue ({className, label, date, format}) {
 			<div className="label">
 				{label}
 			</div>
-			<DateTime className="date" date={date} format={format} />
+			{
+				date
+					? (<DateTime className="date" date={date} format={format} />)
+					: (<div className="no-date">{t('noDate')}</div>)
+			}
 		</Card>
 	);
 }
