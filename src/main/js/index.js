dde870a331ec601677e8a4cb37f4bd6a9cb61b32
Jonathan Grimes <jsg2021@gmail.com>
2017-12-01 16:22:14 -0600

    use web-service-provided config

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -4,9 +4,11 @@ import ReactDOM from 'react-dom';
 import {addFeatureCheckClasses} from 'nti-lib-dom';
 import {ConflictResolutionHandler} from 'nti-web-commons';
 import {init as initLocale} from 'nti-lib-locale';
+import {overrideConfigAndForceCurrentHost} from 'nti-web-client';
 
 initLocale();
 addFeatureCheckClasses();
+overrideConfigAndForceCurrentHost();
 
 //once the all the app is react... this can just be a child of the root component... but for now, we'll mount it to a dummy div.
 ReactDOM.render(
