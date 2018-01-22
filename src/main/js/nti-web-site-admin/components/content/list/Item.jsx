828570a84fc8ff805dcb8dcab1d72cc08846b171
Cory Jones <cory.jones@nextthought.com>
2018-01-22 10:12:50 -0600

    Use course item component from course instead

--- src/main/js/nti-web-site-admin/components/content/list/Item.jsx
+++ src/main/js/nti-web-site-admin/components/content/list/Item.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {scoped} from 'nti-lib-locale';
-
-import CourseInfo from '../CourseInfo';
+import {CourseItem} from 'nti-web-course';
 
 const DEFAULT_TEXT = {
 	totalEnrolled: 'Total Enrolled:'
@@ -17,7 +16,7 @@ export default function SiteAdminCourseItem ({item}) {
 
 	return (
 		<div className="site-admin-course-item">
-			<CourseInfo className="course-info" catalogEntry={item.CatalogEntry} />
+			<CourseItem className="course-info" catalogEntry={item.CatalogEntry} />
 			<div className="total-enrollments">
 				<div className="label">
 					{t('totalEnrolled')}
