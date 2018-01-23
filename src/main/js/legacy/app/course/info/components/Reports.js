e5db2284ba0c93718fe11969514d37983da6f971
Andrew Ligon <andrew.ligon@nextthought.com>
2018-01-23 14:50:01 -0600

    NTI-4877 Replace the reports with the list from nti-web-reports

--- src/main/js/legacy/app/course/info/components/Reports.js
+++ src/main/js/legacy/app/course/info/components/Reports.js
@@ -1,109 +1,153 @@
 const Ext = require('extjs');
-const {wait} = require('nti-commons');
+const {getService} = require('nti-web-client');
+const {List} = require('nti-web-reports');
 
-const {getString} = require('legacy/util/Localization');
-
-
-//TODO: move this in to the strings file once we get that figured out
-const labels = {
-	'report-CourseSummaryReport.pdf': {
-		title: getString('NextThought.view.courseware.reports.View.coursetitle'),
-		description: getString('NextThought.view.courseware.reports.View.coursereport')
-	},
-	'report-SelfAssessmentSummaryReport.pdf': {
-		title: 'Self-Assessment Summary Report',
-		description: 'Viewable summary of self-assessment activity by assessment and by student.'
-	}
-};
-
-require('legacy/mixins/FillScreen');
-
+require('legacy/overrides/ReactHarness.js');
 
 module.exports = exports = Ext.define('NextThought.app.course.info.components.Reports', {
-	extend: 'Ext.Component',
+	extend: 'Ext.container.Container',
 	alias: 'widget.course-info-reports',
-
-	mixins: {
-		FilScreen: 'NextThought.mixins.FillScreen'
-	},
 
 	cls: 'course-reports',
 
-	cardTpl: new Ext.XTemplate(Ext.DomHelper.markup({
-		cls: 'report-card overview-section',
-		cn: [
-			{cls: 'description', cn: [
-				{cls: 'title', html: '{title}'},
-				{cls: 'about', html: '{about}'}
-			]},
-			{cls: 'content-card link target', 'data-link': '{link}', cn: [
-				{cls: 'thumbnail'},
-				{cls: 'meta', cn: [
-					{cls: 'sub-heading', html: '{courseNumber}'},
-					{cls: 'heading', html: '{courseName}'}
-				]}
-			]}
-		]
-	})),
+	layout: 'none',
+	items: [],
 
 
-	afterRender () {
+	initComponent () {
 		this.callParent(arguments);
-
 
 		if (this.bundle) {
 			this.setContent(this.bundle);
 		}
-
-
-		this.mon(this.el, 'click', function (e) {
-			let target = e.getTarget('.target');
-			let link = target && target.getAttribute('data-link');
-
-			if (link) {
-				Ext.widget('iframe-window', {
-					width: 'max',
-					link: link,
-					saveText: getString('NextThought.view.menus.Reports.savetext'),
-					loadingText: getString('NextThought.view.menus.Reports.loadingtext')
-				}).show();
-			}
-		});
-
-		wait()
-			.then(this.fillScreen.bind(this, this.el.dom));
-
-		this.on('activate', this.fillScreen.bind(this, this.el.dom));
 	},
 
-
 	setContent (bundle) {
-		this.bundle = bundle;
-
-		let links = bundle && bundle.getReportLinks();
-		let catalog = bundle && bundle.getCourseCatalogEntry();
-		let courseNumber = catalog && catalog.get('ProviderUniqueID');
-		let courseName = catalog && catalog.get('title');
-
-		if (!this.rendered || !links) {
+		if (this.currentBundle === bundle) {
 			return;
 		}
 
-		this.el.dom.innerHTML = '';
+		this.currentBundle = bundle;
 
-		links.forEach((link) => {
-			let rel = link.rel;
-			let label = labels[rel];
-			let title = label && label.title;
-			let description = label && label.description;
+		getService()
+			.then(service => service.getObject(bundle.rawData))
+			.then(context => {
+				this.removeAll(true);
 
-			this.cardTpl.append(this.el, {
-				title: title || '',
-				about: description || '',
-				link: link.href,
-				courseName: courseName,
-				courseNumber: courseNumber
+				this.add({
+					xtype: 'react',
+					component: List,
+					context
+				});
 			});
-		});
+
 	}
 });
+
+// const {wait} = require('nti-commons');
+// const {getString} = require('legacy/util/Localization');
+
+
+// //TODO: move this in to the strings file once we get that figured out
+// const labels = {
+// 	'report-CourseSummaryReport.pdf': {
+// 		title: getString('NextThought.view.courseware.reports.View.coursetitle'),
+// 		description: getString('NextThought.view.courseware.reports.View.coursereport')
+// 	},
+// 	'report-SelfAssessmentSummaryReport.pdf': {
+// 		title: 'Self-Assessment Summary Report',
+// 		description: 'Viewable summary of self-assessment activity by assessment and by student.'
+// 	}
+// };
+
+// require('legacy/mixins/FillScreen');
+
+
+// module.exports = exports = Ext.define('NextThought.app.course.info.components.Reports', {
+// 	extend: 'Ext.Component',
+// 	alias: 'widget.course-info-reports',
+
+// 	mixins: {
+// 		FilScreen: 'NextThought.mixins.FillScreen'
+// 	},
+
+// 	cls: 'course-reports',
+
+// 	cardTpl: new Ext.XTemplate(Ext.DomHelper.markup({
+// 		cls: 'report-card overview-section',
+// 		cn: [
+// 			{cls: 'description', cn: [
+// 				{cls: 'title', html: '{title}'},
+// 				{cls: 'about', html: '{about}'}
+// 			]},
+// 			{cls: 'content-card link target', 'data-link': '{link}', cn: [
+// 				{cls: 'thumbnail'},
+// 				{cls: 'meta', cn: [
+// 					{cls: 'sub-heading', html: '{courseNumber}'},
+// 					{cls: 'heading', html: '{courseName}'}
+// 				]}
+// 			]}
+// 		]
+// 	})),
+
+
+// 	afterRender () {
+// 		this.callParent(arguments);
+
+
+// 		if (this.bundle) {
+// 			this.setContent(this.bundle);
+// 		}
+
+
+// 		this.mon(this.el, 'click', function (e) {
+// 			let target = e.getTarget('.target');
+// 			let link = target && target.getAttribute('data-link');
+
+// 			if (link) {
+// 				Ext.widget('iframe-window', {
+// 					width: 'max',
+// 					link: link,
+// 					saveText: getString('NextThought.view.menus.Reports.savetext'),
+// 					loadingText: getString('NextThought.view.menus.Reports.loadingtext')
+// 				}).show();
+// 			}
+// 		});
+
+// 		wait()
+// 			.then(this.fillScreen.bind(this, this.el.dom));
+
+// 		this.on('activate', this.fillScreen.bind(this, this.el.dom));
+// 	},
+
+
+// 	setContent (bundle) {
+// 		this.bundle = bundle;
+
+// 		let links = bundle && bundle.getReportLinks();
+// 		let catalog = bundle && bundle.getCourseCatalogEntry();
+// 		let courseNumber = catalog && catalog.get('ProviderUniqueID');
+// 		let courseName = catalog && catalog.get('title');
+
+// 		if (!this.rendered || !links) {
+// 			return;
+// 		}
+
+// 		this.el.dom.innerHTML = '';
+
+// 		links.forEach((link) => {
+// 			let rel = link.rel;
+// 			let label = labels[rel];
+// 			let title = label && label.title;
+// 			let description = label && label.description;
+
+// 			this.cardTpl.append(this.el, {
+// 				title: title || '',
+// 				about: description || '',
+// 				link: link.href,
+// 				courseName: courseName,
+// 				courseNumber: courseNumber
+// 			});
+// 		});
+// 	}
+// });
