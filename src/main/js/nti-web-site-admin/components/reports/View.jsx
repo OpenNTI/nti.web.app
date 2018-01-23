d13bb12aed68cc42e34701a56acd4f61c0d1b226
Cory Jones <cory.jones@nextthought.com>
2018-01-23 10:36:41 -0600

    NTI-4290 Hook reports tab into report list for global context

--- src/main/js/nti-web-site-admin/components/reports/View.jsx
+++ src/main/js/nti-web-site-admin/components/reports/View.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import {getService} from 'nti-web-client';
+import {List} from 'nti-web-reports';
 
 export default class View extends React.Component {
 	constructor (props) {
 		super(props);
 		this.state = {};
+
+		this.initState();
+	}
+
+	async initState () {
+		const service = await getService();
+
+		this.setState({context: service});
 	}
 
 	renderContent () {
-		return (<div>Reports go here</div>);
+		const { context } = this.state;
+
+		if(context) {
+			return (<List context={context}/>);
+		}
 	}
 
 	render () {
