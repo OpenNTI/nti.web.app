03d5aeda7c36e1ce9d0d9cc6abd899fecd5f2148
Josh Birdwell <jbirdwell34@gmail.com>
2018-01-25 14:22:18 -0600

    Port Ext.js Roster

--- src/main/js/legacy/app/course/admin/Index.js
+++ src/main/js/legacy/app/course/admin/Index.js
@@ -12,6 +12,9 @@ require('legacy/login/StateStore');
 
 const DASHBOARD_ACTIVE = /^\/dashboard/;
 const REPORTS_ACTIVE = /^\/reports/;
+const ROSTER_ACTIVE = /^\/roster/;
+const ROSTER_ID = 'course.admin.roster';
+const ADMIN_TOOLS_ID = 'course.admin.tools';
 
 module.exports = exports = Ext.define('NextThought.app.course.admin.Index', {
 	extend: 'Ext.container.Container',
@@ -45,33 +48,61 @@ module.exports = exports = Ext.define('NextThought.app.course.admin.Index', {
 	showSiteAdmin (route) {
 		const baseroute = this.getBaseRoute();
 
-		if (this.siteAdmin) {
-			this.siteAdmin.setBaseRoute(baseroute);
-		} else {
-			this.siteAdmin = this.add({
-				xtype: 'react',
-				component: AdminTools.View,
-				baseroute: baseroute,
-				loading: true,
-				setTitle: (title) => {this.setTitle(title); }
-			});
+		if (this.siteAdminTools && !ROSTER_ACTIVE.test(route.path)) {
+			this.siteAdminRoster.hide();
+			this.siteAdminTools.show();
+			this.siteAdminTools.setBaseRoute(baseroute);	
+		} else if (this.siteAdminRoster && ROSTER_ACTIVE.test(route.path)) {
+			this.siteAdminTools.hide();
+			this.siteAdminRoster.show();
+		} else if (!this.siteAdminTools && !ROSTER_ACTIVE.test(route.path)) {
+			if(this.siteAdminRoster) {
+				this.siteAdminRoster.hide();
+			}
+			this.setupAdminTools();
+		} else if (!this.siteAdminRoster && ROSTER_ACTIVE.test(route.path)) {
+			if(this.siteAdminTools) {
+				this.siteAdminTools.hide();
+			}
+			this.setupRoster();
 		}
 
 		this.setUpNavigation(baseroute, route.path);
+	},
+
+
+	setupAdminTools () {
+		const baseroute = this.getBaseRoute();
+
+		this.siteAdminTools = this.add({
+			xtype: 'react',
+			itemId: ADMIN_TOOLS_ID,
+			component: AdminTools.View,
+			baseroute: baseroute,
+			loading: true,
+			setTitle: (title) => { this.setTitle(title); }
+		});
+
+	},
+
+
+	setupRoster () {
+		this.siteAdminRoster = this.add({
+			xtype: 'course-info-roster',
+			itemId: ROSTER_ID
+		});
 	},
 
 	async setBundle (activeBundle) {
 		this.activeBundle = activeBundle;
 		const service = await getService();
 		const course = await service.getObject(activeBundle.rawData);
-		
-		if (this.siteAdmin) {
-			this.siteAdmin.setProps({ course, loading: false });
-		}
-	},
 
-	setActiveBundle (activeBundle) {
-		
+		if (this.siteAdminRoster && this.siteAdminRoster.isVisible()) {
+			this.siteAdminRoster.setContent(activeBundle);
+		} else if (this.siteAdminTools && this.siteAdminTools.isVisible()) {
+			this.siteAdminTools.setProps({ course, loading: false });
+		}
 	},
 
 	setUpNavigation (baseroute, path) {
@@ -84,12 +115,17 @@ module.exports = exports = Ext.define('NextThought.app.course.admin.Index', {
 			{
 				text: 'Dashboard',
 				route: '/dashboard',
-				active: DASHBOARD_ACTIVE.test(path)
+				active: DASHBOARD_ACTIVE.test(path) || path === '/'
 			},
 			{
 				text: 'Reports',
 				route: '/reports',
 				active: REPORTS_ACTIVE.test(path)
+			},
+			{
+				text: 'Roster',
+				route: '/roster',
+				active: ROSTER_ACTIVE.test(path)
 			}
 		];
 
@@ -101,7 +137,7 @@ module.exports = exports = Ext.define('NextThought.app.course.admin.Index', {
 			noLibraryLink: false,
 			hideBranding: true,
 			onBack: () => {
-				this.pushRootRoute('Library', '/library');
+				this.pushRootRoute(baseroute, '/');
 			}
 		});
 	},
