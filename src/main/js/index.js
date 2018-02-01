3b48b49d890b9f61d7c24b7bbf414aa1bb8c9241
Jonathan Grimes <jsg2021@gmail.com>
2018-02-01 10:51:11 -0600

    wire in update notification

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -2,15 +2,20 @@ import 'legacy'; //kick start the app (the core is still defined in extjs)
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {addFeatureCheckClasses} from 'nti-lib-dom';
-import {ConflictResolutionHandler} from 'nti-web-commons';
+import {ConflictResolutionHandler, Updates} from 'nti-web-commons';
 import {init as initLocale} from 'nti-lib-locale';
-import {overrideConfigAndForceCurrentHost} from 'nti-web-client';
+import {overrideConfigAndForceCurrentHost, getConfigFor} from 'nti-web-client';
 
 initLocale();
 addFeatureCheckClasses();
 overrideConfigAndForceCurrentHost();
 
+const basePath = (x => (x = getConfigFor(x), typeof x === 'string' ? x : '/'))('basepath');
+
 //once the all the app is react... this can just be a child of the root component... but for now, we'll mount it to a dummy div.
 ReactDOM.render(
 	<ConflictResolutionHandler/>,
+	document.createElement('div'));
+ReactDOM.render(
+	<Updates.Monitor baseUrl={basePath}/>,
 	document.createElement('div'));
