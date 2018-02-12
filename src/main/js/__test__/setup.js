7dd37edd2a33c5d65425167b642ea7847a4b7b05
Jonathan Grimes <jsg2021@gmail.com>
2018-02-12 13:02:56 -0600

    update enzyme to get ride of warnings

--- src/main/js/__test__/setup.js
+++ src/main/js/__test__/setup.js
@@ -1,4 +1,8 @@
 /*eslint-disable*/
 require('raf/polyfill');
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+Enzyme.configure({ adapter: new Adapter() });
+
 global.Ext = require('extjs');
 require('../legacy/model/index');
