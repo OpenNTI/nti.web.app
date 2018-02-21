dda90b588885e74dbe412fd905d1da79923d4002
Cory Jones <cory.jones@nextthought.com>
2018-02-21 12:09:14 -0600

    NTI-5128 Stub out appearance for required selection on overview

--- src/main/js/legacy/app/course/overview/components/editing/content/questionset/ListItem.js
+++ src/main/js/legacy/app/course/overview/components/editing/content/questionset/ListItem.js
@@ -1,11 +1,16 @@
 const Ext = require('extjs');
+const {SelectBox} = require('nti-web-commons');
 
 const QuestionSetRef = require('legacy/model/QuestionSetRef');
 const AssignmentRef = require('legacy/model/AssignmentRef');
 
+require('legacy/overrides/ReactHarness');
 require('../../../parts/QuestionSet');
 require('../ListItem');
 
+const REQUIRED = 'Required';
+const OPTIONAL = 'Optional';
+const DEFAULT = 'Default';
 
 module.exports = exports = Ext.define('NextThought.app.course.overview.components.editing.content.questionset.ListItem', {
 	extend: 'NextThought.app.course.overview.components.editing.content.ListItem',
@@ -19,6 +24,38 @@ module.exports = exports = Ext.define('NextThought.app.course.overview.component
 				AssignmentRef.mimeType
 			];
 		}
+	},
+
+	getPreControls: function (record, bundle) {
+		const onChange = (value) => {
+			// TODO: do something with value, either REQUIRED, OPTIONAL, or DEFAULT
+			// once the server API is available
+		};
+
+		return [{
+			xtype: 'react',
+			component: SelectBox,
+			value: DEFAULT,	// TODO: pull the actual value from the record
+			onChange,
+			options: [
+				{ label: REQUIRED, value: REQUIRED },
+				{ label: OPTIONAL, value: OPTIONAL},
+				{ label: DEFAULT, value: DEFAULT}
+			]
+		}];
+	},
+
+	getControls: function (record, bundle) {
+		var controls = [this.callParent(arguments)];
+
+		// need a wrapper to help properly position the controls with
+		// the required select box with wrapper-specific styling
+		return {
+			xtype: 'container',
+			cls: 'controls-wrapper',
+			layout: 'none',
+			items: controls
+		};
 	},
 
 	updateRecord: function (record) {
