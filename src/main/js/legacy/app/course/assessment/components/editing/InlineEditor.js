b63880a51f9679fc7b774c7506fdb49892e9a36e
Cory Jones <cory.jones@nextthought.com>
2018-04-16 16:41:21 -0500

    Implement new assignment inline editor for assignment list

--- src/main/js/legacy/app/course/assessment/components/editing/InlineEditor.js
+++ src/main/js/legacy/app/course/assessment/components/editing/InlineEditor.js
@@ -1,15 +1,7 @@
 const Ext = require('extjs');
-
-const Globals = require('legacy/util/Globals');
-
-const EditingActions = require('../../../editing/Actions');
-
-const PublishState = require('./components/PublishState');
-const DueDate = require('./components/DueDate');
-const ResetMenu = require('./components/Reset');
+const {InlineEditor} = require('nti-assignment-editor');
 
 require('legacy/common/form/fields/DateTimeField');
-
 
 module.exports = exports = Ext.define('NextThought.app.course.assessment.components.editing.InlineEditor', {
 	extend: 'Ext.container.Container',
@@ -22,46 +14,32 @@ module.exports = exports = Ext.define('NextThought.app.course.assessment.compone
 		this.callParent(arguments);
 
 		const {assignment} = this;
-		const clearError = () => this.clearError();
 
-		this.EditingActions = EditingActions.create();
-
-		this.add([
-			{xtype: 'container', isContents: true, layout: 'none', cls: 'contents', items: [
-				new PublishState({assignment, clearError, isPublishEditor: true}),
-				new ResetMenu({assignment, isResetMenu: true, onReset: () => this.onReset(), beforeReset: () => this.beforeReset()}),
-				new DueDate({assignment, clearError, isDueDateEditor: true}),
+		assignment.getInterfaceInstance().then(assignmentModel => {
+			this.add([
 				{
-					xtype: 'box',
-					isError: true,
-					autoEl: {
-						cls: 'error'
-					}
-				}
-			]},
-			{
-				xtype: 'box',
-				autoEl: {
-					cls: 'footer',
-					cn: [
-						{cls: 'save', html: 'Save'},
-						{cls: 'cancel', html: 'Cancel'}
-					]
-				},
-				listeners: {
-					click: {
-						element: 'el',
-						fn: (e) => {
-							if (e.getTarget('.cancel')) {
-								this.doCancel();
-							} else if (e.getTarget('.save')) {
-								this.doSave();
+					xtype: 'react',
+					component: InlineEditor,
+					assignment: assignmentModel,
+					assignmentRef: {
+						NTIID: assignment.get('NTIID')
+					},
+					onDismiss: async (savedData) => {
+						if(savedData) {
+							await this.assignment.updateFromServer();
+
+							//this.setAssignment(this.assignment);
+
+							if (this.onSave) {
+								this.onSave();
 							}
 						}
+
+						this.doCancel();
 					}
 				}
-			}
-		]);
+			]);
+		});
 	},
 
 
@@ -70,116 +48,9 @@ module.exports = exports = Ext.define('NextThought.app.course.assessment.compone
 	},
 
 
-	getPublishEditor () {
-		return this.down('[isPublishEditor]');
-	},
-
-
-	getDueDateEditor () {
-		return this.down('[isDueDateEditor]');
-	},
-
-
-	getResetMenu () {
-		return this.down('[isResetMenu]');
-	},
-
-
-	setAssignment (assignment) {
-		this.assignment = assignment;
-
-		const publishEditor = this.getPublishEditor();
-		const dueDateEditor = this.getDueDateEditor();
-		const resetMenu = this.getResetMenu();
-
-		resetMenu.setAssignment(assignment);
-		publishEditor.setAssignment(assignment);
-		dueDateEditor.setAssignment(assignment);
-	},
-
-
-	beforeReset () {
-		this.addSavingMask();
-	},
-
-
-	onReset () {
-		this.setAssignment(this.assignment);
-		this.removeSavingMask();
-	},
-
-
 	doCancel () {
-		const {assignment} = this;
-		const publishEditor = this.getPublishEditor();
-		const dueDateEditor = this.getDueDateEditor();
-
 		if (this.onCancel) {
 			this.onCancel();
 		}
-
-		publishEditor.setAssignment(assignment);
-		dueDateEditor.setAssignment(assignment);
-	},
-
-	showError: function (msg) {
-		var cmp = this.down('[isError]');
-
-		if (cmp && cmp.el) {
-			cmp.el.update(msg || 'Unable to update dates');
-		}
-	},
-
-	clearError: function () {
-		var cmp = this.down('[isError]');
-
-		if (cmp && cmp.el) {
-			cmp.el.update('');
-		}
-	},
-
-	addSavingMask: function () {
-		if (this.el) {
-			this.el.mask('Saving...');
-		}
-	},
-
-	removeSavingMask: function () {
-		if (this.el) {
-			this.el.unmask();
-		}
-	},
-
-	doSave: function () {
-		const publishEditor = this.getPublishEditor();
-		const dueDateEditor = this.getDueDateEditor();
-		const publishValid = publishEditor.validate();
-		const dueDateValid = dueDateEditor.validate();
-
-		if (!publishValid || !dueDateValid) {
-			return;
-		}
-
-		const publishValues = publishEditor.getValues();
-		const dueDateValues = dueDateEditor.getValues();
-
-		this.addSavingMask();
-
-		this.EditingActions.updateAssignment(this.assignment, publishValues, dueDateValues)
-			.then(Promise.minWait(Globals.WAIT_TIMES.SHORT))
-			.then(() => {
-				this.setAssignment(this.assignment);
-
-				if (this.onSave) {
-					this.onSave();
-				}
-			})
-			.catch((reason) => {
-				var error = Globals.parseError(reason);
-
-				this.setAssignment(this.assignment);
-				this.showError(error && error.message);
-			})
-			.always(() => this.removeSavingMask());
 	}
 });
