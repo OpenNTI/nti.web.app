0d28b423879d64a3fcd87956b6c6e013715587ed
Josh Birdwell <jbirdwell34@gmail.com>
2018-04-26 18:48:43 -0500

    NTI-5337: add videos to topic comments

--- src/main/js/legacy/app/blog/parts/old/Topic.js
+++ src/main/js/legacy/app/blog/parts/old/Topic.js
@@ -271,7 +271,7 @@ module.exports = exports = Ext.define('NextThought.app.blog.parts.old.Topic', {
 		if (this.replyLinkEl) {
 			box.setVisibilityMode(Ext.dom.Element.DISPLAY);
 
-			this.editor = Ext.widget('nti-editor', {ownerCt: this, renderTo: this.commentEditorBox, enableFileUpload: true});
+			this.editor = Ext.widget('nti-editor', {ownerCt: this, renderTo: this.commentEditorBox, enableFileUpload: true, enableVideo: true});
 			this.mon(this.replyLinkEl, 'click', this.showEditor, this);
 			this.mon(this.editor, {
 				scope: this,
