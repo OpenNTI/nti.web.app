82bb7cfce0fcaf0515225c57d1769e86bcdca236
Josh Birdwell <jbirdwell34@gmail.com>
2018-06-28 20:21:42 -0500

    Fix capitalize and link icon for new content types

--- src/main/js/legacy/app/course/overview/components/editing/content/overviewgroup/ChildCreation.js
+++ src/main/js/legacy/app/course/overview/components/editing/content/overviewgroup/ChildCreation.js
@@ -17,7 +17,7 @@ require('../../creation/ChildCreation');
 module.exports = exports = Ext.define('NextThought.app.course.overview.components.editing.content.overviewgroup.ChildCreation', {
 	extend: 'NextThought.app.course.overview.components.editing.creation.ChildCreation',
 	alias: 'widget.overview-editing-overviewgroup-childcreation',
-	title: 'Choose a content type',
+	title: 'Choose a Content Type',
 	backText: 'Content Types',
 	saveText: 'Add to Lesson',
 
