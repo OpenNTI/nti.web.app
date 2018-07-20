7a64e0c690502b70ba7c595f4106de67663b971e
Cory Jones <cory.jones@nextthought.com>
2018-07-19 20:33:05 -0500

    Add empty state to integrations tab

--- src/main/js/nti-web-site-admin/components/advanced/integrations/View.jsx
+++ src/main/js/nti-web-site-admin/components/advanced/integrations/View.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 // import {List} from '@nti/web-integrations';
+import {EmptyState} from '@nti/web-commons';
+import {scoped} from '@nti/lib-locale';
 
 import Card from '../../common/Card';
 
-const List = 'div';
+const t = scoped('web-site-admin.components.advanced.integrations.View', {
+	emptyHeader: 'There are no integrations'
+});
+
+const List = EmptyState;
 
 export default function SiteAdminAdvancedIntegrations () {
 	return (
 		<Card>
-			<List />
+			<List header={t('emptyHeader')}/>
 		</Card>
 	);
 }
