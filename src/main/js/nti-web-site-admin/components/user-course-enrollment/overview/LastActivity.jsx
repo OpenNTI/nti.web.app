21847d084f4650bae1332c9d30d9c7f2a13eb54b
Cory Jones <cory.jones@nextthought.com>
2018-09-06 10:48:12 -0500

    Use CourseInstanceEnrollment's LastSeenTime instead of analytics call

--- src/main/js/nti-web-site-admin/components/user-course-enrollment/overview/LastActivity.jsx
+++ src/main/js/nti-web-site-admin/components/user-course-enrollment/overview/LastActivity.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {getService} from '@nti/web-client';
 
 import DateValue from '../../common/DateValue';
-
-const ANALYTICS_LINK = 'analytics';
 
 export default class LastActivity extends React.Component {
 	static propTypes = {
@@ -17,28 +14,9 @@ export default class LastActivity extends React.Component {
 		this.state = {};
 	}
 
-	componentDidMount () {
-		this.loadData();
-	}
-
-	async loadData () {
-		const { enrollment } = this.props;
-
-		const service = await getService();
-		const analyticsLink = enrollment.Links.filter(x => x.rel === ANALYTICS_LINK)[0];
-		const results = await service.get(analyticsLink.href) || {};
-		const events = results.Items.filter(x => x.Title === 'Events')[0];
-		const eventData = await service.get(events.href);
-
-		this.setState({data: (eventData && eventData.Items) || []});
-	}
-
 	render () {
-		const {data} = this.state;
-		const firstItem = data && data[0];
-		const date = (firstItem || {}).Timestamp || 0;
-		const dateInSeconds = date * 1000;
+		const {enrollment} = this.props;
 
-		return <DateValue date={dateInSeconds} label="Last Activity"/>;
+		return <DateValue date={enrollment.getLastSeenTime()} label="Last Active"/>;
 	}
 }
