da4f562423bbe0c5efd70cb7a083450209cccd72
Andrew Ligon <andrew.ligon@nextthought.com>
2018-09-27 21:34:55 -0500

    remove library search feature flag

--- src/main/js/legacy/app/library/Index.js
+++ src/main/js/legacy/app/library/Index.js
@@ -1,7 +1,1 @@
-const {isFeature} = require('legacy/util/Globals');
-
-if (isFeature('library-searchable')) {
-	require('./LibrarySearchable');
-} else {
-	require('./Library');
-}
+require('./LibrarySearchable');
