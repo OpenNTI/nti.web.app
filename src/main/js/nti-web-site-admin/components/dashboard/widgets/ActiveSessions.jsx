3c9f5b58ad9865efef3bef2368214e85644665b4
Cory Jones <cory.jones@nextthought.com>
2018-10-16 09:01:04 -0500

    NTI-6816 Add active sessions error handling, unavailable state

--- src/main/js/nti-web-site-admin/components/dashboard/widgets/ActiveSessions.jsx
+++ src/main/js/nti-web-site-admin/components/dashboard/widgets/ActiveSessions.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import {NumericValue, UpdateWithFrequency} from '@nti/web-charts';
 import {getService} from '@nti/web-client';
 import {getLink} from '@nti/lib-interfaces';
+import {scoped} from '@nti/lib-locale';
 
 const SESSIONS = 'Sessions';
 const ANALYTICS = 'Analytics';
 const ACTIVE_SESSION_COUNT = 'active_session_count';
+
+const LABELS = {
+	notAvailable: 'Unable to get active session data'
+};
+
+const t = scoped('nti-web-site-admin.components.dashboard.widgets.ActiveSessions', LABELS);
 
 export default class ActiveSessions extends React.Component {
 	constructor (props) {
@@ -15,10 +22,17 @@ export default class ActiveSessions extends React.Component {
 
 	async getActiveSessions () {
 		const service = await getService();
-		const sessionsCollection = service.getCollection(SESSIONS, ANALYTICS);
-		const link = getLink(sessionsCollection, ACTIVE_SESSION_COUNT);
-		const stats = await service.get(link);
-		return {value: stats.Count};
+
+		try {
+			const sessionsCollection = service.getCollection(SESSIONS, ANALYTICS);
+			const link = getLink(sessionsCollection, ACTIVE_SESSION_COUNT);
+			const stats = await service.get(link);
+
+			return {value: stats.Count};
+		}
+		catch (e) {
+			return {value: (<div className="not-available">{t('notAvailable')}</div>)};
+		}
 	}
 
 	render () {
