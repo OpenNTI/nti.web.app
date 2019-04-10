14295d0a8da8f1d15e7a20f2e6f0ded4e57a6cf9
Ray Hatfield <ray.hatfield@nextthought.com>
2019-04-10 14:08:50 -0500

    NTI-7711 - flesh out webinar sidebar

--- src/main/js/nti-web-app-lesson-items/overrides/webinar/Sidebar.jsx
+++ src/main/js/nti-web-app-lesson-items/overrides/webinar/Sidebar.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
+import {Event} from '@nti/web-calendar';
 import {Overview} from '@nti/web-course';
-
-const Button = Overview.Items.Webinar.RegistrationButton;
+import {scoped} from '@nti/lib-locale';
 
 import styles from './Sidebar.css';
 
 const cx = classnames.bind(styles);
+const Button = Overview.Items.Webinar.RegistrationButton;
+
+const t = scoped('lessonitems.events.webinar.sidebar', {
+	instructions: {
+		unregistered: 'Click below to register for this webinar.',
+		registered: 'Click below to join this webinar.',
+		expired: 'No longer available.',
+		unknown: ''
+	}
+});
+
+const keyForStatus = state => ((/^(registered|unregistered|expired)/i).exec(state) || ['unknown'])[0];
+const textForStatus = state => t(['instructions', keyForStatus(state)]);
 
 export default class WebinarSidebar extends React.Component {
 
@@ -15,12 +28,44 @@ export default class WebinarSidebar extends React.Component {
 		item: PropTypes.object.isRequired
 	}
 
+	state = {}
+
+	onStatusChange = status => {
+		this.setState({status});
+	}
+
 	render () {
-		const {item: {webinar}} = this.props;
+		const {
+			props: {
+				item: {
+					webinar,
+					completed,
+					expired
+				}
+			},
+			state: {status}
+		} = this;
+
+		const nearestSession = webinar.getNearestSession();
+		const startTime = nearestSession.getStartTime();
+		const endTime = nearestSession.getEndTime();
 
 		return (
 			<div className={cx('webinar-sidebar')}>
-				<Button webinar={webinar} />
+				<div className={cx('details')}>
+					<div className={cx('instructions')}>{textForStatus(status)}</div>
+					<Event.Availability
+						{...{
+							startTime,
+							endTime,
+							completed,
+							expired,
+							minimal: true,
+							className:  cx('availability')
+						}}
+					/>
+				</div>
+				<Button className={cx('registration-button')} webinar={webinar} onStatusChange={this.onStatusChange} />
 			</div>
 		);
 	}
