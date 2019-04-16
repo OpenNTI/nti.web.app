f2c2a80ba7a6b95cadc1b6a6cf521788c31b566f
Ray Hatfield <ray.hatfield@nextthought.com>
2019-04-16 11:41:18 -0500

    NTI-7814 - handle unavailable discussion topics

--- src/main/js/nti-web-app-lesson-items/overrides/topic/View.jsx
+++ src/main/js/nti-web-app-lesson-items/overrides/topic/View.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
 import {Loading, Layouts} from '@nti/web-commons';
 import {rawContent} from '@nti/lib-commons';
+import {scoped} from '@nti/lib-locale';
 
 import TopicViewer from 'legacy/app/forums/components/topic/Window';
 
@@ -13,6 +14,9 @@ import Store from './Store';
 import ActiveUsers from './ActiveUsers';
 
 const cx = classnames.bind(Styles);
+const t = scoped('NTIWebAppLessonItems.overrides.topic', {
+	unavailable: 'This topic is unavailable'
+});
 
 const {Aside} = Layouts;
 
@@ -32,7 +36,7 @@ const handles = (obj) => {
 
 export default
 @Registry.register(handles)
-@Store.connect(['loading', 'error', 'topicModel', 'activeUsers'])
+@Store.connect(['loading', 'error', 'topicModel', 'topicRef', 'activeUsers'])
 class NTIWebAppLessonItemsTopic extends React.Component {
 	static deriveBindingFromProps (props) {
 		const {location = {}} = props;
@@ -52,6 +56,7 @@ class NTIWebAppLessonItemsTopic extends React.Component {
 		loading: PropTypes.bool,
 		error: PropTypes.any,
 		topicModel: PropTypes.object,
+		topicRef: PropTypes.object,
 		activeUsers: PropTypes.array
 	}
 
@@ -108,21 +113,46 @@ class NTIWebAppLessonItemsTopic extends React.Component {
 	}
 
 	render () {
-		const {loading, activeUsers} = this.props;
+		const {loading, activeUsers, error} = this.props;
 
 		return (
-			<div className={cx('topic-viewe')}>
+			<div className={cx('topic-view')}>
 				<Aside component={ActiveUsers} activeUsers={activeUsers} />
-				{loading && (
-					<div className={cx('loading-container')}>
-						<Loading.Spinner.Large />
-					</div>
-				)}
-				{!loading && this.renderTopic()}
+				{
+					error
+						? this.renderError()
+						: loading
+							? this.renderLoading()
+							: this.renderTopic()
+				}
 			</div>
 		);
 	}
 
+	renderLoading () {
+		return (
+			<div className={cx('loading-container')}>
+				<Loading.Spinner.Large />
+			</div>
+		);
+	}
+
+	renderError () {
+		const {error, topicRef: {icon, title} = {}} = this.props;
+		const is404 = error && error.statusCode === 404;
+
+		return (
+			<div className={cx('error', {'is-404': is404})}>
+				{icon && (
+					<div className={cx('icon')} style={{backgroundImage: `url(${icon})`}} />
+				)}
+				{title && (
+					<div className={cx('title')}>{title}</div>
+				)}
+				<div className={cx('message')}>{t('unavailable')}</div>
+			</div>
+		);
+	}
 
 	renderTopic () {
 		return (
