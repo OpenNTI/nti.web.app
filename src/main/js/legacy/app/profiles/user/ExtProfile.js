d438364daa7c62c10ec261e0a06b0c9f6fb069a0
Andrew Ligon <andrew.ligon@nextthought.com>
2019-07-25 14:52:52 -0500

    NTI-8145 clean up use-react-profile feature flag

--- src/main/js/legacy/app/profiles/user/ExtProfile.js
+++
@@ -1,8 +0,0 @@
-const Ext = require('@nti/extjs');
-
-require('./BaseExtProfile');
-
-module.exports = exports = Ext.define('NextThought.app.profiles.user.Index', {
-	extend: 'NextThought.app.profiles.user.Base',
-	alias: 'widget.profile-user',
-});
