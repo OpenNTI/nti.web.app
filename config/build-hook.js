b5ddb7ebb9be5ad5d13723c930c0c35c02452c29
Jonathan Grimes <jsg2021@gmail.com>
2019-08-20 14:16:45 -0500

    update to use sass instead of node-sass

--- config/build-hook.js
+++ config/build-hook.js
@@ -8,6 +8,7 @@ const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 
 const CSS = path.resolve(paths.assetsRoot, 'resources/css');
 const SCSS = path.resolve(paths.assetsRoot, 'resources/scss');
+const f = x => [`${SCSS}/${x}.scss`, `${CSS}/${x}.css`];
 
 // Warn and crash if required files are missing
 if (!checkRequiredFiles([SCSS])) {
@@ -21,6 +22,8 @@ fs.removeSync(path.resolve(paths.assetsRoot, 'resources/images/sprite.png'));
 //@spritesmith
 call('spritesmith');
 // @node-sass $(SRC)main/resources/scss -o $(SRC)main/resources/css
-call('node-sass', [SCSS, '-o', CSS]);
+call('sass', f`accessibility`);
+call('sass', f`legacy`);
+call('sass', f`nti-override`);
 // @postcss --use autoprefixer -r $(SRC)main/resources/css/*.css
 call('postcss', ['--use', 'autoprefixer', '-r', CSS + '/*.css']);
