d106e92a287f0c3ea246d98bfb16b04e011d1563
Andrew Ligon <andrew.ligon@nextthought.com>
2019-09-03 14:44:38 -0500

    checkpoint for unifying the site community and course community page

--- src/main/js/legacy/app/profiles/community/Index.js
+++ src/main/js/legacy/app/profiles/community/Index.js
@@ -1,16 +1,15 @@
 const Ext = require('@nti/extjs');
-const { encodeForURI, decodeFromURI } = require('@nti/lib-ntiids');
+const { encodeForURI } = require('@nti/lib-ntiids');
+const {Community} = require('@nti/web-profiles');
 
+const CommunityOverrides = require('nti-web-community-overrides');
+const NavigationActions = require('legacy/app/navigation/Actions');
 const lazy = require('legacy/util/lazy-require')
 	.get('ParseUtils', ()=> require('legacy/util/Parsing'));
-const NavigationActions = require('legacy/app/navigation/Actions');
+
+require('legacy/overrides/ReactHarness');
 
 require('legacy/mixins/Router');
-require('./components/activity/Index');
-require('./components/membership/Index');
-require('./components/sidebar/Index');
-require('./components/Header');
-
 
 module.exports = exports = Ext.define('NextThought.app.profiles.community.Index', {
 	extend: 'Ext.container.Container',
@@ -20,127 +19,42 @@ module.exports = exports = Ext.define('NextThought.app.profiles.community.Index'
 		Router: 'NextThought.mixins.Router'
 	},
 
-	cls: 'community-profile profile',
+	cls: `${CommunityOverrides.viewClassName}`,
 	layout: 'none',
+	items: [],
 
-	initComponent: function () {
+	initComponent () {
 		this.callParent(arguments);
-
 		this.NavActions = NavigationActions.create();
-
-		this.headerCmp = this.add({
-			xtype: 'profile-community-header',
-			joinCommunity: this.joinCommunity.bind(this),
-			leaveCommunity: this.leaveCommunity.bind(this),
-			showCommunity: this.showCommunity.bind(this),
-			hideCommunity: this.hideCommunity.bind(this)
-		});
-
-		this.bodyCmp = this.add({
-			xtype: 'container',
-			cls: 'community-body',
-			layout: 'card'
-		});
-
-
-		this.sidebarCmp = this.add({
-			xtype: 'profile-community-sidebar',
-			showForum: this.onShowForum.bind(this),
-			gotoMembership: this.gotoMembership.bind(this)
-		});
 
 		this.initRouter();
 
-		this.addRoute('/', this.showAllActivity.bind(this));
-		this.addRoute('/topic/:id', this.showTopicActivity.bind(this));
-		this.addRoute('/members', this.showMembers.bind(this));
-
-		this.addDefaultRoute('/');
-
-		this.on({
-			activate: this.onActivate.bind(this),
-			deactivate: this.onDeactivate.bind(this)
-		});
+		this.addDefaultRoute(this.showCommunity.bind(this));
 	},
 
-	getContext: function () {
+	getContext () {
 		return this.activeCommunity;
 	},
 
-	onActivate: function () {
-		var cmp = this.bodyCmp.getLayout().getActiveItem();
+	setActiveEntity (id) {
+		const url = Service.getResolveUserURL(id);
 
-		if (cmp) {
-			cmp.fireEvent('activate');
+		if (this.activeCommunity && this.activeCommunity.getId() === id) {
+			return Promise.resolve(this.activeCommunity);
 		}
 
-		this.headerCmp.fireEvent('activate');
-	},
-
-	onDeactivate: function () {
-		var cmp = this.bodyCmp.getLayout().getActiveItem();
-
-		if (cmp) {
-			cmp.fireEvent('deactivate');
-		}
-
-		this.headerCmp.fireEvent('deactivate');
-	},
-
-	getRouteTitle: function () {
-		return this.activeCommunity ? this.activeCommunity.getName() : '';
-	},
-
-	setActiveEntity: function (id, entity) {
-		var me = this,
-			url = Service.getResolveUserURL(id);
-
-		if (me.activeCommunity && me.activeCommunity.getId() === id) {
-			return Promise.resolve(me.activeCommunity);
+		if (this.communityCmp) {
+			this.communityCmp.destroy();
+			delete this.communityCmp;
 		}
 
 		return Service.request(url)
-			.then(function (response) {
-				var json = JSON.parse(response) || {};
-
-				return lazy.ParseUtils.parseItems(json.Items)[0];
-			})
-			.then(function (community) {
-				me.activeCommunity = community;
-
-				//Call this with force to reload the forums incase there are any new ones
-				me.activeCommunity.getForumList(true);
-				return community;
-			});
+			.then(resp => lazy.ParseUtils.parseItems(JSON.parse(resp) || {})[0])
+			.then(community => this.activeCommunity = community);
 	},
 
-	setActiveItem: function (xtype) {
-		var cmp = this.down(xtype),
-			current = this.bodyCmp.getLayout().getActiveItem();
 
-		if (!cmp) {
-			cmp = this.bodyCmp.add(Ext.widget(xtype));
-
-			this.addChildRouter(cmp);
-		}
-
-		this.bodyCmp.getLayout().setActiveItem(cmp);
-
-		if (cmp && !current) {
-			cmp.fireEvent('activate');
-		}
-
-		return cmp;
-	},
-
-	setState: function (state) {
-		state = state || {};
-
-		this.activeState = state;
-
-		this.headerCmp.updateEntity(this.activeCommunity);
-		this.sidebarCmp.updateEntity(this.activeCommunity, state.activeTopic);
-
+	updateNavigation () {
 		this.NavActions.updateNavBar({
 			hideBranding: true
 		});
@@ -148,206 +62,30 @@ module.exports = exports = Ext.define('NextThought.app.profiles.community.Index'
 		this.NavActions.setActiveContent(this.activeCommunity);
 	},
 
-	showAllActivity: function (route, subRoute) {
-		var cmp = this.setActiveItem('profile-community-activity'),
-			link = this.activeCommunity.getLink('Activity');
 
-		cmp.setSourceURL(link, true);
+	async showCommunity () {
+		const community = await this.activeCommunity.getInterfaceInstance();
+		const baseroute = this.getBaseRoute();
 
-		this.activeCommunity.getDefaultForum()
-			.then(cmp.setPostContainer.bind(cmp));
-
-		this.setState({
-			activeTopic: 'all'
-		});
-
-		return cmp.userChanged(this.activeCommunity)
-			.then(cmp.handleRoute.bind(cmp, subRoute, route.precache));
-	},
-
-	showTopicActivity: function (route, subRoute) {
-		var me = this,
-			cmp = me.setActiveItem('profile-community-activity'),
-			entity = me.activeCommunity,
-			id = route.params.id, forum = route.precache.forum || cmp.getActiveForum();
-
-		id = decodeFromURI(id);
-
-		if (forum && forum.getId() === id) {
-			cmp.setSourceURL(forum.getLink('contents'));
-			cmp.setPostContainer(forum);
-		} else {
-			cmp.setSourceURL(null);
-			cmp.setPostContainer(null);
-
-			entity.getForums()
-				.then(function (topics) {
-					var current, i;
-
-					for (i = 0; i < topics.length; i++) {
-						let topic = topics[i];
-
-						if (topic && topic.getId && topic.getId() === id) {
-							current = topic;
-							break;
-						}
-					}
-
-					if (!current) {
-						return Promise.reject();
-					}
-
-					return current;
-				})
-				.catch(function (reason) {
-					console.error('failed to load forum: ', reason);
-
-					cmp.setSourceURL(entity.getLink('Activity'));
-					me.setState();
-
-					entity.getDefaultForum()
-						.then(cmp.setPostContainer.bind(cmp));
-				})
-				.then(function (f) {
-					var link = f.getLink('contents');
-
-					cmp.setSourceURL(link);
-
-					cmp.setPostContainer(f);
-				});
+		if (!community) {
+			throw new Error('Unable to find community.');
 		}
 
-		me.setState({
-			activeTopic: id
-		});
+		this.updateNavigation();
 
-		return cmp.userChanged(me.activeCommunity)
-			.then(cmp.handleRoute.bind(cmp, subRoute, route.precache));
-	},
-
-	showMembers: function (route, subRoute) {
-		var cmp = this.setActiveItem('profile-community-membership');
-
-		this.setState();
-		cmp.updateEntity(this.activeCommunity);
-
-		return cmp.handleRoute(subRoute, route.precache);
-	},
-
-	updateCommunity: function () {
-		this.setState(this.activeState);
-		if (this.activeCommunity) {
-			this.headerCmp.updateEntity(this.activeCommunity);
-		}
-
-		//TODO: reload what ever is active on the body
-	},
-
-	gotoMembership: function () {
-		this.pushRoute('Members', '/members');
-	},
-
-	onShowForum: function (forum) {
-		if (forum === 'all') {
-			this.pushRoute('Activity', '/');
-			return;
-		}
-
-		var id = forum.getId();
-
-		id = encodeForURI(id);
-
-		this.pushRoute('', '/topic/' + id, {
-			forum: forum
-		});
-	},
-
-	joinCommunity: function () {
-		var me = this,
-			join, link = me.activeCommunity.getLink('join');
-
-		if (!link) {
-			join = Promise.reject('No Link');
-		} else {
-			join = Service.post(link);
-		}
-
-		join
-			.then(function (response) {
-				me.activeCommunity = lazy.ParseUtils.parseItems(response)[0];
-				me.updateCommunity();
-				me.replaceRoute('Activity', '/');
-			})
-			.catch(function (reason) {
-				console.error('Error joining community: ', reason);
-				alert('Unable to join community at this time.');
+		if (!this.communityCmp) {
+			this.communityCmp = this.add({
+				xtype: 'react',
+				component: Community.View,
+				overrides: CommunityOverrides.Overrides,
+				topicWindowClassName: CommunityOverrides.topicWindowClassName,
+				community,
+				baseroute,
+				setTitle: (title) => this.setTitle(title)
 			});
-
-	},
-
-	leaveCommunity: function () {
-		var me = this,
-			leave, link = me.activeCommunity.getLink('leave');
-
-		if (!link) {
-			leave = Promise.reject('No Link');
-		} else {
-			leave = Service.post(link);
 		}
-
-		leave
-			.then(function (response) {
-				me.activeCommunity = lazy.ParseUtils.parseItems(response)[0];
-				me.updateCommunity();
-				me.replaceRoute('Activity', '/');
-			})
-			.catch(function (reason) {
-				console.error('Error leaving community: ', reason);
-				alert('Unable to leave community at this time.');
-			});
 	},
 
-	showCommunity: function () {
-		var me = this,
-			show, link = me.activeCommunity.getLink('unhide');
-
-		if (!link) {
-			show = Promise.reject('No Link');
-		} else {
-			show = Service.post(link);
-		}
-
-		show
-			.then(function (response) {
-				me.activeCommunity = lazy.ParseUtils.parseItems(response)[0];
-				me.updateCommunity();
-			})
-			.catch(function (reason) {
-				console.error('Error hiding community: ', reason);
-				alert('Unable to show community at this time.');
-			});
-	},
-
-	hideCommunity: function () {
-		var me = this,
-			hide, link = me.activeCommunity.getLink('hide');
-
-		if (!link) {
-			hide = Promise.reject('No Link');
-		} else {
-			hide = Service.post(link);
-		}
-
-		hide
-			.then(function (response) {
-				me.activeCommunity = lazy.ParseUtils.parseItems(response)[0];
-				me.updateCommunity();
-			})
-			.catch(function (reason) {
-				console.error('Error hiding community: ', reason);
-				alert('Unable to hide community at this time.');
-			});
-	},
 
 	getRouteForPath: function (path, community) {
 		const [/*index*/, forum] = path;
