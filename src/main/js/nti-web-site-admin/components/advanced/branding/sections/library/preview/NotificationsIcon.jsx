4ff023fe01f25bba090664ca076b91dc80e23f3d
Ray Hatfield <ray.hatfield@gmail.com>
2019-11-06 18:08:10 -0600

    consult theme.icon for style

--- src/main/js/nti-web-site-admin/components/advanced/branding/sections/library/preview/NotificationsIcon.jsx
+++ src/main/js/nti-web-site-admin/components/advanced/branding/sections/library/preview/NotificationsIcon.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Theme} from '@nti/web-commons';
 import classnames from 'classnames/bind';
 
 import styles from './NotificationsIcon.css';
@@ -7,7 +8,8 @@ import styles from './NotificationsIcon.css';
 const cx = classnames.bind(styles);
 
 export default function NotificationsIcon (props) {
+	const mode = Theme.useThemeProperty('navigation.icon');
 	return (
-		<div className={cx('notifications-icon')} />
+		<div className={cx('notifications-icon', mode)} />
 	);
 }
