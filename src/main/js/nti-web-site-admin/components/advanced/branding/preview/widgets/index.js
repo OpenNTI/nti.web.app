6323c8b4613ef6bf22a1819a7c62173f0a9ed2f1
Ray Hatfield <ray.hatfield@gmail.com>
2019-11-11 17:29:51 -0600

    add favicon preview

--- src/main/js/nti-web-site-admin/components/advanced/branding/preview/widgets/index.js
+++ src/main/js/nti-web-site-admin/components/advanced/branding/preview/widgets/index.js
@@ -3,11 +3,13 @@ import {types} from '../../sections/assets/constants';
 import FullLogo from './FullLogo';
 import Logo from './Logo';
 import Email from './Email';
+import FavIcon from './FavIcon';
 
 const widgets = {
 	[types.fullLogo]: FullLogo,
 	[types.logo]: Logo,
 	[types.email]: Email,
+	[types.favicon]: FavIcon,
 };
 
 const empty = () => null;
