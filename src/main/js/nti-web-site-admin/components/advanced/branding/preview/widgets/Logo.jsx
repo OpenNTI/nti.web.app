cedef3fcd96ed7495af27eeb72b6ef1e164848c3
Andrew Ligon <andrew.ligon@nextthought.com>
2019-11-12 14:59:18 -0600

    NTI-8704 use the edited assets for the preview

--- src/main/js/nti-web-site-admin/components/advanced/branding/preview/widgets/Logo.jsx
+++ src/main/js/nti-web-site-admin/components/advanced/branding/preview/widgets/Logo.jsx
@@ -13,25 +13,23 @@ const globalTheme = Theme.getGlobalTheme();
 
 export default function Logo (props) {
 	return (
-		<Theme.Apply theme={globalTheme}>
-			<div className={cx('root')} {...props}>
-				<Theme.Asset name="assets.logo" className={cx('logo')} />
-				<div className={cx('course-info')}>
-					<div className={cx('course-label')}>NTI-1001</div>
-					<div className={cx('course-title')}>Taking Courses Online</div>
-					<ul className={cx('course-nav')}>
-						<li>Lessons</li>
-						<li>Assignments</li>
-						<li>Community</li>
-						<li>Course Info</li>
-					</ul>
-				</div>
-				<div className={cx('icons')}>
-					<div className={cx('search')} />
-					<NotificationsIcon mode="dark" />
-					<Avatar presence />
-				</div>
+		<div className={cx('root')} {...props}>
+			<Theme.Asset name="assets.logo" className={cx('logo')} />
+			<div className={cx('course-info')}>
+				<div className={cx('course-label')}>NTI-1001</div>
+				<div className={cx('course-title')}>Taking Courses Online</div>
+				<ul className={cx('course-nav')}>
+					<li>Lessons</li>
+					<li>Assignments</li>
+					<li>Community</li>
+					<li>Course Info</li>
+				</ul>
 			</div>
-		</Theme.Apply>
+			<div className={cx('icons')}>
+				<div className={cx('search')} />
+				<NotificationsIcon mode="dark" />
+				<Avatar presence />
+			</div>
+		</div>
 	);
 }
