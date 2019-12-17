fa2c91ccb78fc4394274f73d70e6aaf0f4db7947
Josh Birdwell <jbirdwell34@gmail.com>
2019-12-17 14:47:07 -0600

    Clean up

---
+++ src/main/js/legacy/app/course/overview/components/editing/content/quotes/BaseQuote.js
@@ -0,0 +1,58 @@
+const Ext = require('@nti/extjs');
+const classnames = require('classnames/bind');
+
+const styles = require('./Quote.css');
+const cx = classnames.bind(styles);
+const QUOTE_COMPLETED = 'inapp-quote-done';
+
+module.exports = exports = Ext.define(
+	'NextThought.app.course.overview.components.editing.content.quotes.BaseQuote',
+	{
+		extend: 'NextThought.app.course.overview.components.editing.content.Editor',
+		alias: 'widget.overview-editing-base-quote',
+
+		cls: '',
+
+		initComponent () {
+			this.callParent(arguments);
+
+			let onPostMessage = this.onPostMessage.bind(this);
+
+			window.addEventListener('message', onPostMessage);
+
+			this.on('destroy', () => {
+				window.removeEventListener('message', onPostMessage);
+			});
+		},
+
+		onPostMessage (event) {
+			if (event.data === QUOTE_COMPLETED) {
+				this.doBack();
+			}
+		},
+
+		showEditor () {
+			this.showItemEditor();
+		},
+
+		showItemEditor () {
+			if (this.itemEditorCmp) {
+				this.itemEditorCmp.destroy();
+				delete this.itemEditorCmp;
+			}
+			let u = $AppConfig.userObject;
+			let firstName = u.get('FirstName') || u.get('FullName') || u.get('realname') || '';
+			let email = u.get('email');
+			let clientSite = $AppConfig.siteName;
+
+			this.itemEditorCmp = this.add({
+				xtype: 'box',
+				cls: cx('quote-iframe'),
+				autoEl: {
+					tag: 'iframe',
+					src: `${this.hubspotPageUrl}?firstname=${firstName}${email ? `&email=${email}` : ''}&client_site_name=${clientSite}`
+				}
+			});
+		},
+	}
+);
--- src/main/js/legacy/app/course/overview/components/editing/content/quotes/VideoQuote.js
+++ src/main/js/legacy/app/course/overview/components/editing/content/quotes/VideoQuote.js
@@ -1,12 +1,9 @@
 const Ext = require('@nti/extjs');
-
-const QUOTE_COMPLETED = 'inapp-quote-done';
-const HUBSPOT_QUOTE_PAGE = 'https://www.nextthoughtstudios.com/video-quote';
-
 module.exports = exports = Ext.define('NextThought.app.course.overview.components.editing.content.quotes.VideoQuote',
 	{
-		extend: 'NextThought.app.course.overview.components.editing.content.Editor',
+		extend: 'NextThought.app.course.overview.components.editing.content.quotes.BaseQuote',
 		alias: 'widget.overview-editing-video-production',
+		hubspotPageUrl: 'https://www.nextthoughtstudios.com/video-quote',
 
 		statics: {
 			getHandledMimeTypes () {
@@ -26,50 +23,6 @@ module.exports = exports = Ext.define('NextThought.app.course.overview.component
 					}
 				];
 			}
-		},
-
-		cls: '',
-
-		initComponent () {
-			this.callParent(arguments);
-
-			let onPostMessage = this.onPostMessage.bind(this);
-
-			window.addEventListener('message', onPostMessage);
-
-			this.on('destroy', () => {
-				window.removeEventListener('message', onPostMessage);
-			});
-		},
-
-		onPostMessage (event) {
-			if (event.data === QUOTE_COMPLETED) {
-				this.doBack();
-			}
-		},
-
-		showEditor () {
-			this.showItemEditor();
-		},
-
-		showItemEditor () {
-			if (this.itemEditorCmp) {
-				this.itemEditorCmp.destroy();
-				delete this.itemEditorCmp;
-			}
-			let u = $AppConfig.userObject;
-			let firstName = u.get('FirstName') || u.get('FullName') || u.get('realname') || '';
-			let email = u.get('email');
-			let clientSite = $AppConfig.siteName;
-
-			this.itemEditorCmp = this.add({
-				xtype: 'box',
-				cls: 'quote-iframe',
-				autoEl: {
-					tag: 'iframe',
-					src: `${HUBSPOT_QUOTE_PAGE}?firstname=${firstName}${email ? `&email=${email}` : ''}&client_site_name=${clientSite}`
-				}
-			});
-		},
+		}
 	}
 );
