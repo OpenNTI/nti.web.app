45b0155e9161ab295e8793161bc29afa8033db13
Andrew Ligon <andrew.ligon@nextthought.com>
2019-12-26 16:52:01 -0600

    update the IE message

--- src/main/js/nti-web-react-components/IEAlert.jsx
+++ src/main/js/nti-web-react-components/IEAlert.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
-import {scoped} from '@nti/lib-locale';
 import {Prompt, Text} from '@nti/web-commons';
 
 import Styles from './IEAlert.css';
 
 const cx = classnames.bind(Styles);
-const t = scoped('nt-web-react-components.IEAlert', {
-	title: 'Internet Explorer',
-	header: 'We are dropping Internet Explorer support, April 2020.',
-	message: 'Microsoft has been phasing out support for the Internet Explore browser, in favor or more modern technologies. As of Windows 10 Edge is the default browser.',
-	upgrade: 'Please upgrade to one of our other supported browsers: <a href="http://www.google.com/chrome">Chrome</a>, <a href="http://www.getfirefox.com">Firefox</a>, <a href="http://www.microsoft.com/edge">Edge</a>, or <a href="http://www.apple.com/safari/download/">Safari.</a>'
-});
+
+const rel = 'noopener noreferrer';
+const target = '_blank';
+const Links = {
+	IESupport: { href: 'https://www.microsoft.com/en-us/microsoft-365/windows/end-of-ie-support', rel, target },
+	SupportedBrowsers: { href: 'https://help.nextthought.com/faq/technicalfaq.html#supported-browsers', rel, target },
+	Chrome: { href: 'http://www.google.com/chrome', rel, target },
+	Firefox: { href: 'http://www.getfirefox.com', rel, target },
+	Safari: { href: 'http://www.apple.com/safari/download/', rel, target },
+	Edge: { href: 'http://www.microsoft.com/edge', rel, target }
+};
 
 IEAlert.show = () => {
 	Prompt.modal(
@@ -28,12 +32,24 @@ export default function IEAlert ({onDismiss}) {
 		<Prompt.BaseWindow
 			doClose={onDismiss}
 			buttons={[]}
-			title={t('title')}
+			title={' '}
 		>
 			<div className={cx('ie-alert')}>
-				<Text.Base as="h1">{t('header')}</Text.Base>
-				<Text.Base as="p">{t('message')}</Text.Base>
-				<Text.Base as="p">{t('upgrade')}</Text.Base>
+				<h1>Internet Explorer - No Longer Supported</h1>
+				<p>
+					Effective March 31, 2020 the NextThought LMS will no longer support Internet Explorer (IE). The NextThought LMS will continue to support 
+					<a {...Links.SupportedBrowsers}>all modern browsers</a>, such as <a {...Links.Chrome}>Chrome</a>, <a {...Links.Firefox}>Firefox</a>, 
+					<a {...Links.Safari}>Safari</a> and <a {...Links.Edge}>Edge.</a> Users attempting to access the NextThought LMS from IE after March 31, 2020 
+					will be prompted to switch browsers.
+				</p>
+				<p>
+					The decision to drop IE support comes after <a {...Links.IESupport}>Microsoft</a> announced it would no longer support older versions of 
+					the browser. Other services are phasing out IE for many of the same reasons we are. Considering the security risks, development costs, 
+					compatibility issues, and the small number of people using IE, we are confident this is the right time to drop IE support.
+				</p>
+				<p>
+					If you prefer to or you are required to continue using a Microsoft Browser, you can use <a {...Links.Edge}>Edge.</a>
+				</p>
 			</div>
 		</Prompt.BaseWindow>
 	);
