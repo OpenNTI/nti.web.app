da747296978f660f3c48a23a3b8c1ad7a083312f
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2020-10-30 16:05:23 -0500

    Correct line number

--- src/main/js/legacy/app/navigation/Index.js
+++ src/main/js/legacy/app/navigation/Index.js
@@ -6,10 +6,10 @@ const { isFlag } = require('@nti/web-client');
 
 const ReactHarness = require('legacy/overrides/ReactHarness');
 
+const NotificationsTab = require('../notifications/Tab');
 const IdentityIndex = require('../account/identity/Index');
 const SearchBar = require('../search/SearchBar');
 const GutterTab = require('../chat/components/gutter/Tab');
-const NotificationsTab = require('../notifications/Tab');
 
 const NavigationStateStore = require('./StateStore');
 const styles = require('./Index.css');
