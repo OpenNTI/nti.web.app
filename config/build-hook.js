f666ecd0d5a9f32ca4a3524b41a86d2d17cc0d11
Jonathan Grimes <jsg2021@gmail.com>
2020-11-05 12:08:01 -0600

    make build hook work with hoisted node_modules

--- config/build-hook.js
+++ config/build-hook.js
@@ -21,6 +21,8 @@ if (CSSMOD >= SCSSMOD && !(isCI || process.env.NODE_ENV === 'production')) {
 
 const f = x => [`${SCSS}/${x}.scss`, `${CSS}/${x}.css`];
 
+ensurePath('spritesmith', 'sass', 'postcss-cli');
+
 // Warn and crash if required files are missing
 if (!checkRequiredFiles([SCSS])) {
 	process.exit(1);
@@ -37,9 +39,9 @@ call('sass', f`accessibility`);
 call('sass', f`legacy`);
 call('sass', f`nti-override`);
 // @postcss --use autoprefixer -r $(SRC)main/resources/css/*.css
-call('postcss', ['--use', 'autoprefixer', '-r', CSS + '/*.css']);
+call('postcss', ['--verbose', '--use', 'autoprefixer', '-r', CSS + '/**/*.css']);
 
-mark(CSS, SCSSMOD);
+mark(CSS, SCSSMOD).catch(e => {console.log(e);});
 
 function getLatest (dir, latest = 0) {
 	try {
@@ -82,4 +84,20 @@ async function mark (dir, time) {
 	}
 
 	await Promise.all(pending);
+}
+
+function ensurePath (...deps) {
+	try {
+		const bins = new Set();
+		const MODULE_DIR = 'node_modules';
+		for(const dep of deps) {
+			const location = require.resolve(dep).split(MODULE_DIR).slice(0, -1).join(MODULE_DIR);
+			bins.add(path.join(location, MODULE_DIR, '.bin'));
+		}
+
+		process.env.PATH = `${[...bins].join(':')}:${process.env.PATH}`;
+	} catch {
+		console.error('Could not resolve dependencies, make sure they are installed');
+		process.exit(1);
+	}
 }
