fa5c5eee3c3a97030ac2dd45564561761652d3d9
Jonathan Grimes <jsg2021@gmail.com>
2020-11-17 13:00:03 -0600

    getConfigFor => getConfig

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {addFeatureCheckClasses} from '@nti/lib-dom';
 import {ConflictResolutionHandler, Updates, Offline} from '@nti/web-commons';
-import {overrideConfigAndForceCurrentHost, getConfigFor, initErrorReporter} from '@nti/web-client';
+import {overrideConfigAndForceCurrentHost, getConfig, initErrorReporter} from '@nti/web-client';
 
 initErrorReporter();
 addFeatureCheckClasses();
@@ -11,7 +11,7 @@ overrideConfigAndForceCurrentHost();
 
 import 'legacy'; //kick start the app (the core is still defined in extjs)
 
-const basePath = (x => (x = getConfigFor(x), typeof x === 'string' ? x : '/'))('basepath');
+const basePath = (x => (x = getConfig(x), typeof x === 'string' ? x : '/'))('basepath');
 
 //once the all the app is react... this can just be a child of the root component... but for now, we'll mount it to a dummy div.
 ReactDOM.render(
