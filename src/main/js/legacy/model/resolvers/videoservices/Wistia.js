52785b6fe051c801e7c34d4c0a904fc27d8e843e
Jonathan Grimes <jsg2021@gmail.com>
2020-11-25 13:43:32 -0600

    NTI-9836 - use lib-interface's implementation to get the ID

--- src/main/js/legacy/model/resolvers/videoservices/Wistia.js
+++ src/main/js/legacy/model/resolvers/videoservices/Wistia.js
@@ -1,9 +1,8 @@
 const Ext = require('@nti/extjs');
-const {Models} = require('@nti/lib-interfaces');
-
-const {MetaDataResolver} = Models.media || {};
-const {services} = MetaDataResolver || {};
-const {wistia: WistiaMetaDataResolver} = services || {};
+const {Models: {media: {
+	MetaDataResolver: {services: {wistia: WistiaMetaDataResolver}},
+	Providers: {WistiaProvider}
+}}} = require('@nti/lib-interfaces');
 
 module.exports = exports = Ext.define('NextThought.model.resolvers.videoservices.Wistia', {
 	alias: 'resolvers.videoservices.wistia',
@@ -14,10 +13,7 @@ module.exports = exports = Ext.define('NextThought.model.resolvers.videoservices
 		RESOLVED: {},
 
 		getIdFromURL (url) {
-			const regEx = /https:\/\/fast\.wistia\.com\/embed\/iframe\/([^./?]*)/;
-			const match = url.match(regEx);
-
-			return (match && match[2]) || null;
+			return WistiaProvider.getID(url) || null;
 		},
 
 		async resolvePosterForID (id) {
