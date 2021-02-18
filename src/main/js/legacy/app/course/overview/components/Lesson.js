a4b022568a2622f7c19a1a31b8d47d3f571fbc8b
Jonathan Grimes <jsg2021@gmail.com>
2021-02-18 11:24:46 -0600

    formatting

--- src/main/js/legacy/app/course/overview/components/Lesson.js
+++ src/main/js/legacy/app/course/overview/components/Lesson.js
@@ -1,9 +1,9 @@
 const Ext = require('@nti/extjs');
-const {Overview} = require('@nti/web-course');
+const { Overview } = require('@nti/web-course');
 
-const {Modal} = require('nti-web-app-lesson-items');
+const { Modal } = require('nti-web-app-lesson-items');
 const ContentUtils = require('legacy/util/Content');
-const {getString} = require('legacy/util/Localization');
+const { getString } = require('legacy/util/Localization');
 const WindowActions = require('legacy/app/windows/Actions');
 const BaseModel = require('legacy/model/Base');
 const CalendarRoutes = require('legacy/app/CalendarRoutes');
@@ -23,14 +23,14 @@ const Lesson = Ext.define('NextThought.app.course.overview.components.Lesson', {
 	ui: 'course',
 
 	mixins: {
-		Router: 'NextThought.mixins.Router'
+		Router: 'NextThought.mixins.Router',
 	},
 
 	isLessonView: true,
 	layout: 'none',
 	cls: 'course-overview',
 
-	initComponent () {
+	initComponent() {
 		this.callParent(arguments);
 
 		this.calendarRoutes = CalendarRoutes(this);
@@ -42,21 +42,24 @@ const Lesson = Ext.define('NextThought.app.course.overview.components.Lesson', {
 		this.maybeMask();
 	},
 
-	getContext () {
-		const itemRoute = (this.activeItemRoute && Modal.pathToSelection(this.activeItemRoute)) || [];
+	getContext() {
+		const itemRoute =
+			(this.activeItemRoute &&
+				Modal.pathToSelection(this.activeItemRoute)) ||
+			[];
 		const video = this.mediaViewer && this.mediaViewer.mediaId;
 		const ids = video ? [...itemRoute, video] : itemRoute;
 
 		return {
 			contentIds: ids,
-			get: (prop) => {
+			get: prop => {
 				if (prop === 'NTIID') {
 					return ids[0];
 				}
 			},
 			getId: () => {
 				return ids[0];
-			}
+			},
 		};
 	},
 
@@ -70,7 +73,10 @@ const Lesson = Ext.define('NextThought.app.course.overview.components.Lesson', {
 		}
 
 		this.addCls('loading');
-		this.el.mask(getString('NextThought.view.courseware.overview.View.loading'), 'loading');
+		this.el.mask(
+			getString('NextThought.view.courseware.overview.View.loading'),
+			'loading'
+		);
 	},
 
 	maybeUnmask: function () {
@@ -87,11 +93,14 @@ const Lesson = Ext.define('NextThought.app.course.overview.components.Lesson', {
 	},
 
 	__updateProgress: function () {
-		if (!this.currentNode || !this.currentNode.getProgress) { return; }
+		if (!this.currentNode || !this.currentNode.getProgress) {
+			return;
+		}
 
 		var me = this;
 
-		me.currentNode.getProgress()
+		me.currentNode
+			.getProgress()
 			.then(function (progress) {
 				me.items.each(function (item) {
 					if (item.setProgress) {
@@ -105,11 +114,14 @@ const Lesson = Ext.define('NextThought.app.course.overview.components.Lesson', {
 	},
 
 	__updateCounts: function () {
-		if (!this.currentNode || !this.currentNode.getCommentCounts) { return; }
+		if (!this.currentNode || !this.currentNode.getCommentCounts) {
+			return;
+		}
 
 		var me = this;
 
-		me.currentNode.getCommentCounts()
+		me.currentNode
+			.getCommentCounts()
 			.then(function (counts) {
 				me.items.each(function (item) {
 					if (item.setCommentCounts) {
@@ -127,27 +139,30 @@ const Lesson = Ext.define('NextThought.app.course.overview.components.Lesson', {
 			course.getAssignments(),
 			course.getWrapper && course.getWrapper(),
 			ContentUtils.getLocation(record.get('ContentNTIID'), course),
-			overviewsrc ? null : course.getVideoIndex()
+			overviewsrc ? null : course.getVideoIndex(),
 		]);
 	},
 
-
-	getRouteFor (object, context) {
+	getRouteFor(object, context) {
 		if (!object || object.isUser) {
 			return null;
 		}
 
 		if (object.MimeType === 'application/vnd.nextthought.note') {
 			return () => {
-				this.WindowActions = this.WindowActions || WindowActions.create();
+				this.WindowActions =
+					this.WindowActions || WindowActions.create();
 
-				this.WindowActions.pushWindow(BaseModel.interfaceToModel(object));
+				this.WindowActions.pushWindow(
+					BaseModel.interfaceToModel(object)
+				);
 			};
 		}
 
 		if (object.isNoteModel) {
 			return () => {
-				this.WindowActions = this.WindowActions || WindowActions.create();
+				this.WindowActions =
+					this.WindowActions || WindowActions.create();
 
 				this.WindowActions.pushWindow(object);
 			};
@@ -157,18 +172,29 @@ const Lesson = Ext.define('NextThought.app.course.overview.components.Lesson', {
 			return this.calendarRoutes(object, context);
 		}
 
-		const builder = (MODAL_ROUTE_BUILDERS[object.MimeType] || MODAL_ROUTE_BUILDERS['default']);
+		const builder =
+			MODAL_ROUTE_BUILDERS[object.MimeType] ||
+			MODAL_ROUTE_BUILDERS['default'];
 
-		return builder ? builder(this.bundle, this.currentOutlineNode, object, context, this.activeItemRoute) : null;
+		return builder
+			? builder(
+					this.bundle,
+					this.currentOutlineNode,
+					object,
+					context,
+					this.activeItemRoute
+			  )
+			: null;
 	},
 
-
-	getRouteForModal (object, context) {
+	getRouteForModal(object, context) {
 		return this.getRouteFor(object, context);
 	},
 
-	onRouteDeactivate () {
-		if (this.deactivating || this.deactivated) { return; }
+	onRouteDeactivate() {
+		if (this.deactivating || this.deactivated) {
+			return;
+		}
 
 		this.deactivating = true;
 		this.deactivateTimeout = setTimeout(() => {
@@ -187,7 +213,7 @@ const Lesson = Ext.define('NextThought.app.course.overview.components.Lesson', {
 		}, 100);
 	},
 
-	onRouteActivate () {
+	onRouteActivate() {
 		this.deactivated = false;
 
 		if (this.deactivating) {
@@ -204,12 +230,15 @@ const Lesson = Ext.define('NextThought.app.course.overview.components.Lesson', {
 		}
 	},
 
-	async renderLesson (record, doNotCache, subRoute) {
+	async renderLesson(record, doNotCache, subRoute) {
 		try {
 			this.buildingOverview = true;
 
 			if (this.currentOverview) {
-				if (!doNotCache && record.getId() === this.activeRecord.getId()) {
+				if (
+					!doNotCache &&
+					record.getId() === this.activeRecord.getId()
+				) {
 					return;
 				} else {
 					this.currentOverview.destroy();
@@ -228,47 +257,44 @@ const Lesson = Ext.define('NextThought.app.course.overview.components.Lesson', {
 				layout: Overview.Lesson.List,
 				baseroute: '/',
 				getRouteFor: this.getRouteFor.bind(this),
-				inlinePlayback: false
+				inlinePlayback: false,
 			});
 
-
-			let outline = await course.getOutline({force: doNotCache});
+			let outline = await course.getOutline({ force: doNotCache });
 			let node = outline.getNode(record.get('ContentNTIID'));
 
 			if (!node) {
-				outline = await course.getOutline({force: true});
+				outline = await course.getOutline({ force: true });
 				node = outline.getNode(record.get('ContentNTIID'));
 			}
 
 			//If another lesson got rendered while we were loading don't do anything
-			if (this.activeRecord.getId() !== record.getId()) { return; }
+			if (this.activeRecord.getId() !== record.getId()) {
+				return;
+			}
 
 			this.currentOutlineNode = node;
 
 			this.currentOverview.setProps({
-				outlineNode: node
+				outlineNode: node,
 			});
-
 		} catch (e) {
 			console.error(e);
 		}
 	},
 
-
-	isShowingContent () {
+	isShowingContent() {
 		return this.activeItemRoute || this.mediaViewer;
 	},
 
-
-	cleanUpMediaViewer () {
+	cleanUpMediaViewer() {
 		if (this.mediaViewer) {
 			this.mediaViewer.destroy();
 			delete this.mediaViewer;
 		}
 	},
 
-
-	maybeAddMediaViewer (viewerRoute, lesson, returnPath) {
+	maybeAddMediaViewer(viewerRoute, lesson, returnPath) {
 		if (!viewerRoute) {
 			return;
 		}
@@ -280,12 +306,12 @@ const Lesson = Ext.define('NextThought.app.course.overview.components.Lesson', {
 				xtype: 'media-window-view',
 				currentBundle: this.bundle,
 				autoShow: true,
-				handleNavigation:  (...args) => {
+				handleNavigation: (...args) => {
 					if (lessonCmp.handleMediaViewerRoute) {
 						lessonCmp.handleMediaViewerRoute(...args);
 					}
 				},
-				handleClose: this.handleMediaClose.bind(this, returnPath)
+				handleClose: this.handleMediaClose.bind(this, returnPath),
 			});
 		}
 
@@ -298,14 +324,17 @@ const Lesson = Ext.define('NextThought.app.course.overview.components.Lesson', {
 		return true;
 	},
 
-	handleMediaClose (returnPath) {
+	handleMediaClose(returnPath) {
 		this.cleanUpMediaViewer();
 		this.pushRoute(null, returnPath);
 	},
 
-	async maybeShowContent (lesson, route, subRoute) {
-		const [itemRoute, viewerRoute] = !subRoute ? [] : subRoute.split('/viewer/');
-		const hasEmptyItemRoute = !this.activeItemRoute || this.activeItemRoute === '/';
+	async maybeShowContent(lesson, route, subRoute) {
+		const [itemRoute, viewerRoute] = !subRoute
+			? []
+			: subRoute.split('/viewer/');
+		const hasEmptyItemRoute =
+			!this.activeItemRoute || this.activeItemRoute === '/';
 		const wasMounted = !!this.itemFlyout;
 
 		if (!this.maybeAddMediaViewer(viewerRoute, lesson, itemRoute)) {
@@ -319,7 +348,7 @@ const Lesson = Ext.define('NextThought.app.course.overview.components.Lesson', {
 				getRouteFor: this.getRouteForModal.bind(this),
 				handleNavigation: this.handleModalNavigation.bind(this),
 				addHistory: true,
-				baseroute: '/'
+				baseroute: '/',
 			});
 		}
 
@@ -327,31 +356,35 @@ const Lesson = Ext.define('NextThought.app.course.overview.components.Lesson', {
 			const course = await this.bundle.getInterfaceInstance();
 
 			this.activeItemRoute = itemRoute;
-			this.itemFlyout.setProps({
-				course,
-				lesson,
-				requiredOnly: Overview.isFilteredToRequired(),
-				dismissPath: MODAL_ROUTE_BUILDERS['dismiss'](this.bundle, lesson),
-				path: itemRoute,
-				firstSelection: hasEmptyItemRoute && wasMounted,
-				activeObjectId: route && route.object && route.object.id,
-				activeHash: route && route.hash
-			}, true);
+			this.itemFlyout.setProps(
+				{
+					course,
+					lesson,
+					requiredOnly: Overview.isFilteredToRequired(),
+					dismissPath: MODAL_ROUTE_BUILDERS['dismiss'](
+						this.bundle,
+						lesson
+					),
+					path: itemRoute,
+					firstSelection: hasEmptyItemRoute && wasMounted,
+					activeObjectId: route && route.object && route.object.id,
+					activeHash: route && route.hash,
+				},
+				true
+			);
 		} catch (e) {
 			console.error(e);
 		}
 	},
 
-
-	handleModalNavigation (title, route, precache) {
+	handleModalNavigation(title, route, precache) {
 		this.pushRoute(title, route, precache);
 	},
-
 
 	navigate: function (obj) {
 		obj.parent = this.currentNode;
 		this.navigateToObject(obj);
-	}
+	},
 });
 
 module.exports = exports = Lesson;
