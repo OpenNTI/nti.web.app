b7f7191f46bcd76043513c1cab1a8bbaca144497
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-04-20 11:24:12 -0500

    Change the way we render calendar window override

--- src/main/js/nti-web-chat-overrides/CalendarWindow.jsx
+++ src/main/js/nti-web-chat-overrides/CalendarWindow.jsx
@@ -1,39 +1,52 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { Layouts } from '@nti/web-commons';
 import Ext from '@nti/extjs';
 
 WebappCalendarWindow.propTypes = {
 	onClose: PropTypes.func.isRequired,
 	target: PropTypes.object.isRequired,
+	visible: PropTypes.bool.isRequired,
 };
 
-export default function WebappCalendarWindow({ onClose, target }) {
+export default function WebappCalendarWindow({ onClose, target, visible }) {
 	const windowRef = React.useRef(null);
-	const rebuildRef = React.useRef(null);
 
-	const onUnmount = React.useCallback(() => {
-		windowRef.current.destroy();
-	}, [windowRef]);
+	React.useEffect(() => {
+		const listen = {
+			single: true,
+			close: onClose,
+		};
 
-	const onMount = React.useCallback(() => {
-		const win = Ext.widget('gutter-list-calendar-window', {
-			renderTo: Ext.get(target),
-			onClose: onClose,
+		windowRef.current?.on(listen);
+		return () => {
+			windowRef.current?.un(listen);
+		}
+	}, [windowRef.current]);
 
-			onItemClick: obj => {
-				// temp solution to prevent having stale webinars in the calendar
-				if (obj.hasLink && obj.hasLink('WebinarRegister')) {
-					// clearTimeout(this.calendarDirty);
-					rebuildRef.current = true;
-				}
-			},
-			navigateToObject: obj => this.navigateToObject(obj),
-		});
+	React.useEffect(() => {
+		if (!visible) {
+			windowRef.current?.hide();
+		}
+		else if (windowRef.current) {
+			windowRef.current?.show();
+		}
+		else if (target) {
+			const win = Ext.widget('gutter-list-calendar-window', {
+				renderTo: Ext.get(target),
+				onClose: onClose,
+				navigateToObject: obj => this.navigateToObject(obj),
+			});
+			if (!visible) {
+				win.hide();
+			}
+			windowRef.current = win;
+		}
 
-		windowRef.current = win;
-	}, [windowRef, rebuildRef]);
+		return () => {
+			windowRef.current?.hide();
+		}
+	}, [visible]);
 
-	return <Layouts.Uncontrolled onMount={onMount} onUnmount={onUnmount} />;
+	return null;
 }
