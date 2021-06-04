85eece710ca9d7ff0218f8dfbc34079e0f3cfebc
Ray Hatfield <ray.hatfield@gmail.com>
2021-06-04 11:04:26 -0500

    NTI-10767 - use existing SeatLimit store for user count

--- src/main/js/nti-web-site-admin/components/dashboard/widgets/UserCount.jsx
+++ src/main/js/nti-web-site-admin/components/dashboard/widgets/UserCount.jsx
@@ -1,46 +1,19 @@
-import { useEffect, useReducer } from 'react';
-
 import { NumericValue } from '@nti/web-charts';
 import { scoped } from '@nti/lib-locale';
-import { useService } from '@nti/web-commons';
+
+import Store from '../../users/list/SharedStore';
 
 const t = scoped('nti-web-site-admin.components.dashboard.widgets.UserCount', {
 	label: 'Total Users',
 });
 
-const useWorkspaceLinkValue = (rel, params) => {
-	const [state, dispatch] = useReducer(
-		(previousState, action) => ({ ...previousState, ...action }),
-		{}
-	);
-	const service = useService();
-	let abort = false;
+function UserCountCmp(props) {
+	const { SeatLimits } = Store.useValue();
+	const usedSeats = SeatLimits?.usedSeats;
 
-	useEffect(() => {
-		const fetchValue = async () => {
-			dispatch({ loading: true, value: undefined });
-			try {
-				const workspace = service.Items.find(w => w.hasLink(rel));
-				if (workspace) {
-					const value = await workspace.fetchLink(rel, params);
-					if (!abort) {
-						dispatch({ value, loading: false, error: undefined });
-					}
-				}
-			} catch (error) {
-				dispatch({ error, loading: false, value: undefined });
-			}
-		};
-		fetchValue();
-		return () => void (abort = true);
-	}, [rel, service]);
-
-	return state;
-};
-
-export function UserCount(props) {
-	const { value } = useWorkspaceLinkValue('SiteUsers', { batchSize: 1 });
-	return value ? (
-		<NumericValue label={t('label')} value={value.TotalItemCount} />
+	return usedSeats != null ? (
+		<NumericValue label={t('label')} value={usedSeats} />
 	) : null;
 }
+
+export const UserCount = Store.compose(UserCountCmp);
