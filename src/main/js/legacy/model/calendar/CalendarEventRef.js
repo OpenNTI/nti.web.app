81b0c8296fcb51101c6b5319ad098e1c191f71ac
Jonathan Grimes <jsg2021@gmail.com>
2021-06-22 13:08:11 -0500

    NTI-10832 - Make calendar event refs legacy model extend RelatedWork legacy model (mirror survey/assignments)

--- src/main/js/legacy/model/calendar/CalendarEventRef.js
+++ src/main/js/legacy/model/calendar/CalendarEventRef.js
@@ -3,7 +3,7 @@ const Ext = require('@nti/extjs');
 require('../Base');
 
 module.exports = exports = Ext.define('NextThought.model.CalendarEventRef', {
-	extend: 'NextThought.model.Base',
+	extend: 'NextThought.model.RelatedWork',
 	mimeType: 'application/vnd.nextthought.nticalendareventref',
 
 	statics: {
