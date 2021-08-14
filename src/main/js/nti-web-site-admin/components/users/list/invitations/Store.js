eb112e5f9da0aab86c42fe9798ee3a8103fa8425
Andrew Ligon <aligon90@gmail.com>
2021-08-14 00:01:30 -0500

    NTI-11043 reset paging when the filter changes

--- src/main/js/nti-web-site-admin/components/users/list/invitations/Store.js
+++ src/main/js/nti-web-site-admin/components/users/list/invitations/Store.js
@@ -36,6 +36,9 @@ export class InvitationsStore extends Base {
 	static FilterParam = 'type_filter';
 	static DefaultFilter = 'all';
 
+	static PageSize = 2;
+	static PageResetParams = ['type_filter'];
+
 	static DefaultSortProperty = 'created_time';
 	static DefaultSortDirection = 'descending';
 
