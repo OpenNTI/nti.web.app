93ef7d563b2231164dca27bd0c0024ab2f8e72c7
Ray Hatfield <ray.hatfield@gmail.com>
2021-09-30 20:04:22 -0500

    [NTI-9825] remove unused

--- src/main/js/nti-web-site-admin/components/configuration/webhooks/columns/index.js
+++
@@ -1,8 +0,0 @@
-import { Table, DateTime } from '@nti/web-core';
-
-export const propertyColumn = (propName, title = propName) => Table.asBasicColumn(({item}) => item?.[propName] ?? null, title);
-
-export const CreatedTime = ({item}) => {
-	const createdTime = item?.getCreatedTime?.();
-	return createdTime ? <DateTime.ISO date={createdTime} /> : null
-}
