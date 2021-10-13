cbca5347810741a51d3aa1aa5ef81a2b3a2f9fba
Jonathan Grimes <jsg2021@gmail.com>
2021-10-13 17:03:43 -0500

    [NTI-9825] add type info

--- src/main/js/nti-web-site-admin/components/configuration/webhooks/SubscriptionListItem.jsx
+++ src/main/js/nti-web-site-admin/components/configuration/webhooks/SubscriptionListItem.jsx
@@ -3,14 +3,21 @@ import { LabeledValue } from '@nti/web-commons';
 
 import { Status } from './Status';
 
+/** @typedef {import('@nti/lib-interfaces').Models.subscriptions.PersistentSubscription} PersistentSubscription */
+
 const Container = styled.div`
 	display: flex;
 	justify-content: space-between;
 	gap: var(--padding-lg, 1em);
 	padding: var(--padding-sm, 0.5em);
-`
-
-export function SubscriptionListItem ({item}) {
+`;
+/**
+ *
+ * @param {object} props
+ * @param {PersistentSubscription} props.item
+ * @returns {JSX.Element}
+ */
+export function SubscriptionListItem({ item }) {
 	return (
 		<Container>
 			<LabeledValue label="Created">
@@ -24,5 +31,5 @@ export function SubscriptionListItem ({item}) {
 				<DisplayName entity={item.OwnerId} />
 			</LabeledValue>
 		</Container>
-	)
+	);
 }
