7b1062664d509c14e2d7a173ba91fc4f5d642129
Andrew Ligon <aligon90@gmail.com>
2021-10-18 15:01:02 -0500

    add a save button

--- src/main/js/nti-web-site-admin/components/users/segment/editor/Header.jsx
+++ src/main/js/nti-web-site-admin/components/users/segment/editor/Header.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
+import { scoped } from '@nti/lib-locale';
 import { Input } from '@nti/web-commons';
+import { Button } from '@nti/web-core';
 
 import { SegmentStore } from '../Store';
 
+const t = scoped('nti-web-site-admin.components.users.segment.editor.Header', {
+	save: 'Save',
+});
+
 const Container = styled.div`
 	padding: 20px 30px 30px;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-between;
+`;
+
+const Controls = styled.div`
+	display: inline-flex;
+	flex-direction: row;
+	align-items: center;
 `;
 
 const Title = styled(Input.Text)`
@@ -20,11 +36,16 @@ const Title = styled(Input.Text)`
 `;
 
 export function Header() {
-	const { title, setTitle } = SegmentStore.useProperties();
+	const { title, setTitle, save } = SegmentStore.useProperties();
 
 	return (
 		<Container>
 			<Title value={title} onChange={setTitle} />
+			<Controls>
+				<Button rounded onClick={save} busy={save.running}>
+					{t('save')}
+				</Button>
+			</Controls>
 		</Container>
 	);
 }
