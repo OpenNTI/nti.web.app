f87a65e072da6f49b8acda67bae95e759ab3ac6d
Jonathan Grimes <jsg2021@gmail.com>
2021-10-19 15:45:19 -0500

    update card

--- src/main/js/nti-web-site-admin/components/common/Card.jsx
+++ src/main/js/nti-web-site-admin/components/common/Card.jsx
@@ -1,4 +1,3 @@
-import './Card.scss';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
@@ -6,5 +5,15 @@ SiteAdminCard.propTypes = {
 	className: PropTypes.string,
 };
 export default function SiteAdminCard({ className, ...otherProps }) {
-	return <div className={cx('site-admin-card', className)} {...otherProps} />;
+	return (
+		<div
+			{...otherProps}
+			className={cx('site-admin-card', className)}
+			css={css`
+				background-color: white;
+				box-shadow: 0 1px 2px 1px rgba(0, 0, 0, 0.15);
+				overflow: hidden;
+			`}
+		/>
+	);
 }
