c68e94ba2cf2d13aaf2de30bcf6de8ffd04f377d
Jonathan Grimes <jsg2021@gmail.com>
2021-10-25 14:22:14 -0500

    cleanup

--- src/main/js/nti-web-app-lesson-items/overrides/topic/ActiveUsers.jsx
+++ src/main/js/nti-web-app-lesson-items/overrides/topic/ActiveUsers.jsx
@@ -1,61 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classnames from 'classnames/bind';
-
 import { LinkTo } from '@nti/web-routing';
 import { User, EmptyState, List } from '@nti/web-commons';
 import { scoped } from '@nti/lib-locale';
 
-import Styles from './ActiveUsers.css';
+import styles from './ActiveUsers.css';
 
-const cx = classnames.bind(Styles);
 const t = scoped('NTIWebAppLessonItems.overrides.topic.ActiveUsers', {
 	heading: 'Active People',
 	empty: 'There are no active people.',
 });
 
-export default class ActiveUsers extends React.Component {
-	static propTypes = {
-		activeUsers: PropTypes.array,
-	};
+export default function ActiveUsers({ activeUsers }) {
+	const empty = activeUsers?.length === 0;
 
-	render() {
-		const { activeUsers } = this.props;
-
-		if (!activeUsers) {
-			return null;
-		}
-
-		const empty = activeUsers.length === 0;
-
-		return (
-			<div className={cx('active-users')}>
-				{empty && <EmptyState subHeader={t('empty')} />}
-				{!empty && <div className={cx('heading')}>{t('heading')}</div>}
-				{!empty && (
-					<List.Unadorned className={cx('user-list')}>
-						{activeUsers.map((user, index) => {
-							return (
-								<li key={index}>
-									<LinkTo.Object
-										className={cx('user-list-item')}
-										object={user}
-									>
-										<User.Avatar
-											className={cx('avatar')}
-											user={user}
-										/>
-										<User.DisplayName
-											className={cx('display-name')}
-											user={user}
-										/>
-									</LinkTo.Object>
-								</li>
-							);
-						})}
+	return !activeUsers ? null : (
+		<div className={styles.container}>
+			{empty ? (
+				<EmptyState subHeader={t('empty')} />
+			) : (
+				<>
+					<div className={styles.heading}>{t('heading')}</div>
+					<List.Unadorned className={styles.list}>
+						{activeUsers.map((user, index) => (
+							<li key={index}>
+								<LinkTo.Object
+									className={styles.item}
+									object={user}
+								>
+									<User.Avatar
+										className={styles.avatar}
+										user={user}
+									/>
+									<User.DisplayName
+										className={styles.name}
+										user={user}
+									/>
+								</LinkTo.Object>
+							</li>
+						))}
 					</List.Unadorned>
-				)}
-			</div>
-		);
-	}
+				</>
+			)}
+		</div>
+	);
 }
