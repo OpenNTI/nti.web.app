d2edbd24aa41d16b25cea6834cb82a4532ba01f8
Jonathan Grimes <jsg2021@gmail.com>
2021-10-29 16:51:20 -0500

    NTI-11308 - Hide books menu item if no books

--- src/main/js/nti-web-site-admin/components/content/list/nav-bar/View.jsx
+++ src/main/js/nti-web-site-admin/components/content/list/nav-bar/View.jsx
@@ -1,33 +1,54 @@
-import './View.scss';
-import React from 'react';
+import { Suspense } from 'react';
 
+import { Text, useAsyncValue, useService } from '@nti/web-core';
 import { scoped } from '@nti/lib-locale';
 import { LinkTo } from '@nti/web-routing';
 
 import Card from '../../../common/Card';
 import Tabs from '../../../common/Tabs';
 
-const DEFAULT_TEXT = {
+const t = scoped('nti-site-admin.content.list.navbar.View', {
 	content: 'Content',
 	courses: 'Courses',
 	books: 'Books',
-};
+});
 
-const t = scoped('nti-site-admin.content.list.navbar.View', DEFAULT_TEXT);
+const T = Text.Translator(t);
+const Tab = ({ localeKey, ...props }) => (
+	<LinkTo.Path {...props} activeClassName="active">
+		<T {...{ localeKey }} />
+	</LinkTo.Path>
+);
 
-export default class ContentListNavBar extends React.Component {
-	render() {
-		return (
-			<Card className="site-admin-content-list-nav-bar">
-				<Tabs header={t('content')}>
-					<LinkTo.Path to="./" activeClassName="active" exact>
-						{t('courses')}
-					</LinkTo.Path>
-					<LinkTo.Path to="./books" activeClassName="active">
-						{t('books')}
-					</LinkTo.Path>
-				</Tabs>
-			</Card>
-		);
-	}
+export default function ContentListNavBar() {
+	return (
+		<Suspense fallback={<div />}>
+			<Content />
+		</Suspense>
+	);
+}
+function Content() {
+	const service = useService();
+	const books = useAsyncValue('admin-library-books-list', () =>
+		service.getContentBundles({ batchSize: 1 })
+	);
+
+	const hasBooks = books?.total > 0;
+
+	return (
+		<Card
+			className="site-admin-content-list-nav-bar"
+			css={css`
+				box-shadow: 0 1px 3px 0 rgba(0 0 0 / 24%);
+				background-color: white;
+				padding-bottom: 20px;
+			`}
+		>
+			<Tabs header={t('content')}>
+				<Tab to="./" exact localeKey="courses" />
+
+				{hasBooks && <Tabs to="./books" localeKey="books" />}
+			</Tabs>
+		</Card>
+	);
 }
